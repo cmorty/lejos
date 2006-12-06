b7acf1fee65e93267bc47f276360506eaa1470be
charles_manning <charles_manning@06c8f799-c779-4783-8af7-179afedb67c8>
2006-12-06 20:55:58 +0000

    Add missing files
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@15 06c8f799-c779-4783-8af7-179afedb67c8

---
+++ nxtvm/platform/nxt/lejos_nxt.h
@@ -0,0 +1,11 @@
+#ifndef __LEJOS_NXT_H__
+#define __LEJOS_NXT_H__
+
+#define MEMORY_SIZE 0x2000
+#define MEMORY_BASE 0x20A00
+
+#define STATUS_BYTE ((char *) 0x20F000)
+
+#define STATUS_WORD ((int *) 0x20F004)
+
+#endif
