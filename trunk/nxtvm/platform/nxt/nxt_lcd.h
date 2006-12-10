4ded40259ef4d104c61a2fc5f6540d55297961a9
charles_manning <charles_manning@06c8f799-c779-4783-8af7-179afedb67c8>
2006-12-10 18:44:44 +0000

    Use smaller LCD buffer

--- trunk/nxtvm/platform/nxt/nxt_lcd.h
+++ trunk/nxtvm/platform/nxt/nxt_lcd.h
@@ -3,12 +3,14 @@
 
 #include "mytypes.h"
 
+#define NXT_LCD_WIDTH 100
+#define NXT_LCD_DEPTH 8
+
 void nxt_lcd_init(void);
 void nxt_lcd_power_up(void);
 void nxt_lcd_power_down(void);
-void nxt_lcd_data(const U8 *buffer, U32 nBytes);
+void nxt_lcd_data(const U8 *buffer);
 
-void nxt_lcd_test(void);
 
 #endif
 
