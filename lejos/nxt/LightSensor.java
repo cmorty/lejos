18c630903058318315bbffed7518dcc7a20ab5b1
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2006-12-22 15:17:55 +0000

    Sound Sensor and updated Light Sensor
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk/classes@86 06c8f799-c779-4783-8af7-179afedb67c8

--- lejos/nxt/LightSensor.java
+++ lejos/nxt/LightSensor.java
@@ -5,9 +5,17 @@ public class LightSensor {
 	
 	public LightSensor(Port port)
 	{
-	   this.port = port;
+		this.port = port;
+		port.activate(); // Default to LED on
 	}
 	
+	public LightSensor(Port port, boolean activate)
+	{
+	   this.port = port;
+	   if (activate) port.activate();
+	   else port.passivate();
+	}
+
 	public int readValue()
 	{
 		return ((1023 - port.readRawValue()) * 100/ 1023);  
