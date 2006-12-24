a779c72089e41e413155140175c5537503c563fc
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2006-12-24 21:57:19 +0000

    Added WaitForPressAndRelease as this now works
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@110 06c8f799-c779-4783-8af7-179afedb67c8

--- examples/TestButton/TestButton.java
+++ examples/TestButton/TestButton.java
@@ -10,16 +10,21 @@ public class TestButton
     String b2 = "LEFT";
     String b3 = "RIGHT";
     String b4 = "ESCAPE";
+    String waiting = "Waiting for ENTER";
+    String pressed = "ENTER Pressed";
 
     for(;;) {
        LCD.clear();
        LCD.drawString(butt,0,1);
        LCD.drawInt(Button.readButtons(),9,1);
-       if (Button.ENTER.isPressed()) LCD.drawString(b1,0,3);
-       if (Button.LEFT.isPressed()) LCD.drawString(b2,0,4);
-       if (Button.RIGHT.isPressed()) LCD.drawString(b3,0,5);
-       if (Button.ESCAPE.isPressed()) LCD.drawString(b4,0,6);
-       // Button.ENTER.waitForPressAndRelease();
+       if (Button.ENTER.isPressed()) LCD.drawString(b1,0,2);
+       if (Button.LEFT.isPressed()) LCD.drawString(b2,0,3);
+       if (Button.RIGHT.isPressed()) LCD.drawString(b3,0,3);
+       if (Button.ESCAPE.isPressed()) LCD.drawString(b4,0,3);
+       LCD.drawString(waiting, 0, 4);
+       LCD.refresh();
+       Button.ENTER.waitForPressAndRelease();
+       LCD.drawString(pressed, 0, 5);
        LCD.refresh();
        Thread.sleep(1000);
     }
