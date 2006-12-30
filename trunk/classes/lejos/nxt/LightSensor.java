18de26e436896e6b8de9ece790131edf7086a1ee
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2006-12-30 12:53:38 +0000

    Changed activate to floodlight and added setADType and setPowerType

--- trunk/classes/lejos/nxt/LightSensor.java
+++ trunk/classes/lejos/nxt/LightSensor.java
@@ -6,14 +6,20 @@ public class LightSensor {
 	public LightSensor(Port port)
 	{
 		this.port = port;
-		port.activate(); // Default to LED on
+		port.setPowerType(0);
+		port.setADType(2); // Default to LED on
 	}
 	
-	public LightSensor(Port port, boolean activate)
+	public LightSensor(Port port, boolean floodlight)
 	{
 	   this.port = port;
-	   if (activate) port.activate();
-	   else port.passivate();
+	   port.setPowerType(0);
+	   port.setADType((floodlight ? 1 : 0));
+	}
+	
+	public void setFloodlight(boolean floodlight)
+	{
+		port.setADType((floodlight ? 1 : 0));
 	}
 
 	public int readValue()
