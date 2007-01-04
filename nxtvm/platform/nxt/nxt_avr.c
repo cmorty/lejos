514d454d3aec2892b065cde950d20e8ad0e02b95
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2007-01-04 23:14:26 +0000

    Fixed setting RCX sensors active - spec is wrong and so is Lego code
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@201 06c8f799-c779-4783-8af7-179afedb67c8

--- nxtvm/platform/nxt/nxt_avr.c
+++ nxtvm/platform/nxt/nxt_avr.c
@@ -266,9 +266,12 @@ void nxt_avr_set_motor(U32 n, int power_percent, int brake)
 
 void nxt_avr_set_input_power(U32 n, U32 power_type)
 {
+  // This does not correspond to the spec.
+  // It is unclear how to set power always on.
+  // Lego code has a bug in it.
   if (n < NXT_AVR_N_INPUTS && power_type <= 3)
   {
-    io_to_avr.input_power &= ~(0x11 << (n<<1));
-    io_to_avr.input_power |= (power_type << (n<<1));
+    //io_to_avr.input_power &= ~(0x11 << (n<<1));
+    io_to_avr.input_power |= (power_type << (n));
   }  
 }  
