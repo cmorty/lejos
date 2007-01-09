7d615cf603702c0fa8cf24d5ba89a472da7e2074
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2007-01-09 22:40:42 +0000

    Added -source -target to lejosjc

--- trunk/scripts/windows/lejosjc.bat
+++ trunk/scripts/windows/lejosjc.bat
@@ -1,7 +1,7 @@
 @echo off
 if "%LEJOS_HOME%" == ""  goto homeless
 
-javac -bootclasspath %LEJOS_HOME%\lib\classes.jar;%CLASSPATH% %1 %2 %3 %4 %5 %6 %7 %8 %9
+javac -source 1.3 -target 1.1 -bootclasspath %LEJOS_HOME%\lib\classes.jar;%CLASSPATH% %1 %2 %3 %4 %5 %6 %7 %8 %9
 goto end
 
 :homeless
