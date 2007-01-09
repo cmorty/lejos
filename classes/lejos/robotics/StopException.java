4663e28a18608e06dc267e0f97f70335517e420e
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2007-01-09 22:05:56 +0000

    Robotic Activity classes
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@242 06c8f799-c779-4783-8af7-179afedb67c8

---
+++ classes/lejos/robotics/StopException.java
@@ -0,0 +1,10 @@
+package lejos.robotics;
+
+/**
+ * This exception indicates that an activity should stop.
+ * @see Activity
+ * @author Paul Andrews
+ */
+public class StopException extends Exception
+{
+}
