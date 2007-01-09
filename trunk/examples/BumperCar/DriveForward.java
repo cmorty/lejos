2cf62cc58e1a0784cca95ba27184fb871bfe74ec
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2007-01-09 11:48:18 +0000

    Brian's behavior example

---
+++ trunk/examples/BumperCar/DriveForward.java
@@ -0,0 +1,19 @@
+import lejos.robotics.*;
+import lejos.nxt.*;
+
+public class DriveForward implements Behavior {
+
+   public boolean takeControl() {
+      return true;
+   }
+
+   public void suppress() {
+      Motor.A.stop();
+      Motor.C.stop();
+   }
+
+   public void action() {
+      Motor.A.forward();
+      Motor.C.forward();
+   }
+}
