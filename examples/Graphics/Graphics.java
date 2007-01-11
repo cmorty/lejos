15e83ac3430208d5f1a680a18b4a525e6a65d4f9
rogergla <rogergla@06c8f799-c779-4783-8af7-179afedb67c8>
2007-01-11 07:25:04 +0000

    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@255 06c8f799-c779-4783-8af7-179afedb67c8

--- examples/Graphics/Graphics.java
+++ examples/Graphics/Graphics.java
@@ -1,36 +1,90 @@
-import lejos.nxt.*;
 
-public class Graphics 
-{
-	static int[] buff;
+
+import lejos.nxt.LCD;
+
+public class Graphics {
+	private static final byte HEIGHT = 64; // Pixels
+	private static final byte WIDTH = 100; // Pixels
+	private int [] buff;
+
+	public Graphics() {
+		 buff = new int[HEIGHT*WIDTH/32];
+	}
 	
-	public static void main (String[] aArg)
-	throws Exception
-	{
-		buff = new int[200];
-		
-		SoundSensor sound = new SoundSensor(Port.S2);
-		
-		for(int i = 0; !Button.ESCAPE.isPressed();i++)
-		{
-			setPixel(i % 100, 7 + (sound.readValue()/2));
-			LCD.setDisplay(buff);
-			LCD.refresh();
-			Thread.sleep(10);
-			if (i % 100 == 0) clear();
+	// TESTING PURPOSES ONLY
+	public static void main(String [] options) throws Exception {
+		Graphics g = new Graphics();
+		g.drawLine(5,5,60,60);
+		g.drawRect(62, 10, 25, 35);
+		g.refresh();
+	}
+
+	public void setPixel(int x, int y) {
+		int xChar = x / 4;
+		int yChar = y / 8;
+		int index = yChar * 25 + xChar;
+		int specificBit = (y % 8) + ((x % 4) * 8);
+		buff[index] = buff[index] | (1 << specificBit);
+	}
+
+	public void drawLine(int x0, int y0, int x1, int y1) {
+		// Uses Bresenham's line algorithm
+		int dy = y1 - y0;
+		int dx = x1 - x0;
+		int stepx, stepy;
+
+		if (dy < 0) { dy = -dy;  stepy = -1; } else { stepy = 1; }
+		if (dx < 0) { dx = -dx;  stepx = -1; } else { stepx = 1; }
+		dy <<= 1; // dy is now 2*dy
+		dx <<= 1; // dx is now 2*dx
+
+		setPixel(x0, y0);
+		if (dx > dy) {
+			int fraction = dy - (dx >> 1);  // same as 2*dy - dx
+			while (x0 != x1) {
+				if (fraction >= 0) {
+					y0 += stepy;
+					fraction -= dx; // same as fraction -= 2*dx
+				}
+				x0 += stepx;
+				fraction += dy; // same as fraction -= 2*dy
+				setPixel(x0, y0);
+			}
+	        	} else {
+				int fraction = dx - (dy >> 1);
+				while (y0 != y1) {
+				if (fraction >= 0) {
+					x0 += stepx;
+					fraction -= dy;
+				}
+				y0 += stepy;
+				fraction += dx;
+				setPixel(x0, y0);
+			}
 		}
-		Button.ESCAPE.waitForPressAndRelease();
-	}
-	
-	public static void setPixel(int x, int y)
-	{
-		y = 63 - y;
-		buff[(x>>2) + ((y>>3) * 25)] |= 1 << ((x<<3) + (y&7));
-	}
-	
-	public static void clear()
-	{
-		for(int i=0;i<200;i++) buff[i] = 0;
 	}
 
+	public void drawRect(int x, int y, int width, int height) {
+		if ((width < 0) || (height < 0))
+			return;
+
+		if (height == 0 || width == 0) {
+	    		drawLine(x, y, x + width, y + height);
+		} else {
+	    	drawLine(x, y, x + width - 1, y);
+			drawLine(x + width, y, x + width, y + height - 1);
+			drawLine(x + width, y + height, x + 1, y + height);
+			drawLine(x, y + height, x, y + 1);
+		}
+    }
+	
+	public void drawString(String str, int x, int y) {
+		LCD.drawString(str, x, y);
+	}
+
+	// Temp for testing purposes until canvas made.
+	public void refresh() {
+		LCD.setDisplay(buff);
+		LCD.refresh(); // Unsure if needed
+	}
 }
