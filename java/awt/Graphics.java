5ef8ff928b0e81deecdb06d5d8810e8ec03ce441
bbagnall <bbagnall@06c8f799-c779-4783-8af7-179afedb67c8>
2007-01-11 20:08:14 +0000

    Added drawOval() method.
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk/classes@257 06c8f799-c779-4783-8af7-179afedb67c8

--- java/awt/Graphics.java
+++ java/awt/Graphics.java
@@ -1,4 +1,4 @@
-package java.awt;
+//package java.awt;
 
 import lejos.nxt.LCD;
 
@@ -19,8 +19,18 @@ public class Graphics {
 	// TESTING PURPOSES ONLY
 	public static void main(String [] options) throws Exception {
 		Graphics g = new Graphics();
-		g.drawLine(5,5,60,60);
-		g.drawRect(62, 10, 25, 35);
+		for(int i=0;i<HEIGHT;i=i+2)
+			g.drawLine(0,HEIGHT - 1 - i,i,0);
+		g.refresh();
+		Thread.sleep(5000);
+		g.clear();
+		for(int i=0;i<HEIGHT;i=i+6)
+			g.drawRect(0, 0, i, i);
+		g.refresh();
+		Thread.sleep(5000);
+		g.clear();
+		for(int i=0;i<8;i++)
+			g.drawOval(WIDTH/2, HEIGHT/2, 7*i, 8*(7-i));
 		g.refresh();
 	}
 
@@ -69,6 +79,65 @@ public class Graphics {
 		}
 	}
 
+	public void drawOval(int x, int y, int width, int height) {
+		
+		int r1 = width/2;
+		int r2 = height/2;
+		
+		int rr1 =  r1 * r1;
+		int r21 = rr1 + rr1;
+		int r41 = r21 + r21;
+		int rr2 = r2 * r2;
+		int r22 = rr2 + rr2;
+		int r42 = r22 + r22;
+		int k = r2 * r21;
+		int p = r41 + r42;
+		int cc = r22 + rr1 - k;
+		k = k + k; // Addition faster than mult.
+		int rf2 = -r41;
+		r22 = r22 + p;
+		int xx = 0;
+		int yy = r2;
+		do {
+			ePlot(x, y, xx, yy);
+		     if(cc >= 0) {
+		    	yy = yy - 1;
+		        k = k - r41;
+		        cc = cc - k;
+		     }
+		     cc = cc + rf2 + r22;
+		     rf2 = rf2 + r42;
+		     xx = xx + 1;
+		} while(rf2 <= k);
+		
+		r22 = r22 - p;
+		k = r1 * r22;
+		cc = r21 + rr2 - k;
+		k = k + k; // Addition faster than mult.
+		int rf1 = -r42;
+		r21 = r21 + p;
+		xx = r1;
+		yy = 0;
+		do {
+			ePlot(x, y, xx, yy);
+		    if(cc >= 0) {
+		    	xx = xx - 1;
+		        k = k - r42;
+		        cc = cc - k;
+		    }
+		    cc = cc + rf1 + r21;
+		    rf1 = rf1 + r41;
+		    yy = yy + 1;
+		} while(rf1 <= k);
+	}
+
+	private void ePlot(int x, int y, int a, int b) {
+		setPixel(x + a, y + b);
+		setPixel(x + a, y - b);
+		setPixel(x - a, y + b);
+		setPixel(x - a, y - b);
+	}
+
 	public void drawRect(int x, int y, int width, int height) {
 		if ((width < 0) || (height < 0))
 			return;
@@ -87,9 +156,16 @@ public class Graphics {
 		LCD.drawString(str, x, y);
 	}
 
-	// Temp for testing purposes until canvas made.
+	// Temp for testing purposes until Canvas made.
 	public void refresh() {
 		LCD.setDisplay(buff);
 		LCD.refresh(); // Unsure if needed
+	}
+	
+	// Temp method for testing. Clears out graphics buffer
+	// and refreshes screen.
+	public void clear() {
+		buff = new int[HEIGHT*WIDTH/32];
+		refresh();
 	}
 }
