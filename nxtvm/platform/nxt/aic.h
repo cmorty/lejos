d765fd0db7620ffd00e4c3f953a1163be3f75c64
davidanderson <davidanderson@06c8f799-c779-4783-8af7-179afedb67c8>
2007-01-21 10:08:29 +0000

    Document the Advanced Interrupt Controller driver.
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@262 06c8f799-c779-4783-8af7-179afedb67c8

--- nxtvm/platform/nxt/aic.h
+++ nxtvm/platform/nxt/aic.h
@@ -1,5 +1,14 @@
+/* Driver for the AT91SAM7's Advanced Interrupt Controller (AIC).
+ *
+ * The AIC is responsible for queuing interrupts from other
+ * peripherals on the board. It then hands them one by one to the ARM
+ * CPU core for handling, according to each peripheral's configured
+ * priority.
+ */
+
 #ifndef __AIC_H__
 #  define __AIC_H__
+
 #  include "mytypes.h"
 
 void aic_initialise(void);
@@ -8,6 +17,7 @@ void aic_mask_on(U32 vector);
 void aic_mask_off(U32 vector);
 void aic_clear(U32 mask);
 
+/* Priority levels for interrupt lines. */
 #  define AIC_INT_LEVEL_LOW    2
 #  define AIC_INT_LEVEL_NORMAL 4
 #  define AIC_INT_LEVEL_ABOVE_NORMAL 5
