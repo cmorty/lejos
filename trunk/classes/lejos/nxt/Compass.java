60be4c05268f2c347d108b24844125a87ae98e0e
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2007-02-02 19:42:31 +0000

    First go at class for Compass sensor

---
+++ trunk/classes/lejos/nxt/Compass.java
@@ -0,0 +1,29 @@
+package lejos.nxt;
+
+/**
+ * Abstraction for a HiTechnic or Mindsensors compass.
+ * 
+ */
+public class Compass {
+	Port port;
+	byte[] buf;
+	
+	public Compass(Port port)
+	{
+		this.port = port;
+		buf = new byte[2];
+		port.setPowerType(2);
+		port.i2cEnable();
+	}
+	
+	public int getDegrees() {		
+		int ret = port.i2cStart(1, 0x42, 2, buf, 2, 0);
+		
+		if (ret == 0) {
+			while (port.i2cBusy() != 0) {
+				Thread.yield();
+			}
+			return (buf[0] & 0xff + ((buf[1] & 0xff) << 8));
+		} else return -1;
+	}
+}
