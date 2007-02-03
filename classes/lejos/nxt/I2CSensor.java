76d33a332f41037ef803f157c50680bcb0c1ae44
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2007-02-03 21:35:55 +0000

    First version of I2CSensor
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@299 06c8f799-c779-4783-8af7-179afedb67c8

---
+++ classes/lejos/nxt/I2CSensor.java
@@ -0,0 +1,64 @@
+package lejos.nxt;
+
+public abstract class I2CSensor {
+	Port port;
+	int address = 1;
+	String version = "        ";
+	String productID = "        ";
+	String sensorType = "        ";
+	char [] versionChars = StringUtils.getCharacters(version);
+	char [] productIDChars = StringUtils.getCharacters(productID);
+	char [] sensorTypeChars = StringUtils.getCharacters(sensorType);
+	byte[] byteBuff = new byte[8]; 
+	
+	public int getData(int register, byte [] buf, int len) {	
+		int ret = port.i2cStart(address, register, len, buf, len, 0);
+		
+		if (ret != 0) return ret;
+		
+		while (port.i2cBusy() != 0) {
+			Thread.yield();
+		}
+		
+		return 0;
+	}
+	
+	public int sendData(int register, byte [] buf, int len) {	
+		int ret = port.i2cStart(address, register, len, buf, len, 1);
+		
+		if (ret != 0) return ret;
+		
+		while (port.i2cBusy() != 0) {
+			Thread.yield();
+		}
+		
+		return 0;
+	}
+	
+	public String getVersion() {
+		int ret = getData(0x00, byteBuff, 8);
+
+		for(int i=0;i<8;i++) {
+			versionChars[i] = (ret == 0 ? (char) byteBuff[i] : ' ');
+		}	
+		return version;
+	}
+	
+	public String getProductID() {
+		int ret = getData(0x08, byteBuff, 8);
+
+		for(int i=0;i<8;i++) {
+			productIDChars[i] = (ret == 0 ? (char) byteBuff[i] : ' ');
+		}	
+		return productID;
+	}
+	
+	public String getSensorType() {
+		int ret = getData(0x10, byteBuff, 8);
+
+		for(int i=0;i<8;i++) {
+			sensorTypeChars[i] = (ret == 0 ? (char) byteBuff[i] : ' ');
+		}	
+		return sensorType;
+	}
+}
