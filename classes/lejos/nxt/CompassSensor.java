7540432ee85f7ddfbf209f6091e1454e58785066
bbagnall <bbagnall@06c8f799-c779-4783-8af7-179afedb67c8>
2007-02-04 23:18:21 +0000

    Changed getDegrees() to return float. Now returns 0-359.9 values.
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@308 06c8f799-c779-4783-8af7-179afedb67c8

--- classes/lejos/nxt/CompassSensor.java
+++ classes/lejos/nxt/CompassSensor.java
@@ -15,12 +15,17 @@ public class CompassSensor extends I2CSensor {
 		port.i2cEnable();
 	}
 	
-	public int getDegrees() {		
+	/**
+	 * Returns the directional heading in degrees. (0 to 359.9)
+	 * 0 is due North (on Mindsensors circuit board a white arrow indicates
+	 * the direction of compass). Reading increases clockwise.
+	 * @return Heading in degrees. Resolution is within 0.1 degrees
+	 */
+	public float getDegrees() {		
 		int ret = getData(0x42, buf, 2);
-
-		return (ret == 0 
-				 ? (((buf[0] & 0xff)<< 1) + buf[1])
-				 : -1);
+		if(ret != 0) return -1;
+		int iHeading = (0xFF & buf[0]) | ((0xFF & buf[1]) << 8);
+		float dHeading = iHeading / 10.00F; 
+		return dHeading;	
 	}
 }
-
