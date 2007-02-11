0e908b90043b432255acde7d26e8a2550685fba3
bbagnall <bbagnall@06c8f799-c779-4783-8af7-179afedb67c8>
2007-02-11 00:17:44 +0000

    Added HiTechnic color sensor class and example.
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@314 06c8f799-c779-4783-8af7-179afedb67c8

---
+++ examples/ColorDetector/ColorDetector.java
@@ -0,0 +1,34 @@
+import lejos.nxt.Button;
+import lejos.nxt.ColorSensor;
+import lejos.nxt.LCD;
+import lejos.nxt.Port;
+
+/**
+ * For testing the HiTechnic color sensor (see lejos.nxt.ColorSensor).
+ * @author BB
+ */
+public class ColorDetector {
+
+	final static int INTERVAL = 200; // milliseconds
+	
+	public static void main(String [] args) throws Exception {
+		ColorSensor cmps = new ColorSensor(Port.S1);
+		
+		while(!Button.ESCAPE.isPressed()) {
+			LCD.clear();
+			LCD.drawString(cmps.getProductID(), 0, 0);
+			LCD.drawString(cmps.getSensorType(), 0, 1);
+			LCD.drawString(cmps.getVersion(), 9, 1);
+			LCD.drawString("Color", 0, 3);
+			LCD.drawInt((int)cmps.getColorNumber(),7,3);
+			LCD.drawString("R", 0, 5);
+			LCD.drawInt((int)cmps.getRed(),1,5);
+			LCD.drawString("G", 5, 5);
+			LCD.drawInt((int)cmps.getGreen(),6,5);
+			LCD.drawString("B", 10, 5);
+			LCD.drawInt((int)cmps.getBlue(),11,5);
+			LCD.refresh();
+			Thread.sleep(INTERVAL);
+		}
+	}
+}
