8b92033e7d7d01cd921b43bd6a529e8e059478ec
bbagnall <bbagnall@06c8f799-c779-4783-8af7-179afedb67c8>
2007-02-23 06:21:41 +0000

    Added preliminary CompassPilot class.
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@332 06c8f799-c779-4783-8af7-179afedb67c8

---
+++ classes/lejos/robotics/CompassPilot.java
@@ -0,0 +1,52 @@
+package lejos.robotics;
+
+import lejos.nxt.CompassSensor;
+import lejos.nxt.*;
+
+/**
+ * Uses a compass to control rotation.
+ * @author BB
+ */
+public class CompassPilot extends SteeringControl {
+  private CompassSensor compass;
+  //private SteeringControl sc;
+  private boolean isCompassTravel = true;
+  
+  public CompassPilot(CompassSensor compass, float wheelDiameter,float trackWidth,Motor leftMotor, Motor rightMotor) {
+    super(wheelDiameter, trackWidth, leftMotor, rightMotor);
+  	this.compass = compass;
+  }
+
+  /**
+   * Rotates the specified number of degrees
+   */
+  public void rotate(int degrees) {
+    int prevReading = (int)compass.getDegreesCartesian();
+    int total = 0; // running total of degrees rotated
+    rotate(degrees* 2); // Start rotating - will use stop() to end rotation
+    degrees = Math.abs(degrees);
+    while(total < degrees) {
+      int curReading = (int)compass.getDegreesCartesian();
+      total += getDifference(curReading, prevReading);
+      prevReading = curReading;
+      Thread.yield();
+    }
+    stop();
+  }
+  
+  /** Returns absolute value of difference */
+  private int getDifference(int cur, int prev) {
+    int diff = Math.abs(cur - prev);
+    if(diff > 180) diff = 360 - diff;
+    return diff;
+  }
+  
+  /**
+   *  Set to true if you want to use compass to maintain straigh line while traveling.
+   *  Warning: If in house, metal objects cause local variations in magnetic field which
+   *  cause it to weave slightly.
+   */
+  public void setCompassTravel(boolean useCompass) {
+    isCompassTravel = useCompass;
+  }
+}
