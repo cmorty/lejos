8aa53ca5496a588f00967e6e3e5202c1ca2372a4
bbagnall <bbagnall@06c8f799-c779-4783-8af7-179afedb67c8>
2007-03-09 22:50:38 +0000

    Changed name to GraphicsSample
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@365 06c8f799-c779-4783-8af7-179afedb67c8

---
+++ examples/Graphics/GraphicsSample.java
@@ -0,0 +1,12 @@
+package Graphics;
+import javax.microedition.lcdui.Graphics;
+
+public class GraphicsSample {
+	
+	public static void main(String [] options) throws Exception {
+		Graphics g = new Graphics();
+		g.drawLine(5,5,60,60);
+		g.drawRect(62, 10, 25, 35);
+		g.refresh();
+	}
+}
