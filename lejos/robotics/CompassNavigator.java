c3dbd95fb12214406c406aa61240ec650661caaa
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2007-03-09 18:29:53 +0000

    CompassNavigator that extends TachoNavigator passing it a CompassPilot
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk/classes@363 06c8f799-c779-4783-8af7-179afedb67c8

---
+++ lejos/robotics/CompassNavigator.java
@@ -0,0 +1,15 @@
+package lejos.robotics;
+import lejos.nxt.*;
+
+public class CompassNavigator extends TachoNavigator {
+
+	public CompassNavigator(CompassSensor compass, float wheelDiameter, float trackWidth, Motor leftMotor, Motor rightMotor) 
+	{
+		super(new CompassPilot(compass, wheelDiameter,trackWidth,leftMotor, rightMotor));
+	}
+	
+	public CompassNavigator(CompassSensor compass, float wheelDiameter, float trackWidth, Motor leftMotor, Motor rightMotor, boolean reverse) 
+	{
+		super(new CompassPilot(compass, wheelDiameter,trackWidth,leftMotor, rightMotor,reverse));
+	}
+}
