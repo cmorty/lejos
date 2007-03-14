870d2e4ab1892d1a2c4a2a29592a6e6778718c81
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2007-03-14 19:57:33 +0000

    Major restructuring of lejos API

--- trunk/examples/TestMotor/TestMotor.java
+++ trunk/examples/TestMotor/TestMotor.java
@@ -10,7 +10,7 @@ public class TestMotor
     String m3 = "Motor C: ";
     String p = "Port S1: ";
 
-    TouchSensor touch = new TouchSensor(Port.S1);
+    TouchSensor touch = new TouchSensor(SensorPort.S1);
 
     for(;;) {
        LCD.clear();
@@ -21,7 +21,7 @@ public class TestMotor
        LCD.drawString(m3,0,3);
        LCD.drawInt(Motor.C.getTachoCount(),9,3);
        LCD.drawString(p,0,4);
-       LCD.drawInt(Port.S1.readRawValue(),9,4);
+       LCD.drawInt(SensorPort.S1.readRawValue(),9,4);
        Motor.A.setSpeed(100);
        if (touch.isPressed()) Motor.A.forward();
        Thread.sleep(1000);
