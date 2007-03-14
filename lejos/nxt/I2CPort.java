8e19ccc5132cd095a62e4fe16e38b161978b0b89
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2007-03-14 18:38:45 +0000

    Major restructuring of lejos API
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk/classes@381 06c8f799-c779-4783-8af7-179afedb67c8

---
+++ lejos/nxt/I2CPort.java
@@ -0,0 +1,15 @@
+package lejos.nxt;
+
+public interface I2CPort extends SimpleSensorPort {
+
+	public void i2cEnable();
+	
+	public void i2cDisable();
+	
+	public int i2cBusy();
+	
+	public int i2cStart(int address, int internalAddress,
+            int numInternalBytes, byte[] buffer,
+            int numBytes, int transferType);
+	
+}
