01d1737626f68249911a1ab417491af0f9af6552
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2007-03-14 19:16:57 +0000

    Major restructuring of lejos API
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@404 06c8f799-c779-4783-8af7-179afedb67c8

---
+++ classes/lejos/subsumption/StopException.java
@@ -0,0 +1,12 @@
+package lejos.subsumption;
+
+
+/**
+ * This exception indicates that an activity should stop.
+ * @see Activity
+ * @author Paul Andrews
+ */
+public class StopException extends Exception
+{
+}
+
