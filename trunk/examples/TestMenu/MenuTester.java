d736dde2e9dbb0120a3a4d67f02e7f1b3e9e64d1
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2007-03-20 18:55:40 +0000

    Changed TestMenu sample to use TextMenu and other tidying up

--- trunk/examples/TestMenu/MenuTester.java
+++ trunk/examples/TestMenu/MenuTester.java
@@ -1,40 +1,23 @@
 
-package lejos.nxt;
-import lejos.nxt.LCD;
-import lejos.nxt.Button;
+import lejos.nxt.*;
 
 public class MenuTester 
 {
-		public static void main(String[] args)
+	public static void main(String[] args)
 	{
 
-		String [] items = new String[12];
-		items[0] = "zero";
-		items[1] =  "one";
-		items[2] = "two";
-		items[3] = "three";
-		items[4] = "four";
-		items[5] =  "five";
-		items[6] = "six";
-		items[7] = "seven";
-		items[8] = "eight";
-		items[9] =  "nine";
-		items[10] = "ten";
-		items[11] = "eleven";
+		String [] items = {
+		  "zero", "one","two","three","four", "five",
+		  "six","seven","eight","nine","ten","eleven"};
 
-//		SimpleMenu menu = new SimpleMenu(items);
-//		SimpleMenu menu = new SimpleMenu(items, 1,4);
-		SimpleMenu menu = new SimpleMenu(items, 7,"Start Menu");
-		
-//		int sel = 	menu.selectFrom(_items);
+		TextMenu menu = new TextMenu(items, 7,"Start Menu");
+
 		int sel = menu.select();
 		LCD.clear();
-		LCD.drawInt(sel,6,0);
+		LCD.drawInt(sel,2,6,0);
 		LCD.refresh();
 
 		while(Button.readButtons()== 0);
 	}
 
-	
-	
 }
