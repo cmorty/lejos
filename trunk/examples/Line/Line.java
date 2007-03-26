84b4366a6a211c220f86ec6f8e0d7da9da94c509
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2007-03-26 22:53:35 +0000

    New Line Follower example using Behavior and Pilot

--- trunk/examples/Line/Line.java
+++ trunk/examples/Line/Line.java
@@ -1,101 +1,55 @@
 import lejos.nxt.*;
+import lejos.subsumption.*;
+import lejos.navigation.*;
 
-public class Line 
-{	
+public class Line {
+	
 	public static void main (String[] aArg)
 	throws Exception
 	{
-		int sweep, tacho;
-		final int BLACK = 40;
-		boolean clockwise = true, white;
-		String l = "Light:";
-		String p = "Press ENTER";
-		int db = 0;
-    
-		LightSensor light = new LightSensor(Port.S1);
-		SoundSensor sound = new SoundSensor(Port.S2);
+		final Pilot pilot = new Pilot(5.5f,11.2f,Motor.A, Motor.C);
+		final LightSensor light = new LightSensor(SensorPort.S1);
 		
-        LCD.clear();
-        LCD.drawString(p, 0, 0);
-        LCD.refresh();
-        
-        // wait for ENTER
-		while (!Button.ENTER.isPressed());
-		
-		Motor.A.regulateSpeed(true);
-		Motor.A.regulateSpeed(true);
-		
-        
-        // Finish when ESCAPE is pressed or shouted at
-        while(!Button.ESCAPE.isPressed() ||
-        		sound.readValue() > 50)
-        {
-        	
-            Motor.A.setSpeed(200);
-            Motor.C.setSpeed(200);
-
-        	// Go forward 30       	
-      	
-    		Motor.A.rotate(30);
-    		Motor.C.rotate(30);     	
-	        
-    		while(Motor.A.isRotating() || Motor.C.isRotating());
-	        
-	        for(int i=0;i<200;i++)
-	        {
-	        	db = sound.readValue();
-	        	if (db > 50) break;
-	        	Thread.sleep(1);
-	        }
-	        
-	        if (db > 50) break;
-
-			clockwise = true;
-			sweep = 10;
-			white = false;
+		Behavior DriveForward = new Behavior()
+		{
+			public boolean takeControl() {return light.readValue() <= 40;}
 			
-			do  
-			{		 				
-		        LCD.clear();
-		        LCD.drawString(l, 0, 0);
-			    LCD.drawInt(light.readValue(),7,0);
-		        LCD.refresh();
-		        
-		        Motor.A.setSpeed(50);
-		        Motor.C.setSpeed(50);
-		        
-				if (clockwise)
-				{
-					Motor.A.backward();
-					tacho = Motor.C.getTachoCount();
-					Motor.C.forward();
-					
-					// Turn by the amount specified by sweep
-					while (Motor.C.getTachoCount() < tacho + sweep
-							&& (white = (light.readValue() > BLACK)));
-				}
-				else
-				{
-					Motor.C.backward();
-					tacho = Motor.A.getTachoCount();
-					Motor.A.forward();
-					
-					while (Motor.A.getTachoCount() < tacho + sweep
-							&& (white = (light.readValue() > BLACK)));
-				}
-				
-				// Reverse the direction and double the sweep
-				clockwise = !clockwise;
-				sweep *= 2;
+			public void suppress() {
+				pilot.stop();
 			}
-			while(white && !Button.ESCAPE.isPressed());
 			
-		}
-        // Tidy up
-        Motor.A.stop();
-        Motor.C.stop();
-        LCD.clear();
-        LCD.drawString("Finished",3,4);
-        LCD.refresh();
+			public void action() {
+				pilot.forward();
+			}					
+		};
+		
+		Behavior OffLine = new Behavior()
+		{
+			private boolean suppress = false;
+			
+			public boolean takeControl() {return light.readValue() > 40;}
+
+			public void suppress() {
+				suppress = true;
+				while (suppress) Thread.yield();
+			}
+			
+			public void action() {
+				int sweep = 10;
+				while (!suppress) {
+					pilot.rotate(sweep,true);
+					while (!suppress && pilot.isMoving()) Thread.yield();
+					sweep *= -2;
+				}
+				pilot.stop();
+				suppress = false;
+			}
+		};
+
+		Button.ENTER.waitForPressAndRelease();
+
+		Behavior[] bArray = {OffLine, DriveForward};		
+	    (new Arbitrator(bArray)).start();
 	}
 }
+
