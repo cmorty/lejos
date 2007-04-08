da6204a2c4bf5512f9ced9a7c76e0be1249fafc8
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2007-04-08 14:12:58 +0000

    Execute program from flash memory
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk/classes@518 06c8f799-c779-4783-8af7-179afedb67c8

--- lejos/nxt/Flash.java
+++ lejos/nxt/Flash.java
@@ -9,4 +9,6 @@ public class Flash {
 	public static native void readPage(byte[] buf, int pageNum);
 
 	public static native void writePage(byte[] buf, int pageNum);
+	
+	public static native void exec(int pageNum);
 }
