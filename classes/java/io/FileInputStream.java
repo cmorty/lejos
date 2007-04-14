1af7797e1c2d719ddafc4993055adc4dc954978b
bbagnall <bbagnall@06c8f799-c779-4783-8af7-179afedb67c8>
2007-04-14 04:42:39 +0000

    Latest changes to File classes.
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@539 06c8f799-c779-4783-8af7-179afedb67c8

--- classes/java/io/FileInputStream.java
+++ classes/java/io/FileInputStream.java
@@ -4,27 +4,34 @@ import lejos.nxt.Flash;
 
 public class FileInputStream extends InputStream{
 	
+	int pointer;
 	int page_pointer;
 	int data_pointer; // Current byte in buff array
 	byte [] buff;
+	File file;
 	
 	public FileInputStream(File f) {
 		buff = new byte[File.PAGE_SIZE];
 		page_pointer = f.page_location;
 		data_pointer = 0; // Start of page
+		pointer = 0; // Overall mark
+		file = f;
 		Flash.readPage(buff, page_pointer);
 	}
 	
 	public int read() throws IOException {
+		// Check against file size for EOF.
+		if(pointer >= file.file_length)
+			return -1; // Indicates EOF
+				
 		int val = buff[data_pointer];
 		data_pointer++;
-		// !! Need to check against file size for EOF.
-		// (how to do EOF?)
+		pointer++;
 		if(data_pointer >= File.PAGE_SIZE) {
 			data_pointer = 0;
 			page_pointer++;
 			Flash.readPage(buff, page_pointer);
 		}
-		return val;
+		return val + 128; // Need to return 0-255 value
 	}	
 }
