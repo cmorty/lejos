b0577f6bce50f2bb5fc93f9450a38021a0a7fd17
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2007-05-01 12:17:13 +0000

    Added nxjflash utility
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@584 06c8f799-c779-4783-8af7-179afedb67c8

--- libnxt/firmware.c
+++ libnxt/firmware.c
@@ -2,6 +2,8 @@
  * NXT bootstrap interface; NXT firmware handling code.
  *
  * Copyright 2006 David Anderson <david.anderson@calixo.net>
+ * Modified 2007 by Lawrie Griffiths <lawrie.griffiths@ntlworld.com> 
+ * to support lejos NXJ firmware flashing.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -35,13 +37,13 @@
 #include "flash_routine.h"
 
 static nxt_error_t
-nxt_flash_prepare(nxt_t *nxt)
+nxt_flash_prepare(nxt_t *nxt, int unlock)
 {
   // Put the clock in PLL/2 mode
   NXT_ERR(nxt_write_word(nxt, 0xFFFFFC30, 0x7));
 
   // Unlock the flash chip
-  NXT_ERR(nxt_flash_unlock_all_regions(nxt));
+  if (unlock) NXT_ERR(nxt_flash_unlock_all_regions(nxt));
 
   // Send the flash writing routine
   NXT_ERR(nxt_send_file(nxt, 0x202000, flash_bin, flash_len));
@@ -74,14 +76,14 @@ nxt_flash_finish(nxt_t *nxt)
 
 
 static nxt_error_t
-nxt_firmware_validate_fd(int fd)
+nxt_firmware_validate_fd(int fd, int max_size)
 {
   struct stat s;
 
   if (fstat(fd, &s) < 0)
     return NXT_FILE_ERROR;
 
-  if (s.st_size > 256*1024)
+  if (s.st_size > max_size)
     return NXT_INVALID_FIRMWARE;
 
   return NXT_OK;
@@ -89,7 +91,7 @@ nxt_firmware_validate_fd(int fd)
 
 
 nxt_error_t
-nxt_firmware_validate(char *fw_path)
+nxt_firmware_validate(char *fw_path, int max_pages)
 {
   nxt_error_t err;
   int fd;
@@ -98,7 +100,7 @@ nxt_firmware_validate(char *fw_path)
   if (fd < 0)
     return NXT_FILE_ERROR;
 
-  err = nxt_firmware_validate_fd(fd);
+  err = nxt_firmware_validate_fd(fd, max_pages * 256);
   close(fd);
 
   return err;
@@ -106,47 +108,62 @@ nxt_firmware_validate(char *fw_path)
 
 
 nxt_error_t
-nxt_firmware_flash(nxt_t *nxt, char *fw_path)
+nxt_firmware_flash(nxt_t *nxt, char *fw_path, 
+                   int start_page, int max_pages, int unlock, int write_len)
 {
-  int fd, i, err;
+  int fd, i, err, len = 0;
+  char buf[256];
 
   fd = open(fw_path, O_RDONLY);
   if (fd < 0)
     return NXT_FILE_ERROR;
 
-  err = nxt_firmware_validate_fd(fd);
+  err = nxt_firmware_validate_fd(fd, max_pages * 1024);
   if (err != NXT_OK)
     {
       close(fd);
       return NXT_INVALID_FIRMWARE;
     }
 
-  NXT_ERR(nxt_flash_prepare(nxt));
+  NXT_ERR(nxt_flash_prepare(nxt, unlock));
 
-  for (i = 0; i < 1024; i++) //256*1024; i += 256)
+  for (i = start_page; i < start_page + max_pages; i++) 
     {
-      char buf[256];
+
       int ret;
 
       memset(buf, 0, 256);
       ret = read(fd, buf, 256);
 
-      if (ret != -1)
-        NXT_ERR(nxt_flash_block(nxt, i, buf));
+      if (ret != -1) 
+        {
+          NXT_ERR(nxt_flash_block(nxt, i, buf));
+          len += ret;
+        }
 
       if (ret < 256)
         {
           close(fd);
-          NXT_ERR(nxt_flash_finish(nxt));
+        
+          if (ret != -1 && write_len)
+            {
+              ((unsigned *) buf)[63] = len;
+              NXT_ERR(nxt_flash_block(nxt,start_page + max_pages -1, buf));
+            }
 
+          NXT_ERR(nxt_flash_finish(nxt));
+          
           return ret == -1 ? NXT_FILE_ERROR : NXT_OK;
         }
-
-      NXT_ERR(nxt_flash_block(nxt, i, buf));
     }
 
   close(fd);
+  
+  ((unsigned *) buf)[63] = len;
+  NXT_ERR(nxt_flash_block(nxt,start_page + max_pages -1, buf));
+  
   NXT_ERR(nxt_flash_finish(nxt));
 
   return NXT_OK;
 }
+
