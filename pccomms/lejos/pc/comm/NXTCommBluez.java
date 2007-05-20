ff01df8da2f4ae7e8ee3619b2ce04538ef6e48f6
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2007-05-20 14:59:30 +0000

    Support for Bluez - Bluetooth on Linux
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@650 06c8f799-c779-4783-8af7-179afedb67c8

---
+++ pccomms/lejos/pc/comm/NXTCommBluez.java
@@ -0,0 +1,140 @@
+package lejos.pc.comm;
+
+public class NXTCommBluez implements NXTComm {
+
+	private static final String BDADDR_ANY = "00:00:00:00:00:00";
+
+	private int sk = -1;
+
+	static {
+		System.loadLibrary("jbluez");
+	}
+	
+	public NXTInfo[] search(String name, int protocol) {
+		String btAddress = search();
+		if (btAddress == null) return new NXTInfo[0];
+		else {
+			NXTInfo nxtInfo = new NXTInfo();
+			System.out.println("Setting address to " + btAddress);
+			nxtInfo.blueZBTAddress = btAddress;
+			NXTInfo[] nxts = new NXTInfo[1];
+			nxts[0] = nxtInfo;
+			return nxts;
+		}
+	}
+
+	public void close() {
+		try {
+			rcSocketShutdown(sk);
+		} catch (BlueZException e) {
+			System.err.println(e.getMessage());
+			e.printStackTrace();
+			if (sk != -1) {
+				try {
+					rcSocketClose(sk);
+				} catch (BlueZException e1) {
+					System.err.println(e.getMessage());
+					e.printStackTrace();
+				}
+				sk = -1;
+			}
+		}
+	}
+
+	public void open(NXTInfo nxt) {
+		try {
+			open(BDADDR_ANY, nxt.blueZBTAddress, 1);
+		} catch (BlueZException e) {
+			System.out.println("Open failed");
+		}
+		
+	}
+
+	public byte [] sendRequest(byte[] request, int replyLen) {
+		
+		// add lsb & msb
+		byte[] lsb_msb = new byte[2];
+		lsb_msb[0] = (byte) request.length;
+		lsb_msb[1] = (byte) 0x00;
+		request = concat(lsb_msb, request);
+	
+		try {
+			rcSocketSend(sk, request);
+		} catch (BlueZException e) {
+			System.err.println(e.getMessage());
+			e.printStackTrace();
+		}
+		
+		byte[] data = null;
+		try {
+			data = rcSocketRecv(sk);
+		} catch (BlueZException e) {
+			System.err.println(e.getMessage());
+			e.printStackTrace();
+		}
+		
+		// remove lsb & msb
+		data = subArray(data, 2, data.length);
+
+		return data;
+	}
+
+	private void open(String l_bdaddr, String r_bdaddr, int channel) throws BlueZException {
+		boolean ok = false;
+
+		try {
+			System.out.println("Creating socket");
+			sk = rcSocketCreate();
+			System.out.println("Binding");
+			rcSocketBind(sk, l_bdaddr);
+			System.out.println("Connecting");
+			rcSocketConnect(sk, r_bdaddr, channel);
+
+			ok = true;
+		} finally {
+			if (!ok) {
+				if (sk != -1) {
+					rcSocketClose(sk);
+					sk = -1;
+				}
+			}
+		}
+	}
+	
+	private byte[] concat(byte[] data1, byte[] data2) {
+		int l1 = data1.length;
+		int l2 = data2.length;
+		
+		byte[] data = new byte[l1 + l2];
+		System.arraycopy(data1, 0, data, 0, l1);
+		System.arraycopy(data2, 0, data, l1, l2);
+		
+		return data;
+	}
+		
+	
+	private byte[] subArray(byte[] data, int start, int end) {	
+
+		byte[] result = new byte[end - start];
+		System.arraycopy(data, start, result, 0, end - start);
+
+		return result;
+	}
+	
+	native private String search();
+	
+	native private int rcSocketCreate() throws BlueZException;
+
+	native private void rcSocketBind(int sk, String bdaddr) throws BlueZException;
+
+	native private void rcSocketConnect(int sk, String bdaddr, int channel) throws BlueZException;
+
+	native private void rcSocketSend(int sk, byte[] data) throws BlueZException;
+
+	native private byte[] rcSocketRecv(int sk) throws BlueZException;
+
+	native private void rcSocketShutdown(int sk) throws BlueZException;
+
+	native private void rcSocketClose(int sk) throws BlueZException;
+
+}
