a31818308ad785fee838d76c1aad29baba42efee
bbagnall <bbagnall@06c8f799-c779-4783-8af7-179afedb67c8>
2007-05-21 18:05:05 +0000

    Added simple CompassNavigator constructor that uses a single CompassPilot object.
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk/classes@657 06c8f799-c779-4783-8af7-179afedb67c8

--- lejos/navigation/CompassNavigator.java
+++ lejos/navigation/CompassNavigator.java
@@ -13,12 +13,16 @@ public class CompassNavigator extends TachoNavigator {
 
 	public CompassNavigator(CompassSensor compass, float wheelDiameter, float trackWidth, Motor leftMotor, Motor rightMotor) 
 	{
-		super(new CompassPilot(compass, wheelDiameter,trackWidth,leftMotor, rightMotor));
+		this(compass, wheelDiameter,trackWidth,leftMotor, rightMotor, false);
 	}
 	
 	public CompassNavigator(CompassSensor compass, float wheelDiameter, float trackWidth, Motor leftMotor, Motor rightMotor, boolean reverse) 
 	{
 		super(new CompassPilot(compass, wheelDiameter,trackWidth,leftMotor, rightMotor,reverse));
+	}
+	
+	public CompassNavigator(CompassPilot cp) {
+		super(cp);
 	}
 }
 
