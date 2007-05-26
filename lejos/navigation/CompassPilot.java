ed17738837dc0f3ed3bbb3aa4700154398bbdf6f
bbagnall <bbagnall@06c8f799-c779-4783-8af7-179afedb67c8>
2007-05-26 20:30:36 +0000

    Added partial CompassPilot. Not fully working yet.
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk/classes@681 06c8f799-c779-4783-8af7-179afedb67c8

--- lejos/navigation/CompassPilot.java
+++ lejos/navigation/CompassPilot.java
@@ -1,61 +1,103 @@
 package lejos.navigation;
+import lejos.nxt.CompassSensor;
+import lejos.nxt.Motor;
+import lejos.nxt.LCD;
 
-import lejos.nxt.*;
-
-/**
- * Uses a compass to control rotation.
- * @author BB
- */
 public class CompassPilot extends Pilot {
-  private CompassSensor compass;
-  //private SteeringControl sc;
-  private boolean isCompassTravel = true;
-  
-  public CompassPilot(CompassSensor compass, float wheelDiameter,float trackWidth,Motor leftMotor, Motor rightMotor) {
-    this(compass, wheelDiameter, trackWidth, leftMotor, rightMotor, false);
-  }
-
-  public CompassPilot(CompassSensor compass, float wheelDiameter,float trackWidth,Motor leftMotor, Motor rightMotor, boolean reverse) {
-    super(wheelDiameter, trackWidth, leftMotor, rightMotor, reverse);
-  	this.compass = compass;
-  	try {Thread.sleep(100);} catch (Exception e) {} // Allow compass to stabalize
-  	compass.resetCartesianZero();
-  }
-  
-  /**
-   * Rotates the specified number of degrees
-   */
-  public void rotate(int degrees) {
-    int prevReading = (int)compass.getDegreesCartesian();
-    int total = 0; // running total of degrees rotated
-    rotate(degrees* 2); // Start rotating - will use stop() to end rotation
-    degrees = Math.abs(degrees);
-    while(total < degrees) {
-      int curReading = (int)compass.getDegreesCartesian();
-      total += getDifference(curReading, prevReading);
-      prevReading = curReading;
-      Thread.yield();
-    }
-    stop();
-  }
-  
-  public int getAngle() {
-  	return Math.round(compass.getDegreesCartesian());
-  }
-  
-  /** Returns absolute value of difference */
-  private int getDifference(int cur, int prev) {
-    int diff = Math.abs(cur - prev);
-    if(diff > 180) diff = 360 - diff;
-    return diff;
-  }
-  
-  /**
-   *  Set to true if you want to use compass to maintain straigh line while traveling.
-   *  Warning: If in house, metal objects cause local variations in magnetic field which
-   *  cause it to weave slightly.
-   */
-  public void setCompassTravel(boolean useCompass) {
-    isCompassTravel = useCompass;
-  }
+	
+	private CompassSensor compass;
+	public Regulator regulator = new Regulator(); // inner regulator for thread
+	private int heading; // Heading to point robot
+	private static final int THRESH = 10; // IMPT! Threshold value for rotate()
+	
+	public CompassPilot(CompassSensor cs, float wheelDiameter,float trackWidth,Motor leftMotor, Motor rightMotor) {
+		this(cs, wheelDiameter, trackWidth, leftMotor, rightMotor, false);
+	}
+	
+	public CompassPilot(CompassSensor cs, float wheelDiameter,float trackWidth,Motor leftMotor, Motor rightMotor, boolean reverse) {
+		super(wheelDiameter, trackWidth, leftMotor, rightMotor, reverse);
+		this.compass = cs;
+		heading = (int)compass.getDegreesCartesian(); // Current compass direction = heading target
+		regulator.start();
+		regulator.setDaemon(true);
+	}
+	
+	public void rotate(int angle, boolean immediateReturn) {
+		//lejos.nxt.Sound.buzz();
+		/** 
+		 * Just change the new heading and the regulate thread will handle the rest.
+		 */
+		synchronized(this) {
+			// Using current heading, calculate heading to rotate to:
+			heading = heading + angle;
+			// Handle wraparound problem:
+			if (heading >= 360) heading = heading - 360;
+			if (heading < 0) heading = heading + 360;
+			
+			if(immediateReturn) return;
+	  	}
+		while(Math.abs(getHeadingError(heading)) > THRESH) Thread.yield();
+	}
+	
+	/**
+	 * Returns the angle from the compass.
+	 */
+	public int getAngle() {
+		return (int)compass.getDegreesCartesian();
+	}
+	
+	/**
+	 * Determines the difference between actual compass direction and target heading in degrees 
+	 * @param heading The target angle (in degrees). 
+	 * @return error (in degrees)
+	 */
+	private int getHeadingError(int heading) {
+		int err = getAngle() - heading;
+	
+		// Handles the wrap-around problem:
+		if (err < -180) err = err + 360;
+		if (err > 180) err = err - 360;
+		return err;
+	}
+	
+	public String CMPS = "Cmps"; // LCD DELETE ME
+	public String HEADING = "Headng"; // LCD DELETE ME
+	public String ERROR = "Error"; // LCD DELETE ME
+	public String FREEMEM = "FREE"; // LCD DELETE ME
+	
+	/**
+	 * Reason for this method is because Regulator can't access super.
+	 * @param angle
+	 */
+	private void performRotation(int angle) {
+		// DISPLAY CODE: ** REMOVE WHEN DONE
+		
+		LCD.drawString(CMPS, 0, 0);
+		LCD.drawInt(getAngle(), 7, 0);
+		LCD.drawString(HEADING, 0, 1);
+		LCD.drawInt(heading, 7, 1);
+		LCD.drawInt(angle, 7, 2);
+		LCD.drawString(ERROR, 0, 2);
+		LCD.drawString(FREEMEM, 0, 4);
+		LCD.drawInt((int)System.getRuntime().freeMemory(), 5, 4);
+		LCD.refresh();
+		LCD.clear();
+		//lejos.nxt.Sound.beep();
+		//try{Thread.sleep(1000);}catch(Exception e){}
+		
+		if(angle != 0) super.rotate(angle, false);
+	}
+	
+	private class Regulator extends Thread {
+		public void run() {
+			while(true) {
+				synchronized(this) {
+					int error = getHeadingError(heading);
+					performRotation(error);
+				}
+				Thread.yield();
+			}
+			
+		}
+	}
 }
