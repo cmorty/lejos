1793be4598a14b0e7ee891ef7504984f2eca8e3e
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2007-06-01 22:49:09 +0000

    Bluetooth streams comms example
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@704 06c8f799-c779-4783-8af7-179afedb67c8

---
+++ examples/BTReceive/BTReceive.java
@@ -0,0 +1,34 @@
+import lejos.nxt.*;
+import lejos.nxt.comm.*;
+import java.io.*;
+
+public class BTReceive {
+
+	public static void main(String [] args)  throws Exception 
+	{
+
+		boolean cmdMode = true;
+		BTConnection btc = null;
+		String connected = "Connected";
+		InputStream is = null;
+		OutputStream os = null;
+		
+		while (true)
+		{
+			if (cmdMode) {
+				btc = Bluetooth.waitForConnection();
+				LCD.clear();
+				LCD.drawString(connected,0,0);
+				LCD.refresh();			
+				cmdMode = false;
+				is = btc.openInputStream();
+				os = btc.openOutputStream();
+			}
+			int b = is.read();
+			LCD.drawInt(b,3,0,1);
+			LCD.refresh();
+			os.write((byte) 100-b);
+		}
+	}
+}
+
