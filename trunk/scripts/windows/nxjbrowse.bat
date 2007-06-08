dbd5fc2dc10fa9baf40448fc6635704380bb64ad
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2007-06-08 15:48:09 +0000

    Set nxj.home to load nxj.properties

--- trunk/scripts/windows/nxjbrowse.bat
+++ trunk/scripts/windows/nxjbrowse.bat
@@ -3,7 +3,7 @@ if "%NXJ_HOME%" == ""  goto homeless
 
 set THIRDPARTY_LIBS="%NXJ_HOME%\3rdparty\lib"
 
-java -Djava.library.path="%NXJ_HOME%\bin" -classpath "%NXJ_HOME%\lib\pctools.jar;%NXJ_HOME%\lib\pccomm.jar;%NXJ_HOME%\3rdparty\lib\bluecove.jar" lejos.pc.tools.NXJBrowser %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -Djava.library.path="%NXJ_HOME%\bin" -Dnxj.home="%NXJ_HOME%" -classpath "%NXJ_HOME%\lib\pctools.jar;%NXJ_HOME%\lib\pccomm.jar;%NXJ_HOME%\3rdparty\lib\bluecove.jar" lejos.pc.tools.NXJBrowser %1 %2 %3 %4 %5 %6 %7 %8 %9
 goto end
 
 :homeless
