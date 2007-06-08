b02b17f115ac2b8b60c5bc8c67804540c7b4cd09
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2007-06-08 15:54:38 +0000

    Fix getSingleton and  nxj.properties
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@741 06c8f799-c779-4783-8af7-179afedb67c8

--- snapshot/bin/nxjupload.bat
+++ snapshot/bin/nxjupload.bat
@@ -3,11 +3,10 @@ if "%NXJ_HOME%" == ""  goto homeless
 
 set THIRDPARTY_LIBS="%NXJ_HOME%\3rdparty\lib"
 
-java -Djava.library.path="%NXJ_HOME%\bin" -classpath "%THIRDPARTY_LIBS%\bcel-5.1.jar;%THIRDPARTY_LIBS%\commons-cli-1.0.jar;%NXJ_HOME%\lib\pctools.jar;%NXJ_HOME%\lib\pccomm.jar;%NXJ_HOME%\lib\jtools.jar;%NXJ_HOME%\3rdparty\lib\bluecove.jar" lejos.pc.tools.NXJUpload %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -Djava.library.path="%NXJ_HOME%\bin" -Dnxj.home="%NXJ_HOME%" -classpath "%THIRDPARTY_LIBS%\bcel-5.1.jar;%THIRDPARTY_LIBS%\commons-cli-1.0.jar;%NXJ_HOME%\lib\pctools.jar;%NXJ_HOME%\lib\pccomm.jar;%NXJ_HOME%\lib\jtools.jar;%NXJ_HOME%\3rdparty\lib\bluecove.jar" lejos.pc.tools.NXJUpload %1 %2 %3 %4 %5 %6 %7 %8 %9
 goto end
 
 :homeless
 echo NXJ_HOME not defined
 
 :end
-
