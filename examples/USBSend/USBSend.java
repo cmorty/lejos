ea411ffbf8745164551ea0c0a3e561033cc2e153
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2007-06-10 18:56:03 +0000

    USB Streams example - PC side
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@775 06c8f799-c779-4783-8af7-179afedb67c8

---
+++ examples/USBSend/USBSend.java
@@ -0,0 +1,34 @@
+import lejos.pc.comm.*;
+import java.io.*;
+
+public class USBSend {
+	
+	public static void main(String[] args) {
+		NXTCommLibnxt nxtComm = new NXTCommLibnxt();
+		
+		NXTInfo[] nxtInfo = nxtComm.search(null,NXTCommand.BLUETOOTH);
+		
+		if (nxtInfo.length == 0) {
+			System.out.println("No NXT Found");
+			System.exit(1);
+		}
+
+		nxtComm.open(nxtInfo[0]);
+		
+		InputStream is = nxtComm.getInputStream();
+		OutputStream os = nxtComm.getOutputStream();
+		
+		for(int i=0;i<100;i++) {
+			try {
+				os.write(i);
+				os.flush();
+				System.out.println("Received " + is.read());
+			} catch (IOException ioe) {
+				System.out.println("IO Exception writing bytes");
+			}
+		}
+		
+		nxtComm.close();
+	}
+
+}
