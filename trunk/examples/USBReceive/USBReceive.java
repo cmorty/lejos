858a0f77706f3d9f51fe4ba94696f1a655d126f6
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2007-06-10 18:55:30 +0000

    USB Streams example

---
+++ trunk/examples/USBReceive/USBReceive.java
@@ -0,0 +1,20 @@
+import lejos.nxt.*;
+import java.io.*;
+import lejos.nxt.comm.*;
+
+
+public class USBReceive {
+
+	public static void main(String [] args) throws Exception 
+	{
+		USBConnection conn = new USBConnection();
+		InputStream is = conn.openInputStream();
+		OutputStream os = conn.openOutputStream();
+		while (true) {
+			int b = is.read();
+			LCD.drawInt(b,3,0,1);
+			LCD.refresh();
+			os.write((byte) 100-b);
+		}
+	}
+}
