1e357ba78527696a704d6fd550690378110874b7
rogergla <rogergla@06c8f799-c779-4783-8af7-179afedb67c8>
2007-06-24 03:49:56 +0000

    BTReceive  uses BTInputStream.available

--- trunk/examples/BTReceive/BTReceive.java
+++ trunk/examples/BTReceive/BTReceive.java
@@ -6,13 +6,17 @@ public class BTReceive {
 
 	public static void main(String [] args)  throws Exception 
 	{
-
 		boolean cmdMode = true;
 		BTConnection btc = null;
 		String connected = "Connected";
+        String waiting = "Waiting";
+        String sending = "sending";
 		InputStream is = null;
 		OutputStream os = null;
-		
+        int count = 0;
+		LCD.drawString(waiting,0,0);
+        LCD.refresh();
+        
 		while (true)
 		{
 			if (cmdMode) {
@@ -24,10 +28,33 @@ public class BTReceive {
 				is = btc.openInputStream();
 				os = btc.openOutputStream();
 			}
-			int b = is.read();
-			LCD.drawInt(b,3,0,1);
-			LCD.refresh();
-			os.write((byte) 100-b);
+
+            BTInputStream ss =(BTInputStream) is;
+            int packetLength = 0;
+            while(packetLength == 0)
+            {
+               packetLength = is.available();
+               LCD.drawInt(packetLength,0,1);
+               LCD.refresh();
+               Thread.yield();
+            }
+            count++;
+            int[] buff = new int[256];
+
+            for(int i = 0; i<packetLength; i++)
+            {  
+               buff[i] = is.read();
+               LCD.drawInt(buff[i], 14, i%8);
+               LCD.refresh();
+            }  
+            is.close();
+            try{Thread.sleep(1000);}
+            catch(InterruptedException e){}
+            for (int i = 0; i<packetLength; i++)  os.write((byte) 100-buff[i]);
+            LCD.drawInt(count,0, 3);
+            LCD.drawString(sending, 0,0);
+            os.flush();
+            Thread.yield();
 		}
 	}
 }
