99b1b7e3d6bfd1f6eeb128d3ac93eaa41f7029c2
rogergla <rogergla@06c8f799-c779-4783-8af7-179afedb67c8>
2007-06-24 03:43:34 +0000

    Implemented available()  in BTInputStream
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk/classes@803 06c8f799-c779-4783-8af7-179afedb67c8

--- lejos/nxt/comm/BTInputStream.java
+++ lejos/nxt/comm/BTInputStream.java
@@ -1,20 +1,48 @@
 package lejos.nxt.comm;
 
+
 import java.io.*;
 import lejos.nxt.*;
 
+/**
+ * extends InputStream for BlueTooth; implements available()
+ * @author   Roger Glassey revised on june 23, 2007
+ */
 public class BTInputStream extends InputStream {
 	private byte buf[] = new byte[256];
-	private int bufIdx = -1, bufSize = -1;
-	
-	public int read() {
-		if (bufIdx == bufSize) {
-			do {
-				bufSize = Bluetooth.readPacket(buf, 256);
-			} while (bufSize == 0);
-			bufIdx = 0;
-		}
-		
-	    return buf[bufIdx++] & 0xFF;
+	private int bufIdx = 0, bufSize = 0;
+	private BTConnection connection = null;
+    
+/**
+ * returns one byte as an integer between 0 and 255.  Returns -1 if the end of the stream is reached.
+ * Does not return till some bytes are available.
+ */
+	public int read() 
+    {
+       while(bufSize == 0) bufSize = available();
+       if (bufIdx < bufSize) return buf[bufIdx++] & 0xFF;
+       else 
+          {
+             bufIdx = 0;
+             bufSize = 0;
+             return -1;
+          }
 	}
+   /**
+    * returns the number of bytes in the input buffer - can be read without blocking
+    */
+    public int available()
+    {
+       bufSize = Bluetooth.readPacket(buf, 256);
+       return bufSize;
+    }
+ /**
+  * the stream is restored to its original state - ready to receive more data.
+  */
+    public void close()
+    { 
+       bufIdx = 0;
+       bufSize = 0;
+    }
+
 }
