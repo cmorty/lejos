d42914547925e4c510086f3be4b4f68780f3bcc2
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2007-06-27 19:48:35 +0000

    Improved error handling

--- trunk/pctools/lejos/pc/tools/BatteryLevel.java
+++ trunk/pctools/lejos/pc/tools/BatteryLevel.java
@@ -1,10 +1,11 @@
 package lejos.pc.tools;
 
 import lejos.pc.comm.*;
+import java.io.*;
 
 public class BatteryLevel {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		System.out.println("Battery Level Test");
 		NXTCommand nxtCommand = NXTCommand.getSingleton();
 		
