8677839baca34cf2a55fbbb5c89000a8e4dc4f18
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2007-06-28 18:57:26 +0000

    Handle multiple NXTs and fix error handling

--- trunk/jbluez/jbluez.c
+++ trunk/jbluez/jbluez.c
@@ -5,60 +5,12 @@
 #include <stdlib.h>
 #include <sys/socket.h>
 
-
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/hci.h>
 #include <bluetooth/hci_lib.h>
 #include <bluetooth/rfcomm.h>
 
 #include "jbluez.h"
-
-//---------------------------------------------------------------------------//
-
-JNIEXPORT jstring JNICALL Java_lejos_pc_comm_NXTCommBluez_search
-  (JNIEnv *env, jobject obj)
-{
-	jstring str = NULL;	
-	inquiry_info *ii = NULL;
-    int max_rsp, num_rsp;
-    int dev_id, sock, len, flags;
-    int i;
-    char addr[19] = { 0 };
-    char name[248] = { 0 };
-    unsigned char cod[3] = {0,0,0};
-
-    dev_id = hci_get_route(NULL);
-    sock = hci_open_dev( dev_id );
-    if (dev_id < 0 || sock < 0) {
-        perror("opening socket");
-        exit(1);
-    }
-
-    len  = 8;
-    max_rsp = 255;
-    flags = IREQ_CACHE_FLUSH;
-    ii = (inquiry_info*)malloc(max_rsp * sizeof(inquiry_info));
-    
-    num_rsp = hci_inquiry(dev_id, len, max_rsp, NULL, &ii, flags);
-    if( num_rsp < 0 ) perror("hci_inquiry");
-
-    for (i = 0; i < num_rsp; i++) {
-        ba2str(&(ii+i)->bdaddr, addr);
-        memset(name, 0, sizeof(name));
-        if (hci_read_remote_name(sock, &(ii+i)->bdaddr, sizeof(name), 
-            name, 0) < 0) strcpy(name, "[unknown]");
-        printf("%s  %s %x %x\n", addr, name, (ii+i)->dev_class[1], (ii+i)->dev_class[0]);
-        if ((ii+i)->dev_class[1] == 8 && (ii+i)->dev_class[0] == 4)
-        	str = (*env)->NewStringUTF (env, addr);
-    }
-
-    free( ii );
-    close( sock );
-    
-	return str;	
-}
-
-//---------------------------------------------------------------------------//
 
 void throwNewBlueZException(JNIEnv *env, char *msg)
 {
@@ -72,6 +24,94 @@ void throwNewBlueZException(JNIEnv *env, char *msg)
 		exit(1);
 	}
 	return;
+}
+
+//---------------------------------------------------------------------------//
+
+void throwIOException(JNIEnv *env, char *msg)
+{
+	jclass exception_cls;
+
+	exception_cls = (*env)->FindClass(env, "java/io/IOException");
+	if ((*env)->ThrowNew(env, exception_cls, msg) < 0)
+	{
+		fprintf(stderr, "** Error throwing IOException - exiting **\n");
+		fprintf(stderr, "Message:\n%s\n", msg);
+		exit(1);
+	}
+	return;
+}
+
+//---------------------------------------------------------------------------//
+
+JNIEXPORT jobjectArray JNICALL Java_lejos_pc_comm_NXTCommBluez_search
+  (JNIEnv *env, jobject obj)
+{
+	jstring str;
+	inquiry_info *ii = NULL;
+    int max_rsp, num_rsp;
+    int dev_id, sock, len, flags;
+    int i, j;
+    char addr[19] = { 0 };
+    char name[60] = { 0 };
+    unsigned char cod[3] = {0,0,0};   
+  	char msg[80];
+  	char return_str[80] = {0};
+  	int num_nxts = 0;
+  	
+  	jclass sclass = (*env)->FindClass(env,"java/lang/String");
+  	
+
+    dev_id = hci_get_route(NULL);
+    sock = hci_open_dev( dev_id );
+    if (dev_id < 0 || sock < 0) {
+        sprintf(msg, "Can't create socket: %s (%d)", strerror(errno), errno);
+		throwNewBlueZException(env, msg);
+		return NULL;
+    }
+
+    len  = 8;
+    max_rsp = 255;
+    flags = IREQ_CACHE_FLUSH;
+    ii = (inquiry_info*)malloc(max_rsp * sizeof(inquiry_info));
+    
+    num_rsp = hci_inquiry(dev_id, len, max_rsp, NULL, &ii, flags);
+    if( num_rsp < 0 ) {
+    	sprintf(msg, "Inquiry failed: %s (%d)", strerror(errno), errno);
+		throwNewBlueZException(env, msg);
+		return NULL;
+    }
+    
+    for (i = 0; i < num_rsp; i++) {
+        if ((ii+i)->dev_class[1] == 8 && (ii+i)->dev_class[0] == 4)
+        	num_nxts++;
+    }
+    
+    if (num_nxts == 0) return NULL;
+    
+    jobjectArray arr = (*env)->NewObjectArray(env, num_nxts, sclass, NULL);
+
+    j=0;
+    
+    for (i = 0; i < num_rsp; i++) {
+        ba2str(&(ii+i)->bdaddr, addr);
+        memset(name, 0, sizeof(name));
+        if (hci_read_remote_name(sock, &(ii+i)->bdaddr, sizeof(name), 
+            name, 0) < 0) strcpy(name, "[unknown]");
+        //printf("%s  %s %x %x\n", addr, name, (ii+i)->dev_class[1], (ii+i)->dev_class[0]);
+        strcpy(return_str,name);
+        strcat(return_str,"::");
+        strcat(return_str,addr);
+        if ((ii+i)->dev_class[1] == 8 && (ii+i)->dev_class[0] == 4) {
+        	str = (*env)->NewStringUTF (env, return_str);
+        	(*env)->SetObjectArrayElement(env, arr, j++, str); 
+        }
+    }
+
+    free( ii );
+    close( sock );
+    
+	return arr;	
 }
 
 //---------------------------------------------------------------------------//
@@ -145,8 +185,6 @@ JNIEXPORT void JNICALL Java_lejos_pc_comm_NXTCommBluez_rcSocketConnect
 
 	status = connect(sk, (struct sockaddr *)&addr, sizeof(addr));
 	if (status < 0) {
-		printf("Can't connect: %s (%d)", strerror(errno), errno);
-		exit(1);
 		sprintf(msg, "Can't connect: %s (%d)", strerror(errno), errno);
 		throwNewBlueZException(env, msg);
 	}
@@ -171,7 +209,7 @@ JNIEXPORT void JNICALL Java_lejos_pc_comm_NXTCommBluez_rcSocketSend
 
 	if (bytes_sent < 0) {
 		sprintf(msg, "Send failed: %s (%d)", strerror(errno), errno);
-		throwNewBlueZException(env, msg);
+		throwIOException(env, msg);
 	}
 	return;
 }
@@ -187,9 +225,11 @@ JNIEXPORT jbyteArray JNICALL Java_lejos_pc_comm_NXTCommBluez_rcSocketRecv
   	char msg[80];
 
 	bytes_recved = recv(sk, response, sizeof(response), 0);
+	
 	if (bytes_recved < 0) {
 		sprintf(msg, "Read failed: %s (%d)", strerror(errno), errno);
-		throwNewBlueZException(env, msg);
+		throwIOException(env, msg);
+		return NULL;
 	}
 
 	jsize len = bytes_recved;
@@ -210,7 +250,7 @@ JNIEXPORT void JNICALL Java_lejos_pc_comm_NXTCommBluez_rcSocketShutdown
 	status = shutdown(sk, SHUT_RDWR);
 	if (status < 0) {
 		sprintf(msg, "Shutdown failed: %s (%d)", strerror(errno), errno);
-		throwNewBlueZException(env, msg);
+		throwIOException(env, msg);
 	}
 	return;
 }
@@ -226,7 +266,7 @@ JNIEXPORT void JNICALL Java_lejos_pc_comm_NXTCommBluez_rcSocketClose
 	status = close(sk);
 	if (status < 0) {
 		sprintf(msg, "Close failed: %s (%d)", strerror(errno), errno);
-		throwNewBlueZException(env, msg);
+		throwIOException(env, msg);
 	}
 	return;
 }
