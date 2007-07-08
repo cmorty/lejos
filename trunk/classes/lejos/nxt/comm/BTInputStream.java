82fb379d9b20f3cbf99f3f0495c65128011b3ed0
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2007-07-08 21:28:54 +0000

    Changed BTInputStream not to return end of file at end of buffer

--- trunk/classes/lejos/nxt/comm/BTInputStream.java
+++ trunk/classes/lejos/nxt/comm/BTInputStream.java
@@ -1,48 +1,46 @@
 package lejos.nxt.comm;
 
-
 import java.io.*;
-import lejos.nxt.*;
 
 /**
- * extends InputStream for BlueTooth; implements available()
+ * Extends InputStream for BlueTooth; implements available()
  * @author   Roger Glassey revised on june 23, 2007
  */
 public class BTInputStream extends InputStream {
 	private byte buf[] = new byte[256];
 	private int bufIdx = 0, bufSize = 0;
-	private BTConnection connection = null;
     
-/**
- * returns one byte as an integer between 0 and 255.  Returns -1 if the end of the stream is reached.
- * Does not return till some bytes are available.
- */
+    /**
+     * Returns one byte as an integer between 0 and 255.  
+     * Returns -1 if the end of the stream is reached.
+     * Does not return till some bytes are available.
+     */
 	public int read() 
     {
+	   if (bufIdx >= bufSize) bufSize = 0;
        while(bufSize == 0) bufSize = available();
-       if (bufIdx < bufSize) return buf[bufIdx++] & 0xFF;
-       else 
-          {
-             bufIdx = 0;
-             bufSize = 0;
-             return -1;
-          }
+       return buf[bufIdx++] & 0xFF;
 	}
-   /**
-    * returns the number of bytes in the input buffer - can be read without blocking
-    */
+	
+    /**
+     * returns the number of bytes in the input buffer - can be read without blocking
+     */
     public int available()
     {
-       bufSize = Bluetooth.readPacket(buf, 256);
-       return bufSize;
+       if (bufIdx >= bufSize) bufSize = 0;
+       if (bufSize == 0) {
+    	   bufIdx = 0;
+    	   bufSize = Bluetooth.readPacket(buf, 256);
+       }
+       return bufSize - bufIdx;
     }
- /**
-  * the stream is restored to its original state - ready to receive more data.
-  */
+    
+    /**
+     * the stream is restored to its original state - ready to receive more data.
+     */
     public void close()
     { 
        bufIdx = 0;
        bufSize = 0;
     }
-
 }
