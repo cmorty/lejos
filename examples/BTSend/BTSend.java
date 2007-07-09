2b6e699e2c6ce086fbd3d0062ef4fbada2270a6b
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2007-07-09 21:10:12 +0000

    Test open by Bluetooth address
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@871 06c8f799-c779-4783-8af7-179afedb67c8

--- examples/BTSend/BTSend.java
+++ examples/BTSend/BTSend.java
@@ -5,16 +5,27 @@ import java.io.*;
 public class BTSend {
 	
 	public static void main(String[] args) {
-		NXTComm nxtComm = new NXTCommBluez();
+		NXTComm nxtComm = NXTCommFactory.createNXTComm(NXTCommFactory.BLUETOOTH);
 		
-		NXTInfo[] nxtInfo = nxtComm.search(null,NXTCommand.BLUETOOTH);
+		/*NXTInfo[] nxtInfo = nxtComm.search(args[0], NXTCommFactory.BLUETOOTH);
 		
 		if (nxtInfo.length == 0) {
 			System.out.println("No NXT Found");
 			System.exit(1);
-		}
+		}*/
+		
+		NXTInfo[] nxtInfo = new NXTInfo[1];
+		
+		nxtInfo[0] = new NXTInfo("NOISY","00:16:53:00:78:48");
+		
+		System.out.println("Connecting to " + nxtInfo[0].btResourceString);
 
-		nxtComm.open(nxtInfo[0]);
+		boolean opened = nxtComm.open(nxtInfo[0]); 
+		
+		if (!opened) {
+			System.out.println("Failed to open " + nxtInfo[0].name);
+			System.exit(1);
+		}
 		
 		InputStream is = nxtComm.getInputStream();
 		OutputStream os = nxtComm.getOutputStream();
