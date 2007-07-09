579abd052ce2d23d76b7dc0dbe8b85f28a38d387
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2007-07-09 23:11:08 +0000

    Simplified BTReceive examples that closes the connection
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@872 06c8f799-c779-4783-8af7-179afedb67c8

--- examples/BTReceive/BTReceive.java
+++ examples/BTReceive/BTReceive.java
@@ -6,32 +6,34 @@ public class BTReceive {
 
 	public static void main(String [] args)  throws Exception 
 	{
-		boolean cmdMode = true;
-		BTConnection btc = null;
+		BTConnection btc;
 		String connected = "Connected";
         String waiting = "Waiting";
-		InputStream is = null;
-		OutputStream os = null;
-		LCD.drawString(waiting,0,0);
-        LCD.refresh();
-        
+		InputStream is;
+		OutputStream os;
+
 		while (true)
 		{
-			if (cmdMode) {
-				btc = Bluetooth.waitForConnection();
-				LCD.clear();
-				LCD.drawString(connected,0,0);
-				LCD.refresh();			
-				cmdMode = false;
-				is = btc.openInputStream();
-				os = btc.openOutputStream();
-			}
+			LCD.drawString(waiting,0,0);
+	        LCD.refresh();
 
-			int b = is.read();
-			LCD.drawInt(b,3,0,1);
-			LCD.refresh();
-			os.write((byte) 100-b);
-		    os.flush();
+			btc = Bluetooth.waitForConnection();
+			LCD.clear();
+			LCD.drawString(connected,0,0);
+			LCD.refresh();			
+			is = btc.openInputStream();
+			os = btc.openOutputStream();
+
+			for(int i=0;i<100;i++) {
+				int b = is.read();
+				LCD.drawInt(b,3,0,1);
+				LCD.refresh();
+				os.write((byte) 100-b);
+				os.flush();
+			}
+			
+			btc.close();
+			LCD.clear();
 		}
 	}
 }
