60a70c2906ed886951c13a199b7d64962c2404eb
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2007-07-14 18:32:05 +0000

    Tidied up example

--- trunk/examples/BTSend/BTSend.java
+++ trunk/examples/BTSend/BTSend.java
@@ -1,4 +1,3 @@
-
 import lejos.pc.comm.*;
 import java.io.*;
 
@@ -7,16 +6,27 @@ public class BTSend {
 	public static void main(String[] args) {
 		NXTComm nxtComm = NXTCommFactory.createNXTComm(NXTCommFactory.BLUETOOTH);
 		
-		/*NXTInfo[] nxtInfo = nxtComm.search(args[0], NXTCommFactory.BLUETOOTH);
+		/* Another way to connect, by discovery:
+
+		NXTInfo[] nxtInfo = nxtComm.search(args[0], NXTCommFactory.BLUETOOTH);
 		
 		if (nxtInfo.length == 0) {
 			System.out.println("No NXT Found");
 			System.exit(1);
-		}*/
+		}
+		*/
+		
+		// arg[0] = name, e.g NXT
+		// arg[1] = address, with optional colons, e.g. 00:16:53:00:78:48
+	
+		if (args.length != 2) {
+			System.out.println("Usage: BTSend name address");
+			System.exit(1);
+		}
 		
 		NXTInfo[] nxtInfo = new NXTInfo[1];
-		
-		nxtInfo[0] = new NXTInfo("NOISY","00:16:53:00:78:48");
+			
+		nxtInfo[0] = new NXTInfo(args[0],args[1]);
 		
 		System.out.println("Connecting to " + nxtInfo[0].btResourceString);
 
@@ -27,28 +37,35 @@ public class BTSend {
 			System.exit(1);
 		}
 		
+		System.out.println("Connected to " + nxtInfo[0].btResourceString);
+		
 		InputStream is = nxtComm.getInputStream();
 		OutputStream os = nxtComm.getOutputStream();
 		
+		DataOutputStream dos = new DataOutputStream(os);
+		DataInputStream dis = new DataInputStream(is);
+				
 		for(int i=0;i<100;i++) {
 			try {
-				os.write(i);
-				os.flush();
-				
+				dos.writeInt((i*30000));
+				dos.flush();			
 			} catch (IOException ioe) {
 				System.out.println("IO Exception writing bytes");
 			}
 			
 			try {
-				System.out.println("Received " + is.read());
+				System.out.println("Received " + dis.readInt());
 			} catch (IOException ioe) {
 				System.out.println("IO Exception reading bytes");
 			}
 		}
 		
 		try {
+			dis.close();
+			//dos.close(); Why does this prevent re-connection?
 			nxtComm.close();
-		} catch (IOException ioe) {}
+		} catch (IOException ioe) {
+			System.out.println("IOException closing connection");
+		}
 	}
-
 }
