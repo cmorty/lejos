99561f8e0e9dd1651cd3cd56922b14fc0ba9578f
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2007-07-14 18:31:36 +0000

    Tidied up example
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@882 06c8f799-c779-4783-8af7-179afedb67c8

--- examples/BTReceive/BTReceive.java
+++ examples/BTReceive/BTReceive.java
@@ -6,32 +6,35 @@ public class BTReceive {
 
 	public static void main(String [] args)  throws Exception 
 	{
-		BTConnection btc;
 		String connected = "Connected";
         String waiting = "Waiting";
-		InputStream is;
-		OutputStream os;
 
 		while (true)
 		{
 			LCD.drawString(waiting,0,0);
-	        LCD.refresh();
+			LCD.refresh();
 
-			btc = Bluetooth.waitForConnection();
+	        BTConnection btc = Bluetooth.waitForConnection();
+	        
 			LCD.clear();
 			LCD.drawString(connected,0,0);
-			LCD.refresh();			
-			is = btc.openInputStream();
-			os = btc.openOutputStream();
-
+			LCD.refresh();	
+			
+			InputStream is = btc.openInputStream();
+			OutputStream os = btc.openOutputStream();
+			DataInputStream dis = new DataInputStream(is);
+			DataOutputStream dos = new DataOutputStream(os);
+			
 			for(int i=0;i<100;i++) {
-				int b = is.read();
-				LCD.drawInt(b,3,0,1);
+				int ii = dis.readInt();
+				LCD.drawInt(ii,3,0,1);
 				LCD.refresh();
-				os.write((byte) 100-b);
-				os.flush();
+				dos.writeInt(-ii);
+				dos.flush();
 			}
 			
+			dis.close();
+			dos.close();
 			btc.close();
 			LCD.clear();
 		}
