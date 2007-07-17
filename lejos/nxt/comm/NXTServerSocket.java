04fd49b58f77825d03a93032434d6eeacf6cd116
ranulfgreen <ranulfgreen@06c8f799-c779-4783-8af7-179afedb67c8>
2007-07-17 10:30:39 +0000

    Added NXTServerSocket enabling server side socket connection and modified NXTSocket to inform proxy of which kind of socket to create
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk/classes@904 06c8f799-c779-4783-8af7-179afedb67c8

---
+++ lejos/nxt/comm/NXTServerSocket.java
@@ -0,0 +1,88 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+import lejos.nxt.comm.BTConnection;
+import lejos.nxt.comm.NXTSocket;
+import lejos.nxt.comm.USBConnection;
+
+public class NXTServerSocket {
+
+	private int port;
+	private BTConnection btc;
+	private USBConnection usbc;
+	private boolean isBluetooth;
+	private final boolean isServer = true;
+	
+	
+	
+	/**
+	 * Constructor. Creates a new Server Socket over an open bluetooth connection
+	 * @param port The port to listen on
+	 * @param btc The bluetooth connection to open
+	 * @throws IOException 
+	 */
+	public NXTServerSocket(int port, BTConnection btc) throws IOException{
+		this.port = port;
+		this.btc = btc;
+		isBluetooth = true;
+		negotiateConnection();
+	}
+	
+	/**
+	 * Constructor. Creates a new Server Socket over an open usb connection
+	 * @param port The port to listen on
+	 * @param usbc The usb connection to open
+	 * @throws IOException 
+	 */
+	public NXTServerSocket(int port, USBConnection usbc) throws IOException{
+		this.port = port;
+		this.usbc = usbc;
+		isBluetooth = false;
+		negotiateConnection();
+	}
+	
+	private void negotiateConnection() throws IOException{
+		DataOutputStream dos = openDataOutputStream();
+		dos.writeBoolean(isServer);
+		dos.writeInt(port);
+		dos.flush();
+		dos.close();
+		
+	}
+	
+	private DataOutputStream openDataOutputStream() throws IOException{
+		DataOutputStream dos;
+		if(isBluetooth){dos = new DataOutputStream(btc.openOutputStream());}
+		else{dos = new DataOutputStream(usbc.openOutputStream());}
+		return dos;
+	}
+	
+	private DataInputStream openDataInputStream() throws IOException{
+		DataInputStream dis;
+		if(isBluetooth){dis = new DataInputStream(btc.openInputStream());}
+		else{dis = new DataInputStream(usbc.openInputStream());}
+		return dis;
+	}
+	
+	/**
+	 * Waits untill there is a socket connection available. When this becomes true
+	 * a new NXTSocket is returned
+	 * @return NXTSocket
+	 * @throws IOException 
+	 */
+	public NXTSocket accept() throws IOException{
+		DataOutputStream dos = openDataOutputStream();
+		DataInputStream dis = openDataInputStream();
+		// inform the proxy of the command
+		dos.writeByte(1);
+		dos.flush();
+		dis.readBoolean();
+		dos.close();
+		dis.close();
+		if(isBluetooth){return new NXTSocket(btc);}
+		else{return new NXTSocket(usbc);}
+		
+	}
+
+}
