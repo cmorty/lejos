65410288eaafc060425ac2c0a5742f1b4e11b98f
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2007-07-21 20:21:49 +0000

    Use TextMenu to list devices and allow remove device

--- trunk/examples/BTDumpTest/BTDumpTest.java
+++ trunk/examples/BTDumpTest/BTDumpTest.java
@@ -1,4 +1,3 @@
-
 import lejos.nxt.*;
 import lejos.nxt.comm.*;
 import java.util.*;
@@ -8,19 +7,34 @@ public class BTDumpTest {
 	public static void main(String[] args) throws Exception {	
 		Vector devList = Bluetooth.getKnownDevicesList();
 		if (devList.size() > 0) {
+			String[] names = new String[devList.size()];
 			for (int i = 0; i < devList.size(); i++) {
 				BTRemoteDevice btrd = ((BTRemoteDevice) devList.elementAt(i));
-				LCD.drawString(btrd.getFriendlyName(),0,i*3);
-				LCD.drawString(btrd.getAddressString(),0,i*3+1);
-				for(int j = 0;j<4; j++) LCD.drawInt(btrd.getDeviceClass()[j],3*j,i*3+2);
-				LCD.refresh();
+				names[i] = btrd.getFriendlyName();
 			}
+				
+			TextMenu menu = new TextMenu(names);
+			String[] subItems = {"Remove"};
+			TextMenu subMenu = new TextMenu(subItems,4);
+
+			int selected;
+			do {
+				LCD.clear();
+				selected = menu.select();
+				if (selected >=0) {
+					BTRemoteDevice btrd = ((BTRemoteDevice) devList.elementAt(selected));
+					LCD.clear();
+					LCD.drawString(names[selected],0,0);
+					LCD.drawString(btrd.getAddressString(), 0, 1);
+					for(int i=0;i<4;i++) LCD.drawInt(btrd.getDeviceClass()[i], 3, i*4, 2);
+					int subSelection = subMenu.select();
+					if (subSelection == 0) Bluetooth.removeDevice(btrd);
+				}
+			} while (selected >= 0);
 
 		} else {
 			LCD.drawString("no known devices", 0, 0);
 			LCD.refresh();
 		}
-				
-		Button.ENTER.waitForPressAndRelease();
 	}
 }
