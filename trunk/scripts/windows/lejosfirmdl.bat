35572797587ba11ee31a08669285cf6032b61534
bbagnall <bbagnall@06c8f799-c779-4783-8af7-179afedb67c8>
2007-08-07 03:27:49 +0000

    Windows scripts now search for nxj substring before using lejos_home variable.

--- trunk/scripts/windows/lejosfirmdl.bat
+++ trunk/scripts/windows/lejosfirmdl.bat
@@ -1,4 +1,17 @@
 @REM Container to allow backward compatibility for old commands
 @REM Feel free to delete lejosdl.bat, lejosjc.bat and lejosfirmdl.bat
-@if NOT "%LEJOS_HOME%" == "" SET NXJ_HOME=%LEJOS_HOME%
+
+@echo off
+@if "%LEJOS_HOME%" == "" GoTo :UNDEFINED
+@REM The following only works with Windows 2000 on up:
+@if NOT %lejos_home:~-3% == nxj GoTo :DONE
+
+:CHANGE
+SET NXJ_HOME=%LEJOS_HOME%
 @nxjflash.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
+GoTo :Done
+
+:UNDEFINED
+echo LEJOS_HOME not defined.
+
+:DONE
