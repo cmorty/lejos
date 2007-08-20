3c72d53835d7f0dea79a2ee6bcc27d18ceecc76c
bbagnall <bbagnall@06c8f799-c779-4783-8af7-179afedb67c8>
2007-08-20 14:56:30 +0000

    lejosfirmdl checks if libusb is installed. If not, installs it.
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@934 06c8f799-c779-4783-8af7-179afedb67c8

--- scripts/windows/lejosfirmdl.bat
+++ scripts/windows/lejosfirmdl.bat
@@ -2,12 +2,20 @@
 @REM Feel free to delete lejosdl.bat, lejosjc.bat and lejosfirmdl.bat
 
 @echo off
+
 @if "%LEJOS_HOME%" == "" GoTo :UNDEFINED
 @REM The following only works with Windows 2000 on up:
 @if NOT %lejos_home:~-3% == nxj GoTo :DONE
 
 :CHANGE
 SET NXJ_HOME=%LEJOS_HOME%
+
+@REM Check if LIBUSB is installed:
+@if exist "%SystemRoot%\system32\libusb0.dll" GoTo :MAIN
+echo LIBUSB not installed. Running setup program...
+START "LIBUSB INSTALLER" /wait %NXJ_HOME%\3rdparty\lib\libusb-win32-filter-bin-0.1.12.1.exe
+
+:MAIN
 @nxjflash.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
 GoTo :Done
 
