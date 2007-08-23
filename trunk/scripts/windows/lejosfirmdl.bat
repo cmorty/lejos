9fd9c4745588171fbf556c285a4b46ef59404e5c
bbagnall <bbagnall@06c8f799-c779-4783-8af7-179afedb67c8>
2007-08-23 15:07:01 +0000

    Now checks for Vista and won't install libusb due to reported problems with Vista.

--- trunk/scripts/windows/lejosfirmdl.bat
+++ trunk/scripts/windows/lejosfirmdl.bat
@@ -10,10 +10,18 @@
 :CHANGE
 SET NXJ_HOME=%LEJOS_HOME%
 
+@REM Check operating system and skip Vista:
+Ver | Find "Version 6" >NUL
+  If ErrorLevel 1 Goto :INSTALL
+  Echo Vista Detected. There have been reports of LibUSB disabling USB devices. Install at your own risk. http://libusb-win32.sourceforge.net/#downloads
+  Goto :DONE
+
+:INSTALL
 @REM Check if LIBUSB is installed:
 @if exist "%SystemRoot%\system32\libusb0.dll" GoTo :MAIN
 echo LIBUSB not installed. Running setup program...
 START "LIBUSB INSTALLER" /wait %NXJ_HOME%\3rdparty\lib\libusb-win32-filter-bin-0.1.12.1.exe
+echo NOTE: If you encounter the error NXT Not Found, reboot your computer, make sure the brick is still in firmware upload mode, and try again.
 
 :MAIN
 @nxjflash.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
