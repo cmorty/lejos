364d65d97a152e466ef8b75c248ef4c136fc3676
bbagnall <bbagnall@06c8f799-c779-4783-8af7-179afedb67c8>
2007-08-26 17:22:17 +0000

    Removed extraneous " symbols from scripts.

--- trunk/scripts/windows/nxjlink.bat
+++ trunk/scripts/windows/nxjlink.bat
@@ -1,10 +1,10 @@
 @echo off
 if "%NXJ_HOME%" == ""  goto homeless
 
-set THIRDPARTY_LIBS="%NXJ_HOME%\3rdparty\lib"
-set LINK_CLASSPATH=".;%THIRDPARTY_LIBS%\bcel-5.1.jar;%THIRDPARTY_LIBS%\commons-cli-1.0.jar;%NXJ_HOME%\lib\jtools.jar;%NXJ_HOME%\lib\classes.jar"
+set THIRDPARTY_LIBS=%NXJ_HOME%\3rdparty\lib
+set LINK_CLASSPATH=.;%THIRDPARTY_LIBS%\bcel-5.1.jar;%THIRDPARTY_LIBS%\commons-cli-1.0.jar;%NXJ_HOME%\lib\jtools.jar;%NXJ_HOME%\lib\classes.jar
 
-java -classpath "%LINK_CLASSPATH%" js.tinyvm.TinyVM --writeorder LE --classpath "%LINK_CLASSPATH%" %1 %2 %3 %4 %5 %6 %7 %8 %9 
+java -classpath %LINK_CLASSPATH% js.tinyvm.TinyVM --writeorder LE --classpath %LINK_CLASSPATH% %1 %2 %3 %4 %5 %6 %7 %8 %9 
 goto end
 
 :homeless
