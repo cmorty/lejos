ae20cb9cf7eeeee9b4767331f49b92ea647b1413
bbagnall <bbagnall@06c8f799-c779-4783-8af7-179afedb67c8>
2007-08-27 22:41:54 +0000

    Added quotes around %nxj_home% for if statement.

--- trunk/scripts/windows/nxjc.bat
+++ trunk/scripts/windows/nxjc.bat
@@ -1,5 +1,5 @@
 @echo off
-if %NXJ_HOME% == ""  goto homeless
+if "%NXJ_HOME%" == ""  goto homeless
 
 javac -source 1.3 -target 1.1 -bootclasspath %NXJ_HOME%\lib\classes.jar;%CLASSPATH% %1 %2 %3 %4 %5 %6 %7 %8 %9
 goto end
