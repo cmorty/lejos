4d0abb1f56ec3e1dcc66ea961be2542c591e8d2b
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2007-09-02 19:03:09 +0000

    Latest versions including Brian's modified Windows scripts
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@973 06c8f799-c779-4783-8af7-179afedb67c8

--- snapshot/bin/nxjc.bat
+++ snapshot/bin/nxjc.bat
@@ -1,7 +1,7 @@
 @echo off
 if "%NXJ_HOME%" == ""  goto homeless
 
-javac -source 1.3 -target 1.1 -bootclasspath "%NXJ_HOME%\lib\classes.jar;%CLASSPATH%" %1 %2 %3 %4 %5 %6 %7 %8 %9
+javac -source 1.3 -target 1.1 -bootclasspath %NXJ_HOME%\lib\classes.jar;%CLASSPATH% %*
 goto end
 
 :homeless
