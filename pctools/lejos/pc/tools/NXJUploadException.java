ec6b8504f7b32878befbcb2a036ab0f46e816c7e
mpscholz <mpscholz@06c8f799-c779-4783-8af7-179afedb67c8>
2007-09-27 10:27:14 +0000

    upload exception
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@988 06c8f799-c779-4783-8af7-179afedb67c8

---
+++ pctools/lejos/pc/tools/NXJUploadException.java
@@ -0,0 +1,24 @@
+package lejos.pc.tools;
+
+public class NXJUploadException extends Exception {
+
+	public NXJUploadException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public NXJUploadException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NXJUploadException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NXJUploadException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+}
