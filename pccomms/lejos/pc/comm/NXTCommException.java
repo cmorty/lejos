2eaf64871a18d6370eadb13e762c64b6a50ab4db
mpscholz <mpscholz@06c8f799-c779-4783-8af7-179afedb67c8>
2007-09-30 18:41:56 +0000

    removed todos
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@995 06c8f799-c779-4783-8af7-179afedb67c8

--- pccomms/lejos/pc/comm/NXTCommException.java
+++ pccomms/lejos/pc/comm/NXTCommException.java
@@ -6,22 +6,18 @@ public class NXTCommException extends Exception {
 
 	public NXTCommException() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
 	public NXTCommException(String arg0, Throwable arg1) {
 		super(arg0, arg1);
-		// TODO Auto-generated constructor stub
 	}
 
 	public NXTCommException(String arg0) {
 		super(arg0);
-		// TODO Auto-generated constructor stub
 	}
 
 	public NXTCommException(Throwable arg0) {
 		super(arg0);
-		// TODO Auto-generated constructor stub
 	}
 
 	public String getMessage() {
