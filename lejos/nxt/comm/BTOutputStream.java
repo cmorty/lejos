7caf6699134568e56781963efc3802b482c51578
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2007-10-13 20:25:29 +0000

    Multi-byte packets for Java streams
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk/classes@1024 06c8f799-c779-4783-8af7-179afedb67c8

--- lejos/nxt/comm/BTOutputStream.java
+++ lejos/nxt/comm/BTOutputStream.java
@@ -3,12 +3,24 @@ package lejos.nxt.comm;
 import java.io.*;
 
 public class BTOutputStream extends OutputStream {
-	byte[] bb = new byte[3];
+	private final int BUFFER_SIZE = 32;	
+	private byte[] buffer = new byte[BUFFER_SIZE+2];
+	private int numBytes = 0;
 	
-	public void write(int b) {
-		bb[0] = 1;
-		bb[1] = 0;
-		bb[2] = (byte) b;
-		Bluetooth.btSend(bb,3);
+    public void write(int b) {
+    	if (numBytes == BUFFER_SIZE) {
+    		flush();
+    	}
+    	buffer[numBytes+2] = (byte) b;
+    	numBytes++;  	
+    }
+    
+	public void flush() {
+		if (numBytes > 0) {
+			buffer[0] = (byte) numBytes;
+			buffer[1] = 0;
+			Bluetooth.btSend(buffer,numBytes+2);
+			numBytes = 0;
+		}
 	}
 }
