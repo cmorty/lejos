e81f49b6c3f72d5c4ce4652c64b159f121d7f0f5
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2007-10-26 20:10:22 +0000

    Revert USB Output streams to one-byte packets
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@1044 06c8f799-c779-4783-8af7-179afedb67c8

--- classes/lejos/nxt/comm/USBOutputStream.java
+++ classes/lejos/nxt/comm/USBOutputStream.java
@@ -3,21 +3,11 @@ package lejos.nxt.comm;
 import java.io.*;
 
 public class USBOutputStream extends OutputStream {
-	private final int BUFFER_SIZE = 32;	
-	private byte[] buffer = new byte[BUFFER_SIZE];
-	private int numBytes;
-	
-    public void write(int b) {
-    	if (numBytes == BUFFER_SIZE) {
-    		flush();
-    	}
-    	buffer[numBytes++] = (byte) b;  	
-    }
-    
-	public void flush() {
-		if (numBytes != 0) {
-		  USB.usbWrite(buffer,numBytes);
-		  numBytes = 0;
-		}
+
+	public void write(int b) {
+		byte[] bb = new byte[1];
+		bb[0] = (byte) b;
+		USB.usbWrite(bb, 1);
 	}
 }
+
