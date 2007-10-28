d922b94b4df67457cedf03736dc80a562a4ffcd9
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2007-10-28 19:42:43 +0000

    Make RCXMotorMultiplexer the same as Brian's book (apart from setSpeed)
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk/classes@1048 06c8f799-c779-4783-8af7-179afedb67c8

--- lejos/nxt/RCXLink.java
+++ lejos/nxt/RCXLink.java
@@ -4,6 +4,10 @@ public class RCXLink extends I2CSensor {
 	
 	private byte[] buf = new byte[2];
 	
+	public RCXMotor A = new RCXMotor(new RCXRemoteMotorPort(this,0));
+	public RCXMotor B = new RCXMotor(new RCXRemoteMotorPort(this,1));
+	public RCXMotor C = new RCXMotor(new RCXRemoteMotorPort(this,2));
+	
 	public RCXLink(I2CPort port) {
 		super(port);
 	}
@@ -31,14 +35,50 @@ public class RCXLink extends I2CSensor {
 		runMacro(0x25 + (id*8));
 	}
 	
+	public void setRCXShortRange() {
+		runMacro(0x01);
+	}
+	
+	public void setRCXLongRange() {
+		runMacro(0x04);
+	}
+	
+	public void powerOff() {
+		runMacro(0x07);
+	}
+	
+	public void stop() {
+		runMacro(0x1D);
+	}
+	
 	public void flush() {
-		buf[0] = 046;
+		buf[0] = 0x46;
 		sendData(0x41, buf, 1);
 	}
 	
 	public void setDefault() {
-		buf[0] = 044;
+		buf[0] = 0x44;
 		sendData(0x41, buf, 1);
 	}
 	
+	public void setHighSpeed() {
+		buf[0] = 0x48;
+		sendData(0x41, buf, 1);
+	}
+	
+	public void setLongRange() {
+		buf[0] = 0x4c;
+		sendData(0x41, buf, 1);
+	}
+
+	public void setShortRange() {
+		buf[0] = 0x53;
+		sendData(0x41, buf, 1);
+	}
+	
+	public void setAPDAOn() {
+ 		getData(0x42,data,numBytes);
+		}
+		return numBytes;
+	}
 }
