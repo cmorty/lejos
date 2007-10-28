119a0c460aa62ae2a966725e9e737816105a6d4b
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2007-10-28 19:42:43 +0000

    Make RCXMotorMultiplexer the same as Brian's book (apart from setSpeed)

--- trunk/classes/lejos/nxt/RCXMotorMultiplexer.java
+++ trunk/classes/lejos/nxt/RCXMotorMultiplexer.java
@@ -1,41 +1,36 @@
 package lejos.nxt;
 
-public class RCXMotorMultiplexer extends I2CSensor implements BasicMotorPort {
+public class RCXMotorMultiplexer extends I2CSensor {
 	private byte[] buf = new byte[1];
-	private int id;
 	
-	public RCXMotorMultiplexer(I2CPort port, int id) {
+	public RCXMotor A = new RCXMotor(new RCXPlexedMotorPort(this,0));
+	public RCXMotor B = new RCXMotor(new RCXPlexedMotorPort(this,1));
+	public RCXMotor C = new RCXMotor(new RCXPlexedMotorPort(this,2));
+	public RCXMotor D = new RCXMotor(new RCXPlexedMotorPort(this,3));
+	
+	
+	public RCXMotorMultiplexer(I2CPort port) {
 		super(port);
-		this.id = id;
 		setAddress(0x5A);
 	}
 	
-	public void setSpeed(int speed) {
+	public void setSpeed(int speed, int id) {
 		buf[0] = (byte) speed;
 		sendData(0x43 + (id*2), buf, 1);
 	}
 	
-	public int getSpeed() {
+	public int getSpeed(int id) {
 		getData(0x43 + (id*2), buf, 1);
 	    return buf[0] & 0xFF;
 	}
 	
-	public void setDirection(int direction) {
+	public void setDirection(int direction, int id) {
 		buf[0] = (byte) direction;
 		sendData(0x42 + (id*2), buf, 1);
 	}
 	
-	public int getDirection() {
+	public int getDirection(int id) {
 		getData(0x42 + (id*2), buf, 1);
 	    return buf[0] & 0xFF;
-	}
-	
-	public void controlMotor(int power, int mode) {
-		int mmMode = mode;
-		if (mmMode == 4) mmMode = 0; // float
-		int mmPower = (int) ((float)power * 2.55f);
-		if (mmMode == 3) mmPower = 255; // Maximum breaking
-		setDirection(mmMode);
-		setSpeed(mmPower);
 	}
 }
---
+++ trunk/classes/lejos/nxt/RCXPlexedMotorPort.java
@@ -0,0 +1,20 @@
+package lejos.nxt;
+
+public class RCXPlexedMotorPort implements BasicMotorPort {
+	private RCXMotorMultiplexer plex;
+	private int id;
+	
+	public RCXPlexedMotorPort(RCXMotorMultiplexer plex, int id) {
+		this.plex = plex;
+		this.id = id;
+	}
+	
+	public void controlMotor(int power, int mode) {
+		int mmMode = mode;
+		if (mmMode == 4) mmMode = 0; // float
+		int mmPower = (int) ((float)power * 2.55f);
+		if (mmMode == 3) mmPower = 255; // Maximum breaking
+		plex.setDirection(mmMode, id);
+		plex.setSpeed(mmPower, id);
+	}
+}
