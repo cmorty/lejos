aa244c009dd7297db8eeb7a946b05b7e62c121f3
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2007-10-28 19:43:57 +0000

    Updated example
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@1049 06c8f799-c779-4783-8af7-179afedb67c8

--- examples/RCXMMTest/RCXMMTest.java
+++ examples/RCXMMTest/RCXMMTest.java
@@ -3,17 +3,19 @@ import lejos.nxt.*;
 public class RCXMMTest {
 
 	public static void main(String[] args) throws Exception {
-		RCXMotorMultiplexer mm = new RCXMotorMultiplexer(SensorPort.S1, 0);
-		RCXMotor m = new RCXMotor(mm);
+		RCXMotorMultiplexer mm = new RCXMotorMultiplexer(SensorPort.S1);
 		
 		while (true) {
 			LCD.drawString(mm.getSensorType(), 0, 0);
-			LCD.drawInt(mm.getDirection(),0,1);
-			LCD.drawInt(mm.getSpeed(),0,2);
+			LCD.drawInt(mm.getDirection(0),3,0,1);
+			LCD.drawInt(mm.getSpeed(0),3,0,2);
 			LCD.refresh();
 			
-			m.setPower(100);
-			m.forward();
+			mm.A.setPower(100);
+			mm.A.forward();
+			Thread.sleep(1000);
+			mm.A.backward();
+			Thread.sleep(1000);
 		}
 	}
 }
