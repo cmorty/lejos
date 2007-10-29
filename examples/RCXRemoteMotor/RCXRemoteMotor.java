24f87b4dcc263bbac4aaf7813879b2f9acd49bf4
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2007-10-29 22:14:13 +0000

    Test of remote RCX motor control using RCXLink class
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@1052 06c8f799-c779-4783-8af7-179afedb67c8

---
+++ examples/RCXRemoteMotor/RCXRemoteMotor.java
@@ -0,0 +1,49 @@
+
+import lejos.nxt.*;
+
+public class RCXRemoteMotor {
+
+	public static void main(String[] args) throws Exception {
+		RCXLink link = new RCXLink(SensorPort.S1);
+		
+		int power = 0;
+		int mode = 1;
+		int motor = 0;
+		RCXMotor[] motors = {link.A, link.B, link.C};
+		String motorString = "Motor:";
+		String modeString = "Mode:";
+		String powerString = "Power:";
+		
+		while (true) {
+			
+			int key = Button.waitForPress();
+			
+			if (key == 1) { // ENTER
+				power += 20;
+				if (power > 100) power = 0;
+			} else if (key == 2) { // LEFT
+				mode++;
+				if (mode > 4) mode = 1;			
+			} else if (key == 4) { // RIGHT
+				motor++;
+				if (motor > 2) motor = 0;
+			} else if (key == 8) { // ESCAPE
+				System.exit(0);
+			}
+			
+			LCD.drawString(motorString,0,0);
+			LCD.drawInt(motor, 10, 0);
+			LCD.drawString(powerString,0,1);
+			LCD.drawInt(power, 3, 10, 1);
+			LCD.drawString(modeString,0,2);
+			LCD.drawInt(mode, 10, 2);
+			LCD.refresh();
+			motors[motor].setPower(power);
+			Thread.sleep(100);
+			if (mode == 1) motors[motor].forward();
+			else if (mode == 2) motors[motor].backward();
+			else if (mode == 3) motors[motor].flt();
+			else if (mode == 4) motors[motor].stop();
+		}
+	}
+}
