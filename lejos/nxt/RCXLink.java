51daf24ee58346b8457ca6625cbb1c1c77b6ef72
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2007-11-05 16:58:28 +0000

    Updated RCXLink - closer to iCommand NRLink class
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk/classes@1054 06c8f799-c779-4783-8af7-179afedb67c8

--- lejos/nxt/RCXLink.java
+++ lejos/nxt/RCXLink.java
@@ -14,83 +14,122 @@ public class RCXLink extends I2CSensor {
 	public RCXMotor A = new RCXMotor(new RCXRemoteMotorPort(this,0));
 	public RCXMotor B = new RCXMotor(new RCXRemoteMotorPort(this,1));
 	public RCXMotor C = new RCXMotor(new RCXRemoteMotorPort(this,2));
+
+	private final static byte SLOW_SPEED = 0x44; // Default 2400 baud
+	private final static byte FLUSH = 0x46; // Flush the FIFO buffer
+	private final static byte HIGH_SPEED = 0x48; // 4800 baudprivate final static byte LONG_RANGE = 0x4C;
+	private final static byte SHORT_RANGE = 0x53;
+	private final static byte LONG_RANGE = 0x4c;
+	private final static byte TRANSMIT_RAW_MACRO = 0x55; // Transmit Unassembled raw macro data;
+	private final static byte COMMAND = 0x41;
+	private final static byte RUN = 0x52;
+	private final static byte ARPA_ON = 0x4E;
+	private final static byte ARPA_OFF = 0x4F;
+	private final static byte STATUS_REG = 0x41;
+	private final static byte RX_DATA_LEN = 0x40;
+	private final static byte TX_DATA_LEN = 0x40;
+	private final static byte RX_DATA = 0x42;
+	private final static byte TX_DATA = 0x42;
+	
+	// ROM Macro Definitions:
+	public final static byte SHORT_RANGE_IR = 0x01;
+	public final static byte LONG_RANGE_IR = 0x04;
+	public final static byte POWER_OFF_RCX = 0x07;
+	public final static byte RUN_PROGRAM_1 = 0x09;
+	public final static byte RUN_PROGRAM_2 = 0x0D;
+	public final static byte RUN_PROGRAM_3 = 0x11;
+	public final static byte RUN_PROGRAM_4 = 0x15;
+	public final static byte RUN_PROGRAM_5 = 0x19;
+	public final static byte STOP_ALL_PROGRAMS = 0x1D;
+	public final static byte MOTOR_A_FORWARD = 0x21;
+	public final static byte MOTOR_A_REVERSED = 0x25;
+	public final static byte MOTOR_B_FORWARD = 0x29;
+	public final static byte MOTOR_B_REVERSED = 0x2D;
+	public final static byte MOTOR_C_FORWARD = 0x31;
+	public final static byte MOTOR_C_REVERSED = 0x35;
+	/**
+	 * NOTE: The BEEP macro is unreliable. 
+     * It works once, and then needs another command executed
+     * before it works again.
+	 */
+	public final static byte BEEP = 0x39;	
 	
 	public RCXLink(I2CPort port) {
 		super(port);
 	}
 
 	public void runMacro(int addr) {
-		buf[0] = 0x52;
+		buf[0] = RUN;
 		buf[1] = (byte) addr;
 		
-		sendData(0x41, buf, 2);
+		sendData(COMMAND, buf, 2);
 	}
 	
 	public void beep() {
-		runMacro(0x39);
+		runMacro(BEEP);
 	}
 	
-	public void runProgram(int prog) {
-		runMacro(0x05 + (prog*4));
+	public void runProgram(int programNumber) {
+		runMacro(RUN_PROGRAM_1 + ((programNumber-1)*4));
 	}
 	
-	public void forwardStep(int id) {
-		runMacro(0x21 + (id*8));
+	public void forwardStep(int id) { // RCX Remote Command
+		runMacro(MOTOR_A_FORWARD + (id*8));
 	}
 	
-	public void backwardStep(int id) {
-		runMacro(0x25 + (id*8));
+	public void backwardStep(int id) { // RCX Remote Command
+		runMacro(MOTOR_A_REVERSED + (id*8));
 	}
 	
 	public void setRCXRangeShort() {
-		runMacro(0x01);
+		runMacro(SHORT_RANGE_IR);
 	}
 	
 	public void setRCXRangeLong() {
-		runMacro(0x04);
+		runMacro(LONG_RANGE_IR);
 	}
 	
 	public void powerOff() {
-		runMacro(0x07);
+		runMacro(POWER_OFF_RCX);
 	}
 	
-	public void stop() {
-		runMacro(0x1D);
+	public void stopAllPrograms() {
+		runMacro(STOP_ALL_PROGRAMS);
 	}
 	
 	public void flush() {
-		buf[0] = 0x46;
-		sendData(0x41, buf, 1);
+		buf[0] = FLUSH;
+		sendData(COMMAND, buf, 1);
 	}
 	
-	public void setDefault() {
-		buf[0] = 0x44;
-		sendData(0x41, buf, 1);
+	public void setDefaultSpeed() {
+		buf[0] = SLOW_SPEED;
+		sendData(COMMAND, buf, 1);
 	}
 	
 	public void setHighSpeed() {
-		buf[0] = 0x48;
-		sendData(0x41, buf, 1);
+		buf[0] = HIGH_SPEED ;
+		sendData(COMMAND, buf, 1);
 	}
 	
 	public void setRangeLong() {
-		buf[0] = 0x4c;
-		sendData(0x41, buf, 1);
+		buf[0] = LONG_RANGE;
+		sendData(COMMAND, buf, 1);
 	}
 
 	public void setRangeShort() {
-		buf[0] = 0x53;
-		sendData(0x41, buf, 1);
+		buf[0] = SHORT_RANGE;
+		sendData(COMMAND, buf, 1);
 	}
 	
 	public void setAPDAOn() {
-		buf[0] = 0x4E;
-		sendData(0x41, buf, 1);
+		buf[0] = ARPA_ON;
+		sendData(COMMAND, buf, 1);
 	}
 	
 	public void setAPDAOff() {
-		buf[0] = 0x4F;
-		sendData(0x41, buf, 1);
+		buf[0] = ARPA_OFF;
+		sendData(COMMAND, buf, 1);
 	}
 	
 	public void defineMacro(int addr, byte[] macro) {
@@ -101,12 +140,12 @@ public class RCXLink extends I2CSensor {
 	}
 	
 	public int getStatus() {
-		getData(0x41, buf, 1);
+		getData(STATUS_REG, buf, 1);
 		return buf[0] & 0xFF;
 	}
 	
 	public int bytesAvailable() {
-		getData(0x40, buf, 1);
+		getData(RX_DATA_LEN, buf, 1);
 		return buf[0] & 0xFF;
 	}
 	
@@ -172,24 +211,24 @@ public class RCXLink extends I2CSensor {
 	}
 	
 	public void setRawMode() {
-		buf[0] = 0x55;
-		sendData(0x41, buf, 1);
+		buf[0] = TRANSMIT_RAW_MACRO;
+		sendData(COMMAND, buf, 1);
 	}
 	
 	public void sendBytes(byte[] data, int len) {
-		sendData(0x42,data,len);
+		sendData(TX_DATA,data,len);
 		sleep();
 		buf[0] = (byte) len;
-		sendData(0x40, buf, 1);
+		sendData(TX_DATA_LEN, buf, 1);
 	}
 	
 	public int readBytes(byte [] data) {
-		getData(0x40,buf,1);
+		getData(RX_DATA_LEN,buf,1);
 		int numBytes = buf[0];
 		if (numBytes > 0) {
 			if (numBytes > data.length) numBytes = data.length;
 			sleep();
-			getData(0x42,data,numBytes);
+			getData(RX_DATA,data,numBytes);
 		}
 		return numBytes;
 	}
@@ -200,9 +239,9 @@ public class RCXLink extends I2CSensor {
 		} catch (InterruptedException e) {}
 	}
 	
-	private void defineAndRun(byte[] macro, int len) {
+	public void defineAndRun(byte[] macro, int len) {
 		buf2[0] = (byte) len;
-		sendData(0x78,buf2,1);
+		sendData(0x78,buf2,1); // Use 078 -0x7F of EEPOM as buffer
 		sleep();
 		sendData(0x79, macro, len);
 		sleep();		
