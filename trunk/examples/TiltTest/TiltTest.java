c251f9c455e833950eba705a416fda625e7212ca
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2007-11-05 22:27:19 +0000

    Updated example to return acceleration data

--- trunk/examples/TiltTest/TiltTest.java
+++ trunk/examples/TiltTest/TiltTest.java
@@ -5,13 +5,15 @@ public class TiltTest {
 	
 	public static void main(String[] args) throws Exception {
 		TiltSensor tilt = new TiltSensor(SensorPort.S1);
-		
-		
+			
 		while(!Button.ESCAPE.isPressed()) {
 			LCD.clear();
-			LCD.drawInt(tilt.getXTilt(), 0, 0);
-			LCD.drawInt(tilt.getYTilt(), 0, 1);
-			LCD.drawInt(tilt.getZTilt(), 0, 2);
+			LCD.drawInt(tilt.getXTilt(), 6, 0, 0);
+			LCD.drawInt(tilt.getYTilt(), 6, 0, 1);
+			LCD.drawInt(tilt.getZTilt(), 6, 0, 2);
+			LCD.drawInt(tilt.getXAccel(), 6, 0, 3);
+			LCD.drawInt(tilt.getYAccel(), 6, 0, 4);
+			LCD.drawInt(tilt.getZAccel(), 6, 0, 5);
 			LCD.refresh();
 			Thread.sleep(500);
 		}
