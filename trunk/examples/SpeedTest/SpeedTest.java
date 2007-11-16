3a3e83b889dfbadd07bb35fb8a36c7083699bfa5
gloomyandy <gloomyandy@06c8f799-c779-4783-8af7-179afedb67c8>
2007-11-16 17:44:47 +0000

    Updated to disable the speed regulation threads, use setPower instead of setSpeed and changed display refresh for use with DMA screen redraw (refresh is now very inexpensive so can be called more often).

--- trunk/examples/SpeedTest/SpeedTest.java
+++ trunk/examples/SpeedTest/SpeedTest.java
@@ -8,9 +8,16 @@ public class SpeedTest {
   byte A = 0;
   LightSensor ls = new LightSensor(SensorPort.S3);
   UltrasonicSensor us = new UltrasonicSensor(SensorPort.S1);
+  /* Disable the speed regulation and close down the associated threads.
+   * This test does not require this type of motion control.
+   */
   Motor.A.regulateSpeed(false);
   Motor.B.regulateSpeed(false);
   Motor.C.regulateSpeed(false);
+  Motor.A.shutdown();
+  Motor.B.shutdown();
+  Motor.C.shutdown();
+  
   Motor.B.forward();
   Motor.C.forward();
   int iteration = 0;
@@ -24,18 +31,18 @@ public class SpeedTest {
    distVal = us.getDistance();
    tacho = Motor.B.getTachoCount();
    int RN = (int)(Math.random() * 100) + 1;
-   
-   LCD.drawInt(tacho,0,0);
-   LCD.drawInt((lightVal + distVal + tacho)*100/RN, 0, 1);
-   LCD.drawInt(A, 0, 2);
-   LCD.drawInt(iteration, 0, 4);
-   if(iteration%10 == 0)
-    LCD.refresh();
+
+   // Uncomment the following to produce display output as per the original test.
+   //LCD.drawInt(tacho,0,0);
+   //LCD.drawInt((lightVal + distVal + tacho)*100/RN, 0, 1);
+   //LCD.drawInt(A, 0, 2);
+   //LCD.drawInt(iteration, 0, 4);
+   //LCD.refresh();
 
    // Set motor speed for B and C to RN (Using Coast)
 
-   Motor.B.setSpeed(RN);
-   Motor.C.setSpeed(RN);
+   Motor.B.setPower(RN);
+   Motor.C.setPower(RN);
    if(RN > 50) ++A;
    if(RN < 50) --A;
    
