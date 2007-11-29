8034f0c2bcd97e8617ed0edaaac47a75fdf8d4bb
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2007-11-29 21:20:24 +0000

    Latest versions of scripts in snapshot

--- trunk/snapshot/bin/nxj.bat
+++ trunk/snapshot/bin/nxj.bat
@@ -2,9 +2,9 @@
 if "%NXJ_HOME%" == ""  goto homeless
 
 set THIRDPARTY_LIBS=%NXJ_HOME%\3rdparty\lib
-set LINK_CLASSPATH=.;%THIRDPARTY_LIBS%\bcel-5.1.jar;%THIRDPARTY_LIBS%\commons-cli-1.0.jar;%NXJ_HOME%\lib\pctools.jar;%NXJ_HOME%\lib\pccomm.jar;%NXJ_HOME%\lib\jtools.jar;%THIRDPARTY_LIBS%\bluecove.jar;%NXJ_HOME%\lib\classes.jar
+set LINK_CLASSPATH=.;%THIRDPARTY_LIBS%\bcel-5.1.jar;%THIRDPARTY_LIBS%\commons-cli-1.0.jar;%NXJ_HOME%\lib\pctools.jar;%NXJ_HOME%\lib\pccomm.jar;%NXJ_HOME%\lib\jtools.jar;%THIRDPARTY_LIBS%\bluecove.jar
 
-java -Djava.library.path=%NXJ_HOME%\bin -Dnxj.home=%NXJ_HOME% -classpath %LINK_CLASSPATH% lejos.pc.tools.NXJLinkAndUpload --writeorder LE --classpath %LINK_CLASSPATH% %*
+java "-Djava.library.path=%NXJ_HOME%\bin" "-Dnxj.home=%NXJ_HOME%" -classpath "%LINK_CLASSPATH%" lejos.pc.tools.NXJLinkAndUpload --writeorder LE --classpath ".;%NXJ_HOME%\lib\classes.jar" %*
 
 goto end
 
