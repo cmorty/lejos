99b8f77377495f574d32e84eee4a306fc597bb90
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2007-12-09 10:36:28 +0000

    Improved BTReceive example
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@1162 06c8f799-c779-4783-8af7-179afedb67c8

--- examples/BTReceive/BTReceive.java
+++ examples/BTReceive/BTReceive.java
@@ -2,13 +2,25 @@ import lejos.nxt.*;
 import lejos.nxt.comm.*;
 import java.io.*;
 
+/**
+ * Receive data from another NXT, a PC, a phone, 
+ * or another bluetooth device.
+ * 
+ * Waits for a connection, receives an int and returns
+ * its negative as a reply, 100 times, and then closes
+ * the connection, and waits for a new one.
+ * 
+ * @author Lawrie Griffiths
+ *
+ */
 public class BTReceive {
 
 	public static void main(String [] args)  throws Exception 
 	{
 		String connected = "Connected";
-        String waiting = "Waiting";
-
+        String waiting = "Waiting...";
+        String closing = "Closing...";
+        
 		while (true)
 		{
 			LCD.drawString(waiting,0,0);
@@ -19,22 +31,24 @@ public class BTReceive {
 			LCD.clear();
 			LCD.drawString(connected,0,0);
 			LCD.refresh();	
-			
-			InputStream is = btc.openInputStream();
-			OutputStream os = btc.openOutputStream();
-			DataInputStream dis = new DataInputStream(is);
-			DataOutputStream dos = new DataOutputStream(os);
+
+			DataInputStream dis = btc.openDataInputStream();
+			DataOutputStream dos = btc.openDataOutputStream();
 			
 			for(int i=0;i<100;i++) {
-				int ii = dis.readInt();
-				LCD.drawInt(ii,3,0,1);
+				int n = dis.readInt();
+				LCD.drawInt(n,7,0,1);
 				LCD.refresh();
-				dos.writeInt(-ii);
+				dos.writeInt(-n);
 				dos.flush();
 			}
 			
 			dis.close();
 			dos.close();
+			Thread.sleep(100); // wait for data to drain
+			LCD.clear();
+			LCD.drawString(closing,0,0);
+			LCD.refresh();
 			btc.close();
 			LCD.clear();
 		}
