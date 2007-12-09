98f201b8c0a2c241415807883b7ddcee689890ce
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2007-12-09 10:37:16 +0000

    Improved BTConnectTest example

--- trunk/examples/BTConnectTest/BTConnectTest.java
+++ trunk/examples/BTConnectTest/BTConnectTest.java
@@ -4,6 +4,28 @@ import lejos.nxt.*;
 import lejos.nxt.comm.*;
 import java.io.*;
 
+/**
+ * 
+ * Test of NXT to NXT Bluetooth comms.
+ * 
+ * Connects to another NXT, sends 100 ints, and receives the 
+ * replies. Then closes the connection and shuts down.
+ * 
+ * Works with the BTReceive sample running on the slave NXT.
+ * 
+ * Change the name string to the name of your slave NXT, and make sure
+ * it is in the known devices list of the master NXT. To do this, turn
+ * on the slave NXT and make sure Bluetooth is on and the device
+ * is visible. Use the Bluetooth menu on the slave for this. Then,
+ * on the master, select the Bluetooth menu and then select Search.
+ * The name of the slave NXT should appear. Select Add to add
+ * it to the known devices of the master. You can check this has
+ * been done by selecting Devices from the Bluetooth menu on the
+ * master.
+ * 
+ * @author Lawrie Griffiths
+ *
+ */
 public class BTConnectTest {
 	public static void main(String[] args) throws Exception {
 		String name = "NXT";
@@ -17,7 +39,7 @@ public class BTConnectTest {
 			LCD.clear();
 			LCD.drawString("No such device", 0, 0);
 			LCD.refresh();
-			Button.ESCAPE.waitForPressAndRelease();
+			Thread.sleep(2000);
 			System.exit(1);
 		}
 		
@@ -27,7 +49,7 @@ public class BTConnectTest {
 			LCD.clear();
 			LCD.drawString("Connect fail", 0, 0);
 			LCD.refresh();
-			Button.ESCAPE.waitForPressAndRelease();
+			Thread.sleep(2000);
 			System.exit(1);
 		}
 		
@@ -40,29 +62,38 @@ public class BTConnectTest {
 				
 		for(int i=0;i<100;i++) {
 			try {
-				LCD.drawInt(i*30000, 0, 2);
+				LCD.drawInt(i*30000, 8, 0, 2);
 				LCD.refresh();
 				dos.writeInt(i*30000);
 				dos.flush();			
 			} catch (IOException ioe) {
-				LCD.drawString("Exception", 0, 0);
+				LCD.drawString("Write Exception", 0, 0);
 				LCD.refresh();
 			}
 			
 			try {
-				LCD.drawInt(dis.readInt(),0,3);
+				LCD.drawInt(dis.readInt(),8, 0,3);
 				LCD.refresh();
 			} catch (IOException ioe) {
+				LCD.drawString("Read Exception ", 0, 0);
+				LCD.refresh();
 			}
 		}
 		
 		try {
+			LCD.drawString("Closing...    ", 0, 0);
+			LCD.refresh();
 			dis.close();
 			dos.close();
 			btc.close();
 		} catch (IOException ioe) {
+			LCD.drawString("Close Exception", 0, 0);
+			LCD.refresh();
 		}
 		
-		Button.ESCAPE.waitForPressAndRelease();
+		LCD.clear();
+		LCD.drawString("Finished",3, 4);
+		LCD.refresh();
+		Thread.sleep(2000);
 	}
 }
