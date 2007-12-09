71ba0739bf64ebff4d0dac195184146b50d32a38
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2007-12-09 20:53:47 +0000

    Improved example

--- trunk/examples/BlueStats/BlueStats.java
+++ trunk/examples/BlueStats/BlueStats.java
@@ -1,40 +1,61 @@
 import lejos.nxt.*;
 import lejos.nxt.comm.*;
 
+/**
+ * BlueStats: display local device Bluetooth information.
+ * 
+ * @author Lawrie Griffiths
+ *
+ */
 public class BlueStats {
 	public static void main(String[] args) {
-		String versionString = "Version";
+		String versionString = "BC4 version ";
 		String nameString = "Name";
-		String freeString = "Free mem";
 		String visString = "Visibility";
 		String statusString = "Status";
 		String portString = "Port Open";
 		String opString = "Op Mode";
+		String addrString = "Adr";
+		String connsString = "Conns";
 
 		while(!Button.ESCAPE.isPressed()) {
 			byte[] name = Bluetooth.getFriendlyName();
 			byte[] version = Bluetooth.getVersion();
 			String fn = byteArrayToString(name);
 			byte [] connections = Bluetooth.getConnectionStatus();
+			String addr = getAddressString(Bluetooth.getLocalAddress());
+			
+			// Friendly name of local device
 			LCD.drawString(nameString,0,0);
 			LCD.drawString(fn, 5, 0);
-			LCD.drawString(versionString,0,1);
-			LCD.drawInt(version[0] & 0xFF, 3, 8, 1);
-			LCD.drawInt(version[1] & 0xFF, 3, 12, 1);
-			for(int i=0;i<4;i++) LCD.drawInt(connections[i], 3, i*4, 2);
-			//LCD.drawString(freeString, 0, 2);
-			//LCD.drawInt((int) Runtime.getRuntime().freeMemory(), 5, 9, 2);
+			
+			// Version of BlueCore software
+			LCD.drawString(versionString + version[0] + "." + version[1],0,1);
+
+			// Local address
+            LCD.drawString(addrString,0,2);
+			LCD.drawString(addr, 4, 2);
+			
+			// Visibility
 			LCD.drawString(visString, 0, 3);
 			LCD.drawInt(Bluetooth.getVisibility(), 11, 3);
+			
+			// Status byte
 			LCD.drawString(statusString, 0, 4);
 			LCD.drawInt(Bluetooth.getStatus() & 0xFF, 7, 4);
-			byte [] addr = Bluetooth.getLocalAddress();
-			String la = getAddressString(addr);
-			LCD.drawString(la, 0, 5);
+			
+			// Connections
+			LCD.drawString(connsString, 0, 5);
+			for(int i=0;i<4;i++) LCD.drawInt(connections[i], 2, 5 + i*3, 5);
+
+			// Port open
 			LCD.drawString(portString, 0, 6);
 			LCD.drawInt(Bluetooth.getPortOpen(), 10, 6);
+			
+			// Operating mode
 			LCD.drawString(opString, 0, 7);
 			LCD.drawInt(Bluetooth.getOperatingMode(), 8, 7);
+			
 			LCD.refresh();			
 		} 	
     }
@@ -47,20 +68,15 @@ public class BlueStats {
 		return sb.toString();
 	}
 
-	private static final char[] cs = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+	private static final char[] hexChars = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
 
 	private static String getAddressString(byte [] addr) {
 		char[] caddr = new char[12];
-		
-		int ci = 0;
-		int nr = 0;
-		int addri = 0;
-		
+
 		for(int i=0; i<6; i++) {
-			addri = (int)addr[i];
-			nr = (addri>=0) ? addri : (256 + addri);	
-			caddr[ci++] = cs[nr / 16];
-			caddr[ci++] = cs[nr % 16];
+			int nr = addr[i] & 0xFF;	
+			caddr[i*2] = hexChars[nr & 0x0F];
+			caddr[i*2+1] = hexChars[nr >> 4];
 		}
 		return new String(caddr, 0, 12);
 	}
