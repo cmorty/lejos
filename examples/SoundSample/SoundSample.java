56b04cf0b14c0f1ce6763f3bb4dfd13890812304
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2007-12-11 21:20:36 +0000

    Added comments
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@1174 06c8f799-c779-4783-8af7-179afedb67c8

--- examples/SoundSample/SoundSample.java
+++ examples/SoundSample/SoundSample.java
@@ -1,11 +1,20 @@
 import lejos.nxt.*;
 import java.io.*;
 
-
+/**
+ * Simple example to demonstrate playing 8-bit WAV files.
+ * 
+ * Use nxjbrowse to upload the wav file. On Windows XP,
+ * rining.wav can be found in the Media subfolder of
+ * the Windows folder.
+ * 
+ * @author Lawrie Griffiths
+ *
+ */
 public class SoundSample {
 	public static void main(String [] options) throws Exception {
 		File f = new File("ringin.wav");
-		Sound.playSample(f,1500,1000);
+		Sound.playSample(f,10000,500);
 		Thread.sleep(2000);
 	}
 
