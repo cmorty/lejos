0137a94ec0a7bd2a2411b997d827001f734dd28e
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2007-12-15 14:23:25 +0000

    Untested class for HiTechnic Gyro Sensor

---
+++ trunk/classes/lejos/nxt/GyroSensor.java
@@ -0,0 +1,37 @@
+package lejos.nxt;
+
+/**
+ * Support the HiTechnic Gyro sensor - untested.
+ * http://www.hitechnic.com/
+ * 
+ * @author Lawrie Griffiths
+ *
+ */
+public class GyroSensor implements SensorConstants {
+	ADSensorPort port;
+	private int offset = 600;
+
+	public GyroSensor(ADSensorPort port)
+	{
+		this.port = port;
+		port.setTypeAndMode(TYPE_CUSTOM,
+                            MODE_RAW);
+	}
+	
+	/**
+	 * Read the gyro value
+	 * 
+	 * @return gyro value
+	 */
+	public int readValue()
+	{ 
+		return (port.readRawValue() - offset); 
+	}
+	
+	/**
+	 * Set the offset
+	 */
+	public void setOffset(int offset) {
+		this.offset = offset;
+	}
+}
