81a131507697db473711cb5ae7646f8c77b5979e
gloomyandy <gloomyandy@06c8f799-c779-4783-8af7-179afedb67c8>
2008-01-02 21:09:07 +0000

    Bluetooth test programs

---
+++ trunk/examples/BtTestPC/Main.java
@@ -0,0 +1,32 @@
+/*
+ * Main.java
+ *
+ * Created on 30 December 2007, 22:30
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package bttestprog;
+
+/**
+ *
+ * @author andy
+ */
+public class Main
+{
+	
+	/** Creates a new instance of Main */
+	public Main()
+	{
+	}
+	
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args)
+	{
+		BluetoothTest.main(args);
+	}
+	
+}
