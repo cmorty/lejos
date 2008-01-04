0627543e46a36751b149b8ca3c38cfea39cbe31f
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2008-01-04 01:14:42 +0000

    Better WAV file playing example.
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@1257 06c8f799-c779-4783-8af7-179afedb67c8

--- examples/SoundSample/SoundSample.java
+++ examples/SoundSample/SoundSample.java
@@ -2,10 +2,11 @@ import lejos.nxt.*;
 import java.io.*;
 
 /**
- * Simple example to demonstrate playing 8-bit WAV files.
+ * Demonstrates playing 8-bit WAV files.
  * 
- * Use nxjbrowse to upload the wav file. On Windows XP,
- * rining.wav can be found in the Media subfolder of
+ * Use nxjbrowse to upload 8-bit WAV files. On Windows XP,
+ * ringin.wav and ringout.wav are 8-bit WAV file, 
+ * which can be found in the Media subfolder of
  * the Windows folder.
  * 
  * @author Lawrie Griffiths
@@ -13,9 +14,45 @@ import java.io.*;
  */
 public class SoundSample {
 	public static void main(String [] options) throws Exception {
-		File f = new File("ringin.wav");
-		Sound.playSample(f,10000,500);
-		Thread.sleep(2000);
-	}
+		File[] allFiles = File.listFiles();
+		String s;
+		int len = 0;
+		
+		// Calculate number of WAV files
+		
+		for(int i=0;i<allFiles.length && allFiles[i] != null;i++) {
+			s = allFiles[i].getName();
+			int l = s.length();
+			if (l > 4 && s.charAt(l-3) =='w' && s.charAt(l-2) == 'a' && s.charAt(l-1) == 'v') len++;	
+		}
+		
+		// Make array of WAV files and fileNames
+		
+		String[] fileNames = new String[len];
+		File[] files = new File[len];
+		int j = 0;
+		for(int i=0;i<allFiles.length && allFiles[i] != null;i++) {
+			s = allFiles[i].getName();
+			int l = s.length();
+			if (l > 4 && s.charAt(l-3) =='w' && s.charAt(l-2) == 'a' && s.charAt(l-1) == 'v') {
+				fileNames[j] = s;
+				files[j++] = allFiles[i];
+			}
+		}
+		
+		LCD.drawString("Play a WAV file",0,0);
+		
+		// Create menu of WAV files
+		TextMenu fileMenu = new TextMenu(fileNames,1);
+		
+		// Play files until the user quits
+		int selected;
+		do {
+			selected = fileMenu.select();
+			if (selected >= 0) {
+				Sound.playSample(files[selected],100);
+			}
+		} while (selected >= 0);
 
+	}
 }
