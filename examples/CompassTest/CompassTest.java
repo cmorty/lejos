44c55e0db1ba8a5b416d6ae183b10fc1cb324b80
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2008-01-07 14:21:51 +0000

    Added comment.
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@1292 06c8f799-c779-4783-8af7-179afedb67c8

--- examples/CompassTest/CompassTest.java
+++ examples/CompassTest/CompassTest.java
@@ -1,15 +1,23 @@
 import lejos.nxt.*;
 
-	public class CompassTest {
-		
-		public static void main(String[] args) throws Exception {
-			CompassSensor compass = new CompassSensor(SensorPort.S1);
-						
-			while(!Button.ESCAPE.isPressed()) {
-				LCD.clear();
-				LCD.drawInt((int) compass.getDegrees(), 0, 0);
-				LCD.refresh();
-				Thread.sleep(500);
-			}
-		}	
-	}
+/**
+ * Simple test of compass sensors.
+ * 
+ * Works with Mindsensors and HiTechnic compass sensors.
+ * 
+ * @author Lawrie Griffiths
+ *
+ */
+ public class CompassTest {
+	
+	public static void main(String[] args) throws Exception {
+		CompassSensor compass = new CompassSensor(SensorPort.S1);
+					
+		while(!Button.ESCAPE.isPressed()) {
+			LCD.clear();
+			LCD.drawInt((int) compass.getDegrees(), 0, 0);
+			LCD.refresh();
+			Thread.sleep(500);
+		}
+	}	
+}
