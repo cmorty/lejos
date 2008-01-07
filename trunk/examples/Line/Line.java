7ebbc4ae8266b4f41f2be48fc61140920943e225
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2008-01-07 13:54:35 +0000

    Added comments to Line example

--- trunk/examples/Line/Line.java
+++ trunk/examples/Line/Line.java
@@ -2,12 +2,29 @@ import lejos.nxt.*;
 import lejos.subsumption.*;
 import lejos.navigation.*;
 
+/**
+ * Demonstration of use of the Behavior and Pilot classes to
+ * implement a simple line following robot.
+ * 
+ * Requires a wheeled vehicle with two independently controlled
+ * wheels with motors connected to motor ports A and C, and a light
+ * sensor mounted forwards and pointing down, connected to sensor port 1.
+ * 
+ * Press ENTER to start the robot.
+ * 
+ * @author Lawrie Griffiths
+ *
+ */
 public class Line {
 	
 	public static void main (String[] aArg)
 	throws Exception
 	{
-		final Pilot pilot = new Pilot(5.5f,11.2f,Motor.A, Motor.C);
+		// Change last parameter of Pilot to specify on which 
+		// direction you want to be "forward" for your vehicle.
+		// The wheel and axle dimension psarameters should be
+		// set for your robot, but are not critical.
+		final Pilot pilot = new Pilot(5.6f,16.0f,Motor.A, Motor.C, true);
 		final LightSensor light = new LightSensor(SensorPort.S1);
 		
 		Behavior DriveForward = new Behavior()
@@ -46,6 +63,7 @@ public class Line {
 			}
 		};
 
+		//Wait for ENTER button to be pressed
 		Button.ENTER.waitForPressAndRelease();
 
 		Behavior[] bArray = {OffLine, DriveForward};		
