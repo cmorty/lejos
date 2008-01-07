57089e49d9ec0abfdf4ebe597594dbf6027a8202
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2008-01-07 13:31:09 +0000

    Added comments to BumperCar example

--- trunk/examples/BumperCar/BumperCar.java
+++ trunk/examples/BumperCar/BumperCar.java
@@ -1,6 +1,16 @@
 import lejos.subsumption.*;
 import lejos.nxt.*;
 
+/**
+ * Demonstration of the Behavior subsumption classes.
+ * 
+ * Requires a wheeled vehicle with two independently controlled
+ * motors connected to motor ports A and C, and a touch
+ * sensor connected to sensport port 2.
+ * 
+ * @author Brian Bagnall and Lawrie Griffiths
+ *
+ */
 public class BumperCar {
    public static void main(String [] args) {
       Behavior b1 = new DriveForward();
