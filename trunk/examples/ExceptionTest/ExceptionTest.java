872b214f9eff7b418fc41e5ef35c1268cc888b17
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2008-01-07 14:13:05 +0000

    Added comment.

--- trunk/examples/ExceptionTest/ExceptionTest.java
+++ trunk/examples/ExceptionTest/ExceptionTest.java
@@ -1,5 +1,17 @@
 import lejos.nxt.*;
 
+/**
+ * Simple test of leJOS exceptions.
+ * 
+ * This causes an ArrayIndexOutOdBoundsException.
+ * 
+ * Use the --verbose (-v) flag on nxj or nxjlink to see
+ * the values of classes and methods (signatures) for
+ * your program.
+ *  
+ * @author Lawrie Griffiths
+ *
+ */
 public class ExceptionTest {
 	
 	public static void main (String[] aArg)
