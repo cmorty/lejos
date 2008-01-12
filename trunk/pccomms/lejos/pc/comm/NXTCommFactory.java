1a013a2857d8604ea4fd242a21ffd0a480efd504
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2008-01-12 21:35:02 +0000

    Support for Bluecove on MAC OS X

--- trunk/pccomms/lejos/pc/comm/NXTCommFactory.java
+++ trunk/pccomms/lejos/pc/comm/NXTCommFactory.java
@@ -6,24 +6,28 @@ package lejos.pc.comm;
  * and protocol (Bluetooth or USB) that is requested.
  *
  */
-public class NXTCommFactory {
-	
-    public static final int USB = 1;
-    public static final int BLUETOOTH = 2;
-	
-	public static NXTComm createNXTComm(int protocol) {
-		
-		String os = System.getProperty("os.name");
-    	boolean windows = false;
-    	
-    	if (os.length() >= 7 && os.substring(0,7).equals("Windows"))
-    		windows = true;
-    	
-		if (protocol == BLUETOOTH) {
-			if (windows) return new NXTCommBluecove();
-			else return new NXTCommBluez();
-		} else return new NXTCommLibnxt();
-			
-	}
+public class NXTCommFactory { 
+    
+    public static final int USB = 1; 
+    public static final int BLUETOOTH = 2; 
+    
+   	public static NXTComm createNXTComm(int protocol) {     
+   		String os = System.getProperty("os.name"); 
+       	boolean windows = false; 
+       	boolean mac = false; 
+        
+       	if (os.length() >= 7 && os.substring(0,7).equals("Windows")) { 
+       		windows = true; 
+       	}
+       	
+       	if (os.equals("Mac OS X")) { 
+       		mac=true;          
+       	} 
+        
+       	if (protocol == BLUETOOTH) { 
+       		if (windows||mac) return new NXTCommBluecove(); 
+       		else return new NXTCommBluez(); 
+       	} else return new NXTCommLibnxt();          
+   } 
+} 
 
-}
