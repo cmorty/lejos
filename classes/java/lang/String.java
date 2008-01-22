1329ba47f8f0ae545183e5852b34621e59cc9c67
bbagnall <bbagnall@06c8f799-c779-4783-8af7-179afedb67c8>
2008-01-22 04:40:59 +0000

    Added indexOf() methods to String and StringBuffer to help with parsing.
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@1361 06c8f799-c779-4783-8af7-179afedb67c8

--- classes/java/lang/String.java
+++ classes/java/lang/String.java
@@ -40,6 +40,202 @@ public final class String
     return characters[index];
   }
 
+  public int indexOf(int ch) {
+      return indexOf(ch, 0);
+  }
+
+  /**
+   * Returns the index within this string of the first occurrence of the
+   * specified character, starting the search at the specified index.
+   *
+   * @param   ch          a character (Unicode code point).
+   * @param   fromIndex   the index to start the search from.
+   * @return  the index of the first occurrence of the character in the
+   *          character sequence represented by this object that is greater
+   *          than or equal to <code>fromIndex</code>, or <code>-1</code>
+   *          if the character does not occur.
+   */
+  public int indexOf(int ch, int fromIndex) {
+      int offset = 0; // Assume string always starts at 0 in characters[]
+	  int max = offset + characters.length;
+      char v[] = characters;
+
+      if (fromIndex < 0) {
+          fromIndex = 0;
+      } else if (fromIndex >= characters.length) {
+          // Note: fromIndex might be near -1>>>1.
+          return -1;
+      }
+
+      int i = offset + fromIndex;
+      if (ch < Character.MIN_SUPPLEMENTARY_CODE_POINT) {
+          // handle most cases here (ch is a BMP code point or a
+          // negative value (invalid code point))
+          for (; i < max ; i++) {
+              if (v[i] == ch) {
+                  return i - offset;
+              }
+          }
+          return -1;
+      }
+
+      if (ch <= Character.MAX_CODE_POINT) {
+          // handle supplementary characters here
+          char[] surrogates = Character.toChars(ch);
+          for (; i < max; i++) {
+              if (v[i] == surrogates[0]) {
+                  if (i + 1 == max) {
+                      break;
+                  }
+                  if (v[i+1] == surrogates[1]) {
+                      return i - offset;
+                  }
+              }
+          }
+      }
+      return -1;
+  }
+  
+  /**
+   * Finds the location of a string within this string 
+   * @param str
+   * @return Index of string location, -1 if string does not exist.
+   */
+  public int indexOf(String str) {
+      return indexOf(str, 0);
+  }
+  
+  /**
+   * 
+   * @param str
+   * @param fromIndex
+   * @return Index of string location, -1 if string does not exist.
+   */
+   
+  public synchronized int indexOf(String str, int fromIndex) {
+      return String.indexOf(characters, 0, characters.length,
+                            str.toCharArray(), 0, str.length(), fromIndex);
+  }
+  
+  public int lastIndexOf(String str) {
+      return lastIndexOf(str, characters.length);
+  }
+
+  public int lastIndexOf(String str, int fromIndex) {
+      return lastIndexOf(characters, 0, characters.length,
+                         str.characters, 0, str.length(), fromIndex);
+  }
+  
+  /**
+   * Code shared by String and StringBuffer to do searches. The
+   * source is the character array being searched, and the target
+   * is the string being searched for.
+   *
+   * @param   source       the characters being searched.
+   * @param   sourceOffset offset of the source string.
+   * @param   sourceCount  count of the source string.
+   * @param   target       the characters being searched for.
+   * @param   targetOffset offset of the target string.
+   * @param   targetCount  count of the target string.
+   * @param   fromIndex    the index to begin searching from.
+   */
+  static int lastIndexOf(char[] source, int sourceOffset, int sourceCount,
+                         char[] target, int targetOffset, int targetCount,
+                         int fromIndex) {
+      /*
+       * Check arguments; return immediately where possible. For
+       * consistency, don't check for null str.
+       */
+      int rightIndex = sourceCount - targetCount;
+      if (fromIndex < 0) {
+          return -1;
+      }
+      if (fromIndex > rightIndex) {
+          fromIndex = rightIndex;
+      }
+      /* Empty string always matches. */
+      if (targetCount == 0) {
+          return fromIndex;
+      }
+
+      int strLastIndex = targetOffset + targetCount - 1;
+      char strLastChar = target[strLastIndex];
+      int min = sourceOffset + targetCount - 1;
+      int i = min + fromIndex;
+
+  startSearchForLastChar:
+      while (true) {
+          while (i >= min && source[i] != strLastChar) {
+              i--;
+          }
+          if (i < min) {
+              return -1;
+          }
+          int j = i - 1;
+          int start = j - (targetCount - 1);
+          int k = strLastIndex - 1;
+
+          while (j > start) {
+              if (source[j--] != target[k--]) {
+                  i--;
+                  continue startSearchForLastChar;
+              }
+          }
+          return start - sourceOffset + 1;
+      }
+  }
+  
+  /**
+   * Code shared by String and StringBuffer to do searches. The
+   * source is the character array being searched, and the target
+   * is the string being searched for.
+   *
+   * @param   source       the characters being searched.
+   * @param   sourceOffset offset of the source string.
+   * @param   sourceCount  count of the source string.
+   * @param   target       the characters being searched for.
+   * @param   targetOffset offset of the target string.
+   * @param   targetCount  count of the target string.
+   * @param   fromIndex    the index to begin searching from.
+   */
+  static int indexOf(char[] source, int sourceOffset, int sourceCount,
+                     char[] target, int targetOffset, int targetCount,
+                     int fromIndex) {
+      if (fromIndex >= sourceCount) {
+          return (targetCount == 0 ? sourceCount : -1);
+      }
+      if (fromIndex < 0) {
+          fromIndex = 0;
+      }
+      if (targetCount == 0) {
+          return fromIndex;
+      }
+
+      char first  = target[targetOffset];
+      int max = sourceOffset + (sourceCount - targetCount);
+
+      for (int i = sourceOffset + fromIndex; i <= max; i++) {
+          /* Look for first character. */
+          if (source[i] != first) {
+              while (++i <= max && source[i] != first);
+          }
+
+          /* Found first character, now look at the rest of v2 */
+          if (i <= max) {
+              int j = i + 1;
+              int end = j + targetCount - 1;
+              for (int k = targetOffset + 1; j < end && source[j] ==
+                       target[k]; j++, k++);
+
+              if (j == end) {
+                  /* Found whole string. */
+                  return i - sourceOffset;
+              }
+          }
+      }
+      return -1;
+  }
+  
   /**
    * Converts the String into an array of characters
    * @return the character Array
