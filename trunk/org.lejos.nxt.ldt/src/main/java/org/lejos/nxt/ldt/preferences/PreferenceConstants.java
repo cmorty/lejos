de9b47b0338139baf0a7ffb6388d86222e70fb97
mpscholz <mpscholz@06c8f799-c779-4783-8af7-179afedb67c8>
2008-01-23 20:03:28 +0000

    added preferences for program upload

--- trunk/org.lejos.nxt.ldt/src/main/java/org/lejos/nxt/ldt/preferences/PreferenceConstants.java
+++ trunk/org.lejos.nxt.ldt/src/main/java/org/lejos/nxt/ldt/preferences/PreferenceConstants.java
@@ -9,11 +9,11 @@ package org.lejos.nxt.ldt.preferences;
 public class PreferenceConstants {
 
 	public static final String P_NXJ_HOME = "nxjHome";
-
 	public static final String P_IS_VERBOSE = "isVerbose";
-
 	public static final String P_CONNECTION_TYPE = "connectionType";
-//
-//	public static final String P_STRING = "stringPreference";
+	public static final String P_CONNECT_TO_BRICK_ADDRESS = "Connect to address";
+	public static final String P_CONNECTION_BRICK_ADDRESS = "Connection address";
+	public static final String P_CONNECT_TO_BRICK_NAME = "Connect to name";
+	public static final String P_CONNECTION_BRICK_NAME = "Connection name";
 	
 }
