a556d64c43efd8225e4daffb5a7d0e2a8dc8b45c
bbagnall <bbagnall@06c8f799-c779-4783-8af7-179afedb67c8>
2008-01-27 04:54:14 +0000

    Added Integer.parseInt() method and NumberFormatException.

--- trunk/classes/java/lang/Integer.java
+++ trunk/classes/java/lang/Integer.java
@@ -37,6 +37,79 @@ package java.lang;
 
     static char buf [] = new char [12] ; 
 
+    public static int parseInt(String s, int radix)
+			throws NumberFormatException {
+		if (s == null) {
+			throw new NumberFormatException();
+			//throw new NumberFormatException("null");
+		}
+
+		if (radix < Character.MIN_RADIX) {
+			throw new NumberFormatException();
+			//throw new NumberFormatException("radix " + radix
+			//		+ " less than Character.MIN_RADIX");
+		}
+
+		if (radix > Character.MAX_RADIX) {
+			throw new NumberFormatException();
+			//throw new NumberFormatException("radix " + radix
+			//		+ " greater than Character.MAX_RADIX");
+		}
+
+		int result = 0;
+		boolean negative = false;
+		int i = 0, len = s.length();
+		int limit = -Integer.MAX_VALUE;
+		int multmin;
+		int digit;
+
+		if (len > 0) {
+			char firstChar = s.charAt(0);
+			if (firstChar < '0') { // Possible leading "+" or "-"
+				if (firstChar == '-') {
+					negative = true;
+					limit = Integer.MIN_VALUE;
+				} else if (firstChar != '+')
+					throw new NumberFormatException();
+					//throw NumberFormatException.forInputString(s);
+
+				if (len == 1) // Cannot have lone "+" or "-"
+					throw new NumberFormatException();
+					//throw NumberFormatException.forInputString(s);
+				i++;
+			}
+			multmin = limit / radix;
+			while (i < len) {
+				// Accumulating negatively avoids surprises near MAX_VALUE
+				digit = Character.digit(s.charAt(i++), radix);
+				if (digit < 0) {
+					throw new NumberFormatException();
+					//throw NumberFormatException.forInputString(s);
+				}
+				if (result < multmin) {
+					throw new NumberFormatException();
+					//throw NumberFormatException.forInputString(s);
+				}
+				result *= radix;
+				if (result < limit + digit) {
+					throw new NumberFormatException();
+					//throw NumberFormatException.forInputString(s);
+				}
+				result -= digit;
+			}
+		} else {
+			throw new NumberFormatException();
+			//throw NumberFormatException.forInputString(s);
+		}
+		return negative ? result : -result;
+	}
+
+
+	public static int parseInt(String s) throws NumberFormatException {
+		return parseInt(s,10);
+	}
+
+    
     /**
      * Returns a new String object representing the specified integer. The 
      * argument is converted to signed decimal representation and returned 
---
+++ trunk/classes/java/lang/NumberFormatException.java
@@ -0,0 +1,5 @@
+package java.lang;
+
+public class NumberFormatException extends IllegalArgumentException {
+
+}
