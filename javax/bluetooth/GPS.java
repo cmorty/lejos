c6e6f19332e72c8bdf7e37aa9c3446de72bcec1c
bbagnall <bbagnall@06c8f799-c779-4783-8af7-179afedb67c8>
2008-01-28 04:24:37 +0000

    Moved demo to examples directory. Works pretty good now.
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk/classes@1382 06c8f799-c779-4783-8af7-179afedb67c8

--- javax/bluetooth/GPS.java
+++ javax/bluetooth/GPS.java
@@ -2,8 +2,6 @@ package javax.bluetooth;
 
 import java.io.*;
 
-import lejos.nxt.LCD; // !! DELETE
-
 /**
  * Class to pull data from a GPS receiver
  * @author BB
@@ -21,8 +19,18 @@ public class GPS extends Thread {
 	private StringBuffer currentSentence = new StringBuffer();
 	
 	private String START_CHAR = "$";
+	private String GGA_STR = "GGA";
 	
-	private InputStream in; 
+	private InputStream in;
+	
+	private String BLANK = ""; // DELETE ME
+	
+	private int time = 0;
+	private float latitude = 0;
+	private float longitude = 0;
+	private float altitude = 0;
+	private byte satellites_tracked = 0;
+	
 	/**
 	 * 
 	 * @param in An input stream from the GPS receiver
@@ -33,20 +41,38 @@ public class GPS extends Thread {
 		this.start();
 	}
 	
-	public String getLatitude() {
-		return "lat";
+	public float getLatitude() {
+		return latitude;
 	}
 	
-	public String getLongitude() {
-		return "long";
+	public float getLongitude() {
+		return longitude;
 	}
 	
-	public String getAltitude() {
-		return "alt";
+	/**
+	 * The altitude above mean sea level
+	 * @return Meters above sea level e.g. 545.4     
+	 */
+	public float getAltitude() {
+		return altitude;
 	}
 	
+	/**
+	 * Returns the last time stamp retrieved from a satellite
+	 * 
+	 * @return The time as a UTC integer. 123519 = 12:35:19 UTC
+	 */
 	public int getTime() {
-		return 0;
+		return time;
+	}
+	
+	/**
+	 * Returns the number of satellites being tracked to
+	 * determine the coordinates.
+	 * @return Number of satellites e.g. 8
+	 */
+	public byte getSatellitesTracked() {
+		return satellites_tracked;
 	}
 	
 	/**
@@ -82,36 +108,35 @@ public class GPS extends Thread {
 		
 	}
 	
-	// Debugging variables (DELETE WHEN DONE)
-	int sentenceCount = 0;
-	
 	/**
 	 * Keeps reading sentences from GPS receiver stream and extracting data.
 	 * This is a daemon thread so when program ends it won't keep running.
 	 */
 	public void run() {
 		/* Code holder for parsing values */
+		
 		while(true) {
 			String s = getNextString();
-
 			// Make NMEASentence
 			NMEASentence sen = new NMEASentence(s);
 			
-			LCD.clear();
-			LCD.drawInt(sentenceCount, 0, 0); // DELETE
-			LCD.drawString(s, 0, 1);
-			LCD.drawString(sen.getPrefix(), 0, 2);
-			LCD.drawString(sen.getDataType(), 0, 3);
-			LCD.drawInt(sen.getChecksum(), 0, 4);
-			LCD.drawInt(sen.calcChecksum(), 0, 5);
-			
-			LCD.refresh();
-			
-			
 			// Check if valid (discard if it is invalid)
-			// Check if contains lat/long data
-			// If so, pull that data and update all global vars
+			if(sen.isValid()) {
+				
+				// Check if contains lat/long data
+				if(sen.getDataType().equals(GGA_STR)) {
+					
+					// Update all global vars
+					//time = Integer.parseInt((String)sen.getDataFields().elementAt(0)); // Convert string to int
+					
+					latitude = Float.parseFloat((String)sen.getDataFields().elementAt(1));
+					longitude = Float.parseFloat((String)sen.getDataFields().elementAt(3));
+					altitude = Float.parseFloat((String)sen.getDataFields().elementAt(7));
+					
+					//satellites_tracked  = (byte)Integer.parseInt((String)sen.getDataFields().elementAt(9));
+				}
 			// Notify appropriate listeners if data changed
+			}
 		}
 	}
 	
@@ -135,7 +160,6 @@ public class GPS extends Thread {
 			// Search for $ symbol (indicates start of new sentence)
 			if(currentSentence.indexOf(START_CHAR, 1) >= 0) {
 				done = true;
-				sentenceCount++; // DELETE
 			}
 			
 		} while(!done);
@@ -148,34 +172,4 @@ public class GPS extends Thread {
 		
 		return sentence;
 	}
-	
-	/**
-	 * Accepts a $GPGGA sentence and rips out lat, long, alt, time
-	 * and updates the global variables for each, then notifies all
-	 * listeners.
-	 *
-	 */
-	private void ripGPGGA_Sentence(StringBuffer sentence) {
-		
-	}
-	
-	/* GPS SAMPLE OUTPUT
-	$GPGSA,A,2,,,,,,,,,,,,,50.0,50.0,50.0*06
-	$GPRMC,140817.000,V,4433.2983,N,08056.3970,W,1.42,77.42,020707,,,E*51
-	$GPGGA,140818.000,4433.2984,N,08056.3964,W,6,00,50.0,168.7,M,-36.0,M,,0000*5C
-	$GPGSA,A,2,,,,,,,,,,,,,50.0,50.0,50.0*06
-	$GPRMC,140818.000,V,4433.2984,N,08056.3964,W,1.42,77.42,020707,,,E*5C
-	$GPGGA,140819.000,4433.2984,N,08056.3959,W,6,00,50.0,168.7,M,-36.0,M,,0000*53
-	$GPGSA,A,2,,,,,,,,,,,,,50.0,50.0,50.0*06
-	$GPGSV,3,1,10,18,71,320,30,21,62,184,32,09,46,130,,22,36,290,20*79
-	$GPGSV,3,2,10,24,29,154,,26,28,050,22,29,18,052,,03,13,300,*78
-	$GPGSV,3,3,10,14,12,233,,19,07,327,*75
-	$GPRMC,140819.000,V,4433.2984,N,08056.3959,W,1.42,77.42,020707,,,E*53
-	$GPGGA,140820.000,4433.2985,N,08056.3954,W,6,00,50.0,168.7,M,-36.0,M,,0000*55
-	$GPGSA,A,2,,,,,,,,,,,,,50.0,50.0,50.0*06
-	$GPRMC,140820.000,V,4433.2985,N,08056.3954,W,1.42,77.42,020707,,,E*55
-	$GPGGA,140821.000,4433.2985,N,08056.3948,W,6,00,50.0,168.7,M,-36.0,M,,0000*59
-	$GPGSA,A,2,,,,,,,,,,,,,50.0,50.0,50.0*06
-	*/
-	
 }
