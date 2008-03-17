1e2d7b7f89df8e63c9de7919c004e58448fbf050
bbagnall <bbagnall@06c8f799-c779-4783-8af7-179afedb67c8>
2008-03-17 21:36:39 +0000

    DeviceClass now functional. Altered some methods in RemoteDevice to match javax.bluetooth.
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk/classes@1474 06c8f799-c779-4783-8af7-179afedb67c8

--- javax/bluetooth/LocalDevice.java
+++ javax/bluetooth/LocalDevice.java
@@ -1,10 +1,56 @@
 package javax.bluetooth;
 
 /**
- * Currently unimplemented
+ * Currently unimplemented. Singleton class.
  * @author BB
  *
  */
 public class LocalDevice {
-
+	
+	private static LocalDevice localDevice;
+	
+	private LocalDevice() {
+		
+	}
+	
+	public static LocalDevice getLocalDevice() throws BluetoothStateException {
+		if(localDevice == null)
+			localDevice = new LocalDevice();
+		return localDevice;
+	}
+	
+	public String getFriendlyName() {
+		return null;
+	}
+	
+	public DeviceClass getDeviceClass() {
+		return null;
+	}
+	
+	public boolean setDiscoverable(int mode) throws BluetoothStateException {
+		return true;
+	}
+	
+	public static boolean isPowerOn() {
+		return true; 
+	}
+	
+	public int getDiscoverable() {
+		return 0;
+	}
+	
+	/**
+	 * Possibly use Properties class in implementation.
+	 * @param property
+	 * @return
+	 */
+	public static String getProperty(String property) {
+		return null;
+	}
+	
+	public String getBluetoothAddress() {
+		return null;
+	}
+	
+	
 }
