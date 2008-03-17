04b29a198173401f9722c1c8ec64cde836f95a74
bbagnall <bbagnall@06c8f799-c779-4783-8af7-179afedb67c8>
2008-03-17 22:43:11 +0000

    Partially complete LocalDevice.

--- trunk/classes/javax/bluetooth/LocalDevice.java
+++ trunk/classes/javax/bluetooth/LocalDevice.java
@@ -1,5 +1,7 @@
 package javax.bluetooth;
 
+import lejos.nxt.comm.Bluetooth;
+
 /**
  * Currently unimplemented. Singleton class.
  * @author BB
@@ -20,26 +22,45 @@ public class LocalDevice {
 	}
 	
 	public String getFriendlyName() {
-		return null;
+		char [] nameChars = ConvertBytesToChars(Bluetooth.getFriendlyName());
+		return new String(nameChars);
+	}
+	
+	private char [] ConvertBytesToChars(byte [] byteArray) {
+		char [] charArray = new char[byteArray.length];
+		for(byte i=0;i<charArray.length;i++)
+			charArray[i] = (char)byteArray[i];
+		return charArray;
 	}
 	
 	public DeviceClass getDeviceClass() {
 		return null;
 	}
-	
+	/**
+	 * 
+	 * @param mode Unsure if this is same as javax.bluetooth.DiscoveryAgent.LIAC
+	 * @return true if successful, false if unsuccessful
+	 * @throws BluetoothStateException
+	 */
 	public boolean setDiscoverable(int mode) throws BluetoothStateException {
-		return true;
+		/*
+		 * DEVELOPER NOTES: I'm not sure if the mode values are
+		 * the same for Bluetooth.setVisibility(mode) and javax.bluetooth.DiscoveryAgent.LIAC
+		 */
+		int ret = Bluetooth.setVisibility((byte)mode);
+		return (ret >=0);
 	}
 	
 	public static boolean isPowerOn() {
-		return true; 
+		return Bluetooth.getPower(); 
 	}
 	
 	public int getDiscoverable() {
-		return 0;
+		return Bluetooth.getVisibility();
 	}
 	
 	/**
+	 * UNIMPLEMENTED!
 	 * Possibly use Properties class in implementation.
 	 * @param property
 	 * @return
@@ -47,10 +68,12 @@ public class LocalDevice {
 	public static String getProperty(String property) {
 		return null;
 	}
-	
+	/**
+	 * !! NOT PROPERLY IMPLEMENTED: Convert from hex values?
+	 * @return
+	 */
 	public String getBluetoothAddress() {
-		return null;
+		char [] nameChars = ConvertBytesToChars(Bluetooth.getLocalAddress());
+		return new String(nameChars);
 	}
-	
-	
 }
