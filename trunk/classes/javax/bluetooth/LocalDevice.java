8622f7f837b10f61d420db59db29784a5186c462
bbagnall <bbagnall@06c8f799-c779-4783-8af7-179afedb67c8>
2008-03-19 00:00:58 +0000

    LocalDevice is now functional. Might add getProperty() in future but pretty irrelevant to NXJ.

--- trunk/classes/javax/bluetooth/LocalDevice.java
+++ trunk/classes/javax/bluetooth/LocalDevice.java
@@ -11,10 +11,20 @@ public class LocalDevice {
 	
 	private static LocalDevice localDevice;
 	
-	private LocalDevice() {
+	/**
+	 * DEVELOPER NOTES:
+	 * The values in cs are redundant from RemoteDevice. 
+	 * Might save some memory by recycling it.
+	 */
+	private static final char[] cs = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
 		
-	}
-	
+	private LocalDevice() {}
+
+	/*
+	 * DEVELOPER NOTES: Technically the constructor doesn't throw
+	 * a BluetoothStateException like the Bluetooth API demands.
+	 * Get rid of exception?
+	 */
 	public static LocalDevice getLocalDevice() throws BluetoothStateException {
 		if(localDevice == null)
 			localDevice = new LocalDevice();
@@ -23,7 +33,8 @@ public class LocalDevice {
 	
 	public String getFriendlyName() {
 		char [] nameChars = ConvertBytesToChars(Bluetooth.getFriendlyName());
-		return new String(nameChars);
+		String fName = new String(nameChars);  
+		return fName.substring(0, fName.indexOf(0)); // Clip off extra chars
 	}
 	
 	private char [] ConvertBytesToChars(byte [] byteArray) {
@@ -33,34 +44,54 @@ public class LocalDevice {
 		return charArray;
 	}
 	
-	public DeviceClass getDeviceClass() {
-		return null;
-	}
 	/**
-	 * 
-	 * @param mode Unsure if this is same as javax.bluetooth.DiscoveryAgent.LIAC
+	 * The Lego Bluecore code can't retrieve this from the chip.
+	 * Always returns hardcoded 0x3e0100 DeviceClass
+	 * Untested if this is correct.
+	 * @return
+	 */
+	public DeviceClass getDeviceClass() {
+		return new DeviceClass(0x3e0100);
+	}
+
+	/**
+	 * Normally the mode values are found in javax.bluetooth.DiscoveryAgent.
+	 * We don't have this yet in NXJ so use 0 for invisible, any other
+	 * value for visible.
+	 * @param mode 0 = invisible, all other = visible.
 	 * @return true if successful, false if unsuccessful
 	 * @throws BluetoothStateException
 	 */
 	public boolean setDiscoverable(int mode) throws BluetoothStateException {
 		/*
-		 * DEVELOPER NOTES: I'm not sure if the mode values are
-		 * the same for Bluetooth.setVisibility(mode) and javax.bluetooth.DiscoveryAgent.LIAC
+		 * DEVELOPER NOTES: javax.bluetooth.DiscoveryAgent.NOT_DISCOVERABLE = 0x00
 		 */
-		int ret = Bluetooth.setVisibility((byte)mode);
+		
+		// 0x00 = invisible, 0x01=visible 
+		int ret = Bluetooth.setVisibility((byte)(mode == 0 ? 0 : 1));
 		return (ret >=0);
 	}
 	
+	/**
+	 * Power state of the Bluecore 4 chip in the NXT brick. 
+	 * @return
+	 */
 	public static boolean isPowerOn() {
 		return Bluetooth.getPower(); 
 	}
 	
+	/**
+	 * Indicates whether the NXT brick is visible to other devices 
+	 * @return 0 = not discoverable, all other = discoverable
+	 */
 	public int getDiscoverable() {
 		return Bluetooth.getVisibility();
 	}
 	
 	/**
-	 * UNIMPLEMENTED!
+	 * UNIMPLEMENTED! Returns null always.
+	 * Returns various properties about the bluetooth implementation
+	 * such as version, whether master/slave switch allowed, etc..
 	 * Possibly use Properties class in implementation.
 	 * @param property
 	 * @return
@@ -69,11 +100,29 @@ public class LocalDevice {
 		return null;
 	}
 	/**
-	 * !! NOT PROPERLY IMPLEMENTED: Convert from hex values?
+	 * Returns the local Bluetooth address of NXT brick.
 	 * @return
 	 */
 	public String getBluetoothAddress() {
-		char [] nameChars = ConvertBytesToChars(Bluetooth.getLocalAddress());
-		return new String(nameChars);
+		/*
+		 * DEVELOPER NOTES:
+		 * Most of the code in this method is redundant from 
+		 * RemoteDevice.getBluetoothAddress(). Might be able to 
+		 * save memory by reusing code. 
+		 */
+		char[] caddr = new char[12];
+		
+		byte [] addr = Bluetooth.getLocalAddress();
+		int ci = 0;
+		int nr = 0;
+		int addri = 0;
+		
+		for(int i=0; i<6; i++) {
+			addri = (int)addr[i];
+			nr = (addri>=0) ? addri : (256 + addri);	
+			caddr[ci++] = cs[nr / 16];
+			caddr[ci++] = cs[nr % 16];
+		}
+		return new String(caddr);
 	}
 }
