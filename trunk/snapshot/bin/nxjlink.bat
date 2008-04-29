6820ecedf5889f63cc5cd0971f6249476bb523a1
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2008-04-29 14:07:22 +0000

    NXTCommFactory and usage changes, etc.

--- trunk/snapshot/bin/nxjlink.bat
+++ trunk/snapshot/bin/nxjlink.bat
@@ -4,7 +4,7 @@ if "%NXJ_HOME%" == ""  goto homeless
 set THIRDPARTY_LIBS=%NXJ_HOME%\3rdparty\lib
 set LINK_CLASSPATH=.;%THIRDPARTY_LIBS%\bcel-5.1.jar;%THIRDPARTY_LIBS%\commons-cli-1.0.jar;%NXJ_HOME%\lib\jtools.jar;%NXJ_HOME%\lib\classes.jar
 
-java -classpath "%LINK_CLASSPATH%" js.tinyvm.TinyVM --writeorder LE --classpath ".;%NXJ_HOME%\lib\classes.jar" %*
+java -DCOMMAND_NAME=nxjlink -classpath "%LINK_CLASSPATH%" js.tinyvm.TinyVM --writeorder LE --classpath ".;%NXJ_HOME%\lib\classes.jar" %*
 goto end
 
 :homeless
