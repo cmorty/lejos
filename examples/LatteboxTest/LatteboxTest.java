5ebb728691c98a82d3b354dcf7c9f26791f983f1
esmetaman <esmetaman@06c8f799-c779-4783-8af7-179afedb67c8>
2008-05-05 21:44:01 +0000

    Improved Lattebox support. Now NXTe Supports multiple servos contected with LSC
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@1606 06c8f799-c779-4783-8af7-179afedb67c8

--- examples/LatteboxTest/LatteboxTest.java
+++ examples/LatteboxTest/LatteboxTest.java
@@ -5,6 +5,7 @@ public class LatteboxTest{
 	private static NXTe NXTeObj;
 	private static DebugMessages dm;
 	private static int angle;
+	private static int angle2;
 	private static int motion;
 	
 	//Main
@@ -18,53 +19,52 @@ public class LatteboxTest{
 			NXTeObj = new NXTe(SensorPort.S1);//NXTe Controller pluged in Port1
 			NXTeObj.addLSC(0);
 			dm.echo("Calibrating LSC");			
-			NXTeObj.LSC(0).addServo(0,"SAVOX, Digital SC-0352");
-			NXTeObj.LSC(0).addServo(1,"SAVOX, Digital SC-0352");
+			NXTeObj.LSC(0).addServo(1,"SAVOX, Digital SC-0352");//Servo 1 connected in location 1
+			NXTeObj.LSC(0).addServo(3,"SAVOX, Digital SC-0352");//Servo 2 connected in location 3
 			//NXTeObj.LSC(0).addServo(2,"HITEC, HS-785HB");
 			NXTeObj.LSC(0).calibrate();			
 			dm.echo("Load all servos");
 			NXTeObj.LSC(0).loadAllServos();
-			dm.echo(NXTeObj.LSC(0).Servo(0).getName());
-			NXTeObj.LSC(0).Servo(0).setMinAngle(200);
-			NXTeObj.LSC(0).Servo(0).setMaxAngle(1700);
-			
-			NXTeObj.LSC(0).Servo(0).setDelay(1, 2);
+			NXTeObj.LSC(0).Servo(0).setMinAngle(0);
+			NXTeObj.LSC(0).Servo(0).setMaxAngle(2000);
+			NXTeObj.LSC(0).Servo(1).setMinAngle(0);
+			NXTeObj.LSC(0).Servo(1).setMaxAngle(2000);
+
 			
 			while(!Button.ESCAPE.isPressed()){
 
 				if (Button.LEFT.isPressed()){
-					//NXTeObj.LSC(0).Servo(0).setAngle(1, 400);
-					NXTeObj.LSC(0).Servo(0).goToMinAngle();				
-					while(NXTeObj.LSC(0).Servo(0).isMoving() == true){
-						//dm.echo(NXTeObj.LSC(0).Servo(0).readMotion());
-					}
-					angle = NXTeObj.LSC(0).Servo(0).getAngle(1);
-					
+					NXTeObj.LSC(0).Servo(0).goToMinAngle();
+					NXTeObj.LSC(0).Servo(1).goToMinAngle();
+					while(NXTeObj.LSC(0).Servo(0).isMoving() == true){}
+					angle = NXTeObj.LSC(0).Servo(0).getAngle();
+					angle2 = NXTeObj.LSC(0).Servo(1).getAngle();
 					dm.echo("Goto Min");
 					dm.echo(angle);
 				}
 
 				if (Button.ENTER.isPressed()){
 					NXTeObj.LSC(0).Servo(0).goToMiddleAngle();
-					while(NXTeObj.LSC(0).Servo(0).isMoving() == true){
-						//dm.echo(NXTeObj.LSC(0).Servo(0).readMotion());
-					}
-					angle = NXTeObj.LSC(0).Servo(0).getAngle(1);								
+					NXTeObj.LSC(0).Servo(1).goToMiddleAngle();
+					while(NXTeObj.LSC(0).Servo(0).isMoving() == true){}
+					angle = NXTeObj.LSC(0).Servo(0).getAngle();								
+					angle = NXTeObj.LSC(0).Servo(1).getAngle();
 					
 					dm.echo("Goto Middle");
 					dm.echo(angle);
+					dm.echo(angle2);
 				}
 				
 				if (Button.RIGHT.isPressed()){
-					//NXTeObj.LSC(0).Servo(0).setAngle(1, 2000);
 					NXTeObj.LSC(0).Servo(0).goToMaxAngle();
-					while(NXTeObj.LSC(0).Servo(0).isMoving() == true){
-						//dm.echo(NXTeObj.LSC(0).Servo(0).readMotion());
-					}
-					angle = NXTeObj.LSC(0).Servo(0).getAngle(1);								
+					NXTeObj.LSC(0).Servo(1).goToMaxAngle();
+					while(NXTeObj.LSC(0).Servo(0).isMoving() == true){}
+					angle = NXTeObj.LSC(0).Servo(0).getAngle();	
+					angle = NXTeObj.LSC(0).Servo(1).getAngle();
 					
 					dm.echo("Goto Middle");
 					dm.echo(angle);
+					dm.echo(angle2);
 				}						
 			}
 			
