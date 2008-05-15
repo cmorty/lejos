a44e4972011eff113ddf38c8ac7cd70ee5eed58a
esmetaman <esmetaman@06c8f799-c779-4783-8af7-179afedb67c8>
2008-05-15 21:59:21 +0000

    I have updated the class to use it. I use the original idea from: https://openjdk.dev.java.net/source/browse/*checkout*/openjdk/jdk/trunk/jdk/src/share/classes/

--- trunk/classes/java/lang/RuntimeException.java
+++ trunk/classes/java/lang/RuntimeException.java
@@ -1,5 +1,24 @@
 package java.lang;
 
-public class RuntimeException extends Exception
-{
+public class RuntimeException extends Exception{
+
+    /** Constructs a new runtime exception with <code>null</code> as its
+     * detail message.  The cause is not initialized, and may subsequently be
+     * initialized by a call to {@link #initCause}.
+     */
+    public RuntimeException() {
+        super();
+    }
+
+    /** Constructs a new runtime exception with the specified detail message.
+     * The cause is not initialized, and may subsequently be initialized by a
+     * call to {@link #initCause}.
+     *
+     * @param   message   the detail message. The detail message is saved for
+     *          later retrieval by the {@link #getMessage()} method.
+     */
+    public RuntimeException(String message) {
+        super(message);
+    }
+
 }
