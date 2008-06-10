466a9355d64df0f6749d9a10b2fd191e60b79683
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2008-06-10 15:45:46 +0000

    Moved PC examples to a new pcsamples project.

---
+++ trunk/pcsamples/USBSend/USBSend.java
@@ -0,0 +1,89 @@
+import lejos.pc.comm.*;
+import java.io.*;
+ 
+/**
+ * This is a PC sample. It connects to the NXT, and then
+ * sends an integer and waits for a reply, 100 times.
+ * 
+ * Compile this program with javac (not nxjc), and run it 
+ * with java.
+ * 
+ * You need pccomm.jar on the CLASSPATH and the jlibnxt
+ * DLL or shared library on the Java library path.
+ * 
+ * Run the program by:
+ * 
+ *   java USBSend
+ * 
+ * Your NXT should be running a sample such as USBReceive. 
+ * 
+ * @author Lawrie Griffiths
+ *
+ */
+public class USBSend {
+	
+	public static void main(String[] args) {
+		NXTComm nxtComm = null;
+		try {
+			nxtComm = NXTCommFactory.createNXTComm(NXTCommFactory.USB);
+		} catch (NXTCommException e) {
+			System.out.println("Failure to load comms driver");
+		}
+		
+		NXTInfo[] nxtInfo = null;
+		
+		try {
+			nxtInfo = nxtComm.search(null, NXTCommFactory.USB);
+		} catch (NXTCommException e) {
+			System.out.println("Exception in search");
+		}
+		
+		if (nxtInfo.length == 0) {
+			System.out.println("No NXT Found");
+			System.exit(1);
+		}
+
+		try {
+			nxtComm.open(nxtInfo[0]);
+		} catch (NXTCommException e) {
+			System.out.println("Exception in open");
+		}
+		
+		InputStream is = nxtComm.getInputStream();
+		OutputStream os = nxtComm.getOutputStream();
+		DataInputStream inDat = new DataInputStream(is);
+		DataOutputStream outDat = new DataOutputStream(os);
+		int x = 0;
+		for(int i=0;i<100;i++) 
+		{
+			try {
+			   outDat.writeInt(i);
+			   outDat.flush();
+	
+			} catch (IOException ioe) {
+				System.out.println("IO Exception writing bytes");
+			}
+	         try {x = inDat.readInt();}
+	         catch (IOException ioe) {
+	           System.out.println("IO Exception reading reply");
+	         }            
+	       System.out.println("Sent "+i+ " Received "+x);
+		}
+		
+		try {
+			inDat.close();
+			outDat.close();
+		} catch (IOException ioe) {
+			System.out.println("IO Exception Closing connection");
+		}
+		
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException ie) {}
+		
+		try {
+			nxtComm.close();
+		} catch (IOException ioe) {}
+	}
+
+}
