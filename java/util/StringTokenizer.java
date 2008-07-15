b31c0ffc017ff8975eb4f6a22d9e364a9508990c
esmetaman <esmetaman@06c8f799-c779-4783-8af7-179afedb67c8>
2008-07-15 10:51:49 +0000

    I have uploaded a general class to manage strings, StringTokenizer which uses Enumeration Interface. I have used it to parse GPS GPGGA NMEA Sentences
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk/classes@1695 06c8f799-c779-4783-8af7-179afedb67c8

---
+++ java/util/StringTokenizer.java
@@ -0,0 +1,91 @@
+package java.util;
+//import java.util.*;
+
+/*
+2008/07/15
+I have discovered a bug
+*/
+
+/**
+ * This class has been developed to parse strings with delimiters
+ * 
+ * @author Juan Antonio Breña Moral
+ */
+public class StringTokenizer implements Enumeration{
+	/**
+	 * Delimiter string.
+	 */
+	private String delimiter = ",";
+	private int total;
+	private int currentPosition;
+	private int nextPosition;
+	private boolean detectLastElement;//Detect lastElement
+	private String s;
+
+	/**
+	 * The constructor
+	 * 
+	 * @param s String to be StringTokenizer
+	 */
+	public StringTokenizer(String s) {
+		this.s = s;
+		this.total = s.length();
+		this.currentPosition = 0;
+		this.nextPosition = s.indexOf(delimiter, currentPosition);
+	}
+
+	public StringTokenizer(String s, String _delimiter) {
+		this.delimiter = _delimiter;
+		this.s = s;
+		this.total = s.length();
+		this.currentPosition = 0;
+		this.nextPosition = s.indexOf(delimiter, currentPosition);
+	}
+
+	/**
+	 * Method used to know if exists new tokens
+	 * 
+	 * @return
+	 */
+	public boolean hasMoreTokens(){
+		return ((nextPosition != -1) && (currentPosition <= nextPosition));
+	}
+	
+	/**
+	 * Method implemented by interface requirement
+	 */
+	public boolean hasMoreElements() {
+		return hasMoreTokens();
+	}
+	
+	/**
+	 * 
+	 * @return Next token
+	 * @throws NoSuchElementException If there is no token left
+	 */
+	public String nextToken() throws Exception{// 
+		if (!hasMoreElements()){
+			throw new Exception();//NoSuchElementException
+		}
+
+		String next = s.substring(currentPosition, nextPosition);
+
+		currentPosition = nextPosition + 1;
+		nextPosition = s.indexOf(delimiter, currentPosition);
+
+		return next;
+	}
+
+	/**
+	 * Method implemented by interface requirement
+	 */
+	public Object nextElement(){
+		Object ne = null;
+		try{
+			ne = nextToken();
+		}catch(Exception e){
+			
+		}
+		return ne;
+	}
+}
