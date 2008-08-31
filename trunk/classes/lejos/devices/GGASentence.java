0a850180a3e2ddab60943056eafbee694eb5d649
esmetaman <esmetaman@06c8f799-c779-4783-8af7-179afedb67c8>
2008-08-31 17:36:52 +0000

    I have updated GPS support without latest comments from Charles Manning. I am working on the code. :D

---
+++ trunk/classes/lejos/devices/GGASentence.java
@@ -0,0 +1,157 @@
+package lejos.devices;
+
+import java.util.*;
+
+/**
+ * This class has been designed to manage a GGA Sentence
+ * 
+ * GGA - essential fix data which provide 3D location and accuracy data.
+ * 
+ * $GPGGA,123519,4807.038,N,01131.000,E,1,08,0.9,545.4,M,46.9,M,,*47
+ * 
+ * Where:
+ *      GGA          Global Positioning System Fix Data
+ *      123519       Fix taken at 12:35:19 UTC
+ *      4807.038,N   Latitude 48 deg 07.038' N
+ *      01131.000,E  Longitude 11 deg 31.000' E
+ *      1            Fix quality: 0 = invalid
+ *                                1 = GPS fix (SPS)
+ *                                2 = DGPS fix
+ *                                3 = PPS fix
+ * 			       4 = Real Time Kinematic
+ * 			       5 = Float RTK
+ *                                6 = estimated (dead reckoning) (2.3 feature)
+ * 			       7 = Manual input mode
+ * 			       8 = Simulation mode
+ *      08           Number of satellites being tracked
+ *      0.9          Horizontal dilution of position
+ *      545.4,M      Altitude, Meters, above mean sea level
+ *      46.9,M       Height of geoid (mean sea level) above WGS84
+ *                       ellipsoid
+ *      (empty field) time in seconds since last DGPS update
+ *      (empty field) DGPS station ID number
+ *      *47          the checksum data, always begins with *
+ * 
+ * @author Juan Antonio Brenha Moral
+ */
+public class GGASentence extends NMEASentence{
+	
+	//GGA
+	private String nmeaHeader = "";
+	private float dateTimeOfFix = 0;
+	private float latitude = 0;
+	private String latitudeDirection = "";
+	private float longitude = 0;
+	private String longitudeDirection = "";
+	private String quality;
+	private float satellitesTracked = 0;
+	private float horizontalDilution = 0;
+	private float altitude = 0;
+	private String altitudeUnits;
+	private float geoidalSeparation;
+	private String geoidalSeparationUnit;
+
+	//Header
+	public static final String HEADER = "$GPGGA";
+
+	/*
+	 * GETTERS & SETTERS
+	 */
+
+	/**
+	 * Get Latitude
+	 * 
+	 */
+	public float getLatitude() {
+		return latitude;
+	}
+	
+	/**
+	 * Get Latitude Direction
+	 * 
+	 * @return
+	 */
+	public String getLatitudeDirection(){
+		return latitudeDirection;
+	}
+	
+	/**
+	 * Get Longitude
+	 * 
+	 */
+	public float getLongitude() {
+		return longitude;
+	}
+
+	/**
+	 * Get Longitude Direction
+	 * @return
+	 */
+	public String getLongitudeDirection(){
+		return longitudeDirection;
+	}
+	
+	/**
+	 * Get Altitude
+	 * 
+	 * @return
+	 */
+	public float getAltitude(){
+		return altitude;
+	}
+
+	/**
+	 * Returns the last time stamp retrieved from a satellite
+	 * 
+	 * @return The time as a UTC integer. 123519 = 12:35:19 UTC
+	 */
+	public int getTime(){
+		return Math.round(dateTimeOfFix);
+	}
+	
+	/**
+	 * Returns the number of satellites being tracked to
+	 * determine the coordinates.
+	 * @return Number of satellites e.g. 8
+	 */
+	public int getSatellitesTracked() {
+		return Math.round(satellitesTracked);
+	}
+	
+	/**
+	 * Method used to parse a GGA Sentence
+	 */
+	public void parse(){
+		//StringTokenizer st = new StringTokenizer(nmeaSentence,",");
+		st = new StringTokenizer(nmeaSentence,",");
+		
+		try{
+			nmeaHeader = st.nextToken();//Global Positioning System Fix Data
+			dateTimeOfFix = Float.parseFloat((String)st.nextToken());//UTC Time
+			//latitude = Float.parseFloat((String)(st.nextToken());
+			latitude = degreesMinToDegrees(st.nextToken(),0);
+			latitudeDirection = st.nextToken();//N
+			//longitude = Float.parseFloat((String)st.nextToken());
+			longitude = degreesMinToDegrees(st.nextToken(),1);
+			longitudeDirection = st.nextToken();//E
+			quality = st.nextToken();//Fix quality
+			satellitesTracked = Float.parseFloat((String)st.nextToken());//Number of satellites being tracked
+			//horizontalDilution = Float.parseFloat((String)st.nextToken());//Horizontal dilution of position
+			st.nextToken();
+			altitude = Float.parseFloat((String)st.nextToken());
+		}catch(NoSuchElementException e){
+			//Empty
+		}catch(NumberFormatException e2){
+			//Empty
+		}
+
+		//Improve quality data
+		if (longitudeDirection.equals("E") == false) {
+			longitude = -longitude;
+		}
+		if (latitudeDirection.equals("N") == false) {
+			latitude = -latitude;
+		}
+	}//End parse
+	
+}//End class
---
+++ trunk/classes/lejos/devices/VTGSentence.java
@@ -0,0 +1,75 @@
+package lejos.devices;
+
+import java.util.*;
+
+/**
+ * VTGSentence is a Class designed to manage VTG Sentences from a NMEA GPS Receiver
+ * 
+ * $GPVTG
+ * 
+ * Track Made Good and Ground Speed.
+ * 
+ * eg1. $GPVTG,360.0,T,348.7,M,000.0,N,000.0,K*43
+ * eg2. $GPVTG,054.7,T,034.4,M,005.5,N,010.2,K*41
+ * 
+ *            054.7,T      True course made good over ground, degrees
+ *            034.4,M      Magnetic course made good over ground, degrees
+ *            005.5,N      Ground speed, N=Knots
+ *            010.2,K      Ground speed, K=Kilometers per hour
+ * 
+ * eg3. for NMEA 0183 version 3.00 active the Mode indicator field
+ *      is added at the end
+ *      $GPVTG,054.7,T,034.4,M,005.5,N,010.2,K,A*53
+ *            A            Mode indicator (A=Autonomous, D=Differential,
+ *                         E=Estimated, N=Data not valid)
+ * 
+ * @author Juan Antonio Brenha Moral
+ */
+public class VTGSentence extends NMEASentence{
+
+	//RMC Sentence
+	private String nmeaHeader = "";
+	private float speed = 0;
+
+	//Header
+	public static final String HEADER = "$GPVTG";
+
+	/*
+	 * GETTERS & SETTERS
+	 */
+
+	/**
+	 * Get Speed in Kilometers
+	 * 
+	 * @return
+	 */
+	public float getSpeed(){
+		return speed;  
+	}
+
+	/**
+	 * Parase a RMC Sentence
+	 * 
+	 * $GPVTG,054.7,T,034.4,M,005.5,N,010.2,K,A*53
+	 */
+	public void parse (){
+		st = new StringTokenizer(nmeaSentence,",");
+
+		try{
+			nmeaHeader = st.nextToken();//$GPVTG
+			st.nextToken();//True course made good over ground, degrees
+			st.nextToken();//Letter
+			st.nextToken();//Magnetic course made good over ground
+			st.nextToken();//Letter
+			st.nextToken();//Ground speed, N=Knots
+			st.nextToken();//Letter
+			speed = Float.parseFloat((String)st.nextToken());//Ground speed, K=Kilometers per hour
+			//st.nextToken();//Letter
+		}catch(NoSuchElementException e){
+			//Empty
+		}catch(NumberFormatException e2){
+			//Empty
+		}
+	}//End Parse
+
+}//End Class
