f5911d138797263f138f24d3b4083e077f81a307
esmetaman <esmetaman@06c8f799-c779-4783-8af7-179afedb67c8>
2008-08-31 17:36:52 +0000

    I have updated GPS support without latest comments from Charles Manning. I am working on the code. :D
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk/classes@1766 06c8f799-c779-4783-8af7-179afedb67c8

--- lejos/devices/GPS.java
+++ lejos/devices/GPS.java
@@ -1,21 +1,29 @@
 package lejos.devices;
-
 import java.io.*;
-import lejos.nxt.comm.RConsole;
+import java.util.*;
+import lejos.nxt.*;
 
 /**
- * Class to pull data from a GPS receiver
+ * This class manages a data received from a GPS Device.
+ * GPS Class manages the following NMEA Sentences:
+ * 
+ * GPGGA
+ * GPRMC
+ * GPVTG
+ * 
+ * Processing this NMEA Classes, it is possible receive the following data:
+ * 
+ * Latitude
+ * Longitude
+ * Altitude
+ * Number of Satellites tracked
+ * Time & Date
+ * Speed in Kilometers per Hour
+ * Azimuth
+ * 
  * @author BB
+ * @author Juan Antonio Brenha Moral
  *
- */
-
-/* DEVELOPER NOTES:
- * In general this could be improved by parsing print the appropriate 
- * fields only at the time they are called, rather than parsing them
- * all at once as can be seen in the run() method. 
- * This would save some CPU cycles since it would work to extract
- * data that the user wants, rather than parsing data that ends 
- * up going unused.
  */
 public class GPS extends Thread {
 
@@ -29,93 +37,179 @@ public class GPS extends Thread {
 	private StringBuffer currentSentence = new StringBuffer();
 	
 	private String START_CHAR = "$";
-	private String GGA_STR = "GGA";
 	
 	private InputStream in;
 	
-	private String BLANK = ""; // DELETE ME
-	
-	private int time = 0;
+	//GGA
+	private int RAWtime = 0;
 	private float latitude = 0;
+	private String latitudeDirection = "";
 	private float longitude = 0;
+	private String longitudeDirection = "";
 	private float altitude = 0;
-	private byte satellites_tracked = 0;
+	private int satellitesTracked = 0;
 	
+	//RMC
+	private float speed = 0;
+	private int RAWdate = 0;
+	private String azimuth = "";
+	private String azimuthLetter = "";
+	
+	private String sentence;
+
+	private StringTokenizer tokenizer;
+
+	//Classes which manages GGA, RMC, VTG Sentences
+	private GGASentence ggaSentence;
+	private RMCSentence rmcSentence;
+	private VTGSentence vtgSentence;
+
+	//Date Object with use GGA & RMC Sentence
+	private Date date;
+
+	private boolean shutdown = false;
+	private boolean UPDATE_MODE = false;
+	
+	// Use Vector to keep compatibility with J2ME
+	private Vector listeners = new Vector();
+
 	/**
+	 * The constructor. It needs an InputStream
 	 * 
 	 * @param in An input stream from the GPS receiver
 	 */
 	public GPS(InputStream in) {
+		ggaSentence = new GGASentence();
+		rmcSentence = new RMCSentence();
+		vtgSentence = new VTGSentence();
+
+		date = new Date();
+		
 		this.in = in;
 		this.setDaemon(true); // Must be set before thread starts
 		this.start();
 	}
 	
-	public float getLatitude() {
+
+	/* GETTERS & SETTERS */
+
+	/**
+	 * Get NMEA Sentence
+	 * 
+	 * @return
+	 */
+	public String getSentence(){
+		return sentence;
+	}
+
+	/**
+	 * Get Latitude
+	 * 
+	 * @return
+	 */
+	public synchronized float getLatitude() {
+		notify();
 		return latitude;
 	}
-	
-	public float getLongitude() {
+
+	/**
+	 * Get Latitude Direction
+	 * 
+	 * @return
+	 */
+	public synchronized String getLatitudeDirection(){
+		notify();
+		return latitudeDirection;
+	}
+
+	/**
+	 * Get Longitude
+	 * 
+	 * @return
+	 */
+	public synchronized float getLongitude() {
+		notify();
 		return longitude;
 	}
+
+	/**
+	 * Get Longitude Direction
+	 * 
+	 * @return
+	 */
+	public synchronized String getLongitudeDirection(){
+		notify();
+		return longitudeDirection;
+	}
+
 	
 	/**
 	 * The altitude above mean sea level
-	 * @return Meters above sea level e.g. 545.4     
+	 * 
+	 * @return Meters above sea level e.g. 545.4
 	 */
-	public float getAltitude() {
+	public synchronized float getAltitude(){
+		notify();
 		return altitude;
 	}
-	
-	/**
-	 * Returns the last time stamp retrieved from a satellite
-	 * 
-	 * @return The time as a UTC integer. 123519 = 12:35:19 UTC
-	 */
-	public int getTime() {
-		return time;
-	}
-	
+
 	/**
 	 * Returns the number of satellites being tracked to
 	 * determine the coordinates.
 	 * @return Number of satellites e.g. 8
 	 */
-	public byte getSatellitesTracked() {
-		return satellites_tracked;
+	public synchronized int getSatellitesTracked(){
+		notify();
+		return satellitesTracked;
 	}
+
+	/**
+	 * Get speed in kilometers per hour
+	 * 
+	 * @return
+	 */
+	public synchronized float getSpeed() {
+		notify();
+		return speed;
+	}
+
+	/**
+	 * Return the Azimuth
+	 * 
+	 * @return
+	 */
+	public String getAzimuth() {
+		return azimuth;
+	}
+
+	/**
+	 * Return a Date Object with data from GGA and RMC NMEA Sentence
+	 * 
+	 * @return
+	 */
+	public synchronized Date getDate(){
+		notify();
+		return date;
+	}
+
 	
 	/**
-	 * Placeholder idea: returns heading (from North) based on
-	 * previous lat/long reading.
-	 * @return heading
+	 * Set if GPS Object is going to update internal values or not.
+	 * this method is critic to avoid to Crash VM
+	 * 
+	 * With this way the robot get GPS data onDemand
+	 * 
+	 * @param STATUS
 	 */
-	public int getHeading() {
-		return -1;
+	public void updateValues(boolean STATUS){
+		UPDATE_MODE = STATUS;
 	}
-	
+
 	/**
-	 * Placeholder idea: returns speed based on previous lat/long reading.
-	 * @return speed
+	 * Method used to finish the Thread
 	 */
-	public int getSpeed() {
-		return -1;
-	}
-	
-	public void addGPSListener() {
-		/* Placeholder method */
-	}
-	
-	/**
-	 * Placeholder Idea: Set a latitude/longitude as origin, then
-	 * it will return x, y coordinates (in CM or Inches).
-	 * By default, uses first reading as origin.
-	 * Need to make methods for getX() and getY(), setUnits()
-	 * @param longitude
-	 * @param latitude
-	 */
-	public void setOriginPoint(String longitude, String latitude) {
-		
+	public void shutDown(){
+		this.shutdown = true;
 	}
 	
 	/**
@@ -123,46 +217,59 @@ public class GPS extends Thread {
 	 * This is a daemon thread so when program ends it won't keep running.
 	 */
 	public void run() {
-		/* Code holder for parsing values */
+		String token;
 		
-		while(true) {
+		while(!shutdown) {
 			String s = getNextString();
-			
-			// Check if sentence is valid:
-			if(s.indexOf('*') < 0) { 
-				RConsole.print("Error no * caught!\n");
-				RConsole.print("String: " + s + "\n");
-				continue;
-			}
-			if(s.indexOf('$') < 0) {
-				RConsole.print("Error no $ caught!\n");
-				RConsole.print("String: " + s + "\n");
-				continue;
-			}
-			
-			// Make NMEASentence
-			NMEASentence sen = new NMEASentence(s);
-			RConsole.print("String: " + s + "\n");
-			
-			// Check if valid (discard if it is invalid)
-			if(sen.isValid()) {
-				
-				// Check if contains lat/long data
-				if(sen.getDataType().equals(GGA_STR)) {
-					
-					// Update all global vars
-					//time = Integer.parseInt((String)sen.getDataFields().elementAt(0)); // Convert string to int
-					
-					latitude = Float.parseFloat((String)sen.getDataFields().elementAt(1));
-					longitude = Float.parseFloat((String)sen.getDataFields().elementAt(3));
-					altitude = Float.parseFloat((String)sen.getDataFields().elementAt(7));
-					
-					//satellites_tracked  = (byte)Integer.parseInt((String)sen.getDataFields().elementAt(9));
+
+			//2008/08/02
+			//If update mode is True, internal values can be updated
+			//It is a way to save CPU
+			if(UPDATE_MODE){
+
+				// Check if sentence is valid:
+				if(s.indexOf('*') < 0) { 
+					continue;
 				}
-			// Notify appropriate listeners if data changed
+				if(s.indexOf('$') < 0) {
+					continue;
+				}
+	
+				//2008/07/28
+				//Debug checksum validation
+				//Class 19: java.lang.StringIndexOutOfBoundsException
+				try{
+					if(NMEASentence.isValid(s)){
+						tokenizer = new StringTokenizer(s);
+						token = tokenizer.nextToken();
+
+						if (token.equals(GGASentence.HEADER)){
+							parseGGA(s);
+
+						}else if (token.equals(RMCSentence.HEADER)){
+							parseRMC(s);
+						}else if (token.equals(VTGSentence.HEADER)){
+							parseVTG(s);
+						}
+					}
+				}catch(StringIndexOutOfBoundsException e){
+					//Sound.beep();
+				}catch(ArrayIndexOutOfBoundsException e2){
+					//Jab
+					//Bug detected: 06/08/2008
+					//Sound.buzz();
+				}
+	
+				//2008/07/18
+				//Increase the list with more NMEA Sentences
 			}
 		}
+		
+		//Experimental
+		LCD.drawString("END",0,7);
+		LCD.refresh();
 	}
+
 	
 	/**
 	 * Pulls the next NMEA sentence as a string
@@ -186,7 +293,7 @@ public class GPS extends Thread {
 				done = true;
 			}
 			
-		} while(!done);
+		}while(!done);
 		
 		int endIndex = currentSentence.indexOf(START_CHAR, 1);
 		String sentence = currentSentence.substring(0, endIndex);
@@ -195,5 +302,200 @@ public class GPS extends Thread {
 		currentSentence.delete(0, endIndex);
 		
 		return sentence;
+	}
+
+	/* NMEA */
+
+	/**
+	 * This method parse a GGA Sentence
+	 * 
+	 * @param nmeaSentece
+	 */
+	private void parseGGA(String nmeaSentence){
+
+		ggaSentence.setSentence(nmeaSentence);
+		ggaSentence.parse();
+		
+		this.RAWtime = ggaSentence.getTime();
+		updateTime();
+
+		this.latitude = ggaSentence.getLatitude();
+		this.latitudeDirection = ggaSentence.getLatitudeDirection();
+		this.longitude = ggaSentence.getLongitude();
+		this.longitudeDirection = ggaSentence.getLongitudeDirection();
+		this.satellitesTracked = ggaSentence.getSatellitesTracked();
+		this.altitude = ggaSentence.getAltitude();
+
+		//JSR-179
+		//coor.setLatitude((double)ggaSentence.getLatitude());
+		//coor.setLongitude((double)ggaSentence.getLongitude());
+		//coor.setAltitude((double)ggaSentence.getAltitude());
+
+		//Events
+		fireGGASentenceReceived(ggaSentence);
+	}
+
+	/**
+	 * Update Time values
+	 */
+	private void updateTime(){
+		String rt = Integer.toString(this.RAWtime);
+		int hh;
+		int mm;
+		int ss;
+
+		//LCD.drawString("                        ", 0, 2);
+		if(rt.length()<6){
+			hh = Integer.parseInt(rt.substring(0, 1));
+			mm = Integer.parseInt(rt.substring(1, 3));
+			ss = Integer.parseInt(rt.substring(3, 5));
+		}else{
+			hh = Integer.parseInt(rt.substring(0, 2));
+			mm = Integer.parseInt(rt.substring(2, 4));
+			ss = Integer.parseInt(rt.substring(4, 6));
+		}
+
+		//updateTimeValues(hh, mm, ss);
+		date.setHours(hh);
+		date.setMinutes(mm);
+		date.setSeconds(ss);
+		
+		//p.setTimeStamp(this.RAWtime);
+	}
+
+	/**
+	 * This method parse a RMC Sentence
+	 * 
+	 * @param nmeaSentece
+	 */
+	private void parseRMC(String nmeaSentence){
+		rmcSentence.setSentence(nmeaSentence);
+		rmcSentence.parse();
+		
+		//Charles Manning notes
+		//Is better use VTG instead of RMC
+		//this.speed = rmcSentence.getSpeed();
+		this.RAWdate = rmcSentence.getDate();
+		
+		updateDate();
+
+		this.azimuth = rmcSentence.getAzimuth();
+		//this.azimuthLetter = rmcSentence.getAzimuthLetter();
+		
+		//Events
+		fireRMCSentenceReceived(rmcSentence);
+	}
+
+	/**
+	 * Update Date values
+	 */
+	private void updateDate(){
+		String rd = Integer.toString(this.RAWdate);
+		int yy;
+		int mm;
+		int dd;
+
+		//LCD.drawString("                        ", 0, 2);
+		if(rd.length()<6){
+			dd = Integer.parseInt(rd.substring(0, 1));
+			mm = Integer.parseInt(rd.substring(1, 3));
+			yy = Integer.parseInt(rd.substring(3, 5));
+		}else{
+			dd = Integer.parseInt(rd.substring(0, 2));
+			mm = Integer.parseInt(rd.substring(2, 4));
+			yy = Integer.parseInt(rd.substring(4, 6));
+		}
+
+		//updateDateValues(dd,mm,yy);
+		date.setDay(dd);
+		date.setMonth(mm);
+		date.setYear(yy);
+	}
+
+	/**
+	 * This method parse a VTG Sentence
+	 * 
+	 * @param nmeaSentece
+	 */
+	private void parseVTG(String nmeaSentence){
+		vtgSentence.setSentence(nmeaSentence);
+		vtgSentence.parse();
+		
+		this.speed = vtgSentence.getSpeed();
+
+		//Events
+		fireVTGSentenceReceived (vtgSentence);
+	}
+
+	/* EVENTS*/
+
+	/**
+	 * add a listener to manage events with GPS
+	 * 
+	 * @param listener
+	 */
+	public void addListener (GPSListener listener){
+		listeners.addElement(listener); 
+	}
+
+	/**
+	 * Remove a listener
+	 * 
+	 * @param listener
+	 */
+	public void removeListener (GPSListener listener)
+	{
+		listeners.removeElement(listener); 
+	}
+
+	/**
+	 * Method which is used when system parse a GGA Sentence
+	 * 
+	 * @param ggaSentence
+	 */
+	private void fireGGASentenceReceived (GGASentence ggaSentence){
+		GPSListener GPSL;
+		for(int i=0; i<listeners.size();i++){
+			try{
+				GPSL = (GPSListener)listeners.elementAt(i);
+				GPSL.ggaSentenceReceived(this, ggaSentence);
+			}catch(Throwable t){
+
+			}
+		}
+	}
+
+	/**
+	 * Method which is used when system parse a RMC Sentence
+	 * 
+	 * @param rmcSentence
+	 */
+	private void fireRMCSentenceReceived (RMCSentence rmcSentence){
+		GPSListener GPSL;
+		for(int i=0; i<listeners.size();i++){
+			try{
+				GPSL = (GPSListener)listeners.elementAt(i);
+				GPSL.rmcSentenceReceived(this, rmcSentence);
+			}catch(Throwable t){
+
+			}
+		}
+	}
+
+	/**
+	 * Method which is used when system parse a VTG Sentence
+	 * 
+	 * @param VTGSentence
+	 */
+	private void fireVTGSentenceReceived (VTGSentence vtgSentence){
+		GPSListener GPSL;
+		for(int i=0; i<listeners.size();i++){
+			try{
+				GPSL = (GPSListener)listeners.elementAt(i);
+				GPSL.vtgSentenceReceived(this, vtgSentence);
+			}catch(Throwable t){
+
+			}
+		}
 	}
 }
---
+++ lejos/devices/GPSListener.java
@@ -0,0 +1,18 @@
+package lejos.devices;
+
+import java.util.*;
+
+/**
+ * This is the interface to manage events with GPS
+ * 
+ * @author Juan Antonio Brenha Moral
+ *
+ */
+public interface GPSListener extends EventListener{
+	
+	public void ggaSentenceReceived (GPS gpsReceiver, GGASentence ggaSentence);
+	
+	public void rmcSentenceReceived (GPS gpsReceiver, RMCSentence rmcSentence);
+
+	public void vtgSentenceReceived (GPS gpsReceiver, VTGSentence vtgSentence);
+}
