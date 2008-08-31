84ad95c4f344b24a8c16cf1b69b7db60a015db81
esmetaman <esmetaman@06c8f799-c779-4783-8af7-179afedb67c8>
2008-08-31 17:36:52 +0000

    I have updated GPS support without latest comments from Charles Manning. I am working on the code. :D
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@1766 06c8f799-c779-4783-8af7-179afedb67c8

--- classes/lejos/devices/NMEASentence.java
+++ classes/lejos/devices/NMEASentence.java
@@ -1,145 +1,89 @@
-package lejos.devices;
-
-import java.util.Vector;
+import java.util.*;
 
 /**
- * An NMEA sentence consists of (in order) a '$' character, some 
- * header information, many data fields seperated by ',' a '*' symbol
- * at the end, and a final hexidecimal checksum number (two chars = one byte).
- * e.g. $GPGGA,140819.000,4433.2984,N,08056.3959,W,6,00,50.0,168.7,M,-36.0,M,,0000*53
- * The first two chars (GP) are the talker id
- * The next 3 characters (GGA) are the sentence id
+ * Class designed to manage all NMEA Sentence.
+ * 
+ * GGA and RMC Sentence needs to validate data.
+ * This class has methods to validate receivedad data
+ * 
  * @author BB
+ * @author Juan Antonio Brenha Moral
  *
  */
-
-// TO DO:
-// 1. GGA sentences
-// 2. GLL sentences? (none by Holux)
-// 3. GSA sentences (satellite info)
-// 4. GSV sentences (detailed sat info)
-// 5. VTG (velocity)
-// 6. RMC (time and date, etc..)
-// OPTION: Instead of refreshing all values at once in refreshVals()
-// could get rid of setSentence() and refresh each only when called.
 public class NMEASentence {
 
-	private String sentence;
-	
-	private String prefix; // GP = standard NMEA sentence, others are LC, PS, HC, PG 
-	private String dataType;
-	private byte checksum;
-	
-	private Vector fields = null;
-	
-	private String COMMA = ",";
-	private String BLANK = "";
-	
-	public NMEASentence(String sentence) {
-		setSentence(sentence);
-	}
-	
-	public void setSentence(String sentence) {
-		this.sentence = sentence;
-		refreshVals();
-	}
+	static byte checksum;
+	static protected String nmeaSentence = "";
+	protected StringTokenizer st;
+
+	/* GETTERS & SETTERS */
 	
 	/**
-	 * Returns the prefix of the NMEA sentence. e.g. "GP" 
-	 * @return prefix
+	 * Set a new nmea sentence into the object
+	 * 
+	 * @param sentence
 	 */
-	public String getPrefix() {
-		return prefix;
+	public void setSentence(String sentence){
+		nmeaSentence = sentence;
 	}
+
+	private static byte getChecksum() {
+		return checksum;
+	}
+
+	/* CHECKSUM METHODS */
 	
 	/**
-	 * Returns the data type of the NMEA sentence. e.g. "GGA" 
-	 * @return  data type
+	 * Return if your NMEA Sentence is valid or not
+	 * 
+	 * @return
 	 */
-	public String getDataType() {
-		return dataType;
+	static public boolean isValid(){
+		int end = nmeaSentence.indexOf('*');
+		String checksumStr = nmeaSentence.substring(end + 1, end + 3);
+		checksum = convertChecksum(checksumStr);
+		return(getChecksum() == calcChecksum());
 	}
-	
-	public Vector getDataFields() {
-		return fields;
-	}
-	
+
 	/**
-	 * Compares the checksum values to see if this is a corrupt sentence
-	 * @return true iff the checksum is valid
+	 * Return if your NMEA Sentence is valid or not
+	 * 
+	 * @param sentence
+	 * @return
 	 */
-	public boolean isValid() {
+	static public boolean isValid(String sentence){
+		nmeaSentence = sentence;
+		int end = nmeaSentence.indexOf('*');
+		String checksumStr = nmeaSentence.substring(end + 1, end + 3);
+		checksum = convertChecksum(checksumStr);
 		return(getChecksum() == calcChecksum());
 	}
 	
-	public byte getChecksum() {
-		return checksum;
-	}
-	
-	public byte calcChecksum() {
-		int start = sentence.indexOf('$');
-		int end = sentence.indexOf('*');
-		if(end < 0) {
-			end = sentence.length();
-		}
-		byte checksum = (byte) sentence.charAt(start + 1);
-		for (int index = start + 2; index < end; ++index) {
-			checksum ^= (byte) sentence.charAt(index);
-		}
-		return checksum;
-	}
-	
+
 	/**
-	 * This method now checks for two things (thanks Janusz):
-	 * 1. Check if final * is present.
-	 * 2. Check if sentence is at least 6 chars long (req header info)
-	 * Note: Currently returns if error.
-	 * Unsure if returning without values will cause problems elsewhere.
+	 * Method designed to calculate a checksum using data
+	 * 
+	 * @return
 	 */
-	private void refreshVals() {
-		
-		// ** POTENTIAL BUGFIX 1:
-		//if(sentence.length() < 6) return; // See if has minimum length
-		prefix = sentence.substring(1, 3); // Skip over '$'
-		dataType = sentence.substring(3, 6);
-		int end = sentence.indexOf('*');
-		// ** POTENTIAL BUGFIX 2:
-		//if(end < 0) return; // -1 indicates no * in string
- 
-		String checksumStr = sentence.substring(end + 1, end + 3);
-		
-		fields = extractDataFields();
-		
-		checksum = convertChecksum(checksumStr);
+	private static byte calcChecksum() {
+		int start = nmeaSentence.indexOf('$');
+		int end = nmeaSentence.indexOf('*');
+		if(end < 0) {
+			end = nmeaSentence.length();
+		}
+		byte checksum = (byte) nmeaSentence.charAt(start + 1);
+		for (int index = start + 2; index < end; ++index) {
+			checksum ^= (byte) nmeaSentence.charAt(index);
+		}
+		return checksum;
 	}
-	
-	private Vector extractDataFields() {
-		Vector df = new Vector(15);
-		int end = sentence.indexOf('*');
-		// Find index of first ','
-		int firstIndex = sentence.indexOf(',');
-		int nextIndex = 0;
-		
-		do {
-			// Find index of next starting past first index','
-			nextIndex = sentence.indexOf(',', firstIndex+1);
-			// If nextIndex = -1 then use end (*) as nextIndex
-			if(nextIndex == -1) nextIndex = end;
-			// Substring the string in between them
-			String dataField = null;
-			if(sentence.substring(firstIndex+1) == COMMA)
-				dataField = BLANK;
-			else
-				dataField = sentence.substring(firstIndex+1, nextIndex);
-			// Add to fields vector
-			df.addElement(dataField);
-			
-			firstIndex = nextIndex;
-		} while(nextIndex != end);
-		
-		return df;
-	}
-	
+
+	/**
+	 * Method used to create a checksum with String
+	 * 
+	 * @param checksum_string
+	 * @return
+	 */
 	private static byte convertChecksum(String checksum_string) {
 		byte checksum;
 		checksum = (byte)((hexCharToByte(checksum_string.charAt(0)) & 0xF ) << 4 );
@@ -158,5 +102,74 @@ public class NMEASentence {
 		  return((byte)(hex_char - 55));
 	  else
 		  return((byte)(hex_char - 48));
-	  }
+	}
+	
+	/* UTILITIES METHODS */
+
+	/**
+	 * Any GPS Receiver gives Lat/Lon data in the following way:
+	 * 
+	 * http://www.gpsinformation.org/dale/nmea.htm
+	 * http://www.teletype.com/pages/support/Documentation/RMC_log_info.htm
+	 * 
+	 * 4807.038,N   Latitude 48 deg 07.038' N
+	 * 01131.000,E  Longitude 11 deg 31.000' E
+	 * 
+	 * This data is necessary to convert to Decimal Degrees.
+	 * 
+	 * Latitude values has the range: -90 <-> 90
+	 * Longitude values has the range: -180 <-> 180
+	 * 
+	 * @param DD_MM
+	 * @param CoordenateType
+	 * @return
+	 */
+	protected float degreesMinToDegrees(String DD_MM,int CoordenateType) {//throws NumberFormatException
+		float decDegrees = 0;
+		float degrees = 0;
+		float minutes = 0;
+		float seconds = 0;
+		String doubleCharacterSeparator = ".";
+		String DDMM;
+
+		//1. Count characters until character '.'
+		int dotPosition = DD_MM.indexOf(doubleCharacterSeparator);
+		if(CoordenateType == 0){
+			//Latitude Management
+			DDMM = DD_MM.substring(0, dotPosition);
+			if(DDMM.length() == 4){
+				degrees = Float.parseFloat(DDMM.substring(0, 2));
+				minutes = Float.parseFloat(DD_MM.substring(2));
+			}else if(DDMM.length() == 3){
+				degrees = Float.parseFloat(DDMM.substring(0, 1));
+				minutes = Float.parseFloat(DD_MM.substring(1));
+			}
+
+			if((degrees >=0) && (degrees <=90)){
+				//throw new NumberFormatException();
+			}
+		}else{
+			//Latitude Management
+			DDMM = DD_MM.substring(0, dotPosition);
+			if(DDMM.length() == 5){
+				degrees = Float.parseFloat(DDMM.substring(0, 3));
+				minutes = Float.parseFloat(DD_MM.substring(3));
+			}else if(DDMM.length() == 4){
+				degrees = Float.parseFloat(DDMM.substring(0, 2));
+				minutes = Float.parseFloat(DD_MM.substring(2));
+			}else if(DDMM.length() == 3){
+				degrees = Float.parseFloat(DDMM.substring(0, 1));
+				minutes = Float.parseFloat(DD_MM.substring(1));
+			}
+
+			if((degrees >=0) && (degrees <=180)){
+				//throw new NumberFormatException();
+			}
+		}
+		//Idea
+		//http://id.mind.net/%7Ezona/mmts/trigonometryRealms/degMinSec/degMinSec.htm
+		decDegrees = (float)(degrees + (minutes * (1.0 / 60.0)) + (seconds * (1.0 / 3600.0)));
+		
+		return decDegrees;
+	}
 }
---
+++ classes/lejos/devices/RMCSentence.java
@@ -0,0 +1,147 @@
+package lejos.devices;
+
+import java.util.*;
+
+/**
+ * RMC is a Class designed to manage RMC Sentences from a NMEA GPS Receiver
+ * 
+ * RMC - NMEA has its own version of essential gps pvt (position, velocity, time) data. It is called RMC, The Recommended Minimum, which will look similar to:
+ * 
+ * $GPRMC,123519,A,4807.038,N,01131.000,E,022.4,084.4,230394,003.1,W*6A
+ * 
+ * Where:
+ *      RMC          Recommended Minimum sentence C
+ *      123519       Fix taken at 12:35:19 UTC
+ *      A            Status A=active or V=Void.
+ *      4807.038,N   Latitude 48 deg 07.038' N
+ *      01131.000,E  Longitude 11 deg 31.000' E
+ *      022.4        Speed over the ground in knots
+ *      084.4        Track angle in degrees True
+ *      230394       Date - 23rd of March 1994
+ *      003.1,W      Magnetic Variation
+ *      *6A          The checksum data, always begins with *
+ * 
+ * @author Juan Antonio Brenha Moral
+ */
+public class RMCSentence extends NMEASentence{
+
+	//RMC Sentence
+	private String nmeaHeader = "";
+	private float dateTimeOfFix = 0;
+	private String warning = "";
+	private float latitude = 0;
+	private String latitudeDirection = "";
+	private float longitude = 0;
+	private String longitudeDirection = "";
+	private float groundSpeed;//In knots
+	private String courseMadeGood;
+	private float dateOfFix = 0;
+	private String magneticVariation = "";
+	//private String magneticVariationLetter = "";
+
+	private float speed;//In Kilometers per hour
+
+	//Header
+	public static final String HEADER = "$GPRMC";
+
+	/*
+	 * GETTERS & SETTERS
+	 */
+	
+	/**
+	 * Get Latitude
+	 * 
+	 */
+	public float getLatitude(){
+		return latitude;  
+	}
+
+	/**
+	 * Get Longitude
+	 * 
+	 * @return
+	 */
+	public float getLongitude(){
+		return longitude;
+	}
+
+	/**
+	 * Get Speed in Kilometers
+	 * 
+	 * @return
+	 */
+	public float getSpeed(){
+		return speed;  
+	}
+
+	/**
+	 * Get date in integer format
+	 * 
+	 * @return
+	 */
+	public int getDate(){
+		return Math.round(dateOfFix);
+	}
+
+	/**
+	 * Returns the azimuth of the current direction of the movement. So if the gps device is still,
+	 * the value will most likely random. Returns 0 if cannot be determined yet.
+	 *
+	 * @return The azimuth in degrees.
+	 */
+	public String getAzimuth() {
+		return magneticVariation;
+	}
+
+	/**
+	 * Parase a RMC Sentence
+	 * 
+	 * $GPRMC,081836,A,3751.65,S,14507.36,E,000.0,360.0,130998,011.3,E*62
+	 */
+	public void parse (){
+		//StringTokenizer st = new StringTokenizer(nmeaSentence,",");
+		st = new StringTokenizer(nmeaSentence,",");
+
+		try{
+			nmeaHeader = (String)st.nextToken();//$GPRMC
+			dateTimeOfFix = Float.parseFloat((String)st.nextToken());
+			warning = (String)st.nextToken();
+			//latitude = Float.parseFloat((String)(st.nextToken());
+			latitude = degreesMinToDegrees(st.nextToken(),0);
+			latitudeDirection = (String)st.nextToken();
+			//longitude = Float.parseFloat((String)st.nextToken());
+			longitude = degreesMinToDegrees(st.nextToken(),1);
+			longitudeDirection = (String)st.nextToken();
+			String s = st.nextToken();
+			groundSpeed = s.equals("") ? 0 : Float.parseFloat(s);
+			courseMadeGood = (String)st.nextToken();
+			dateOfFix = Float.parseFloat((String)st.nextToken());
+			magneticVariation = (String)st.nextToken();//Float.parseFloat((String)st.nextToken());
+			//magneticVariationLetter = (String)st.nextToken();
+		}catch(NoSuchElementException e){
+			//Empty
+		}catch(NumberFormatException e2){
+			//Empty
+		}
+
+		//Improve quality data
+		if (longitudeDirection.equals("E") == false) {
+			longitude = -longitude;
+		}
+		if (latitudeDirection.equals("N") == false) {
+			latitude = -latitude;
+		}
+
+		//Speed
+		if (groundSpeed > 0) {
+			// km/h = knots * 1.852
+			speed = (float) ((groundSpeed) * 1.852);
+		}
+		// A negative speed doesn't make sense.
+		if (speed < 0) {
+			speed = 0;
+		}
+
+	}//End Parse
+
+}//End Class
