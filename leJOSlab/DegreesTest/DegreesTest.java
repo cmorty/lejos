d7cb4f34543acb63f85f47eeb07ad12c12081263
esmetaman <esmetaman@06c8f799-c779-4783-8af7-179afedb67c8>
2008-09-07 21:41:59 +0000

    I have created this folder on SVN to exchange ideas and test between leJOS developers to tests "Beta software". In this case, the first example is about latest GPS Objects (Latitude,Longitude). the content is temporal.
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@1852 06c8f799-c779-4783-8af7-179afedb67c8

---
+++ leJOSlab/DegreesTest/DegreesTest.java
@@ -0,0 +1,48 @@
+import lejos.util.*;
+import lejos.gps.*;
+
+/**
+ * @author Juan Antonio Brenha Moral
+ *
+ */
+public class DegreesTest{
+	private static DebugMessages dm;
+	private static double latitudeExample = 4807.038;
+	private static double longitudeExample = 01131.000;
+	private static Latitude lat;
+	private static Longitude lon;
+	
+	public static void main(String[] args){
+		dm = new DebugMessages();
+		dm = new DebugMessages();
+		dm.setLCDLines(7);
+		dm.setDelay(500);
+		dm.setDelayEnabled(true);
+
+		lat = new Latitude(latitudeExample);
+		lon = new Longitude(longitudeExample);
+		dm.echo("RAW Lat:" + latitudeExample);
+		dm.echo("DD  Lat:" + lat.getDecimalDegrees());
+		dm.echo("L   Lat:" + lat.getDirectionLeter());
+		dm.echo("");
+		dm.echo("DMS Lat:");
+		dm.echo("D   Lat:" + lat.getDegrees() + "º");
+		dm.echo("M   Lat:" + lat.getMinutes() + "'");
+		dm.echo("S   Lat:" + lat.getSeconds() + "''");
+		try {Thread.sleep(10000);} catch (Exception e) {}
+		
+		dm.clear();
+		credits();
+	}
+
+	private static void credits(){
+		dm.echo("");
+		dm.echo("LEGO Mindstorms");
+		dm.echo("NXT Robots  ");
+		dm.echo("run better with");
+		dm.echo("Java leJOS");
+		dm.echo("");
+		dm.echo("www.lejos.org");
+		try {Thread.sleep(20000);} catch (Exception e) {}
+	}
+}
