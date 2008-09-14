f8bc3d2625adfa65157b41aadc9721b9306fdd29
esmetaman <esmetaman@06c8f799-c779-4783-8af7-179afedb67c8>
2008-09-14 20:05:07 +0000

    I have uploaded latest BTGPS Example with latest features:
    + Better navigation system among the Screens.
    + Show Satellite Data (GSV,GSA)
    + Compass Data, if you have. (Runs but I don't have a GPS with Compass) -> For the moment experimental.

--- trunk/examples/BTGPS/BTGPS.java
+++ trunk/examples/BTGPS/BTGPS.java
@@ -5,7 +5,7 @@ import lejos.util.Stopwatch;
 import java.util.*;
 import java.io.*;
 import javax.bluetooth.*;
-import javax.microedition.location.*;
+//import javax.microedition.location.*;
 import lejos.gps.*;
 
 /**
@@ -15,19 +15,25 @@ import lejos.gps.*;
  * + Get Data from GGA NMEA Sentence
  * + Get Data from RMC NMEA Sentence
  * + Get Data from VTG NMEA Sentence
- * + Use JRS-179 Objects
+ * + Get Data from GSV NMEA Sentence
+ * + Get Data from GSA NMEA Sentence
  * + Use Date Objects with leJOS
  * 
  * This example is experimental. It is necessary to test more time
  * 
- * Click on left and right button to show GGA, RMC & VTG Data
+ * Click on left and right button to show to show more data about GPS.
+ * 
+ * Usage Notes:
+ * Please, wait 20 seconds until GPS receiver get all data.
+ * Sometimes, if you read GSV/GSA without this waiting, it is possible that
+ * you see a exception.
  * 
  * @author BB
  * @author Juan Antonio Brenha Moral
  */
 public class BTGPS{
 	private static String appName = "GPS";
-	private static String appVersion = "v6.3";
+	private static String appVersion = "v6.7";
 
 	//Inquire code
 	private static byte[] cod = {0,0,0,0}; // 0,0,0,0 picks up every Bluetooth device regardless of Class of Device (cod).
@@ -40,10 +46,9 @@ public class BTGPS{
 	//GPS Pin
 	private static final byte[] pin = {(byte) '0', (byte) '0', (byte) '0', (byte) '0'};
 
-	//private static BTGPSGUI GUIObj;
 	private static Date date;
-	private static Coordinates coor;
-	
+	private static NMEASatellite ns;
+
 	public static void main(String[] args) {
 
 		//Detect GPS Device
@@ -70,11 +75,11 @@ public class BTGPS{
 			LCD.drawString("No detected GPS", 0, 3);
 			LCD.refresh();
 		}
-		
-		credits();
+		try {Thread.sleep(2000);} catch (Exception e) {}
+		//credits(3);
 		System.exit(0);
 	}//End main
-
+	
 	/**
 	 * This method, show all BT Devices with BT Services enable
 	 * User choose a GPS device to connect
@@ -169,19 +174,23 @@ public class BTGPS{
 	 */
 	static void showData(){
 		LCD.clear();
-		Stopwatch sw;
-
 		int sentenceCount = 0;
-		boolean flag = true;
+		
+		Stopwatch sw;
 		sw = new Stopwatch();
-		int CMD = 1;
+
+		boolean flag = true;
 		int GPSDataQuality = 0;
+		
+		//Circular System
+		int GPSScreens = 8;
+		int GPSCurrentScreen = 1;
 
 		LCD.drawString(appName + " " + appVersion, 0,0);
+
 		while(!Button.ESCAPE.isPressed()){
 			LCD.drawInt(++sentenceCount, 10, 0);
 			GPSDataQuality = gps.getSatellitesTracked();
-			//LCD.drawInt((int)Runtime.getRuntime().freeMemory(), 0, 0);
 
 			if(sw.elapsed() >= 10000){
 				sw.reset();
@@ -193,46 +202,81 @@ public class BTGPS{
 			}
 			
 			date = gps.getDate();
-			coor = new Coordinates(gps.getLatitude(),gps.getLongitude(),gps.getAltitude());
 
+			//Circular System
 			if (Button.LEFT.isPressed()){
-				CMD = 1;
-			}
-
-			if (Button.ENTER.isPressed()){
-				CMD = 2;
+				if(GPSCurrentScreen == 1){
+					GPSCurrentScreen = GPSScreens;
+				}else{
+					GPSCurrentScreen--;
+				}
 			}
 
 			if (Button.RIGHT.isPressed()){
-				CMD = 3;
+				if(GPSCurrentScreen == GPSScreens){
+					GPSCurrentScreen = 1;
+				}else{
+					GPSCurrentScreen++;
+				}
+			}
+
+			//Reset
+			if (Button.ENTER.isPressed()){
+				GPSCurrentScreen  =1;
+			}
+
+			if(GPSCurrentScreen==1){
+				showGGAUIBasic();
+			}else if(GPSCurrentScreen == 2){
+				showGGAUIAdvanced();
+			}else if(GPSCurrentScreen == 3){
+				showRMCUI();
+			}else if(GPSCurrentScreen == 4){
+				showVTGUI();
+			}else if(GPSCurrentScreen == 5){
+				showGPSTimeUI();
+			}else if(GPSCurrentScreen == 6){
+				showSatTableUI();
+			}else if(GPSCurrentScreen == 7){
+				showSatUI();
+			}else if(GPSCurrentScreen == 8){
+				showSatIDUI();
 			}
 			
-			if(CMD==1){
-				showGGAUI();
-			}else if(CMD == 2){
-				showRMCUI();
-			}else{
-				showVTGUI();
-			}
+			
+			
 
 			LCD.refresh();
 			try {Thread.sleep(1000);} catch (Exception e) {}
 		}
 	}
-	
+
 	/**
-	 * Show GGA Data from GPS
+	 * Show GGA Basic Data from GPS
 	 */
-	private static void showGGAUI(){
-		LCD.drawString("GGA", 0, 2);
+	private static void showGGAUIBasic(){
+		refreshSomeLCDLines();
+		LCD.drawString("GGA Basic", 0, 2);
 
 		LCD.drawString("Tim " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + "", 0, 3);
-		LCD.drawString("Lat " + coor.getLatitude(), 0, 4);
+		LCD.drawString("Lat " + gps.getLatitude().getDecimalDegrees(), 0, 4);
 		LCD.drawString("" + gps.getLatitudeDirection() , 15, 4);
-		LCD.drawString("Lon " + coor.getLongitude(), 0, 5);
+		LCD.drawString("Lon " + gps.getLongitude().getDecimalDegrees(), 0, 5);
 		LCD.drawString("" + gps.getLongitudeDirection() , 15, 5);
-		LCD.drawString("Alt " + coor.getAltitude(), 0, 6);
-		LCD.drawString("Sat " + gps.getSatellitesTracked(), 0, 7);
+		LCD.drawString("Alt " + gps.getAltitude(), 0, 6);
+		LCD.refresh();
+	}
+
+	/**
+	 * Show GGA Advanced Data from GPS
+	 */
+	private static void showGGAUIAdvanced(){
+		refreshSomeLCDLines();
+		LCD.drawString("GGA Advanced", 0, 2);
+		
+		LCD.drawString("Sat  " + gps.getSatellitesTracked(), 0, 3);
+		LCD.drawString("HDOP " + gps.getGGAHDOP(), 0, 4);
+		LCD.drawString("QOS  " + gps.getQuality(), 0, 5);
 		LCD.refresh();
 	}
 
@@ -240,10 +284,12 @@ public class BTGPS{
 	 * Show RMC Data from GPS
 	 */
 	private static void showRMCUI(){
-		LCD.drawString("RMC", 0, 2);
 		refreshSomeLCDLines();
+		LCD.drawString("RMC", 0, 2);
+		
 		LCD.drawString("Dat " + date.getDay() + "/" + date.getMonth() + "/" + date.getYear() + "", 0, 3);
 		LCD.drawString("Azi " + gps.getAzimuth(), 0, 4);
+		LCD.drawString("Com " + gps.getCompassDegrees(), 0, 5);
 		LCD.refresh();
 	}
 
@@ -251,17 +297,102 @@ public class BTGPS{
 	 * Show VTG Data from GPS
 	 */
 	private static void showVTGUI(){
-		LCD.drawString("VTG", 0, 2);
 		refreshSomeLCDLines();
+		LCD.drawString("VTG", 0, 2);
+		
 		LCD.drawString("Spe " + gps.getSpeed(), 0, 3);
 		LCD.refresh();
 	}
-	
+
+	/**
+	 * Show Time Data from GPS
+	 */
+	private static void showGPSTimeUI(){
+		refreshSomeLCDLines();
+		LCD.drawString("GPS Time data", 0, 2);
+
+		LCD.drawString("Tim " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + "", 0, 3);
+		LCD.drawString("Dat " + date.getDay() + "/" + date.getMonth() + "/" + date.getYear() + "", 0, 4);
+		LCD.refresh();
+	}
+
+	private static void showSatTableUI(){
+		refreshSomeLCDLines();
+		LCD.drawString("Sat Table", 0, 2);
+		
+		
+		NMEASatellite ns1 = gps.getSatellite(0);
+		NMEASatellite ns2 = gps.getSatellite(1);
+		NMEASatellite ns3 = gps.getSatellite(2);
+		NMEASatellite ns4 = gps.getSatellite(3);
+		
+		LCD.drawString(" PRN Ele Azi SRN",0,3);
+		LCD.drawString("1" + ns1.getPRN(),0,4);
+		LCD.drawString("2" + ns2.getPRN(),0,5);
+		LCD.drawString("3" + ns3.getPRN(),0,6);
+		LCD.drawString("4" + ns4.getPRN(),0,7);
+		LCD.drawString("" + ns1.getElevation(),5,4);
+		LCD.drawString("" + ns2.getElevation(),5,5);
+		LCD.drawString("" + ns3.getElevation(),5,6);
+		LCD.drawString("" + ns4.getElevation(),5,7);
+		LCD.drawString("" + ns1.getAzimuth(),9,4);
+		LCD.drawString("" + ns2.getAzimuth(),9,5);
+		LCD.drawString("" + ns3.getAzimuth(),9,6);
+		LCD.drawString("" + ns4.getAzimuth(),9,7);
+		LCD.drawString("" + ns1.getSNR(),13,4);
+		LCD.drawString("" + ns2.getSNR(),13,5);
+		LCD.drawString("" + ns3.getSNR(),13,6);
+		LCD.drawString("" + ns4.getSNR(),13,7);
+		LCD.refresh();
+	}
+
+	/**
+	 * Show Sat Data
+	 */
+	private static void showSatUI(){
+		refreshSomeLCDLines();
+		LCD.drawString("Sat data", 0, 2);
+
+		LCD.drawString("Mode1 " + gps.getMode1(), 0, 3);
+		LCD.drawString("Mode2 " + gps.getMode2(), 0, 4);
+		LCD.drawString("PDOP  " + gps.getPDOP(), 0, 5);
+		LCD.drawString("HDOP  " + gps.getHDOP(), 0, 6);
+		LCD.drawString("VDOP  " + gps.getVDOP(), 0, 7);
+		LCD.refresh();
+	}
+
+	/**
+	 * Show Sat ID
+	 */
+	private static void showSatIDUI(){
+		refreshSomeLCDLines();
+		LCD.drawString("Satellite Table", 0, 2);
+
+		int SV[] = gps.getSV();
+		
+		int cols[] = {0,5,9,13};
+		int rows[] = {3,4,5};
+
+		LCD.drawString("" + SV[0], cols[0], rows[0]);
+		LCD.drawString("" + SV[1], cols[1], rows[0]);
+		LCD.drawString("" + SV[2], cols[2], rows[0]);
+		LCD.drawString("" + SV[3], cols[3], rows[0]);
+		LCD.drawString("" + SV[4], cols[0], rows[1]);
+		LCD.drawString("" + SV[5], cols[1], rows[1]);
+		LCD.drawString("" + SV[6], cols[2], rows[1]);
+		LCD.drawString("" + SV[7], cols[3], rows[1]);
+		LCD.drawString("" + SV[8], cols[0], rows[2]);
+		LCD.drawString("" + SV[9], cols[1], rows[2]);
+		LCD.drawString("" + SV[10], cols[2], rows[2]);
+		LCD.drawString("" + SV[11], cols[3], rows[2]);
+		LCD.refresh();
+	}
+
 	/**
 	 * Clear some LCD lines
 	 */
 	private static void refreshSomeLCDLines(){
-		//LCD.drawString("                     ", 0, 2);
+		LCD.drawString("                     ", 0, 2);
 		LCD.drawString("                     ", 0, 3);
 		LCD.drawString("                     ", 0, 4);
 		LCD.drawString("                     ", 0, 5);
@@ -269,7 +400,7 @@ public class BTGPS{
 		LCD.drawString("                     ", 0, 7);
 	}
 	
-	private static void credits(){
+	private static void credits(int seconds){
 		LCD.clear();
 		LCD.drawString("LEGO Mindstorms",0,1);
 		LCD.drawString("NXT Robots  ",0,2);
@@ -277,6 +408,6 @@ public class BTGPS{
 		LCD.drawString("Java leJOS",0,4);
 		LCD.drawString("www.lejos.org",0,6);
 		LCD.refresh();
-		try {Thread.sleep(20000);} catch (Exception e) {}
+		try {Thread.sleep(seconds*1000);} catch (Exception e) {}
 	}
 }//End Class
