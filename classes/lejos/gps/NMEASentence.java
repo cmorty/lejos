801f8e62b2c7a54d777a441bbfc40fdd608d0e07
esmetaman <esmetaman@06c8f799-c779-4783-8af7-179afedb67c8>
2008-09-21 19:43:42 +0000

    I have updated some GPS Classes, removing some useless methods:
    RMC: Azimith.
    GGA: Hdop. With GSA Error methods is sufficient
    
    now GPS doesn't remove Latitude/Longitude Objects.
    
    I restored a method to convert float lat/lon data into DecimalDegrees.
    
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@1885 06c8f799-c779-4783-8af7-179afedb67c8

--- classes/lejos/gps/NMEASentence.java
+++ classes/lejos/gps/NMEASentence.java
@@ -105,4 +105,73 @@ public class NMEASentence {
 	  else
 		  return((byte)(hex_char - 48));
 	}
+
+	/* UTILITIES METHODS */
+
+	/**
+	 * Any GPS Receiver gives Lat/Lon data in the following way:
+	 * 
+	 * http://www.gpsinformation.org/dale/nmea.htm
+	 * http://www.teletype.com/pages/support/Documentation/RMC_log_info.htm
+	 * 
+	 * 4807.038,N   Latitude 48 deg 07.038' N
+	 * 01131.000,E  Longitude 11 deg 31.000' E
+	 * 
+	 * This data is necessary to convert to Decimal Degrees.
+	 * 
+	 * Latitude values has the range: -90 <-> 90
+	 * Longitude values has the range: -180 <-> 180
+	 * 
+	 * @param DD_MM
+	 * @param CoordenateType
+	 * @return
+	 */
+	protected float degreesMinToDegrees(String DD_MM,int CoordenateType) {//throws NumberFormatException
+		float decDegrees = 0;
+		float degrees = 0;
+		float minutes = 0;
+		float seconds = 0;
+		String doubleCharacterSeparator = ".";
+		String DDMM;
+
+		//1. Count characters until character '.'
+		int dotPosition = DD_MM.indexOf(doubleCharacterSeparator);
+		if(CoordenateType == 0){
+			//Latitude Management
+			DDMM = DD_MM.substring(0, dotPosition);
+			if(DDMM.length() == 4){
+				degrees = Float.parseFloat(DDMM.substring(0, 2));
+				minutes = Float.parseFloat(DD_MM.substring(2));
+			}else if(DDMM.length() == 3){
+				degrees = Float.parseFloat(DDMM.substring(0, 1));
+				minutes = Float.parseFloat(DD_MM.substring(1));
+			}
+
+			if((degrees >=0) && (degrees <=90)){
+				//throw new NumberFormatException();
+			}
+		}else{
+			//Latitude Management
+			DDMM = DD_MM.substring(0, dotPosition);
+			if(DDMM.length() == 5){
+				degrees = Float.parseFloat(DDMM.substring(0, 3));
+				minutes = Float.parseFloat(DD_MM.substring(3));
+			}else if(DDMM.length() == 4){
+				degrees = Float.parseFloat(DDMM.substring(0, 2));
+				minutes = Float.parseFloat(DD_MM.substring(2));
+			}else if(DDMM.length() == 3){
+				degrees = Float.parseFloat(DDMM.substring(0, 1));
+				minutes = Float.parseFloat(DD_MM.substring(1));
+			}
+
+			if((degrees >=0) && (degrees <=180)){
+				//throw new NumberFormatException();
+			}
+		}
+		//Idea
+		//http://id.mind.net/%7Ezona/mmts/trigonometryRealms/degMinSec/degMinSec.htm
+		decDegrees = (float)(degrees + (minutes * (1.0 / 60.0)) + (seconds * (1.0 / 3600.0)));
+		
+		return decDegrees;
+	}
 }
