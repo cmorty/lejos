7be22b4e0e7f2874f0063bdc58853d99572da7af
esmetaman <esmetaman@06c8f799-c779-4783-8af7-179afedb67c8>
2008-10-26 13:05:16 +0000

    I have updated Mindsensors Support for NXTServo. Now it is possible to use pulses and angles without any problem. Mindsensors, internally use pulses/10. I don't know the reason. The range is 500-2500, but internally class send 50-250. If the user uses angles then it is necessary to make a Lineal interpolation. it runs nice. I have made some videos on Youtube, with 1 RC Servo, 2 RC Servos and 4 RC Servos + 2 Continous RC Servos + 1 PC Fan.

--- trunk/classes/lejos/nxt/addon/MServo.java
+++ trunk/classes/lejos/nxt/addon/MServo.java
@@ -6,38 +6,37 @@ import lejos.nxt.*;
  * MServo, is a abstraction to model any RC Servo (continous and non continous)  plugged to
  * 
  * @author Juan Antonio Brenha Moral
-  * @author Luis Bunuel
-   * @author Lawrie Griffiths
  */
 public class MServo extends I2CSensor{
+	private SensorPort portConnected;//Where is plugged in NXT Brick
 	private String name = "";//String to describe any Motor connected to LSC
-	private int MSC_position; //Position where Servo has been plugged
-	private byte servoPosition[] = {0x5A,0x5B,0x5C,0x5D,0x5E,0x5F,0x60,0x61};
-	//Servo ID
-	private SensorPort portConnected;//What
+	private int servoPosition = 0; //Position where Servo has been plugged
+	private final byte servoPositions[] = {0x5A,0x5B,0x5C,0x5D,0x5E,0x5F,0x60,0x61};//The place where RC Servo has been plugged
+	private final byte servoSpeeds[] = {0x52,0x53,0x54,0x55,0x56,0x57,0x58,0x59};
 
-	private int angle;
-	private int min_angle = 0;
-	private int max_angle = 2500;//Ms
-	private int init_angle = 500;//Ms
+	//Default Values
+	private int angle = 0;
+	private int pulse = 0;
+	private int minAngle = 0;//Degree
+	private int maxAngle = 180;//Degrees
+	private int minPulse = 500;//Ms
+	private int maxPulse = 2500;//Ms
 
 	/**
 	 *
-	 * Constructor with the feature to set min, max and init angle
+	 * The initial Constructor.
+	 * This constructor establish where is plugged NXTServo on NXT Brick, 
+	 * where the RC Servo is plugged into NXTServo
 	 *
 	 * @param port
 	 * @param location
 	 * @param servoName
-	 * @param SPI_PORT
-	 * @param min_angle
-	 * @param max_angle
-	 * @param init_angle
 	 *
 	 */
 	public MServo(SensorPort port, int location, String servoName){
 		super(port);
 		this.name = servoName;
-		this.MSC_position = location;
+		this.servoPosition = location;
 	}
 	
 	/**
@@ -47,18 +46,57 @@ public class MServo extends I2CSensor{
 	 * @param port
 	 * @param location
 	 * @param servoName
-	 * @param SPI_PORT
 	 * @param min_angle
 	 * @param max_angle
-	 * @param init_angle
 	 *
 	 */
-	public MServo(SensorPort port, int location, String servoName, int min_angle, int max_angle,int init_angle){
+	public MServo(SensorPort port, int location, String servoName, int min_angle, int max_angle){
 		this(port,location,servoName);
 		
-		this.min_angle = min_angle;
-		this.max_angle = max_angle;		
-		this.init_angle = init_angle;
+		this.minAngle = min_angle;
+		this.maxAngle = max_angle;
+	}
+
+	/*
+	 * Used to make a Lineal Interpolation
+	 * 
+	 * From the HP Calculator idea:
+	 * http://h10025.www1.hp.com/ewfrf/wc/fastFaqLiteDocument?lc=es&cc=mx&docname=bsia5214&dlc=es&product=20037
+	 *
+	 */
+	private float getLinealInterpolation(int x,int x1, int x2, int y1, int y2){
+		float y;
+		y = ((y2-y1)/(x2-x1))*(x-x1) + y1;
+		
+		return y;
+	}
+
+	/**
+	 * This method set the pulse in a RC Servo.
+	 * 
+	 * Note:Pulse range is: 500-2500, but internally
+	 * it is necessary to divide into 2
+	 * 
+	 * @param pulse
+	 * 
+	 */
+	public void setPulse(int pulse){
+		this.pulse = pulse;
+		int internalPulse = Math.round(pulse/10);
+		int I2C_Response = 0;
+		this.setAddress(MSC.NXTSERVO_ADDRESS);
+		int index = servoPosition - 1;
+		I2C_Response = this.sendData((int)servoPositions[index], (byte)internalPulse);
+	}
+	
+	/**
+	 * Return the pulse used in last operation
+	 * 
+	 * @return
+	 *
+	 */
+	public int getPulse(){
+		return pulse;
 	}
 
 	/**
@@ -68,10 +106,32 @@ public class MServo extends I2CSensor{
 	 * 
 	 */
 	public void setAngle(int angle){
-		int I2C_Response = 0;
-		this.setAddress(MSC.NXTSERVO_ADDRESS);
-		int index = MSC_position - 1;
-		I2C_Response = this.sendData((int)servoPosition[index], (byte)angle);
+		this.angle = angle;
+		this.pulse = Math.round(getLinealInterpolation(angle,minAngle,maxAngle,minPulse,maxPulse));
+		this.setPulse(pulse);
 	}
 
+	/**
+	 * Return the angle used in last operation
+	 * 
+	 * @return
+	 *
+	 */
+	public int getAngle(){
+		return angle;
+	}
+
+	
+	/**
+	 * Method to set the Speed in a RC Servo. 
+	 * 
+	 * @param angle
+	 * 
+	 */
+	public void setSpeed(int speed){
+		int I2C_Response = 0;
+		this.setAddress(MSC.NXTSERVO_ADDRESS);
+		int index = servoPosition - 1;
+		I2C_Response = this.sendData((int)servoPositions[index], (byte)speed);
+	}
 }
