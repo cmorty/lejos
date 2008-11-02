5d476485070e7405b13750201a311a5a64cddf9e
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2008-11-02 19:42:24 +0000

    Linux scripts for new commands, remove libusb from lejosfirmdl, and simplify nxjflashg.bat

--- trunk/scripts/windows/lejosfirmdl.bat
+++ trunk/scripts/windows/lejosfirmdl.bat
@@ -10,22 +10,7 @@
 :CHANGE
 SET NXJ_HOME=%LEJOS_HOME%
 
-@REM Check if LIBUSB is installed:
-@if exist "%SystemRoot%\system32\libusb0.dll" GoTo :MAIN
-
-@REM Check operating system and skip Vista:
-Ver | Find "Version 6" >NUL
-  If ErrorLevel 1 Goto :INSTALL
-  Echo Vista Detected. Please manually install libusb in XP compatibility mode, otherwise USB devices will be deactivated. http://libusb-win32.sourceforge.net/#downloads
-  Goto :DONE
-
-:INSTALL
-echo LIBUSB not installed. Running setup program...
-START "LIBUSB INSTALLER" /wait "%NXJ_HOME%\3rdparty\lib\libusb-win32-filter-bin-0.1.12.1.exe"
-echo NOTE: If you encounter the error NXT Not Found, reboot your computer, make sure the brick is still in firmware upload mode, and try again.
-
-:MAIN
-@nxjflash.exe %*
+@nxjflash.bat %*
 GoTo :Done
 
 :UNDEFINED
