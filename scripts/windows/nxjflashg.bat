3f15995453645bd31a1ac093013475874e548ff1
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2008-11-07 19:44:57 +0000

    Removed unnecessary pause from nxjflashg
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@1987 06c8f799-c779-4783-8af7-179afedb67c8

--- scripts/windows/nxjflashg.bat
+++ scripts/windows/nxjflashg.bat
@@ -2,7 +2,6 @@ rem @echo off
 if "%NXJ_HOME%" == ""  goto homeless
 
 java "-Djava.library.path=%NXJ_HOME%\bin" "-Dnxj.home=%NXJ_HOME%" -classpath "%NXJ_HOME%\lib\pctools.jar;%NXJ_HOME%\lib\pccomm.jar" lejos.pc.tools.NXJFlashG %*
-pause
 goto end
 
 :homeless
