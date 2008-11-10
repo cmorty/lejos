e7a1362ac53d7dc7b5c504ed2a14231efe928890
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2008-11-10 16:44:45 +0000

    New scripts for running PC programs and version of nxjflashg for the installer

--- trunk/scripts/windows/nxjflashginst.bat
+++ trunk/scripts/windows/nxjflashginst.bat
@@ -1,10 +1,6 @@
 @echo off
-if "%NXJ_HOME%" == ""  goto homeless
+rem This is for use by the Windows Installer only
+set NXJ_HOME=..
 
 java "-Djava.library.path=%NXJ_HOME%\bin" "-Dnxj.home=%NXJ_HOME%" -classpath "%NXJ_HOME%\lib\pctools.jar;%NXJ_HOME%\lib\pccomm.jar" lejos.pc.tools.NXJFlashG %*
-goto end
 
-:homeless
-echo NXJ_HOME not defined
-
-:end
