e832a317454dbf64aa3a5f0be9fc9bead3eabdc2
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2008-11-10 16:55:14 +0000

    Bring batch files up to date
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@2038 06c8f799-c779-4783-8af7-179afedb67c8

--- snapshot/bin/nxjflashg.bat
+++ snapshot/bin/nxjflashg.bat
@@ -1,4 +1,4 @@
-rem @echo off
+@echo off
 if "%NXJ_HOME%" == ""  goto homeless
 
 java "-Djava.library.path=%NXJ_HOME%\bin" "-Dnxj.home=%NXJ_HOME%" -classpath "%NXJ_HOME%\lib\pctools.jar;%NXJ_HOME%\lib\pccomm.jar" lejos.pc.tools.NXJFlashG %*
