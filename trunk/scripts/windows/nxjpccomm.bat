8bbcb509337c466dec035dc858e10f1e9b6078e6
gloomyandy <gloomyandy@06c8f799-c779-4783-8af7-179afedb67c8>
2008-11-10 22:19:33 +0000

    Add current dir into classpath

--- trunk/scripts/windows/nxjpccomm.bat
+++ trunk/scripts/windows/nxjpccomm.bat
@@ -4,7 +4,7 @@ if "%NXJ_HOME%" == ""  goto homeless
 
 set THIRDPARTY_LIBS=%NXJ_HOME%\3rdparty\lib
 
-java "-Djava.library.path=%NXJ_HOME%\bin" "-Dnxj.home=%NXJ_HOME%" -classpath "%NXJ_HOME%\lib\pctools.jar;%NXJ_HOME%\lib\pccomm.jar;%THIRDPARTY_LIBS%\bluecove.jar" %*
+java "-Djava.library.path=%NXJ_HOME%\bin" "-Dnxj.home=%NXJ_HOME%" -classpath "%NXJ_HOME%\lib\pctools.jar;%NXJ_HOME%\lib\pccomm.jar;%THIRDPARTY_LIBS%\bluecove.jar;." %*
 goto end
 
 :homeless
