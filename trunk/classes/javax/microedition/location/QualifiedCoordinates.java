3a896cb391d62510dc6e095666d00572faed4302
bbagnall <bbagnall@06c8f799-c779-4783-8af7-179afedb67c8>
2008-11-28 23:21:51 +0000

    Initial check-in of Location API. Not fully functioning yet.

---
+++ trunk/classes/javax/microedition/location/QualifiedCoordinates.java
@@ -0,0 +1,8 @@
+package javax.microedition.location;
+
+public class QualifiedCoordinates extends Coordinates {
+	
+	public QualifiedCoordinates(double latitude, double longitude,double altitude) {
+		super(latitude, longitude, altitude);
+	}
+}
