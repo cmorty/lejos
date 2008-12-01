99fd19583b698d1911d5ade9968b02e964dcbace
rogergla <rogergla@06c8f799-c779-4783-8af7-179afedb67c8>
2008-12-01 20:34:41 +0000

    More robust Arbitrator,  more thread safe. Javadocs clarified
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk/classes@2102 06c8f799-c779-4783-8af7-179afedb67c8

--- lejos/subsumption/Arbitrator.java
+++ lejos/subsumption/Arbitrator.java
@@ -1,107 +1,122 @@
-package lejos.subsumption;
 
 /**
  * Arbitrator controls which behavior should currently be active in 
  * a behavior control system. Make sure to call start() after the 
- * Arbitrator is instantiated.
-*  This class has two major responsibilities: <br> 
- * 1. Suppress the currently active behavior when  a behavior of higher priority returns  true  to takeControl()<br>
- * 2. When the active behavior  exits its action() method, activate  the behavior of highest priority that returns 
- *  true  to takeControl().
+ * Arbitrator is instantiated.<br>
+ *  This class has three major responsibilities: <br> 
+ * 1. Determine the highest priority  behavior that returns <b> true </b> to takeControl()<br>   
+ * 2. Suppress the currently active behavior if its prioirty is less than highest
+ * priority. <br>   
+ * 3. When the active behavior  exits its action() method, activate  the highest priority behavior 
+ * (unless it was the most recently active behavior.)     
  * <br> Requirement for a Behavior:  When suppress() is called, terminate  action() immediately. 
  * @see Behavior
- * @author Roger
+ * @author Roger Glassey
  */
-public class Arbitrator
-{
+public class Arbitrator {
 
-   private final int NONE = -1;
-   private Behavior[] _behavior;
-   private int _highestPriority = NONE; // highest priority behavior that wants control
-   private int _current = NONE; // currently active behavior
-   private boolean _returnWhenInactive;
-   /**
-    * Monitor is an inner class.  It polls the behavior array to find the behavior of hightst
-    * priority.  If higher than the current active behavior, it calls current.suppress()
-    */
-   private Monitor monitor ;
+    private final int NONE = -1;
+    private Behavior[] _behavior;
+    // highest priority behavior that wants control
+    private int _highestPriority = NONE;
+    private int _current = NONE; // currently active behavior
+    private boolean _returnWhenInactive;
+    /**
+     * Monitor is an inner class.  It polls the behavior array to find the behavior of hightst
+     * priority.  If higher than the current active behavior, it calls current.suppress()
+     */
+    private Monitor monitor;
 
-   /**
-    * Allocates an Arbitrator object and initializes it with an array of
-    * Behavior objects. The highest index in the Behavior array will have the
-    * highest order behavior level, and hence will suppress all lower level
-    * behaviors if it becomes active. The Behaviors in an Arbitrator can not
-    * be changed once the arbitrator is initialized.<BR>
-    * <B>NOTE:</B> Once the Arbitrator is initialized, the method start() must be
-    * called to begin the arbitration.
-    * @param behaviorList an array of Behavior objects.
-    * @param returnWhenInactive if <B>true</B>, the <B>start()</B> method returns when no Behavior is active.
-    */
-   public Arbitrator(Behavior[] behaviorList, boolean returnWhenInactive)
-   {
-      _behavior = behaviorList;
-      _returnWhenInactive = returnWhenInactive;
-       monitor = new Monitor();    
-   }
+    /**
+     * Allocates an Arbitrator object and initializes it with an array of
+     * Behavior objects. The index of a behavior in this array is its priority level, so 
+     * the behavior of the largest index has the highest the priority level. 
+     * The behaviors in an Arbitrator can not
+     * be changed once the arbitrator is initialized.<BR>
+     * <B>NOTE:</B> Once the Arbitrator is initialized, the method start() must be
+     * called to begin the arbitration.
+     * @param behaviorList an array of Behavior objects.
+     * @param returnWhenInactive if <B>true</B>, the <B>start()</B> method returns when no Behavior is active.
+     */
+    public Arbitrator(Behavior[] behaviorList, boolean returnWhenInactive) {
+        _behavior = behaviorList;
+        _returnWhenInactive = returnWhenInactive;
+        monitor = new Monitor();
+        monitor.setDaemon(true);
+    }
 
-   /**
-    * Same as Arbitrator4(behaviorList, true).
-    * @param behaviorList An array of Behavior objects.
-    */  
-   public Arbitrator(Behavior[] behaviorList)
-   {
-      this(behaviorList, true);
-   }
-   /**
-    * This method starts the arbitration of Behaviors. <BR>
-    * Note: Arbitrator does not run in a separate thread, and hence the start()
-    * method will never return unless returnWhileInacative is true and no behavior wants to take control.
-    */
-   public void start() 
-   {
-      monitor.start();  
-      while(_highestPriority == NONE);//wait for some behavior to take control
-      while(true)
-      {   
-         if(_highestPriority != NONE)
-         {
-            _current = _highestPriority;
-            _behavior[_current].action();
-            _current = NONE;  // no active behavior at the moment
-         }
-         else if (_returnWhenInactive) return;
-         Thread.yield();      
-      }
-   }
-   /**
-    * Finds the highest priority behavior that wants control
-    * If this priority is higher than the current behavior, it calls current.suppress().
-    */
-   private class Monitor extends Thread 
-   {
-     
-      boolean more = true;
-      int maxPriority = _behavior.length - 1;
-      public void run()
-      {
-         while(more)
-         {
-            //FIND HIGHEST PRIORITY BEHAVIOR THAT WANTS CONTROL
-           _highestPriority = NONE;
-            for( int i = maxPriority; i >= 0; i--)
+    /**
+     * Same as Arbitrator(behaviorList, true).
+     * @param behaviorList An array of Behavior objects.
+     */
+    public Arbitrator(Behavior[] behaviorList) {
+        this(behaviorList, true);
+    }
+
+    /**
+     * This method starts the arbitration of Behaviors and runs an endless loop.  <BR>
+     * Note: Arbitrator does not run in a separate thread. The start()
+     * method will never return unless <br>1.  no action() method is running  and
+     * <br>2. no behavior  takeControl()
+     * returns <B> true </B>  and  <br> 3. the <i>returnWhenInacative </i> flag is true,
+     */
+    public void start() {
+        int lastActive =1+_behavior.length;
+        monitor.start();
+        while (_highestPriority == NONE) {
+            Thread.yield();//wait for some behavior to take contro                    
+        }
+              
+        while (true) 
+        {
+            if (_highestPriority != NONE ) 
             {
-               if(_behavior[i].takeControl())
-               {
-                  _highestPriority = i;
-                  break;
-               }
+                if ( _highestPriority!= lastActive)
+                {
+                    _current = _highestPriority;
+                    _behavior[_current].action();
+                    lastActive = _current;
+                    _current = NONE;  // no active behavior at the moment
+                }
+            } else if (_returnWhenInactive)
+            {
+                monitor.more = false;// shut down monitor thread
+                return;
             }
-            if(_highestPriority > _current && _current != NONE)
-            {
-               _behavior[_current].suppress();
-            }                    
             Thread.yield();
-         }
-      }
-   }
+        }
+    }
+
+    /**
+     * Finds the highest priority behavior that returns <B>true </B> to takeControl();
+     * If this priority is higher than the current behavior, it calls current.suppress().
+     */
+    private class Monitor extends Thread {
+
+        boolean more = true;
+        int maxPriority = _behavior.length - 1;
+
+        public void run() 
+        {
+            while (more) 
+            {
+                //FIND HIGHEST PRIORITY BEHAVIOR THAT WANTS CONTROL
+                int wantsControl = NONE;
+                for (int i = maxPriority; i >= 0; i--) 
+                {
+                    if (_behavior[i].takeControl()) {
+                        wantsControl = i;
+                        break;
+                    }
+                }
+                _highestPriority = wantsControl;
+                int current = _current;
+                if (wantsControl > current && current != NONE) 
+                {
+                    _behavior[current].suppress();
+                }
+                Thread.yield();
+            }
+        }
+    }
 }
--- lejos/subsumption/Behavior.java
+++ lejos/subsumption/Behavior.java
@@ -1,4 +1,4 @@
-package lejos.subsumption;
+
 
 
 /**
@@ -6,10 +6,10 @@ package lejos.subsumption;
 * behavior belonging to a robot. Each behavior must define three things: <BR>
 * 1) The circumstances to make this behavior seize control of the robot.
 * e.g. When the touch sensor determines the robot has collided with an object.<BR>
-* 2) The action to exhibit when this behavior takes control. 
+* 2) The action to perform when this behavior takes control. 
 * e.g. Back up and turn.<BR>
-* 3) The actions to perform when another behavior has seized control from this
-* behavior. 
+* 3) The tasks to perform when another behavior has seized control from this
+* behavior, including interrupting it.  S
 * e.g. Stop the current movement and update coordinates.<BR>
 * These are represented by defining the methods takeControl(), action(),
 * and suppress() respectively. <BR>
@@ -18,31 +18,34 @@ package lejos.subsumption;
 * Arbitrator object.
 *
 * @see Arbitrator
-* @author <a href="mailto:bbagnall@escape.ca">Brian Bagnall</a>
-* @version 0.1  27-July-2001
+
+* @version 0.4  27-November-2008
 */
 public interface Behavior {
-   
+     
    /**
-   * Returns a boolean to indicate if this behavior should seize control of the robot.
+   * The boolean return  indicates  if this behavior should seize control of the robot.
    * For example, a robot that reacts if a touch sensor is pressed: <BR>
    * public boolean takeControl() { <BR>
    *    return touch.isPressed(); <BR>
    * } <BR>
-   * @return boolean Indicates if this Behavior should seize control.
+   * @return boolean  Indicates if this Behavior should seize control.
    */
    public boolean takeControl();
    
    /**
-   * The code in action() represents the actual action of the robot when this
+   * The code in action() represents the tasks  the robot performs when this
    * behavior becomes active. It can be as complex as navigating around a
    * room, or as simple as playing a tune.<BR>
    * <B>The contract for implementing this method is:</B><BR>
-   * Any action can be started in this method. This method should not start a 
-   * never ending loop. This method can return on its own, or when the suppress()
-   * method is called; but it must return eventually. The action can run in
-   * a seperate thread if the designer wishes it, and can therefore continue
-   * running after this method call returns. 
+   * Any action can be started in this method. If the action is complete, the
+    * method should return.  It <B> must </B> return when the suppress() method 
+    * is called, even if it runs in a  separate thread. <br>
+    * The Arbitrator will only call suppress() if the action() method is still 
+    * running. 
+    * If some side effects remain after it returns, such as motor are still 
+    * moving, the other behaviors  must cope this condition.  
+    *    
    */
    public void action();
    
@@ -51,9 +54,11 @@ public interface Behavior {
    * stopping motors, or even calling methods to update internal data (such
    * as navigational coordinates). <BR>
    * <B>The contract for implementing this method is:</B><BR>
-   * This method will stop the action running in this Behavior class. This method
-   * will <I>not</I> return until that action has been stopped. It is acceptable for a 
-   * delay to occur while the action() method finishes up.
+   * This method will stop the action running in this Behavior class and cause action()
+    * to exit promptly. This method
+   * must <I>not</I> return until that action has been stopped. It is acceptable for a 
+   * delay to occur while the action() method finishes up, but the next behavior cannot 
+    * begin its action before the current behavior action() exits. 
    */
    public void suppress();
    
