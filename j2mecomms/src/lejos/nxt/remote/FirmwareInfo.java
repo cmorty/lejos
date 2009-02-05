aacda7ad584023ffc2b42704a377b64cb68151e3
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2009-02-05 18:18:58 +0000

    Initial version
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@2243 06c8f799-c779-4783-8af7-179afedb67c8

---
+++ j2mecomms/src/lejos/nxt/remote/FirmwareInfo.java
@@ -0,0 +1,12 @@
+package lejos.nxt.remote;
+
+/**
+ * 
+ * Firmware information for a remote NXT accessed via LCP.
+ *
+ */
+public class FirmwareInfo {
+	public byte status;
+	public String protocolVersion;
+	public String firmwareVersion;
+}
