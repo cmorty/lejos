34d1717d87ce0595d72d727cf3e43159ba42c040
rogergla <rogergla@06c8f799-c779-4783-8af7-179afedb67c8>
2009-02-13 19:39:02 +0000

    Added some Javadoc comments to SimpleNavigator new  TachoNavigator  for compatibility with v0,7
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@2263 06c8f799-c779-4783-8af7-179afedb67c8

--- classes/lejos/navigation/SimpleNavigator.java
+++ classes/lejos/navigation/SimpleNavigator.java
@@ -47,7 +47,17 @@ public class SimpleNavigator implements Navigator
    {
       _pilot = new TachoPilot(wheelDiameter,trackWidth,leftMotor, rightMotor,reverse);
    }
-
+ /**
+    * Allocates a SimpleNavigator object and initializes it with a TachoPilot
+    * The x and y values and the direction angle are all initialized to 0, so if the first move is forward() the robot will run along
+    * the x axis. <BR>
+    * @param wheelDiameter The diameter of the wheel, usually printed right on the
+    * wheel, in centimeters (e.g. 49.6 mm = 4.96 cm = 1.95 in)
+    * @param trackWidth The distance from the center of the left tire to the center
+    * of the right tire, in units of your choice
+    * @param rightMotor The motor used to drive the right wheel e.g. Motor.C.
+    * @param leftMotor The motor used to drive the left wheel e.g. Motor.A
+    */
    public SimpleNavigator(float wheelDiameter, float trackWidth, Motor leftMotor, Motor rightMotor)
    {
       _pilot = new TachoPilot(wheelDiameter,trackWidth,leftMotor, rightMotor);
