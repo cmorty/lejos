26e3269e1c4b413b3e9acb531838d9d31cf69be0
skoehler <skoehler@06c8f799-c779-4783-8af7-179afedb67c8>
2009-02-21 23:31:58 +0000

    java.lang.Void
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk/classes@2274 06c8f799-c779-4783-8af7-179afedb67c8

---
+++ java/lang/Void.java
@@ -0,0 +1,15 @@
+package java.lang;
+
+/**
+ * An uninstantiable placeholder class. 
+ * @author Sven Köhler
+ */
+public final class Void
+{
+	//MISSING public static final Class TYPE
+	
+	private Void()
+	{
+		//nothing
+	}
+}
