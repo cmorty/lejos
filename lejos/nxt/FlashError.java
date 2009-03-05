6c4cea40004f3a99234baa0d2404bc7be220e035
gloomyandy <gloomyandy@06c8f799-c779-4783-8af7-179afedb67c8>
2009-03-05 19:14:25 +0000

    New class to indicate problems with flash I/O
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk/classes@2310 06c8f799-c779-4783-8af7-179afedb67c8

---
+++ lejos/nxt/FlashError.java
@@ -0,0 +1,15 @@
+package lejos.nxt;
+
+import java.lang.*;
+
+public class FlashError extends Error
+{
+	public FlashError() {
+		super();
+	}
+
+	public FlashError(String message) {
+		super(message);
+	}
+
+}
