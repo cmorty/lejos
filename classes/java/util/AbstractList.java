e509a9133653a33b70d4450b42f37c1ce42ecd83
skoehler <skoehler@06c8f799-c779-4783-8af7-179afedb67c8>
2009-03-27 14:08:42 +0000

    more preparations for the new ArrayList
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@2393 06c8f799-c779-4783-8af7-179afedb67c8

--- classes/java/util/AbstractList.java
+++ classes/java/util/AbstractList.java
@@ -8,18 +8,57 @@ package java.util;
  */
 abstract class AbstractList<E> extends AbstractCollection<E> implements List<E>
 {
-	public ListIterator<E> listIterator()
-	{
-		return this.listIterator(0);
-	}
-
+	//TODO hashCode
+	
 	public boolean contains(Object o)
 	{
 		return this.indexOf(o) >= 0;
+	}
+	
+	public boolean equals(Object o)
+	{
+		if (o == this)
+			return true;
+		if (!(o instanceof List))
+			return false;
+		
+		Iterator<?> it1 = this.iterator();
+		Iterator<?> it2 = ((List<?>)o).iterator();
+		
+		boolean n1, n2;
+		while (true)
+		{
+			n1 = it1.hasNext();
+			n2 = it2.hasNext();
+			if (!n1 || !n2)
+				break;
+			
+			Object o1 = it1.next();
+			Object o2 = it2.next();
+			
+			if (o1 == null)
+			{
+				if (o2 != null)
+				{
+					return false;
+				}
+			}
+			else if (!o1.equals(o2))
+			{
+				return false;
+			}
+		}
+		
+		return n1 == n2;
 	}
 
 	public Iterator<E> iterator()
 	{
 		return this.listIterator();
+	}
+	
+	public ListIterator<E> listIterator()
+	{
+		return this.listIterator(0);
 	}
 }
