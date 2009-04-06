4b6a8b3917ab0043cd74f58fb04a3ea63304d37c
skoehler <skoehler@06c8f799-c779-4783-8af7-179afedb67c8>
2009-04-06 00:07:20 +0000

    mc
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@2457 06c8f799-c779-4783-8af7-179afedb67c8

--- scripts/windows/lejosfirmdl.bat
+++ scripts/windows/lejosfirmdl.bat
@@ -2,14 +2,12 @@
 @REM Feel free to delete lejosdl.bat, lejosjc.bat and lejosfirmdl.bat
 
 @echo off
-
 @if "%LEJOS_HOME%" == "" GoTo :UNDEFINED
 @REM The following only works with Windows 2000 on up:
 @if NOT %lejos_home:~-3% == nxj GoTo :DONE
 
 :CHANGE
 SET NXJ_HOME=%LEJOS_HOME%
-
 @nxjflash.bat %*
 GoTo :Done
 
