6299c4a570e9f2758592932dab32a43f5c83f713
skoehler <skoehler@06c8f799-c779-4783-8af7-179afedb67c8>
2009-04-07 21:44:08 +0000

    more constructors
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@2465 06c8f799-c779-4783-8af7-179afedb67c8

--- classes/java/lang/ClassCastException.java
+++ classes/java/lang/ClassCastException.java
@@ -2,4 +2,13 @@ package java.lang;
 
 public class ClassCastException extends RuntimeException
 {
+	public ClassCastException()
+	{
+		super();
+	}
+
+	public ClassCastException(String message)
+	{
+		super(message);
+	}	
 }
--- classes/java/lang/NullPointerException.java
+++ classes/java/lang/NullPointerException.java
@@ -2,4 +2,13 @@ package java.lang;
 
 public class NullPointerException extends RuntimeException
 {
+	public NullPointerException()
+	{
+		super();
+	}
+
+	public NullPointerException(String message)
+	{
+		super(message);
+	}	
 }
