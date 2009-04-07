f13e04fa958de9665850515ff169f269814712e5
skoehler <skoehler@06c8f799-c779-4783-8af7-179afedb67c8>
2009-04-07 21:23:32 +0000

    StringIndexOutOfBoundsException now extends IndexOutOfBoundsException
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@2464 06c8f799-c779-4783-8af7-179afedb67c8

--- classes/java/lang/StringIndexOutOfBoundsException.java
+++ classes/java/lang/StringIndexOutOfBoundsException.java
@@ -1,5 +1,17 @@
 package java.lang;
 
-public class StringIndexOutOfBoundsException extends RuntimeException 
+public class StringIndexOutOfBoundsException extends IndexOutOfBoundsException 
 {
+    public StringIndexOutOfBoundsException() {
+        super();
+    }
+
+    public StringIndexOutOfBoundsException(int index) {
+        super("String index out of range: " + index);
+    }
+
+    public StringIndexOutOfBoundsException(String s) {
+        super(s);
+    }
+
 }
