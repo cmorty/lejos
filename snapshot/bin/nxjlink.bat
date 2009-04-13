3d72f19cb29b8cbd74ae1429b86e8b2eeff3ef2a
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2009-04-13 21:56:14 +0000

    Update windows scripts
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@2523 06c8f799-c779-4783-8af7-179afedb67c8

--- snapshot/bin/nxjlink.bat
+++ snapshot/bin/nxjlink.bat
@@ -2,9 +2,9 @@
 if "%NXJ_HOME%" == ""  goto homeless
 
 set THIRDPARTY_LIBS=%NXJ_HOME%\3rdparty\lib
-set LINK_CLASSPATH=.;%THIRDPARTY_LIBS%\bcel-5.1.jar;%THIRDPARTY_LIBS%\commons-cli-1.0.jar;%NXJ_HOME%\lib\jtools.jar;%NXJ_HOME%\lib\classes.jar
+set LINK_CLASSPATH=.;%THIRDPARTY_LIBS%\bcel-5.1.jar;%THIRDPARTY_LIBS%\commons-cli-1.0.jar;%NXJ_HOME%\lib\jtools.jar;%THIRDPARTY_LIBS%\bluecove.jar
 
-java -DCOMMAND_NAME=nxjlink -classpath "%LINK_CLASSPATH%" js.tinyvm.TinyVM --writeorder LE --classpath ".;%NXJ_HOME%\lib\classes.jar" %*
+java -DCOMMAND_NAME=nxjlink -classpath "%LINK_CLASSPATH%" js.tinyvm.TinyVM --writeorder LE --bootclasspath "%NXJ_HOME%\lib\classes.jar" --classpath "." %*
 goto end
 
 :homeless
