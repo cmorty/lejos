9aa7f6adece6aa788341337edeb37f1c1f2b9070
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2009-04-14 23:16:03 +0000

    Added command line parser

--- trunk/pctools/lejos/pc/tools/Console.java
+++ trunk/pctools/lejos/pc/tools/Console.java
@@ -1,5 +1,8 @@
 package lejos.pc.tools;
 
+import java.io.*;
+import js.tinyvm.TinyVMException;
+import org.apache.commons.cli.*;
 import lejos.pc.comm.*;
 
 /**
@@ -10,13 +13,28 @@ import lejos.pc.comm.*;
  *
  */ 
 public class Console implements ConsoleViewerUI {
-	public static void main(String[] args) throws Exception {
-		(new Console()).run();
+	public static void main(String[] args){
+		try {
+			(new Console()).run(args);
+		} catch (Throwable t) {
+			System.err.println("An error has occurred: " + t.getMessage());
+		}
 	}
 	
-	private void run() {
+	private void run(String[] args) throws TinyVMException {
+		int protocols = 0;
+		ConsoleCommandLineParser fParser = new ConsoleCommandLineParser();
+		CommandLine commandLine = fParser.parse(args);
+		boolean blueTooth = commandLine.hasOption("b");
+		boolean usb = commandLine.hasOption("u");
+		String name = commandLine.getOptionValue("n");
+		String address = commandLine.getOptionValue("d");
 		ConsoleViewComms comm = new ConsoleViewComms(this, false);
-		comm.connectTo(null, null, NXTCommFactory.ALL_PROTOCOLS);
+		if (blueTooth) protocols |= NXTCommFactory.BLUETOOTH;
+		if (usb) protocols |= NXTCommFactory.USB;
+		if (protocols == 0) protocols = NXTCommFactory.ALL_PROTOCOLS;
+		boolean connected = comm.connectTo(name, address, protocols);
+		if (!connected) System.exit(1);
 	}
 
 	public void append(String value) {
@@ -32,4 +50,70 @@ public class Console implements ConsoleViewerUI {
 
 	public void setStatus(String msg) {
 	}
+}
+
+/**
+ * CommandLineParser
+ */
+class ConsoleCommandLineParser 
+{
+   /**
+    * Parse commandline.
+    * 
+    * @param args command line
+    * @throws TinyVMException
+    */
+   public CommandLine parse (String[] args) throws TinyVMException
+   {
+      assert args != null: "Precondition: args != null";
+
+      Options options = new Options();
+      options.addOption("h", "help", false, "help");
+      options.addOption("b", "bluetooth", false, "use bluetooth");
+      options.addOption("u", "usb", false, "use usb");
+      
+      Option nameOption = new Option("n", "name", true,"look for named NXT");
+      nameOption.setArgName("name");
+      options.addOption(nameOption);
+      
+      Option addressOption = new Option("d", "address", true,
+    		 "look for NXT with given address");
+      addressOption.setArgName("address");
+      options.addOption(addressOption);
+      
+      CommandLine result;
+      try
+      {
+         try
+         {
+            result = new GnuParser().parse(options, args);
+         }
+         catch (ParseException e)
+         {
+            throw new TinyVMException(e.getMessage(), e);
+         }
+
+         if (result.hasOption("h"))
+         {
+            throw new TinyVMException("Help:");
+         }
+      }
+      catch (TinyVMException e)
+      {
+         StringWriter writer = new StringWriter();
+         PrintWriter printWriter = new PrintWriter(writer);
+         printWriter.println(e.getMessage());
+         
+         String commandName = System.getProperty("COMMAND_NAME", "lejos.pc.tools.Console");
+
+         String usage = commandName + " [options]";
+         new HelpFormatter().printHelp(printWriter, 80, usage.toString(), null,
+            options, 0, 2, null);
+
+         throw new TinyVMException(writer.toString());
+      }
+
+      assert result != null: "Postconditon: result != null";
+      return result;
+   }
 }
