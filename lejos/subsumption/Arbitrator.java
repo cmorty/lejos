7b7b72ab8daeee46b02833eff1d365b3a8c5d0ec
rogergla <rogergla@06c8f799-c779-4783-8af7-179afedb67c8>
2009-04-27 19:59:51 +0000

    Arbitrator deleted ArbitratorS renamed Arbitrator
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk/classes@2559 06c8f799-c779-4783-8af7-179afedb67c8

--- lejos/subsumption/Arbitrator.java
+++ lejos/subsumption/Arbitrator.java
@@ -1,158 +1,141 @@
 package lejos.subsumption;
-                                     
+
 
 /**
- * Arbitrator controls which behavior should currently be active in 
+ * Arbitrator controls which behavior should become active in
  * a behavior control system. Make sure to call start() after the 
  * Arbitrator is instantiated.<br>
  *  This class has three major responsibilities: <br> 
  * 1. Determine the highest priority  behavior that returns <b> true </b> to takeControl()<br>   
- * 2. Suppress the currently active behavior if its prioirty is less than highest
- * priority. If no action() method is running, and some Behavior should take control,
- * suppress the last active behavior; <br>
- * 3. When the active behavior  exits its action() method, activate  the behavior of highest priority 
- * (unless it was the most recently active behavior.)     
- * <br> Requirement for a Behavior:  When suppress() is called, terminate  action() immediately. 
+ * 2. Suppress the active behavior if its priority is less than highest
+ * priority. <br>   
+ * 3. When the action() method exits, it calls action() on the Behavior of highest priority.
+ * This Behavior becomes active.
+ * Chances since release 0.7:
+ * <br> 1. It assumes that a Behavior is no longer active when action() exits.
+ * <br> 2. Therefore it will only call suppress() on the Behavior whose action() method is running.
+ * <br> 3. It can make consecutives calls of action() on the same Behavior.
+ * <br> 4. Requirements for a Behavior:
+ * <br>    When suppress() is called, terminate  action() immediately.
  * @see Behavior
- * @author Roger Glassey  and Lawrie Griffith
+ * @author Roger Glassey
  */
 public class Arbitrator
 {
 
-    private final int NONE = -1;
-    private Behavior[] _behavior;
-    // highest priority behavior that wants control
-    private int _highestPriority = NONE;
-    private int _current = NONE; // currently active behavior
-    private int _lastActive = NONE; // last acative behavior
-    private boolean _returnWhenInactive;
+  private final int NONE = -1;
+  private Behavior[] _behavior;
+  // highest priority behavior that wants control ; set by start() usec by monitor
+  private int _highestPriority = NONE;
+  private int _active = NONE; //  active behavior; set by montior, used by start();
+  private boolean _returnWhenInactive;
+  /**
+   * Monitor is an inner class.  It polls the behavior array to find the behavior of hightst
+   * priority.  If higher than the active behavior, it calls active.suppress()
+   */
+  private Monitor monitor;
 
-    /**
-     * Monitor is an inner class.  It polls the behavior array to find the behavior of hightst
-     * priority.  If higher than the current active behavior, it calls current.suppress()
-     */
-    private Monitor monitor;
+  /**
+   * Allocates an Arbitrator object and initializes it with an array of
+   * Behavior objects. The index of a behavior in this array is its priority level, so 
+   * the behavior of the largest index has the highest the priority level. 
+   * The behaviors in an Arbitrator can not
+   * be changed once the arbitrator is initialized.<BR>
+   * <B>NOTE:</B> Once the Arbitrator is initialized, the method start() must be
+   * called to begin the arbitration.
+   * @param behaviorList an array of Behavior objects.
+   * @param returnWhenInactive if <B>true</B>, the <B>start()</B> method returns when no Behavior is active.
+   */
+  public Arbitrator(Behavior[] behaviorList, boolean returnWhenInactive)
+  {
+    _behavior = behaviorList;
+    _returnWhenInactive = returnWhenInactive;
+    monitor = new Monitor();
+    monitor.setDaemon(true);
+  }
 
-    /**
-     * Allocates an Arbitrator object and initializes it with an array of
-     * Behavior objects. The index of a behavior in this array is its priority level, so 
-     * the behavior of the largest index has the highest the priority level. 
-     * The behaviors in an Arbitrator can not
-     * be changed once the arbitrator is initialized.<BR>
-     * <B>NOTE:</B> Once the Arbitrator is initialized, the method start() must be
-     * called to begin the arbitration.
-     * @param behaviorList an array of Behavior objects.
-     * @param returnWhenInactive if <B>true</B>, the <B>start()</B> method returns when no Behavior is active.
-     */
-    public Arbitrator(Behavior[] behaviorList, boolean returnWhenInactive)
+  /**
+   * Same as Arbitrator(behaviorList, false) Arbitrator start() never exits
+   * @param behaviorList An array of Behavior objects.
+   */
+  public Arbitrator(Behavior[] behaviorList)
+  {
+    this(behaviorList, false);
+  }
+
+  /**
+   * This method starts the arbitration of Behaviors and runs an endless loop.  <BR>
+   * Note: Arbitrator does not run in a separate thread. The start()
+   * method will never return unless <br>1.  no action() method is running  and
+   * <br>2. no behavior  takeControl()
+   * returns <B> true </B>  and  <br> 3. the <i>returnWhenInacative </i> flag is true,
+   */
+  public void start()
+  {
+    monitor.start();
+    while (_highestPriority == NONE)
     {
-        _behavior = behaviorList;
-        _returnWhenInactive = returnWhenInactive;
-        monitor = new Monitor();
-        monitor.setDaemon(true);
+      Thread.yield();//wait for some behavior to take contro                    
     }
-
-    /**
-     * Same as Arbitrator(behaviorList, false) Arbitrator start never exits
-     * @param behaviorList An array of Behavior objects.
-     */
-    public Arbitrator(Behavior[] behaviorList)
+    while (true)
     {
-        this(behaviorList, false);
-
-    }
-
-    /**
-     * This method starts the arbitration of Behaviors and runs an endless loop.  <BR>
-     * Note: Arbitrator does not run in a separate thread. The start()
-     * method will never return unless <br>1.  no action() method is running  and
-     * <br>2. no behavior  takeControl()
-     * returns <B> true </B>  and  <br> 3. the <i>returnWhenInacative </i> flag is true,
-     */
-    public void start()
-    {
-        _lastActive = NONE;
-        monitor.start();
-        
-        while (_highestPriority == NONE)
+      synchronized (monitor)
+      {
+        if (_highestPriority != NONE)
         {
-            Thread.yield();//wait for some behavior to take control                   
+          _active = _highestPriority;
+
+        } else if (_returnWhenInactive)
+        {// no behavior wants to run
+          monitor.more = false;//9 shut down monitor thread
+          return;
         }
-        
-        while (true)
-        {
-        	synchronized (monitor) {
-	            if (_highestPriority != NONE)
-	            {
-	                if (_highestPriority != _lastActive) // no repetition of action()
-	                {
-	                     _current = _highestPriority;
-	             		_lastActive = _current;
-	                }
-	            } else if (_returnWhenInactive)
-	            {// no behavior wants to run
-	                monitor.more = false;// shut down monitor thread
-	                return;
-	            }
-        	}
-        	// monitor released, so _current could be suppressed before run
-        	if (_current != NONE) {
-        		_behavior[_current].action(); // Run the action
-        		_current = NONE;
-        	}
-            Thread.yield();
-        }
+      }// monotor released before action is called
+      if (_active != NONE)  //_highestPrioirty could be NONE
+      {
+        _behavior[_active].action();
+        _active = NONE;  // no active behavior at the moment
+      }
+      Thread.yield();
     }
+  }
 
-    /**
-     * Finds the highest priority behavior that returns <B>true </B> to takeControl();
-     * If this priority is higher than the current behavior, it calls current.suppress().
-     * If there is no current behavior, calls suppress() on the most recently active behavior.
-     */
-    private class Monitor extends Thread
+  /**
+   * Finds the highest priority behavior that returns <B>true </B> to takeControl();
+   * If this priority is higher than the active behavior, it calls active.suppress().
+   * If there is no active behavior, calls suppress() on the most recently acrive behavior.
+   */
+  private class Monitor extends Thread
+  {
+
+    boolean more = true;
+    int maxPriority = _behavior.length - 1;
+
+    public void run()
     {
-
-        boolean more = true;
-        int maxPriority = _behavior.length - 1;
-
-        public void run()
+      while (more)
+      {
+        //FIND HIGHEST PRIORITY BEHAVIOR THAT WANTS CONTROL
+        _highestPriority = NONE;
+        synchronized (this)
         {
-            while (more)
+          for (int i = maxPriority; i >= 0; i--)
+          {
+            if (_behavior[i].takeControl())
             {
-                //FIND HIGHEST PRIORITY BEHAVIOR THAT WANTS CONTROL
-                synchronized (this)
-                {
-                    _highestPriority = NONE;
-                    for (int i = maxPriority; i >= 0; i--)
-                    {
-                        if (_behavior[i].takeControl())
-                        {
-                            _highestPriority = i;
-                            break;
-                        }
-                    }
-                    if (_current != NONE)
-                    {
-                        if (_highestPriority > _current)
-                        {
-                            _behavior[_current].suppress();
-                            _current = NONE;
-                            _lastActive = NONE;
-                        }
-                    } else // current == NONE
-                    {
-                    	// When current is finished, 
-                    	// suppress the last active unless it still should run
-                        if (_lastActive != NONE  &&  _highestPriority != _lastActive )
-                        {
-                            _behavior[_lastActive].suppress();
-                            _lastActive = NONE;
-                        }
-                    }
-                }// end sync
-                Thread.yield();
+              _highestPriority = i;
+              break;
             }
-        }
+          }
+          int active = _active;// local copy: avoid out of bounds error in 134
+          if (active != NONE && _highestPriority > active)
+          {
+            _behavior[active].suppress();
+          }
+        }// end synchronize block - main thread can run now
+        Thread.yield();
+      }
     }
+  }
 }
   
