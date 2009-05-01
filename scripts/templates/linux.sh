026f98d31980e1923f3f6f09f2b034f71e431919
skoehler <skoehler@06c8f799-c779-4783-8af7-179afedb67c8>
2009-05-01 19:29:37 +0000

    build.xml for generating wrapper scripts (linux roughly tested, windows untested)
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@2564 06c8f799-c779-4783-8af7-179afedb67c8

---
+++ scripts/templates/linux.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+NXJ_COMMAND="$(basename -- "$0")"
+if [ -z "$NXJ_HOME" ]; then
+  NXJ_HOME="$(readlink -f -- "$0")"
+  NXJ_HOME="$(dirname -- "$NXJ_HOME")"
+  NXJ_HOME="$(dirname -- "$NXJ_HOME")"
+fi
+
+NXJ_BIN="$NXJ_HOME/bin"
+NXJ_LIBS="$NXJ_HOME/lib"
+NXJ_LIBS_3rd="$NXJ_HOME/3rdparty/lib"
+
+NXJ_JAR_BCEL="$NXJ_LIBS_3rd/bcel-5.1.jar"
+NXJ_JAR_BLUECOVE="$NXJ_LIBS_3rd/bluecove.jar"
+NXJ_JAR_BLUECOVE_GPL="$NXJ_LIBS_3rd/bluecove-gpl.jar"
+NXJ_JAR_COMMONS_CLI="$NXJ_LIBS_3rd/commons-cli-1.0.jar"
+
+NXJ_JAR_CLASSES="$NXJ_LIBS/classes.jar"
+NXJ_JAR_JTOOLS="$NXJ_LIBS/jtools.jar"
+NXJ_JAR_PCCOMM="$NXJ_LIBS/pccomm.jar"
+NXJ_JAR_PCTOOLS="$NXJ_LIBS/pctools.jar"
+
+if [ "$(uname -o)" == "Cygwin" ]; then
+	SEP=";"
+else
+	SEP=":"
+fi
+
+if [ "$(uname -a)" == "Linux" ]; then
+	NXJ_CP_BLUECOVE="$NXJ_JAR_BLUECOVE$SEP$NXJ_JAR_BLUECOVE_GPL"
+else
+	NXJ_CP_BLUECOVE="$NXJ_JAR_BLUECOVE"
+fi
+
+NXJ_CP_BOOT="$NXJ_JAR_CLASSES"
+NXJ_CP_LINK="$NXJ_JAR_BCEL$SEP$NXJ_JAR_COMMONS_CLI$SEP$NXJ_JAR_JTOOLS"
+NXJ_CP_TOOL="$NXJ_CP_BLUECOVE$SEP$NXJ_CP_LINK$SEP$NXJ_JAR_PCCOMM$SEP$NXJ_JAR_PCTOOLS"
+
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$NXJ_BIN"
+export DYLD_LIBRARY_PATH="$DYLD_LIBRARY_PATH:$NXJ_BIN"
