28a229f9ea64b82791112dc1147a91dfbcfe175c
skoehler <skoehler@06c8f799-c779-4783-8af7-179afedb67c8>
2009-05-01 19:29:37 +0000

    build.xml for generating wrapper scripts (linux roughly tested, windows untested)

---
+++ trunk/scripts/templates/windows.cmd
@@ -0,0 +1,23 @@
+@echo off
+
+if "%NXJ_HOME%" == "" set NXJ_HOME=%0\..\..
+
+set NXJ_BIN="%NXJ_HOME%\bin"
+set NXJ_LIBS="%NXJ_HOME%\lib"
+set NXJ_LIBS_3rd="%NXJ_HOME%\3rdparty\lib"
+
+set NXJ_JAR_BCEL="%NXJ_LIBS_3rd%\bcel-5.1.jar"
+set NXJ_JAR_BLUECOVE="%NXJ_LIBS_3rd%\bluecove.jar"
+set NXJ_JAR_BLUECOVE_GPL="%NXJ_LIBS_3rd%\bluecove-gpl.jar"
+set NXJ_JAR_COMMONS_CLI="%NXJ_LIBS_3rd%\commons-cli-1.0.jar"
+
+set NXJ_JAR_CLASSES="%NXJ_LIBS%\classes.jar"
+set NXJ_JAR_JTOOLS="%NXJ_LIBS%\jtools.jar"
+set NXJ_JAR_PCCOMM="%NXJ_LIBS%\pccomm.jar"
+set NXJ_JAR_PCTOOLS="%NXJ_LIBS%\pctools.jar"
+
+set NXJ_CP_BLUECOVE="%NXJ_JAR_BLUECOVE%"
+
+set NXJ_CP_BOOT="%NXJ_JAR_CLASSES%"
+set NXJ_CP_LINK="%NXJ_JAR_BCEL%;%NXJ_JAR_COMMONS_CLI%;%NXJ_JAR_JTOOLS%"
+set NXJ_CP_TOOL="%NXJ_CP_BLUECOVE%;%NXJ_CP_LINK%;%NXJ_JAR_PCCOMM%;%NXJ_JAR_PCTOOLS%"
