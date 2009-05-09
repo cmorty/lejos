b3cbb2334d060aecae2f7f5db73ff8a3d36cb4f3
skoehler <skoehler@06c8f799-c779-4783-8af7-179afedb67c8>
2009-05-09 22:45:25 +0000

    Win9x/ME compatible scripts, hopefully

--- trunk/snapshot/bin/nxjc.bat
+++ trunk/snapshot/bin/nxjc.bat
@@ -1,35 +1,41 @@
 @echo off
-setlocal
+if "%OS%" == "Windows_NT" setlocal
 
-if "%NXJ_HOME%" == "" goto home_unset
-	set "NXJ_BIN=%NXJ_HOME%\bin"
-	goto home_endif
+if "%NXJ_HOME%" == "" goto :home_unset
+	set NXJ_BIN=%NXJ_HOME%\bin
+	goto :home_endif
 :home_unset
-	pushd "%0\.."
+	if "%OS%" == "Windows_NT" goto :home_nt
+	echo You have to set the NXJ_HOME Variable on Windows 9x/ME.
+	goto :EOF
+:home_nt
+	set "NXJ_TEMP=%cd%"
+	cd /d "%0\.."
 	set "NXJ_BIN=%cd%"
-	cd ..
+	cd ".."
 	set "NXJ_HOME=%cd%"
-	popd
+	cd /d "%NXJ_TEMP%"
 :home_endif
 
-set "NXJ_LIBS=%NXJ_HOME%\lib"
-set "NXJ_LIBS_3rd=%NXJ_HOME%\3rdparty\lib"
+set NXJ_LIBS=%NXJ_HOME%\lib
+set NXJ_LIBS_3rd=%NXJ_HOME%\3rdparty\lib
 
-set "NXJ_JAR_BCEL=%NXJ_LIBS_3rd%\bcel-5.1.jar"
-set "NXJ_JAR_BLUECOVE=%NXJ_LIBS_3rd%\bluecove.jar"
-set "NXJ_JAR_BLUECOVE_GPL=%NXJ_LIBS_3rd%\bluecove-gpl.jar"
-set "NXJ_JAR_COMMONS_CLI=%NXJ_LIBS_3rd%\commons-cli-1.0.jar"
+set NXJ_JAR_BCEL=%NXJ_LIBS_3rd%\bcel-5.1.jar
+set NXJ_JAR_BLUECOVE=%NXJ_LIBS_3rd%\bluecove.jar
+set NXJ_JAR_BLUECOVE_GPL=%NXJ_LIBS_3rd%\bluecove-gpl.jar
+set NXJ_JAR_COMMONS_CLI=%NXJ_LIBS_3rd%\commons-cli-1.0.jar
 
-set "NXJ_JAR_CLASSES=%NXJ_LIBS%\classes.jar"
-set "NXJ_JAR_JTOOLS=%NXJ_LIBS%\jtools.jar"
-set "NXJ_JAR_PCCOMM=%NXJ_LIBS%\pccomm.jar"
-set "NXJ_JAR_PCTOOLS=%NXJ_LIBS%\pctools.jar"
+set NXJ_JAR_CLASSES=%NXJ_LIBS%\classes.jar
+set NXJ_JAR_JTOOLS=%NXJ_LIBS%\jtools.jar
+set NXJ_JAR_PCCOMM=%NXJ_LIBS%\pccomm.jar
+set NXJ_JAR_PCTOOLS=%NXJ_LIBS%\pctools.jar
 
-set "NXJ_CP_BLUECOVE=%NXJ_JAR_BLUECOVE%"
+set NXJ_CP_BLUECOVE=%NXJ_JAR_BLUECOVE%
 
-set "NXJ_CP_BOOT=%NXJ_JAR_CLASSES%"
-set "NXJ_CP_LINK=%NXJ_JAR_BCEL%;%NXJ_JAR_COMMONS_CLI%;%NXJ_JAR_JTOOLS%"
-set "NXJ_CP_TOOL=%NXJ_CP_BLUECOVE%;%NXJ_CP_LINK%;%NXJ_JAR_PCCOMM%;%NXJ_JAR_PCTOOLS%"
+set NXJ_CP_BOOT=%NXJ_JAR_CLASSES%
+set NXJ_CP_LINK=%NXJ_JAR_BCEL%;%NXJ_JAR_COMMONS_CLI%;%NXJ_JAR_JTOOLS%
+set NXJ_CP_TOOL=%NXJ_CP_BLUECOVE%;%NXJ_CP_LINK%;%NXJ_JAR_PCCOMM%;%NXJ_JAR_PCTOOLS%
 
 
 javac -bootclasspath "%NXJ_CP_BOOT%" %*
+:EOF
