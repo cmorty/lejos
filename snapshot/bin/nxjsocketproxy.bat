77cc44d659d430bf26b5c5d817cb30d330c711e2
skoehler <skoehler@06c8f799-c779-4783-8af7-179afedb67c8>
2009-05-10 15:33:40 +0000

    new scripts
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@2604 06c8f799-c779-4783-8af7-179afedb67c8

--- snapshot/bin/nxjsocketproxy.bat
+++ snapshot/bin/nxjsocketproxy.bat
@@ -5,16 +5,19 @@ if "%NXJ_HOME%" == "" goto :home_unset
 	set NXJ_BIN=%NXJ_HOME%\bin
 	goto :home_endif
 :home_unset
-	if "%OS%" == "Windows_NT" goto :home_nt
-	echo You have to set the NXJ_HOME Variable on Windows 9x/ME.
-	goto :EOF
-:home_nt
-	set "NXJ_TEMP=%cd%"
-	cd /d "%0\.."
-	set "NXJ_BIN=%cd%"
-	cd ".."
-	set "NXJ_HOME=%cd%"
-	cd /d "%NXJ_TEMP%"
+	if "%OS%" == "Windows_NT" goto :home_unset_nt
+	REM echo Windows 9x/ME detected. You have to set the NXJ_HOME variable.
+	REM goto :EOF
+	set NXJ_BIN=%0\..
+	set NXJ_HOME=%0\..\..
+	goto :home_endif
+:home_unset_nt
+	call :home_nt_normalize NXJ_BIN "%~dp0\."
+	call :home_nt_normalize NXJ_HOME "%~dp0\.."
+	goto :home_endif
+:home_nt_normalize
+	set "%1=%~f2"
+	goto :eof
 :home_endif
 
 set NXJ_LIBS=%NXJ_HOME%\lib
