accdcfc757efa4a41fad1beabf3f9cb169a9e67d
skoehler <skoehler@06c8f799-c779-4783-8af7-179afedb67c8>
2009-05-21 23:45:04 +0000

    bugfix for bug in handling of broken UTF8
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@2753 06c8f799-c779-4783-8af7-179afedb67c8

--- classes/lejos/charset/UTF8Decoder.java
+++ classes/lejos/charset/UTF8Decoder.java
@@ -2,33 +2,34 @@ package lejos.charset;
 
 public class UTF8Decoder implements CharsetDecoder
 {
+	private static final int MIN_NON_ASCII = 0x80;
+	private static final int MIN_SEQ2_BYTE = 0xC0;
+	private static final int MIN_SEQ3_BYTE = 0xE0;
+	private static final int MIN_SEQ4_BYTE = 0xF0;
+	private static final int MIN_SEQ5_BYTE = 0xF8;
+	
 	public int decode(byte[] source, int offset, int limit)
 	{
 		//assert limit > offset;
 		
 		int first = source[offset] & 0xFF;
-		if (first < 0x80)
+		if (first < MIN_NON_ASCII)
 			return first;
-		if (first < 0xC0 || first >= 0xF8)
+		if (first < MIN_SEQ2_BYTE || first >= MIN_SEQ5_BYTE)
 			return '?';
 
 		int len;
-		if (first < 0xE0)
-		{
-			first &= ~0xE0;
+		if (first < MIN_SEQ3_BYTE)
 			len = 2;
-		}		
-		else if (first < 0xF0)
-		{
-			first &= ~0xF0;
+		else if (first < MIN_SEQ4_BYTE)
 			len = 3;
-		}		
 		else
-		{
-			first &= ~0xF8;
 			len = 4;
-		}	
 		
+		if (len > limit - offset)
+			return '?';
+		
+		first &= 0x3F >> len;
 		for (int i = 1; i < len; i++)
 		{
 			int b = source[offset + i];
@@ -46,24 +47,33 @@ public class UTF8Decoder implements CharsetDecoder
 			return 1;
 		
 		int first = source[offset] & 0xFF;		
-		if (first < 0xC0 || first >= 0xF8)
+		if (first < MIN_SEQ2_BYTE || first >= MIN_SEQ5_BYTE)
 			return 1;
 		
 		int len;
-		if (first < 0xE0)
+		if (first < MIN_SEQ3_BYTE)
 			len = 2;
-		else if (first < 0xF0)
+		else if (first < MIN_SEQ4_BYTE)
 			len = 3;
 		else
 			len = 4;
 		
-		int end = offset + len;
-		if (end > limit)
-			end = limit;
+		int maxlen = limit - offset;
+		if (len > maxlen)
+			len = maxlen;
 		
-		for (int i=offset+1; i<end; i++)
-			if ((source[i] & 0xC0) != 0x80)
-				return i-offset;
+		switch (len)
+		{
+			case 4:
+				if ((source[offset + 3] & 0xC0) != 0x80)
+					len = 3;
+			case 3:
+				if ((source[offset + 2] & 0xC0) != 0x80)
+					len = 2;
+			case 2:
+				if ((source[offset + 1] & 0xC0) != 0x80)
+					len = 1;
+		}
 		
 		return len;
 	}
