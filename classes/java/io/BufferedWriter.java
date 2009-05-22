3496387d2f633138914faac74b358a76e1482a57
skoehler <skoehler@06c8f799-c779-4783-8af7-179afedb67c8>
2009-05-22 16:10:00 +0000

    BufferedWriter implementation
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@2757 06c8f799-c779-4783-8af7-179afedb67c8

--- classes/java/io/BufferedWriter.java
+++ classes/java/io/BufferedWriter.java
@@ -1,5 +1,8 @@
 package java.io;
 
+import lejos.charset.Latin1Encoder;
+import lejos.io.LejosOutputStreamWriter;
+
 /**
  * This is a minimal implementation of BufferedWriter.
  * Normally BufferedWriter extends the abstract class Writer. 
@@ -10,44 +13,141 @@ package java.io;
  * @author BB
  *
  */
-public class BufferedWriter {
-
-	OutputStream out;
+public class BufferedWriter extends Writer
+{
+	private static final int DEFAULT_BUFFERSIZE = 64;
+	private static final int MIN_BUFFERSIZE = 1;
+	
+	private final Writer parent;
+	private final char[] buffer;
+	private int limit;
+	
+	/**
+	 * @deprecated use OutputStreamWriter instead.
+	 */
+	@Deprecated
+	public BufferedWriter(OutputStream os)
+	{
+		this(new LejosOutputStreamWriter(os, new Latin1Encoder(), 32));
+	}
 	
 	/**
 	 * In the standard Java API the constructor accepts a Writer
 	 * object, such as OutputStreamWriter.
 	 * @param out
 	 */
-	public BufferedWriter(OutputStream out) {
-		this.out = out;
+	public BufferedWriter(Writer out)
+	{
+		this(out, DEFAULT_BUFFERSIZE);
 	}
 	
-	/**
-	 * Currently does not translate ASCII escape sequences like \n
-	 * into the appropriate character.
-	 * @param s
-	 */
-	public void write(String s) throws IOException {
-		// NEEDS TO WRITE INT instead of BYTE for PC???
-		for(int i=0;i<s.length();i++)
-			out.write((int)s.toCharArray()[i]);
+	public BufferedWriter(Writer out, int size)
+	{
+		if (size < MIN_BUFFERSIZE)
+			size = MIN_BUFFERSIZE;
 		
-		flush(); // Does real one do this?
+		this.parent = out;
+		this.buffer = new char[size];
 	}
 	
-	/**
-	 * Writes char(13) to the destination.
-	 *
-	 */
-	public void newLine() throws IOException {
-		// !! Might need to write 4 bytes for int value. 
-		out.write(13);
-		out.write(10);
-		flush(); // Does real one do this?
+	@Override
+	public void close() throws IOException
+	{
+		this.flushAny();
+		this.parent.close();
 	}
 	
-	public void flush() throws IOException {
-		out.flush();
+	private void flushAny() throws IOException
+	{
+		if (this.limit > 0)
+		{
+			this.parent.write(this.buffer, 0, this.limit);
+			this.limit = 0;
+		}
+	}
+	
+	private int flushFull() throws IOException
+	{
+		if (this.limit >= this.buffer.length)
+		{
+			this.parent.write(this.buffer, 0, this.limit);
+			this.limit = 0;
+		}
+		return this.buffer.length - this.limit;
+	}
+
+	@Override
+	public void flush() throws IOException
+	{
+		this.flushAny();
+		this.parent.flush();
+	}
+	
+	public void newLine() throws IOException
+	{
+		this.write('\r');
+		this.write('\n');
+	}
+	
+	@Override
+	public Writer append(CharSequence str, int start, int end) throws IOException
+	{
+		int len = end - start;
+		while (len > 0)
+		{
+			int plen = this.flushFull();
+			if (plen > len)
+				plen = len;
+			
+			for (int i=0; i<plen; i++)
+				this.buffer[this.limit + i] = str.charAt(start + i);
+		
+			len -= plen;
+			start += plen;
+			this.limit += plen;
+		}
+		return this;
+	}
+
+	@Override
+	public void write(char[] c, int off, int len) throws IOException
+	{
+		while (len > 0)
+		{
+			int plen = this.flushFull();
+			if (plen > len)
+				plen = len;
+			
+			for (int i=0; i<plen; i++)
+				this.buffer[this.limit + i] = c[off + i];
+		
+			len -= plen;
+			off += plen;
+			this.limit += plen;
+		}
+	}
+
+	@Override
+	public void write(int c) throws IOException
+	{
+		this.flushFull();		
+		this.buffer[this.limit++] = (char)c;
+	}
+
+	@Override
+	public void write(String str, int off, int len) throws IOException
+	{
+		while (len > 0)
+		{
+			int plen = this.flushFull();
+			if (plen > len)
+				plen = len;
+
+			str.getChars(off, off+plen, this.buffer, this.limit);
+		
+			len -= plen;
+			off += plen;
+			this.limit += plen;
+		}
 	}
 }
