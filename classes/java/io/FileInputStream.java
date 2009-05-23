10441038f6f77ec267a57b8074a0943750229bca
skoehler <skoehler@06c8f799-c779-4783-8af7-179afedb67c8>
2009-05-23 14:27:41 +0000

    more efficient FileInputStream
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@2770 06c8f799-c779-4783-8af7-179afedb67c8

--- classes/java/io/FileInputStream.java
+++ classes/java/io/FileInputStream.java
@@ -4,56 +4,83 @@ import lejos.nxt.Flash;
 
 /**
  * Reads a stream of bytes from a file.
+ * This stream uses an internal Buffer of 256 bytes.
  * 
  * @author Brian Bagnall
- *
+ * @author Sven Köhler
  */
-public class FileInputStream extends InputStream {
+public class FileInputStream extends InputStream
+{	
+	private int offset;
+	private int file_limit;
+	private int page_limit;
+	private int first_page;
+	private byte[] buff;
 	
-	int pointer;
-	int page_pointer;
-	int data_pointer; // Current byte in buff array
-	byte [] buff;
-	File file;
-	
-	public FileInputStream(File f) throws FileNotFoundException  {
-        if (!f.exists()) throw new FileNotFoundException();
-        file = f;
-		buff = new byte[Flash.BYTES_PER_PAGE];
-		page_pointer = file.page_location;
-		data_pointer = 0; // Start of page
-		pointer = 0; // Overall mark;
-		Flash.readPage(buff, page_pointer);
+	public FileInputStream(File f) throws FileNotFoundException
+	{
+        if (!f.exists())
+        	throw new FileNotFoundException();
+        
+		this.buff = new byte[Flash.BYTES_PER_PAGE];
+		
+		this.offset = 0;
+		this.file_limit = f.file_length;
+		this.page_limit = 0;
+		this.first_page = f.page_location;
 	}
 	
-	public int available() throws IOException {
-        return file.file_length - pointer;
+	@Override
+	public int available() throws IOException
+	{
+        return this.file_limit - this.offset;
     }
+	
+	private void buffPage()
+	{
+		if (this.offset < this.page_limit)
+			return;
+		
+		int pnum = this.offset / Flash.BYTES_PER_PAGE;
+		Flash.readPage(this.buff, this.first_page + pnum);
+		this.page_limit = (pnum + 1) * Flash.BYTES_PER_PAGE;
+	}
 
-    public int read() throws IOException {
-		// Check against file size for EOF.
-		if(this.available() <= 0)
-			return -1; // Indicates EOF
-				
-		int val = buff[data_pointer];
-		data_pointer++;
-		pointer++;
-		if(data_pointer >= Flash.BYTES_PER_PAGE) {
-			data_pointer = 0;
-			page_pointer++;
-			Flash.readPage(buff, page_pointer);
-		}
-		return val & 0xff; // Need to return 0-255 value
-	}	
-    
-    /**
-     * resets pointers so next read() is from the start of the file;
-     */
-    public void reset()
+    @Override
+	public int read() throws IOException
     {
-        page_pointer = file.page_location;
-        data_pointer = 0; // Start of page
-        pointer = 0; 
-        Flash.readPage(buff, page_pointer);
+		if (this.offset >= this.file_limit)
+			return -1;
+
+		this.buffPage();
+				
+		return buff[this.offset++ % Flash.BYTES_PER_PAGE] & 0xFF;
+	}
+    
+    @Override
+	public int read(byte[] b, int off, int len) throws IOException
+    {
+		if (this.offset >= this.file_limit)
+			return -1;
+
+		int favail = this.file_limit - this.offset;
+		if (len > favail)
+			len = favail;
+
+		int offorig = off;
+		while (len > 0)
+		{
+			this.buffPage();
+			
+			int pavail = this.page_limit - this.offset;
+			if (pavail > len)
+				pavail = len;
+			
+			System.arraycopy(this.buff, this.offset % Flash.BYTES_PER_PAGE, b, off, pavail);
+			this.offset += pavail;
+			off += pavail;
+			len -= pavail;
+		}
+		return off - offorig;
     }
 }
