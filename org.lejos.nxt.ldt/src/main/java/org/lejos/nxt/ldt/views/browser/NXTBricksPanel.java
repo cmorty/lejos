ac7f8c932dda2c0fe63a139d5079b49d52e03d40
mpscholz <mpscholz@06c8f799-c779-4783-8af7-179afedb67c8>
2009-05-24 08:18:04 +0000

    version 0.8 of the plug-in
    git-svn-id: http://svn.code.sf.net/p/lejos/code/tags/eclipse_plugin_0.8@2779 06c8f799-c779-4783-8af7-179afedb67c8

---
+++ org.lejos.nxt.ldt/src/main/java/org/lejos/nxt/ldt/views/browser/NXTBricksPanel.java
@@ -0,0 +1,356 @@
+package org.lejos.nxt.ldt.views.browser;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.HashSet;
+
+import lejos.pc.comm.NXTCommFactory;
+import lejos.pc.comm.NXTConnectionState;
+import lejos.pc.comm.NXTInfo;
+
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.ColumnWeightData;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.ITableLabelProvider;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.TableLayout;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.TableItem;
+import org.lejos.nxt.ldt.LeJOSNXJPlugin;
+import org.lejos.nxt.ldt.comm.IConnectionListener;
+import org.lejos.nxt.ldt.comm.ISearchListener;
+import org.lejos.nxt.ldt.util.LeJOSNXJUtil;
+
+public class NXTBricksPanel {
+
+	private TableViewer bricksTable;
+	private Group bricksGroup;
+	private Collection<IConnectionListener> connectionListeners;
+	private Collection<ISearchListener> searchListeners;
+	private Button connect;
+	private Button detach;
+
+	public NXTBricksPanel(Composite parent) {
+		connectionListeners = new HashSet<IConnectionListener>();
+		searchListeners = new HashSet<ISearchListener>();
+		init(parent);
+
+		// TODO deactivate
+		// NXTBrowserInfo testInfo = new NXTBrowserInfo(new NXTInfo("test",
+		// "xyz"));
+		// NXTBrowserInfo[] testinfos = { testInfo };
+		// updateBricksTable(testinfos);
+	}
+
+	public void addConnectionListener(IConnectionListener listener) {
+		connectionListeners.add(listener);
+	}
+
+	public void removeConnectionListener(IConnectionListener listener) {
+		connectionListeners.remove(listener);
+	}
+
+	public void addSearchlistener(ISearchListener listener) {
+		searchListeners.add(listener);
+	}
+
+	public void removeSearchlistener(ISearchListener listener) {
+		searchListeners.remove(listener);
+	}
+
+	public Control getControl() {
+		return bricksGroup;
+	}
+
+	private void init(Composite parent) {
+		// bricks group
+		bricksGroup = new Group(parent, SWT.NULL);
+		GridLayout layout = new GridLayout();
+		layout.numColumns = 1;
+		GridData gridData = new GridData();
+		gridData.grabExcessHorizontalSpace = true;
+		gridData.horizontalAlignment = GridData.FILL;
+		gridData.grabExcessVerticalSpace = true;
+		gridData.verticalAlignment = GridData.FILL;
+		bricksGroup.setLayoutData(gridData);
+		bricksGroup.setLayout(layout);
+		// bricks table
+		createBricksTable(bricksGroup);
+		// buttons
+		createButtons(bricksGroup);
+	}
+
+	private void createBricksTable(Group parent) {
+		bricksTable = new TableViewer(parent, SWT.SINGLE | SWT.FULL_SELECTION);
+		bricksTable.setContentProvider(new ArrayContentProvider());
+		bricksTable.setLabelProvider(new BricksTableLabelProvider());
+		final Table table = bricksTable.getTable();
+		final TableLayout tableLayout = new TableLayout();
+		table.setLayout(tableLayout);
+		table.setHeaderVisible(true);
+		// table.setLinesVisible(true);
+		GridData gridData = new GridData();
+		gridData.grabExcessHorizontalSpace = true;
+		gridData.horizontalAlignment = GridData.FILL;
+		gridData.grabExcessVerticalSpace = true;
+		gridData.verticalAlignment = GridData.FILL;
+		table.setLayoutData(gridData);
+		final TableColumn nameColumn = new TableColumn(table, SWT.NONE);
+		nameColumn.setText("Name");
+		nameColumn.setWidth(500);
+		ColumnWeightData nameLayoutData = new ColumnWeightData(50, true);
+		tableLayout.addColumnData(nameLayoutData);
+		final TableColumn connectionTypeColumn = new TableColumn(table,
+				SWT.NONE);
+		connectionTypeColumn.setText("Connection type");
+		ColumnWeightData connectionTypeLayoutData = new ColumnWeightData(25,
+				true);
+		tableLayout.addColumnData(connectionTypeLayoutData);
+		final TableColumn statusColumn = new TableColumn(table, SWT.NONE);
+		statusColumn.setText("Status");
+		ColumnWeightData statusLayoutData = new ColumnWeightData(25, true);
+		tableLayout.addColumnData(statusLayoutData);
+		// selection listener
+		table.addSelectionListener(new BricksTableSelectionListener());
+
+	}
+
+	private void createButtons(Composite parent) {
+		Composite buttons = new Composite(parent, SWT.NULL);
+		GridData gridData = new GridData();
+		gridData.horizontalAlignment = SWT.END;
+		buttons.setLayoutData(gridData);
+		GridLayout layout = new GridLayout();
+		layout.numColumns = 3;
+		buttons.setLayout(layout);
+		// connect
+		connect = new Button(buttons, SWT.NULL);
+		connect.setText("Connect");
+		connect.setEnabled(false);
+		connect.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				ISelection selection = bricksTable.getSelection();
+				if ((selection != null)
+						&& (selection instanceof IStructuredSelection)) {
+					Object selected = ((IStructuredSelection) selection)
+							.getFirstElement();
+					if ((selected != null) && (selected instanceof NXTInfo)) {
+						connectToBrick((NXTInfo) selected);
+						// if
+						// (!LeJOSNXJPlugin.getDefault().getConnectionManager()
+						// .connectToBrick(browserInfo)) {
+						// LeJOSNXJUtil.message("Brick "
+						// + browserInfo.name
+						// + " could not be connected");
+						// } else {
+						// // update table
+						// updateTable(browserInfo,
+						// NXTConnectionState.CONNECTED);
+						// // notify listeners
+						// for (IConnectionListener listener :
+						// connectionListeners) {
+						// listener.brickConnected(browserInfo);
+						// }
+						// }
+						//
+					}
+				}
+			}
+		});
+		// detach
+		detach = new Button(buttons, SWT.NULL);
+		detach.setText("Detach");
+		detach.setEnabled(false);
+		detach.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				ISelection selection = bricksTable.getSelection();
+				if ((selection != null)
+						&& (selection instanceof IStructuredSelection)) {
+					Object selected = ((IStructuredSelection) selection)
+							.getFirstElement();
+					if ((selected != null) && (selected instanceof NXTInfo)) {
+						detachFromBrick((NXTInfo) selected);
+						// LeJOSNXJPlugin.getDefault().getConnectionManager()
+						// .detachFromBricks();
+						// NXTInfo browserInfo = (NXTInfo) selected;
+						// // update table
+						// updateTable(browserInfo,
+						// NXTConnectionState.DISCONNECTED);
+						// // notify listeners
+						// for (IConnectionListener listener :
+						// connectionListeners) {
+						// listener.brickDetached(browserInfo);
+						// }
+						// // do a re-search
+						// final NXTInfo[] nxtBricks = LeJOSNXJPlugin
+						// .getDefault().getConnectionManager()
+						// .searchForNXTBricks();
+						// updateBricksTable(nxtBricks);
+						//
+					}
+				}
+			}
+		});
+		// detach all
+		Button detachAll = new Button(buttons, SWT.NULL);
+		detachAll.setText("Detach All");
+		detachAll.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				detachAllBricks();
+			}
+		});
+		// search
+		Button search = new Button(buttons, SWT.NULL);
+		search.setText("Search");
+		search.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				searchForBricks();
+			}
+		});
+	}
+
+	private void searchForBricks() {
+		for (ISearchListener listener : searchListeners) {
+			listener.searchStarted();
+		}
+		NXTInfo[] nxtBricks = null;
+		// close all existing connections
+		try {
+			LeJOSNXJPlugin.getDefault().getConnectionManager().closeAll();
+			// TODO show progress and hour glass
+			nxtBricks = LeJOSNXJPlugin.getDefault().getConnectionManager()
+					.search();
+		} catch (IOException ioExc) {
+			LeJOSNXJUtil.message(ioExc);
+		}
+		for (ISearchListener listener : searchListeners) {
+			listener.searchFinished();
+		}
+		// update bricks table
+		updateBricksTable(nxtBricks);
+		// disable connect and detach buttons
+		connect.setEnabled(false);
+		detach.setEnabled(false);
+
+	}
+
+	// TODO connection state gets not set correctly when brick has been detached before
+	private void connectToBrick(NXTInfo nxtInfo) {
+		if (nxtInfo == null)
+			return;
+		LeJOSNXJUtil.message("Connecting to NXT brick " + nxtInfo.name);
+		if (LeJOSNXJPlugin.getDefault().getConnectionManager().connectToBrick(
+				nxtInfo))
+			LeJOSNXJUtil.message("NXT brick " + nxtInfo.name
+					+ " successfully connected");
+		updateTable(nxtInfo);
+	}
+
+	private void detachFromBrick(NXTInfo nxtInfo) {
+		// TODO detachFromBrick
+	}
+
+	private void detachAllBricks() {
+		try {
+			// close all
+			LeJOSNXJPlugin.getDefault().getConnectionManager().closeAll();
+			// reset table
+			NXTInfo[] infos = (NXTInfo[]) bricksTable.getInput();
+			for (NXTInfo nxtInfo : infos) {
+				nxtInfo.connectionState = NXTConnectionState.DISCONNECTED;
+			}
+			bricksTable.setInput(infos);			
+		} catch (IOException ioExc) {
+			LeJOSNXJUtil.message(ioExc);
+		}
+	}
+
+	private void updateBricksTable(NXTInfo[] nxtBricks) {
+		bricksTable.setInput(nxtBricks);
+	}
+
+	private void updateTable(NXTInfo info) {
+		NXTInfo[] infos = (NXTInfo[]) bricksTable.getInput();
+		for (NXTInfo nxtInfo : infos) {
+			if (nxtInfo.equals(info)) {
+				nxtInfo.connectionState = info.connectionState;
+				break;
+			}
+		}
+		bricksTable.setInput(infos);
+	}
+
+	class BricksTableLabelProvider extends LabelProvider implements
+			ITableLabelProvider {
+
+		public Image getColumnImage(Object element, int columnIndex) {
+			// TODO use brick icon
+			return null;
+		}
+
+		public String getColumnText(Object element, int columnIndex) {
+			if (!(element instanceof NXTInfo))
+				return null;
+			NXTInfo info = (NXTInfo) element;
+			switch (columnIndex) {
+			case 0:
+				return info.name;
+			case 1:
+				if (info.protocol == NXTCommFactory.BLUETOOTH)
+					return "Bluetooth";
+				else
+					return "USB";
+			case 2:
+				return info.connectionState.name();
+			default:
+				return null;
+			}
+		}
+
+	}
+
+	private class BricksTableSelectionListener implements SelectionListener {
+
+		public void widgetDefaultSelected(SelectionEvent e) {
+			// ignore
+		}
+
+		public void widgetSelected(SelectionEvent e) {
+			// get selected brick
+			NXTInfo selectedNxtInfo = null;
+			final Table table = bricksTable.getTable();
+			int selectedIndex = table.getSelectionIndex();
+			if (selectedIndex >= 0) {
+				TableItem item = table.getItem(selectedIndex);
+				if (item != null) {
+					selectedNxtInfo = (NXTInfo) item.getData();
+				}
+			}
+			// enable/disable connect and detach buttons dependent of state of
+			// selected brick
+			if (selectedNxtInfo == null) {
+				connect.setEnabled(false);
+				detach.setEnabled(false);
+			} else {
+				connect.setEnabled(!selectedNxtInfo.connectionState
+						.equals(NXTConnectionState.PACKET_STREAM_CONNECTED));
+				detach.setEnabled(selectedNxtInfo.connectionState
+						.equals(NXTConnectionState.PACKET_STREAM_CONNECTED));
+			}
+		}
+	}
+}
