e90c49818c47721447d35690e2a2730f536c85f5
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2009-06-28 15:41:40 +0000

    More complete Point implementation
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk/classes@2929 06c8f799-c779-4783-8af7-179afedb67c8

--- java/awt/Point.java
+++ java/awt/Point.java
@@ -3,7 +3,7 @@ package java.awt;
 import java.awt.geom.*;
 
 /**
- * Minimal Point implementation.
+ * Represents a point in two dimensional space using integer co-ordinates
  * 
  * @author Lawrie Griffiths
  *
@@ -11,9 +11,17 @@ import java.awt.geom.*;
 public class Point extends Point2D {
 	public int x,y;
 	
+	public Point() {
+		this(0,0);
+	}
+	
 	public Point(int x, int y) {
 		this.x = x;
 		this.y = y;
+	}
+	
+	public Point(Point p) {
+		this(p.x, p.y);
 	}
 
 	@Override
@@ -25,4 +33,46 @@ public class Point extends Point2D {
 	public double getY() {
 		return (double) y;
 	}
+
+	@Override
+	public void setLocation(double x, double y) {
+		this.x = (int) x;
+		this.y = (int) y;
+	}
+	
+	public void setLocation(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public void setLocation(Point p) {
+		setLocation(p.x, p.y);
+	}
+	
+    public void move(int x, int y) {
+        setLocation(x,y);
+    }
+    
+    public void translate(int dx, int dy) {
+        this.x += dx;
+        this.y += dy;
+    }
+    
+    public Point getLocation() {
+        return new Point(x, y);
+    }
+	
+	@Override
+    public String toString() {
+        return "Point[x=" + x + ",y=" + y + "]";
+    }
+	
+	@Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Point) {
+            Point pt = (Point)obj;
+            return (x == pt.x) && (y == pt.y);
+        }
+        return super.equals(obj);
+    }
 }
