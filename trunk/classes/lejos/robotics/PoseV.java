68288ea18134848a9c685698fac5696a4eb9b678
rogergla <rogergla@06c8f799-c779-4783-8af7-179afedb67c8>
2009-07-02 17:57:04 +0000

    PoseV is an experimental Pose, using Vector2D, which represents  mathametical a mathematical 2 dimensional vector.

---
+++ trunk/classes/lejos/robotics/PoseV.java
@@ -0,0 +1,88 @@
+package lejos.robotics;
+
+/**
+ * Exeprimental  Pose  Uses a Vector2D to contntain the location
+ *
+ * @author owner
+ */
+public class PoseV
+{
+
+public PoseV(float x, float y, float heading)
+{
+  _location = new Vector2D(x,y);
+  _heading = heading;
+
+}
+/**
+ * return distance to destination
+ * @param destination
+ * @return distance to destination
+ */
+public float distanceTo(Vector2D destination)
+{
+  return destination.subtract(_location).getR();
+}
+/**
+ * return the true bearing (direction angle) of the  destination
+ * @param destination
+ * @return angle to destination
+ */
+public float angleTo(Vector2D destination)
+{
+  return destination.subtract(_location).getTheta();
+}
+/**
+ * rotate the pose through  angle  degrees 
+ * @param angle
+ */
+public void rotate(float angle)
+{
+  _heading += angle;
+}
+/**
+ * rotate the pose so the new heading is  angle
+ * @param angle
+ */
+public void rotateTo(float newAngle)
+{
+  rotate(newAngle - _heading);
+}
+/**
+ * translate the pose by the move vector
+ * @param move
+ */
+public void translate(Vector2D move)
+{
+  _location = _location.add(move);
+}
+/**
+ * move  the pose in the directin of its heading by an amount distance
+ */
+public void move(float distance)
+{
+  Vector2D  move = new Vector2D();
+  move.setPolarValues(distance, _heading);
+  translate(move);
+}
+/**
+ * return X coordinate of the pose
+ * @return
+ */
+public float getX(){ return _location.getX();}
+/**
+ * return Y coordinate of the pose
+ * @return
+ */
+public float getY(){return _location.getY();}
+/**
+ * return the heading (direction angle) of the pose
+ * @return
+ */
+public float getHeading(){ return _heading ;}
+
+private Vector2D _location;
+private float _heading;
+
+}
+
