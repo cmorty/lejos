5dd4622dcd2d494da66cf8d7cdcef4d4789c4187
gloomyandy <gloomyandy@06c8f799-c779-4783-8af7-179afedb67c8>
2009-07-05 20:36:33 +0000

    Added warnings
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk/classes@2948 06c8f799-c779-4783-8af7-179afedb67c8

--- lejos/robotics/PoseV.java
+++ lejos/robotics/PoseV.java
@@ -4,6 +4,10 @@ package lejos.robotics;
  * Exeprimental  Pose  Uses a Vector2D to contntain the location
  *
  * @author owner
+ *
+ * <br/><br/>WARNING: THIS CLASS IS SHARED BETWEEN THE classes AND pccomms PROJECTS.
+ * DO NOT EDIT THE VERSION IN pccomms AS IT WILL BE OVERWRITTEN WHEN THE PROJECT IS BUILT.
+
  */
 public class PoseV
 {
--- lejos/robotics/Vector2D.java
+++ lejos/robotics/Vector2D.java
@@ -10,6 +10,10 @@ package lejos.robotics;
  *  all retun values are float because the trig functions do not achieve double
  * precision yet 
  * @author Roger Glassey
+ *
+ * <br/><br/>WARNING: THIS CLASS IS SHARED BETWEEN THE classes AND pccomms PROJECTS.
+ * DO NOT EDIT THE VERSION IN pccomms AS IT WILL BE OVERWRITTEN WHEN THE PROJECT IS BUILT.
+
 */
 
 public class Vector2D {
