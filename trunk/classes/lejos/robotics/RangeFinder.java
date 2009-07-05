087821839ed7696c6af82c0994cdeb8a68856bf4
gloomyandy <gloomyandy@06c8f799-c779-4783-8af7-179afedb67c8>
2009-07-05 20:36:33 +0000

    Added warnings

--- trunk/classes/lejos/robotics/RangeFinder.java
+++ trunk/classes/lejos/robotics/RangeFinder.java
@@ -5,6 +5,9 @@ package lejos.robotics;
  * 
  * @author Lawrie Griffiths
  *
+ * <br/><br/>WARNING: THIS CLASS IS SHARED BETWEEN THE classes AND pccomms PROJECTS.
+ * DO NOT EDIT THE VERSION IN pccomms AS IT WILL BE OVERWRITTEN WHEN THE PROJECT IS BUILT.
+
  */
 public interface RangeFinder {
 	/**
