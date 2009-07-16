0af8fa27f29c4b9f931453a99b329edeaf383df2
rogergla <rogergla@06c8f799-c779-4783-8af7-179afedb67c8>
2009-07-16 00:32:55 +0000

    Corrected typos
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk/classes@2974 06c8f799-c779-4783-8af7-179afedb67c8

--- lejos/robotics/navigation/Navigator1.java
+++ lejos/robotics/navigation/Navigator1.java
@@ -1,14 +1,13 @@
 package lejos.robotics.navigation;
-
+import lejos.robotics.Pose;
 
 
 /**
-* The Navigator interface contains methods for performing basic navigational
-* movements.  <br>
+* The Navigator interface contains methods for moving to a destination.
 * <b>A note about coordinates:</b>  Angles that relate to positions in the plane are in degrees relative to the
  * direction of the X axis.  The direction of the Y axis is 90 degrees.
 *
-* @version 0.8  - June-2009
+* @version 0.8  - July-2009
 */
 public interface Navigator1
 {
@@ -19,7 +18,7 @@ public interface Navigator1
   public void stop();
  
   /**
-   * Sets robot location (x,y) and direction angle
+   * Sets robot pose: location (x,y) and direction angle
    * @param x  the x coordinate of the robot
    * @param y the y coordinate of the robot
    * @param directionAngle  the angle the robot is heading, measured from the x axis
@@ -31,35 +30,34 @@ public interface Navigator1
    * @param aPose the new robot pose
    * @param directionAngle  the angle the robot is heading, measured from the x axis
    */
-  public void setPost(Pose aPose);
+  public void setPose(Pose aPose);
 /**
  * returns the current pose of the robot
  */
   public  Pose getPose();
 
   /**
-   * Rotates the NXT robot towards the target point and moves the required distance.
-   * Method returns when the point is reached, and the robot position is updated;
+   * Rotates the NXT robot towards the destination point (X,Y) and moves the required distance.
+   * Method returns when the destination is rached, and the robot pose is updated;
    * @param x The x coordinate to move to.
    * @param y The y coordinate to move to.
    */
   public void goTo(float x, float y);
 
   /**
-   * Rotates the NXT robot towards the target point (x,y)  and moves the required distance.
-   * If immediateReturnis true, method returns immediately and your code MUST call updatePostion()
-   * when the robot has stopped.  Otherwise, the robot position is lost.
+   * Rotates the NXT robot towards the destination  point (x,y)  and moves the required distance.
+   * If immediateReturnis true, method returns immediately and your code MUST call updatePose()
+   * or stop()  Otherwise, the robot position is lost.
    * @param x The x coordinate to move to.
    * @param y The y coordinate to move to.
-   * @param immediateReturn if true,  method returns immediately and your code must call
-   * updatePosition() before the robot moves again.
+   * @param immediateReturn if true,  method returns immediately.
    */
    public void goTo(float x, float y, boolean immediateReturn);
 
 
   /**
-   * Updates robot location (x,y) and direction angle. Called by stop, and movement commands that terminate when complete.
-   * Must be called after a command that returns immediately, but after robot movement stops, and before another movement method is called.
+   * Updates robot location (x,y) and direction angle. Called by stop, and goTo(x,y)
+   * Must by called after goTo(x,y,true) unless stop() is called;
    */ 
   public void updatePose();
 }
