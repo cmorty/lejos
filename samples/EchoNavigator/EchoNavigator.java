54a565d998d4059aeb941a4f49b604470665e92b
rogergla <rogergla@06c8f799-c779-4783-8af7-179afedb67c8>
2009-07-24 05:38:54 +0000

    Updated examples of users of SimpleNavigator
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@2993 06c8f799-c779-4783-8af7-179afedb67c8

--- samples/EchoNavigator/EchoNavigator.java
+++ samples/EchoNavigator/EchoNavigator.java
@@ -4,11 +4,13 @@ import java.util.Random;
 
  
 /**
- * EchoNavigator is a path planning robot that attempts to avoid obstacles and still
- * reach its destination.  Requires  two touch sensors.
+ * EchoNavigator is a obstacle avoiding  robot that attempts reach its destination.
+ *  Uses SimpleNavigator
+ * Hareware rquirements:   an ultrasonic sensor mounted on a vertical axle
+ * driven by the  third motor.
  * Since it relies on dead reckoning to keep track of its
  * location, the accuracy of navigation degrades with each obstacle.  Does not
- * mep the obstacles, but uses a randomized strategy of avoidance.
+ * mep the obstacles, but uses a randomized avoiding strategy.
  * @author Roger
  */
 public class EchoNavigator
@@ -22,31 +24,33 @@ public class EchoNavigator
 
 /**
  * attempt to reach a destinaton at coordinates x,y despite obstacle.
- * @param x
- * @param y
+ * @param x coordinate of destination
+ * @param y coordinate of destination.
  */
 
 public void goTo(float x, float y)
 {
-  navigator.getPilot().setMoveSpeed(20);
-  navigator.getPilot().setTurnSpeed(180);
+  navigator.setMoveSpeed(20);
+  navigator.setTurnSpeed(180);
   float destX = x;
   float destY = y;
   while (navigator.distanceTo(destX,destY) > 5)
   {
     navigator.goTo(destX, destY,true);
-    boolean clear  = move();
-    if (!clear)
+    boolean clear  = readDistance();
+    if (!clear) //  obstacle found
     {
-      while (!avoid()) Thread.yield();
+      while (!avoid()) Thread.yield();  // keeps calling avoid until no obstacle is in view
     }
   }
 }
 /**
- * @param side  on which hit was detected
- * @return  side on which hit was detected during move
+ * backs up, rotates away from the obstacle, and travels forward;
+ * returns true if no obstacle was discovered while traveling
+ * calls readSensor()
+ * @return
  */
-  public boolean  avoid()
+  private  boolean  avoid()
   {
       int leftDist = 0;
       int rightDist = 0;
@@ -63,29 +67,28 @@ public void goTo(float x, float y)
          if(leftDist>rightDist) turnDirection = 1;
          else turnDirection = -1;
          more = leftDist < _limit && _limit< _limit;
-         if(more) navigator.getPilot().travel(-4);
+         if(more) navigator.travel(-4);
          LCD.drawInt(leftDist,4,0,5);
          LCD.drawInt(rightDist,4,8,5);
       }
       scanner.rotateTo(0);
-
-
     navigator.travel(-10 - rand.nextInt(10));
     int angle = 60+rand.nextInt(60);
-    navigator.getPilot().rotate(turnDirection * angle);
-    navigator.getPilot().travel(10 + rand.nextInt(60), true);
-    return  move ();  // watch for hit while moving forward
+    navigator.rotate(turnDirection * angle);
+    navigator.travel(10 + rand.nextInt(60), true);
+    return  readDistance ();  // watch for hit while moving forward
   }
   /**
-   * stop when movement is completed or hit is detected
-   * @return side on which hit was detected;  0 means none.
+   * Monitors the ultrasonic sensor while the robot is moving.
+   * Returns if an obstacle is detected or if the robot stops
+   * @return false if obstacle was detected
    */
-  public boolean move()
+  public boolean readDistance()
   {
-    System.out.println(" MOVE ");
+    System.out.println(" Moving ");
     int distance = 255;
     boolean clear = true;
-    while( navigator.getPilot().isMoving()& distance > _limit )
+    while( navigator.isMoving()& distance > _limit )
     {
       distance = sonar.getDistance();
       LCD.drawString("D "+distance, 0, 0);
@@ -96,10 +99,19 @@ public void goTo(float x, float y)
     navigator.stop();
     return clear;
   }
-  SimpleNavigator navigator ;
+    public static void main(String[] args)
+    {
+      System.out.println("Any Button");
+      TachoPilot p = new TachoPilot(5.6f, 14.2f, Motor.A, Motor.C);
+      EchoNavigator  robot  = new EchoNavigator( new SimpleNavigator(p),SensorPort.S3, Motor.B);
+      Button.waitForPress();
+      robot.goTo(200,0);
+    }
+
+  public SimpleNavigator navigator ;
   Random rand = new Random();
   UltrasonicSensor sonar;
   private Motor scanner;
-  int _limit = 15;
+  int _limit =20; //cm
 
 }
