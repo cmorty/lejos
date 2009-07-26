91a1f2d44f53dd286bbcc5e45cf9657776b0ca29
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2009-07-26 14:42:05 +0000

    Added common class warning

--- trunk/classes/lejos/robotics/DCMotor.java
+++ trunk/classes/lejos/robotics/DCMotor.java
@@ -1,5 +1,10 @@
 package lejos.robotics;
 
+/*
+ * WARNING: THIS CLASS IS SHARED BETWEEN THE classes AND pccomms PROJECTS.
+ * DO NOT EDIT THE VERSION IN pccomms AS IT WILL BE OVERWRITTEN WHEN THE PROJECT IS BUILT.
+ */
+
 /**
  * Interface for a regular DC motor with no encoder.
  * TODO: Reconcile this with lejos.nxt.BasicMotor. Maybe BasicMotor implements this? 
--- trunk/classes/lejos/robotics/TachoMotor.java
+++ trunk/classes/lejos/robotics/TachoMotor.java
@@ -1,5 +1,10 @@
 package lejos.robotics;
 
+/*
+ * WARNING: THIS CLASS IS SHARED BETWEEN THE classes AND pccomms PROJECTS.
+ * DO NOT EDIT THE VERSION IN pccomms AS IT WILL BE OVERWRITTEN WHEN THE PROJECT IS BUILT.
+ */
+
 /**
  * Interface for encoded motors without limited range of movement (e.g. NXT motor).
  * TODO: Alternate names: EncodedMotor,EncoderMotor. 
