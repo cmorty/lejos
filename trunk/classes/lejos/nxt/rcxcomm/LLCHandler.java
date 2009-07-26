28d026ffd8dd48c69b73ec98854fa906cfeada6d
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2009-07-26 12:14:09 +0000

    Moved common classes warning out of javadocs

--- trunk/classes/lejos/nxt/rcxcomm/LLCHandler.java
+++ trunk/classes/lejos/nxt/rcxcomm/LLCHandler.java
@@ -1,14 +1,17 @@
 package lejos.nxt.rcxcomm;
 
 import lejos.nxt.*;
+
+/*
+ * WARNING: THIS CLASS IS SHARED BETWEEN THE classes AND pccomms PROJECTS.
+ * DO NOT EDIT THE VERSION IN pccomms AS IT WILL BE OVERWRITTEN WHEN THE PROJECT IS BUILT.
+ */
+
 /**
  * Packet handler than implement the LLC packet protocol.
  * Deals with packets and acks. 
  * Supports independent streams of data in both directions.
  * 
- * <br/><br/>WARNING: THIS CLASS IS SHARED BETWEEN THE classes AND pccomms PROJECTS.
- * DO NOT EDIT THE VERSION IN pccomms AS IT WILL BE OVERWRITTEN WHEN THE PROJECT IS BUILT.
- *
  **/
 public class LLCHandler extends PacketHandler {
 
