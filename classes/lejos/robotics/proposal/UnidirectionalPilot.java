43154f6627ee74d3f5305c7e53b50e8c71635d52
bbagnall <bbagnall@06c8f799-c779-4783-8af7-179afedb67c8>
2009-07-31 02:03:03 +0000

    Renamed SimplePilot as UnidirectionalPilot in proposal.
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@3062 06c8f799-c779-4783-8af7-179afedb67c8

--- classes/lejos/robotics/proposal/UnidirectionalPilot.java
+++ classes/lejos/robotics/proposal/UnidirectionalPilot.java
@@ -1,7 +1,7 @@
 package lejos.robotics.proposal;
 
 /**
- * Added non-functional SimplePilot with three methods under Lawrie's instructions. I applaud the idea that we would 
+ * Added non-functional UnidirectionalPilot with three methods under Lawrie's instructions. I applaud the idea that we would 
  * allow robots with limited functionality in here, but it seems too theoretical at this point. I disagree this is 
  * useful, since a Pilot MUST be able to change heading in order to travel to different places. A robot that can't 
  * change heading is useless to us in the API. All robots, whether analog steering, walking, or differential, can 
@@ -12,7 +12,7 @@ package lejos.robotics.proposal;
  * that can't more or rotate? It's too abstract in my opinion, and there is nothing stopping us from decomposing
  * these interfaces later without having to deprecate methods. 
  * 
- * However: Some robots probably can't go backward. So maybe we should have Pilot or SimplePilot that can change heading, and
+ * However: Some robots probably can't go backward. So maybe we should have Pilot or UnidirectionalPilot that can change heading, and
  * go forward only, and then a ReversablePilot that can also go backward?
  * 
  * Or maybe break down each of these movements into an interface, and then build different interfaces using these?
@@ -25,7 +25,7 @@ package lejos.robotics.proposal;
  * @author NXJ Team
  *
  */
-public interface SimplePilot {
+public interface UnidirectionalPilot {
 	/**
 	   *Starts the  NXT robot moving  forward.
 	   */
