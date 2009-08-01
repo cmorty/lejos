1903e83550e86a43cf5b589a991977174d4a292d
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2009-08-01 10:16:02 +0000

    Implementation of DeadReckoningPoseProvider using code from old SimpleNavigator updatePose method
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@3068 06c8f799-c779-4783-8af7-179afedb67c8

--- classes/lejos/robotics/proposal/DeadReckonerPoseProvider.java
+++ classes/lejos/robotics/proposal/DeadReckonerPoseProvider.java
@@ -13,8 +13,9 @@ import lejos.robotics.Pose;
  * Alternate names:  DeadReckonerPoseProvider, DeadReckoner, OrienteeringPoseProvider, OdometryPoseProvider 
  * 
  */
-public class DeadReckonerPoseProvider implements PoseProvider {
-
+public class DeadReckonerPoseProvider implements PilotListener, PoseProvider {
+	private float x = 0, y = 0, heading = 0;
+	
 	/**
 	 * Internally, the constructor  listens to movements from the Pilot. This allows it to keep
 	 * track of all vector movements made.
@@ -22,12 +23,52 @@ public class DeadReckonerPoseProvider implements PoseProvider {
 	 * @param pilot
 	 */
 	public DeadReckonerPoseProvider(Pilot pilot) {
-		
+		pilot.addPilotListener(this);
 	}
 		
 	public Pose getPose() {
-		// TODO Auto-generated method stub
-		return null;
+		return new Pose(x,y,heading);
 	}
 
+	public void movementStarted(Movement event,
+			lejos.robotics.navigation.Pilot p) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void movementStopped(Movement event,
+			lejos.robotics.navigation.Pilot p) {
+		float angle = event.getAngleTurned();
+		float distance = event.getDistanceTraveled();
+        double dx = 0, dy = 0;
+        double headingRad = (Math.toRadians(heading));
+        
+        if (Math.abs(angle) > .5) { // rotate or arc
+            double turnRad = Math.toRadians(angle);
+            double radius = event.getArcRadius(); // zero for rotate
+            dy = radius * (Math.cos(headingRad) - Math.cos(headingRad + turnRad));
+            dx = radius * (Math.sin(headingRad + turnRad) - Math.sin(headingRad));
+        } else if (Math.abs(distance) > .01) { // travel
+            dx = distance * (float) Math.cos(headingRad);
+            dy = distance * (float) Math.sin(headingRad);
+        }
+        
+        heading = normalize(heading + angle); // keep angle between -180 and 180
+        x += dx;
+        y += dy;		
+	}
+
+    /*
+     * returns equivalent angle between -180 and +180
+     */
+    private float normalize(float angle) {
+        float a = angle;
+        while (a > 180) {
+            a -= 360;
+        }
+        while (a < -180) {
+            a += 360;
+        }
+        return a;
+    }
 }
