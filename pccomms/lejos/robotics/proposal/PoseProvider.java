d0e2683fb82344f82b16c96fd7733c13a919184b
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2009-08-04 13:02:16 +0000

    Copy more classes including the proposal classes as they are used by the new version of MCL
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@3098 06c8f799-c779-4783-8af7-179afedb67c8

---
+++ pccomms/lejos/robotics/proposal/PoseProvider.java
@@ -0,0 +1,14 @@
+package lejos.robotics.proposal;
+
+import lejos.robotics.Pose;
+
+/**
+ * Provides the coordinate and heading info via a Pose object.
+ * @author NXJ Team
+ *
+ */
+public interface PoseProvider {
+	
+	public Pose getPose();
+		
+}
---
+++ pccomms/lejos/robotics/proposal/SimplePathFinder.java
@@ -0,0 +1,45 @@
+package lejos.robotics.proposal;
+
+import lejos.geom.Point;
+import lejos.robotics.Pose;
+
+/**
+ * This class directs a pilot from the current known coordinates to a destination set of coordinates.
+ * The SimplePathPlanner is not capable of avoiding objects or planning a route. It can only drive in a straight
+ * line that is not obstructed.  
+ * 
+ *
+ */
+public class SimplePathFinder implements PathFinder {
+	private ArcPilot pilot;
+	private PoseProvider poseProvider;
+	
+	public SimplePathFinder(ArcPilot pilot, PoseProvider poseProvider) {
+		this.pilot = pilot;
+		this.poseProvider = poseProvider;
+	}
+	
+	public ArcPilot getPilot() {
+		return pilot;
+	}
+
+	public PoseProvider getPoseProvider() {
+		return poseProvider;
+	}
+
+	public void setPoseProvider(PoseProvider replacement) {
+		poseProvider = replacement;
+	}
+
+	public Pose goTo(Point destination) {
+		Pose pose = poseProvider.getPose();
+		if (pilot instanceof RotatePilot) { // optimize for RotatePilot
+			((RotatePilot) pilot).rotate(pose.angleTo(destination));
+			pilot.travel(pose.distanceTo(destination));			
+		} else {
+			//TODO: Calculate arc of minimum radius needed to point to the
+			// destination and the do an arc and a travel.
+		}
+		return poseProvider.getPose();
+	}
+}
