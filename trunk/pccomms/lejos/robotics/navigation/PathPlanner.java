3dce53328561333aa247834cb17b17612e7e4b1c
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2009-08-10 10:29:11 +0000

    Deleted obsolete classes

--- trunk/pccomms/lejos/robotics/navigation/PathPlanner.java
+++
@@ -1,5 +0,0 @@
-package lejos.robotics.navigation;
-
-public interface PathPlanner {
-	public void goTo(float x, float y);
-}
--- trunk/pccomms/lejos/robotics/navigation/PilotListener.java
+++
@@ -1,5 +0,0 @@
-package lejos.robotics.navigation;
-
-public interface PilotListener {
-	public void move(float angle, float distance);
-}
