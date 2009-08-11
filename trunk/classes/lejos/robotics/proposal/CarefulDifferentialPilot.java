8c35b12d453fd983b70fcd31b0d011b47168d116
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2009-08-11 11:41:58 +0000

    Added CarefulDifferentialPilot which stops if it detects an obstacle

---
+++ trunk/classes/lejos/robotics/proposal/CarefulDifferentialPilot.java
@@ -0,0 +1,24 @@
+package lejos.robotics.proposal;
+
+import lejos.robotics.RangeFinder;
+import lejos.robotics.TachoMotor;
+
+public class CarefulDifferentialPilot extends DifferentialPilot {
+	private RangeFinder rangeFinder;
+	private float tolerance;
+
+	public CarefulDifferentialPilot(
+			RangeFinder rangeFinder, float tolerance,
+			float wheelDiameter, float trackWidth,
+			TachoMotor leftMotor, TachoMotor rightMotor, boolean reverse) {
+		super(wheelDiameter, trackWidth, leftMotor, rightMotor, reverse);
+		this.rangeFinder = rangeFinder;
+		this.tolerance = tolerance;
+	}
+
+	@Override
+	protected boolean continueMoving() {
+		return (rangeFinder.getRange() > tolerance);
+	}
+
+}
