0b65f4b90d272839327464e9ce1525b5b301d440
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2009-08-11 09:20:30 +0000

    Added the ability to validate a pose against a way point
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@3149 06c8f799-c779-4783-8af7-179afedb67c8

--- classes/lejos/robotics/proposal/WayPoint.java
+++ classes/lejos/robotics/proposal/WayPoint.java
@@ -6,7 +6,8 @@ import lejos.robotics.Pose;
 public class WayPoint extends Point {
 	protected float heading = 0;
 	protected boolean headingRequired;
-	protected float maxDistanceError = -1;
+	protected float maxPositionError = -1;
+	protected float maxHeadingError = -1;
 	
 	public WayPoint(float x, float y) {
 		super(x,y);
@@ -28,15 +29,35 @@ public class WayPoint extends Point {
 		return heading;
 	}
 	
-	public float getMaxDistanceError() {
-		return maxDistanceError;
+	public float getMaxPositionError() {
+		return maxPositionError;
 	}
 	
-	public void setMaxDistanceError(float distance) {
-		maxDistanceError = distance;
+	public void setMaxPositionError(float distance) {
+		maxPositionError = distance;
+	}
+	
+	public float getMaxHeadingError() {
+		return maxHeadingError;
+	}
+	
+	public void setMaxHeadingError(float distance) {
+		maxHeadingError = distance;
 	}
 	
 	public Pose getPose() {
 		return new Pose(x,y,heading);
+	}
+	
+	/**
+	 * Check that the given pose satisfies the conditions for this way point
+	 * @param p the Pose
+	 */
+	public boolean checkValidity(Pose p) {
+		if (maxPositionError >= 0 && 
+		    p.distanceTo(this) > maxPositionError) return false;
+		if (headingRequired && maxHeadingError >= 0 && 
+			    Math.abs(p.getHeading() - heading) > maxPositionError) return false;
+		return true;
 	}
 }
