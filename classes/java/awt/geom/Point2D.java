08950459d46411582d80fbb69af2f3347d8f9b84
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2009-08-21 09:01:19 +0000

    More geometry
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@3270 06c8f799-c779-4783-8af7-179afedb67c8

--- classes/java/awt/geom/Point2D.java
+++ classes/java/awt/geom/Point2D.java
@@ -12,10 +12,26 @@ public abstract class Point2D {
 	 * A point with float coordinates.
 	 */
 	public static class Float extends Point2D {
-		public float x, y;
+		/**
+		 * The x coordinate of the point
+		 */
+		public float x;
+		/**
+		 * The y coordinate of the point
+		 */
+		public float y;
 		
+		/**
+		 * Create a point at (0,0) with float coordinates
+		 */
 		public Float() {}
 		
+		/**
+		 * Create a point at (x,y) with float coordinates
+		 * 
+		 * @param x the x coordinate
+		 * @param y the y coordinate
+		 */
 		public Float(float x, float y) {
 			this.x = x;
 			this.y = y;
@@ -37,6 +53,12 @@ public abstract class Point2D {
 			this.y = (float) y;			
 		}
 		
+		/**
+		 * Set the location of the point
+		 * 
+		 * @param x the new x coordinate
+		 * @param y the new y coordinate
+		 */
 		public void setLocation(float x, float y) {
 			this.x = x;
 			this.y = y;			
@@ -52,10 +74,26 @@ public abstract class Point2D {
 	 * A point with double coordinates.
 	 */
 	public static class Double extends Point2D {
-		public double x,y;
+		/**
+		 * The x coordinate of the point
+		 */
+		public double x;
+		/**
+		 * The y coordinate of the point
+		 */
+		public double y;
 		
+		/**
+		 * Create a point at (0,0) with double coordinates
+		 */
 		public Double() {}
 		
+		/**
+		 * Create a point at (x,y) with double coordinates
+		 * 
+		 * @param x the x coordinate
+		 * @param y the y coordinate
+		 */
 		public Double(double x, double y) {
 			this.x = x;
 			this.y = y;
@@ -84,24 +122,31 @@ public abstract class Point2D {
 	}
 	
 	/**
-	 * Get the x co-ordinate as a double
+	 * This is abstract class that cannot be instantiated.
+	 * Use one of its subclasses.
+	 */
+    protected Point2D() {
+    }
+    
+	/**
+	 * Get the x coordinate as a double
 	 * 
 	 * @return the x co-ordinate (as a double) 
 	 */
 	public abstract double getX();
 	
 	/**
-	 * Get the y co-ordinate as a double
+	 * Get the y coordinate as a double
 	 * 
-	 * @return the y co-ordinate (as a double)
+	 * @return the y coordinate (as a double)
 	 */
 	public abstract double getY();
 	
 	/**
-	 * Set the location of this Point2D using double co-ordinates
+	 * Set the location of this Point2D using double coordinates
 	 * 
-	 * @param x the x co-ordinate
-	 * @param y the y co-ordinate
+	 * @param x the new x coordinate
+	 * @param y the new y coordinate
 	 */
 	public abstract void setLocation(double x, double y);
 	
@@ -114,34 +159,81 @@ public abstract class Point2D {
         setLocation(p.getX(), p.getY());
     }
     
+    /**
+     * Get the square of the distance between two points
+     * 
+     * @param x1 the x coordinate of the first point
+     * @param y1 the y coordinate of the first point
+     * @param x2 the x coordinate of the second point
+     * @param y2 the y coordinate of the second point
+     * @return the square of the distance between the points as a double
+     */
     public static double distanceSq(double x1, double y1, double x2, double y2) {
     	double tx = x1 - x2;
     	double ty = y1 - y2;
     	return (tx * tx + ty * ty);
     }
     
+    /**
+     * Get the the distance between two points
+     * 
+     * @param x1 the x coordinate of the first point
+     * @param y1 the y coordinate of the first point
+     * @param x2 the x coordinate of the second point
+     * @param y2 the y coordinate of the second point
+     * @return the distance between the points as a double
+     */
     public static double distance(double x1, double y1, double x2, double y2) {
     	return Math.sqrt(distanceSq(x1,y1,x2,y2));
     }
     
+    /**
+     * Get the square of the distance between two points
+     * 
+     * @param px the first point
+     * @param py the second point
+     * @return the square of the distance between the points as a double
+     */
     public double distanceSq(double px, double py) {
         double tx = px - getX();
         double ty = py - getY();
         return (tx * tx + ty * ty);
     }
-    
+    /**
+     * Get the square of the distance of this point to a given point
+     * 
+     * @param pt the given point
+     * @return the square of the distance to the given point as a double
+     */
     public double distanceSq(Point2D pt) {
         return distanceSq(pt.getX(), pt.getY());
     }
     
+    /**
+     * Get the distance from this point to a given point as a double
+     * 
+     * @param px the x coordinate of the given point
+     * @param py the y coordinate of the given point
+     * @return the distance to the given point as a double
+     */
     public double distance(double px, double py) {
     	return Math.sqrt(distanceSq(px,py));
     }
     
+    /**
+     * Get the distance from this point to a given point asa double
+     * 
+     * @param pt the given point
+     * @return the distance to the given point as a double
+     */
     public double distance(Point2D pt) {
     	return Math.sqrt(distanceSq(pt));
     }
     
+    /**
+     * Test if this point is equal to a given object
+     */
+    @Override
     public boolean equals(Object obj) {
 	    if (obj instanceof Point2D) {
 	        Point2D p2d = (Point2D) obj;
