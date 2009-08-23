b80c5a04fe4df4ad04c43ca5644a019c11ba4bc5
gloomyandy <gloomyandy@06c8f799-c779-4783-8af7-179afedb67c8>
2009-08-23 21:22:53 +0000

    Increase reserved Flash space

--- trunk/pctools/lejos/pc/tools/NXJFlashUpdate.java
+++ trunk/pctools/lejos/pc/tools/NXJFlashUpdate.java
@@ -12,7 +12,7 @@ import java.io.*;
  */
 public class NXJFlashUpdate {
 
-	private static final int MAX_FIRMWARE_PAGES = 352;
+	private static final int MAX_FIRMWARE_PAGES = 368;
 	private static final int TOTAL_PAGES = 1024;
 	private static final int SETTINGS_PAGES = 1;
 	private static final int DIRECTORY_PAGES = 2;
