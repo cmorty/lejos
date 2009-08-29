1f776fd52c5f665083b3ac90be800bf580429d4b
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2009-08-29 14:11:23 +0000

    More geometry
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@3341 06c8f799-c779-4783-8af7-179afedb67c8

--- classes/java/awt/Point.java
+++ classes/java/awt/Point.java
@@ -9,17 +9,38 @@ import java.awt.geom.*;
  *
  */
 public class Point extends Point2D {
-	public int x,y;
+	/**
+	 * The x coordinate of the point
+	 */
+	public int x;
+	/**
+	 * The y coordinate of the point
+	 */
+	public int y;
 	
+	/**
+	 * Create a point at (0,0)
+	 */
 	public Point() {
 		this(0,0);
 	}
 	
+	/**
+	 * Create a point at (x,y)
+	 * 
+	 * @param x the x coordinate
+	 * @param y the y coordinate
+	 */
 	public Point(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
 	
+	/**
+	 * Create a point from a given point
+	 * 
+	 * @param p the given point
+	 */
 	public Point(Point p) {
 		this(p.x, p.y);
 	}
@@ -40,28 +61,59 @@ public class Point extends Point2D {
 		this.y = (int) y;
 	}
 	
+	/**
+	 * Set the location of the point using integer coordinates
+	 * 
+	 * @param x the x coordinate
+	 * @param y the y coordinate
+	 */
 	public void setLocation(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
 	
+	/**
+	 * Set the location of the point to a given point
+	 * 
+	 * @param p the given point
+	 */
 	public void setLocation(Point p) {
 		setLocation(p.x, p.y);
 	}
 	
+	/**
+	 * Move the location of the point to (x,y)
+	 * 
+	 * @param x the new x coordinate
+	 * @param y the new y coordinate
+	 */
     public void move(int x, int y) {
         setLocation(x,y);
     }
     
+    /**
+     * Move the point by the vector (dx, dy)
+     * 
+     * @param dx the increment of the x coordinate
+     * @param dy the increment of the y coordinate
+     */
     public void translate(int dx, int dy) {
         this.x += dx;
         this.y += dy;
     }
     
+    /**
+     * Get the location of the point
+     * 
+     * @return the location (i.e. the point itself)
+     */
     public Point getLocation() {
         return new Point(x, y);
     }
 	
+    /** 
+     * Represent the point as a String
+     */
 	@Override
     public String toString() {
         return "Point[x=" + x + ",y=" + y + "]";
