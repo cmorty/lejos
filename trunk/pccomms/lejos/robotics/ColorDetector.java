bfdd42546f21593cfac28aaeb233f4c6e5f74d45
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2009-09-01 16:49:11 +0000

    Copied classes

--- trunk/pccomms/lejos/robotics/ColorDetector.java
+++ trunk/pccomms/lejos/robotics/ColorDetector.java
@@ -11,24 +11,24 @@ public interface ColorDetector {
 	 * 
 	 * @return The calibrated/normalized red value (0-255)
 	 */
-	public int getRed();
+	public int getRedComponent();
 	
 	/**
 	 * 
 	 * @return The calibrated/normalized green value (0-255)
 	 */
-	public int getGreen();
+	public int getGreenComponent();
 	
 	/**
 	 * 
 	 * @return The calibrated/normalized blue value (0-255)
 	 */
-	public int getBlue();
+	public int getBlueComponent();
 	
 	/**
 	 * Return the Red, Green and Blue values together in one array. All values are 0-255.
 	 * @return All three color values. The array index for Red is 0, Green is 1, Blue is 2.
 	 */
-	public int [] getRGB();
+	public int [] getColor();
 	
 }
