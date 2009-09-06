faec07d6c7950f57b2fcba70ea95260c9edf43ce
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2009-09-06 09:43:34 +0000

    Create tag for 0.8.5 Eclipse plugin
    git-svn-id: http://svn.code.sf.net/p/lejos/code/tags/eclipse_plugin_0.8.5@3400 06c8f799-c779-4783-8af7-179afedb67c8

---
+++ org.lejos.nxt.ldt/src/main/java/org/lejos/nxt/ldt/preferences/PreferenceInitializer.java
@@ -0,0 +1,28 @@
+package org.lejos.nxt.ldt.preferences;
+
+import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.lejos.nxt.ldt.LeJOSNXJPlugin;
+
+/**
+ * Class used to initialize default preference values.
+ * 
+ * @author Matthias Paul Scholz
+ * 
+ */
+public class PreferenceInitializer extends AbstractPreferenceInitializer {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer#initializeDefaultPreferences()
+	 */
+	public void initializeDefaultPreferences() {
+		IPreferenceStore store = LeJOSNXJPlugin.getDefault()
+				.getPreferenceStore();
+		store.setDefault(PreferenceConstants.P_IS_VERBOSE, false);
+		store.setDefault(PreferenceConstants.P_RUN_AFTER_UPLOAD, false);
+		store.setDefault(PreferenceConstants.P_CONNECTION_TYPE, "u");
+	}
+
+}
