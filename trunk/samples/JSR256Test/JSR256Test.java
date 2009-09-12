fbb9cfc45acb8386c42b93b6eb495a4144b999ad
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2009-09-12 11:03:16 +0000

    JSR 256 samples

---
+++ trunk/samples/JSR256Test/JSR256Test.java
@@ -0,0 +1,71 @@
+import java.io.IOException;
+import java.io.PrintStream;
+
+import javax.microedition.io.Connector;
+import javax.microedition.sensor.Channel;
+import javax.microedition.sensor.ChannelInfo;
+import javax.microedition.sensor.Condition;
+import javax.microedition.sensor.ConditionListener;
+import javax.microedition.sensor.Data;
+import javax.microedition.sensor.I2CSensorConnection;
+import javax.microedition.sensor.LimitCondition;
+import javax.microedition.sensor.SensorConnection;
+import javax.microedition.sensor.SensorInfo;
+
+import lejos.nxt.Button;
+import lejos.nxt.Motor;
+import lejos.nxt.comm.RConsole;
+import lejos.robotics.navigation.TachoPilot;
+
+public class JSR256Test implements ConditionListener {
+	TachoPilot pilot = new TachoPilot(5.6f,16.0f,Motor.B, Motor.C,true);
+	
+	public void run() throws InterruptedException {
+		RConsole.openBluetooth(0);	
+		System.setOut(new PrintStream(RConsole.openOutputStream()));
+		SensorConnection sonic = null;
+		try {
+			sonic =(SensorConnection)Connector.open("sensor:proximity");
+		} catch (IOException e) {
+			System.err.println("No such sensor");
+			Button.waitForPress();
+			System.exit(1);
+		}
+
+		SensorInfo sonicInfo = sonic.getSensorInfo();
+
+		Condition condition = new LimitCondition(100,Condition.OP_LESS_THAN);
+
+		ChannelInfo channelInfo = sonic.getSensorInfo().getChannelInfos()[0];
+		System.out.println("Got channelInfo: " + (channelInfo == null ? "null" : channelInfo));
+		Channel channel = sonic.getChannel(channelInfo);
+		System.out.println("Got channel: " + (channel == null ? "null" : channel));
+		channel.addCondition(this, condition);
+		System.out.println("Added condition");
+		pilot.forward();
+		
+
+		while (pilot.isMoving()) {
+			//System.out.println("Vendor: " + sonicInfo.getProperty(SensorInfo.PROP_VENDOR));
+			//System.out.println("Version: " + sonicInfo.getProperty(SensorInfo.PROP_VERSION));
+			try {
+				Data[] data = sonic.getData(1);
+				System.out.println("Range = " + data[0].getIntValues()[0]);
+				Thread.sleep(100);
+			} catch (IOException ioe) {
+				System.err.println("Failed to read sensor");
+			}
+		}
+		Button.waitForPress();
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		new JSR256Test().run();
+	}
+
+	@Override
+	public void conditionMet(SensorConnection sensor, Data data, Condition condition) {
+		System.out.println("Condition met: " + data.getIntValues()[0] + condition);
+		pilot.stop();	
+	}	
+}
