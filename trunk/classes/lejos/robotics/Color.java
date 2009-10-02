97a2bf00089da5e6bfd58932c315db6269e8fb92
gloomyandy <gloomyandy@06c8f799-c779-4783-8af7-179afedb67c8>
2009-10-02 20:04:03 +0000

    Removed todo. No longer any need for RGB_RED etc. They are now called RED_INDEX etc. and are in SensorConstants.

--- trunk/classes/lejos/robotics/Color.java
+++ trunk/classes/lejos/robotics/Color.java
@@ -1,9 +1,6 @@
 package lejos.robotics;
 
-public class Color {
-
-	// TODO: Andy thinks we should have separate RGB_RED, RGB_BLUE, RGB_GREEN values that are always 0-2. Undecided.
-	
+public class Color {	
 	public static final int RED = 0;
 	public static final int GREEN = 1;
 	public static final int BLUE = 2;
