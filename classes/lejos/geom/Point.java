f5546f158a9173a2878ba452f5993cea3fa4d217
rogergla <rogergla@06c8f799-c779-4783-8af7-179afedb67c8>
2009-10-27 17:06:35 +0000

    Add pointAt(distance,angle) to point
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@3494 06c8f799-c779-4783-8af7-179afedb67c8

--- classes/lejos/geom/Point.java
+++ classes/lejos/geom/Point.java
@@ -14,14 +14,40 @@ public class Point extends Point2D.Float {
 	public Point(float x, float y) {
 		super(x,y);
 	}
-  
+
+ /**
+ * Returns the direction angle from this point to the Point p p
+ * @param p
+ * @return
+ */
     public float angleTo(Point p)
     {
       return (float)Math.toDegrees(Math.atan2(p.getY()-y,  p.getX()-x));
     }
+
+     /**
+   *Translates this point, at location (x, y), by dx along the x axis and
+   * dy along the y axis so that it now represents the point (x + dx, y + dy).
+   * @param dx
+   * @param dy
+   */
     public void translate(float dx, float dy)
     {
       x += dx;
       y += dy;
+    }
+
+       /**
+     * Returns a new point at the specified distance in the direction angle  from
+     * this point.
+     * @param distance
+     * @param angle
+     * @return
+     */
+    public Point pointAt(float distance, float angle)
+    {
+      float xx = distance*(float)Math.cos(Math.toRadians(angle)) + (float)getX();
+      float yy = distance*(float)Math.sin(Math.toRadians(angle)) + (float)getY();
+      return new Point(xx,yy);
     }
 }
