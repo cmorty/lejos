d920bd5b5474213220b1f4cfd358483fe48ea379
gloomyandy <gloomyandy@06c8f799-c779-4783-8af7-179afedb67c8>
2009-11-08 19:24:59 +0000

    Added new debug and run time command line options
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@3511 06c8f799-c779-4783-8af7-179afedb67c8

--- jtools/js/tinyvm/RunTimeOptions.java
+++ jtools/js/tinyvm/RunTimeOptions.java
@@ -6,7 +6,7 @@ package js.tinyvm;
  * @author andy
  */
 public enum RunTimeOptions {
-    EnableTypeChecks(0x1), EnableAssert(0x2);
+    EnableTypeChecks(0x1), EnableAssert(0x2), EnableCompact(0x4);
     private int value;
 
     RunTimeOptions(int value)
