f57f56d05264ba8428e250b91f6cb30f0d543dbc
bgard6977 <bgard6977@06c8f799-c779-4783-8af7-179afedb67c8>
2009-12-29 03:24:04 +0000

    Added lejos.robotics.inertialProposal namespace.

---
+++ trunk/classes/lejos/robotics/inertialProposal/TachoPoseProvider.java
@@ -0,0 +1,164 @@
+package lejos.robotics.inertialProposal;
+
+import lejos.geom.Point;
+import lejos.robotics.Move;
+import lejos.robotics.Move.MoveType;
+import lejos.robotics.MoveListener;
+import lejos.robotics.MoveProvider;
+import lejos.robotics.Pose;
+import lejos.robotics.TachoMotor;
+import lejos.robotics.localization.PoseProvider;
+
+public class TachoPoseProvider implements PoseProvider, MoveListener
+{
+    private lejos.robotics.Move currentMove;
+    private Regulator reg = new Regulator(this);
+    private float wheelDiameter;
+    private float wheelRatio;
+    private TachoMotor leftMotor;
+    private TachoMotor rightMotor;
+    private Pose pose = new Pose();
+
+    public TachoPoseProvider(TachoMotor leftMotor, TachoMotor rightMotor, float wheelDiameter, float wheelBase)
+    {
+        this.leftMotor = leftMotor;
+        this.rightMotor = rightMotor;
+        this.wheelDiameter = wheelDiameter;
+        this.wheelRatio = wheelBase / wheelDiameter;
+    }
+
+    public Pose getPose()
+    {
+        return pose;
+    }
+
+    public void setMoveProvider(MoveProvider mp)
+    {
+        mp.addMoveListener(this);
+        reg.start();
+    }
+
+    public void updateLocation(Point location)
+    {
+        pose.getLocation().x = location.x;
+        pose.getLocation().y = location.y;
+    }
+
+    public void updateHeading(float heading)
+    {
+        pose.setHeading(heading);
+    }
+
+    public void moveStarted(Move event, MoveProvider mp)
+    {
+        synchronized(this)
+        {
+            currentMove = event;
+        }
+    }
+
+    public void moveStopped(Move event, MoveProvider mp)
+    {
+        synchronized(this)
+        {
+            currentMove = null;
+        }
+    }
+
+    public lejos.robotics.Move CurrentMove()
+    {
+        synchronized(this)
+        {
+            return currentMove;
+        }
+    }
+
+    private class Regulator extends Thread
+    {
+        private TachoPoseProvider parent;
+
+        public Regulator(TachoPoseProvider parent)
+        {
+            setDaemon(true);
+            this.parent = parent;
+        }
+
+        @Override
+        public void run()
+        {
+            long lastUpdate = System.currentTimeMillis();
+            lejos.robotics.Move cm;
+            while (true)
+            {
+                // Check time
+                Thread.yield();
+                long now = System.currentTimeMillis();
+                if(now - lastUpdate == 0)
+                    continue;
+                cm = parent.CurrentMove();
+                if(cm == null)
+                    continue;
+
+                // Rotate or travel
+                if(cm.getMoveType() == MoveType.ROTATE)
+                    WaitRotateComplete();
+                if(cm.getMoveType() == MoveType.TRAVEL)
+                    WaitTravelComplete();
+                
+                // Move on
+                lastUpdate = now;
+            }
+
+        }
+
+        private void WaitRotateComplete()
+        {
+            lejos.robotics.Move cm = CurrentMove();
+            leftMotor.resetTachoCount();
+            rightMotor.resetTachoCount();
+            int leftCount = 0;
+            int rightCount = 0;
+            float oldHeading = parent.pose.getHeading();
+            while(cm != null && cm.getMoveType() == MoveType.ROTATE)
+            {
+                // Read tacho's
+                leftCount = parent.leftMotor.getTachoCount();
+                rightCount = parent.rightMotor.getTachoCount();
+                float thisHeading = (float)(rightCount - leftCount) / wheelRatio / 2.0f;
+                parent.pose.setHeading(oldHeading + thisHeading);
+
+                // Move on
+                Thread.yield();
+                cm = parent.CurrentMove();
+            }
+        }
+
+        private void WaitTravelComplete()
+        {
+            Point dir = new Point(0, 0);
+            int lastLeftCount = 0;
+            lastLeftCount = parent.leftMotor.getTachoCount();
+            lejos.robotics.Move cm = CurrentMove();
+            while(cm != null && cm.getMoveType() == MoveType.TRAVEL)
+            {
+                // Read tacho's
+                int leftCount = parent.leftMotor.getTachoCount();
+                long tachoDiff = leftCount - lastLeftCount;
+                float deltaDist = (float)tachoDiff / 360F * wheelDiameter * (float)Math.PI;
+
+                // Update position
+                dir.x = (float)Math.cos(Math.toRadians(parent.pose.getHeading())) * deltaDist;
+                dir.y = -(float)Math.sin(Math.toRadians(parent.pose.getHeading())) * deltaDist;
+                parent.pose.getLocation().x += dir.x;
+                parent.pose.getLocation().y += dir.y;
+
+                // Move on
+                lastLeftCount = leftCount;
+                Thread.yield();
+                cm = parent.CurrentMove();
+            }
+        }
+
+    }
+
+}
