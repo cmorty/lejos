2303692048fb93aaed2bf9033bf7dbdfb3ece8da
bbagnall <bbagnall@06c8f799-c779-4783-8af7-179afedb67c8>
2010-01-03 21:57:10 +0000

    ArcPoseController can now control both ArcRotateMoveController and ArcMoveController. Eliminated MotorEvent from API. Some SteeringPilot methods now throw IllegalArgumentException.
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@3613 06c8f799-c779-4783-8af7-179afedb67c8

--- classes/lejos/robotics/proposal/ArcPoseController.java
+++ classes/lejos/robotics/proposal/ArcPoseController.java
@@ -3,6 +3,7 @@ package lejos.robotics.proposal;
 import java.awt.geom.Point2D;
 
 import lejos.geom.Point;
+import lejos.nxt.Button;
 import lejos.robotics.Move;
 import lejos.robotics.Pose;
 import lejos.robotics.localization.PoseProvider;
@@ -52,26 +53,55 @@ public class ArcPoseController implements PoseController {
 
 	public Pose goTo(Pose destination) {
 		
-		// 1. Get shortest path:
-		Move [] moves = ArcAlgorithms.getBestPath(poseProvider.getPose(), pilot.getMinRadius(), destination, pilot.getMinRadius());
-
-		// 2. Drive the path
-		for(int i=0;i<moves.length;i++) {
-			pilot.travelArc(moves[i].getArcRadius(), moves[i].getDistanceTraveled());
-		}
+		Pose pose = poseProvider.getPose();
+	   	if (pilot instanceof RotateMoveController & pilot.getMinRadius() == 0) { // optimize for RotateMoveController
+	   	   // TODO: What the heck happened to the rotateTo() method? Would make this so much easier.
+	   		float turnAngle = pose.angleTo(destination.getLocation()) - pose.getHeading();
+	   	    while (turnAngle < -180)turnAngle += 360;
+	   	    while (turnAngle > 180) turnAngle -= 360;
+	   	    ((RotateMoveController) pilot).rotate(turnAngle);
+	   		pilot.travel(pose.distanceTo(destination.getLocation()));
+	   		// TODO: What the heck happened to the rotateTo() method? Would make this so much easier.
+	   		float turnAngle2 = destination.getHeading() - poseProvider.getPose().getHeading();
+	   		while (turnAngle2 < -180)turnAngle += 360;
+	   	    System.out.println("BUGSTOP"); // TODO Seems to stop it from freezing bug?
+	   	    while (turnAngle2 > 180) turnAngle -= 360;
+	   	    System.out.println("BUGSTOP"); // TODO Seems to stop it from freezing bug?
+	   	    ((RotateMoveController) pilot).rotate(turnAngle2);
+	   	} else {
 		
+			// 1. Get shortest path:
+			Move [] moves = ArcAlgorithms.getBestPath(poseProvider.getPose(), pilot.getMinRadius(), destination, pilot.getMinRadius());
+	
+			// 2. Drive the path
+			// TODO: A method to execute (drive) a Move might be nice here due to repetition of this code block below:
+			for(int i=0;i<moves.length;i++) {
+				pilot.travelArc(moves[i].getArcRadius(), moves[i].getDistanceTraveled());
+			}
+	   	}
 		return poseProvider.getPose();
 	}
 	
 	public Pose goTo(Point destination) {
-		// 1. Get shortest moves
-		Move [] moves = ArcAlgorithms.getBestPath(poseProvider.getPose(), destination, pilot.getMinRadius());
 		
-		// 2. Drive the path
-		for(int i=0;i<moves.length;i++) {
-			pilot.travelArc(moves[i].getArcRadius(), moves[i].getDistanceTraveled());
-		}
-		return poseProvider.getPose();
+		   	Pose pose = poseProvider.getPose();
+		   	if (pilot instanceof RotateMoveController & pilot.getMinRadius() == 0) { // optimize for RotateMoveController
+		   	    float turnAngle = pose.angleTo(destination) - pose.getHeading();
+		   	    while (turnAngle < -180)turnAngle += 360;
+		   	    while (turnAngle > 180) turnAngle -= 360;
+		   		((RotateMoveController) pilot).rotate(turnAngle);
+		   		pilot.travel(pose.distanceTo(destination));
+		   	} else { 
+   			// 1. Get shortest moves
+    			Move [] moves = ArcAlgorithms.getBestPath(poseProvider.getPose(), destination, pilot.getMinRadius());
+		   
+		   		// 2. Drive the path
+		   		for(int i=0;i<moves.length;i++) {
+		   			pilot.travelArc(moves[i].getArcRadius(), moves[i].getDistanceTraveled());
+		   		}
+		   	}
+		   	return poseProvider.getPose();
+
 	}
 
 	public Pose goTo(float x, float y) {
