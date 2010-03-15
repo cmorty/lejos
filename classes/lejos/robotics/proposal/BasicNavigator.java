de29acb7b369ba55d8beb953c51429bdde1903e4
rogergla <rogergla@06c8f799-c779-4783-8af7-179afedb67c8>
2010-03-15 19:06:14 +0000

    Better JavaDoc for  BasicNavigator and WayPointNavigator
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@3751 06c8f799-c779-4783-8af7-179afedb67c8

---
+++ classes/lejos/robotics/proposal/BasicNavigator.java
@@ -0,0 +1,114 @@
+package lejos.robotics.proposal;
+
+
+import lejos.geom.Point;
+import lejos.robotics.*;
+
+/**
+ * Moves the robot to a destination.
+ * Uses any pilot that implements the MoveControl face.
+ * You can executed methods on the Pilot if you wish; the Navitator pose will
+ * automatically updated.
+ * @author Roger
+ */
+
+public class BasicNavigator
+{
+  public BasicNavigator(MoveControl pilot )
+  {
+    _pilot = pilot;
+    drpp = new DeadReckonerPoseProvider((DifferentialPilot)_pilot);
+  }
+/**
+ * Sets the initial pose of the robot;
+ * @param x robot X location
+ * @param y roboe Y location
+ * @param heading robot initial heading
+ */
+  public void setInitialPose(float x, float y, float heading)
+  {
+    _pose = new Pose(x,y,heading);
+  }
+
+  /**
+   * returns the current pose of the robot.
+   * The pose is kept up to date by a DeadReckonerPoseProvider
+   * @return
+   */
+  public Pose getPose( ){
+    return drpp.getPose();
+  }
+  /**
+   * returns a referenct to the pilot.
+   * The Navigator pose will be automatically updated as a result of methods
+   * executed on the pilot.
+   * @return
+   */
+public MoveControl getPilot(){ return _pilot;}
+
+/**
+ * Moves the robot to the destinatin location
+ * @param destination
+ * @param immediateReturn  if true, this method will return as soon as the
+ * rotation is complete;
+ */
+  public void goTo(Point destination, boolean immediateReturn)
+  {
+    _keepGoing = true;
+    _destination = destination;
+    _pose = drpp.getPose();
+    float angle = _pose.relativeBearing(_destination);
+    _pilot.rotate(normalize(angle));
+    float distance = _pose.distanceTo(_destination);
+    _pilot.travel(distance, immediateReturn);
+    _pose = drpp.getPose();
+  }
+
+  /**
+   * Goes to a location specified by the x,y coordinates
+   * @param x
+   * @param y
+   * @param immediateReturn
+   */
+  public void goTo(float x, float y, boolean immediateReturn)
+  {
+    goTo(new Point(x,y),immediateReturn);
+  }
+
+  /**
+   * goes to location specified by x and y coordinates.  Returns when it gets there.
+   * @param x
+   * @param y
+   */
+  public void goTo(float x, float y)
+  {
+    goTo(x,y,false);
+  }
+/**
+ * Stops the robot immediately.
+ */
+  public void stop()
+  {
+    _keepGoing = false;
+    _pilot.stop();
+  }
+/**
+ * returns the equivalent angle between -180 and +180 degrees
+ * @param angle
+ * @return
+ */
+  float normalize(float angle)
+  {
+    while(angle > 180 )angle -=  360;
+    while(angle < -180) angle += 360;
+    return angle;
+  }
+  /**
+   *
+   */
+    public boolean _keepGoing = false;
+    protected MoveControl _pilot;
+    protected DeadReckonerPoseProvider drpp;
+    protected Pose _pose = new Pose();
+    protected Point _destination;
+}
---
+++ classes/lejos/robotics/proposal/WayPointNavigator.java
@@ -0,0 +1,82 @@
+package lejos.robotics.proposal;
+
+
+import java.util.*;
+
+/**
+ *This  class can follow a sequence of way points;
+ *Uses an inner class that has it own thresd to do the work.
+ * @author Roger
+ */
+public class WayPointNavigator  extends BasicNavigator
+{
+/**
+ * can use any pilot the impolements the MoveControl interrface
+ * @param pilot
+ */
+  WayPointNavigator(MoveControl pilot )
+  {
+    super(pilot);
+    _nav = new Nav();
+    _nav.start();
+  }
+/**
+ * Follows the route.  Can be a non-blocking method
+ * @param aRoute seqiemce of way points to be visited
+ * @param immediateReturn if true, returns immidiately
+ */
+  public void folowRoute( ArrayList<WayPoint>  aRoute, boolean immediateReturn)
+  {
+    _route = aRoute;
+    _keepGoing = true;
+    _count = 0;
+    System.out.println("follow "+_keepGoing+" "+_route.size());
+    if(immediateReturn)return;
+    else while(_keepGoing) Thread.yield();
+  }
+  /**
+   * Returns the index of the current waypoint to which the robot is moving
+   * @return
+   */
+public int getCount() { return _count;}
+
+  protected  class Nav extends Thread
+  {
+    boolean more = true;
+
+    public void run()
+    {
+      System.out.println("NAV run "+_keepGoing);
+      while (more)
+      {
+        if(_keepGoing) System.out.println("keepGoing "+_keepGoing);
+        while (_keepGoing)
+        {
+          _destination = _route.get(_count);
+          _pose = drpp.getPose();
+          float angle = _pose.relativeBearing(_destination);
+          _pilot.rotate(normalize(angle), true);
+          while (_pilot.isMoving() && _keepGoing)
+          {
+            Thread.yield();
+          }
+          float distance = _pose.distanceTo(_destination);
+          _pilot.travel(distance, true);
+          while (_pilot.isMoving() && _keepGoing)
+          {
+            Thread.yield();
+          }
+          _pose = drpp.getPose();
+          _count++;
+          _keepGoing = _keepGoing && _count < _route.size();
+        }
+      }
+    }
+  }
+
+   int _count = 0;
+   protected Nav _nav ;
+   protected ArrayList<WayPoint>  _route ;
+
+
+}
