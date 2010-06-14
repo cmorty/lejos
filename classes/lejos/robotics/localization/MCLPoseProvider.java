b04f388537b04447bf8e6990b16aad3f06ca057c
rogergla <rogergla@06c8f799-c779-4783-8af7-179afedb67c8>
2010-06-14 18:45:55 +0000

    Modified MCLParticleSet  to allow generation of a set centered on an original pose MCLParticle  applyMove()  uses the same random travel distance for both x and y;  Calculatiion of estimated pose statistics moved from ParticleSet to MCLParticlePoseProvider. Various simplifications and minor refactoring
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@3874 06c8f799-c779-4783-8af7-179afedb67c8

--- classes/lejos/robotics/localization/MCLPoseProvider.java
+++ classes/lejos/robotics/localization/MCLPoseProvider.java
@@ -1,49 +1,273 @@
 package lejos.robotics.localization;
 
+
 import lejos.robotics.Pose;
-import lejos.robotics.localization.MCLParticleSet;
 import lejos.robotics.mapping.RangeMap;
 import lejos.robotics.MoveListener;
 import lejos.robotics.MoveProvider;
 import lejos.robotics.RangeReadings;
 import lejos.robotics.RangeScanner;
 import lejos.robotics.Move;
+import java.awt.Rectangle;
+import java.io.*;
+public class MCLPoseProvider implements PoseProvider, MoveListener
+{
 
-public class MCLPoseProvider implements PoseProvider,MoveListener{
-	private MCLParticleSet particles;
-	private RangeScanner scanner;
-	private RangeMap map;
-	private boolean readingsRequired = true;
-	
-	public MCLPoseProvider(MoveProvider mp, RangeScanner scanner, 
-		                   RangeMap map, int numParticles, int border) {
-		particles = new MCLParticleSet(map, numParticles, border);
-		this.scanner = scanner;
-		this.map = map;
-		mp.addMoveListener(this);
-	}
-	
-	public MCLParticleSet getParticles() {
-		return particles;
-	}
+  private MCLParticleSet particles;
+  private RangeScanner scanner;
+  private RangeMap map;
+  private int numParticles;
+  float _x,_y,_heading;
+  private float minX, maxX, minY, maxY;
+  double varX, varY, varH;
+  private boolean autoUpdate = false;
 
-	public void moveStarted(Move event, MoveProvider mp) {		
-	}
+  public MCLPoseProvider(MoveProvider mp, RangeScanner scanner,
+          RangeMap map, int numParticles, int border)
+  {
+    this.numParticles = numParticles;
+    particles = new MCLParticleSet(map, numParticles, border);
+    this.scanner = scanner;
+    this.map = map;
+    mp.addMoveListener(this);
+  }
 
-	public void moveStopped(Move event, MoveProvider mp) {
-		readingsRequired = true;
-		particles.applyMove(event);
-	}
+  public MCLPoseProvider(MoveProvider mp, RangeScanner scanner,
+          RangeMap map, int numParticles)
+  {
+    this.numParticles = numParticles;
+    this.scanner = scanner;
+    this.map = map;
+    mp.addMoveListener(this);
+  }
+/**
+ * Sets the inital particle set to a circular cloud centered on  aPose
+ * @param aPose
+ * @param radiusNoise
+ * @param headingNoise
+ */
+  public void setInitialPose( Pose aPose,float radiusNoise,float headingNoise)
+  {
+    _x = aPose.getX();
+    _y = aPose.getY();
+    _heading = aPose.getHeading();
+       particles = new MCLParticleSet(map, numParticles, aPose,
+            radiusNoise,  headingNoise);
+  }
 
-	public Pose getPose() {
-		if (readingsRequired) {
-			RangeReadings rr = scanner.getRangeValues();
-			readingsRequired = false;
-			if (!rr.incomplete()) {
-				particles.calculateWeights(rr, map);
-				particles.resample(); // Cannot indicate robot is lost
-			}
-		}
-		return particles.getEstimatedPose();
-	}
+  /**
+   * returns the particle set
+   * @return the particle set
+   */
+  public MCLParticleSet getParticles()
+  {
+    return particles;
+  }
+
+  /**
+    require by MoveListener interface; does nothing
+   */
+  public void moveStarted(Move event, MoveProvider mp)
+  {
+    
+  }
+/**
+ * required by MoveListener interface.
+ * Applies the move to all particles; updates the estimated pose after Travel
+ * @param event the event just completed
+ * @param mp
+ */
+  public void moveStopped(Move event, MoveProvider mp)
+  {
+    particles.applyMove(event);
+    if(autoUpdate && event.getMoveType() ==  Move.MoveType.TRAVEL)
+    {
+    update();
+    }
+  }
+
+ /**
+  * returns the difference between max and min x
+  * @return
+  */
+  public float getXRange()
+  {
+    return getMaxX()-  getMinX();
+  }
+  /**
+   * return difference between max and min y
+   * @return
+   */
+  public float getYRange()
+  {
+    return getMaxY()- getMinY();
+  }
+/**
+ * updates the estimated pose using monte carlo method applied to particles.
+ * Gets range readings from the scanner, calculates weights and resamples the
+ * the particles.  Tries at most 4 times  range values are incpmplete and/or
+ * the probabilities of the sensor readings are too small;.
+ * @return  true if the update was successful.  Otherwise, the dead reckoning pose
+ * is the only one availabale.
+ */
+  public boolean update()
+  {
+    int maxTries = 4;
+     int tries = 0;
+    while (tries < maxTries)
+    {
+
+    RangeReadings rr = scanner.getRangeValues();
+    if(rr.incomplete())rr = scanner.getRangeValues();
+    if(rr.incomplete())
+    {
+      System.out.println("READINGS INCOMPOETE");
+              tries = maxTries;
+              break;
+    }
+       if (particles.calculateWeights(rr, map)) break;
+      else  tries++;
+    }
+     if(tries == maxTries )
+     {
+       System.out.println(" sensor data is too improbable from the current pose ");
+       return false;
+     }
+
+    particles.resample();
+    estimatePose();
+//        particles.logParticles(dl);
+    return true;
+  }
+  /**
+   * returns the best best estimate of the current pose;
+   * @return
+   */
+  public Pose getPose()
+  {
+    estimatePose();
+    return new Pose(_x, _y, _heading);
+
+    }
+/**
+   * Estimate pose from weighted average of the particles
+   */
+  private void estimatePose()
+  {
+
+    float totalWeights = 0;
+    float estimatedX = 0;
+    float estimatedY = 0;
+    float estimatedAngle = 0;
+     varX = 0;
+    varY = 0;
+    varH = 0;
+    for (int i = 0; i < numParticles; i++)
+    {
+      Pose p = particles.getParticle(i).getPose();
+      float x = p.getX();
+      float y = p.getY();
+      float weight = particles.getParticle(i).getWeight();
+       estimatedX += (x * weight);
+      varX += (x * x * weight);
+      estimatedY += (y * weight);
+      varY += (y * y * weight);
+      float head = p.getHeading();
+      estimatedAngle += (head * weight);
+      varH += (head * head * weight);
+      totalWeights += weight;
+
+      if (x < minX) minX = x;
+      if (x > maxX) maxX = x;
+      if (y < minY) minY = y;
+      if (y > maxY) maxY = y;
+    }
+
+    estimatedX /= totalWeights;
+    varX /= totalWeights;
+    varX -= (estimatedX * estimatedX);
+    estimatedY /= totalWeights;
+    varY /= totalWeights;
+    varY -= (estimatedY * estimatedY);
+    estimatedAngle /= totalWeights;
+    varH /= totalWeights;
+     varH -= (estimatedAngle * estimatedAngle);
+     while(estimatedAngle > 180) estimatedAngle -=360;
+     while(estimatedAngle < -180) estimatedAngle +=360;
+     _x = estimatedX;
+     _y = estimatedY;
+     _heading = estimatedAngle;
+  }
+
+
+   /**
+   * Returns the minimum rectangle enclosing all the particles
+   * @return rectangle : the minimum rectangle enclosing all the particles
+   */
+  public Rectangle getErrorRect() {
+	  return new Rectangle((int) minX, (int) minY,
+			               (int) (maxX-minX), (int) (maxY-minY));
+  }
+  /**
+   * returns the maximum value of  X in the particle set
+   * @return
+   */
+  public float getMaxX() { return maxX;}
+  /**
+   * returns the minimum value of   X in the particle set;
+   * @return
+   */
+  public float getMinX() { return minX;}
+  /**
+   * rturns the maximum value of Y in the particle set;
+   * @return
+   */
+  public float getMaxY() { return maxY;}
+  /**
+   * returns the minimum value of Y in the particle set;
+   * @return
+   */
+  public float getMinY() { return minY;}
+
+  /**
+   * returns the standard deviation of the X values in the particle set;
+   * @return
+   */
+  public float getSigmaX() { return (float)Math.sqrt(varX);}
+
+  /**
+   * returns the standard deviation of the Y values in the particle set;
+   * @return
+   */
+  public float getSigmaY() { return (float)Math.sqrt(varY);}
+
+  /**
+   * returns the standard deviation of the Y values in the particle set;
+   * @return
+   */
+  public float getSigmaHeading() {return (float)Math.sqrt(varH);}
+
+
+   /**
+   * Dump the serialized estimate of pose to a data output stream
+   * @param dos the data output stream
+   * @throws IOException
+   */
+  public void dumpEstimation(DataOutputStream dos) throws IOException {
+      Pose pose = getPose();
+
+
+      dos.writeFloat(pose.getX());
+      dos.writeFloat(pose.getY());
+      dos.writeFloat(pose.getHeading());
+      dos.writeFloat(minX);
+      dos.writeFloat(maxX);
+      dos.writeFloat(minY);
+      dos.writeFloat(maxY);
+      dos.writeFloat((float)varX);
+      dos.writeFloat((float)varY);
+      dos.writeFloat((float)varH);
+      dos.flush();
+  }
+ 
 }
