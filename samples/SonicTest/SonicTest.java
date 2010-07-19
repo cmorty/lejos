541d6569cd8c1686e487e83370e3a558c4eee8e4
skoehler <skoehler@06c8f799-c779-4783-8af7-179afedb67c8>
2010-07-19 15:47:07 +0000

    mc
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@3952 06c8f799-c779-4783-8af7-179afedb67c8

--- samples/SonicTest/SonicTest.java
+++ samples/SonicTest/SonicTest.java
@@ -14,15 +14,17 @@ public class SonicTest {
 		sonic.continuous();
 		
 		LCD.setAutoRefresh(false);
+		LCD.clear();
+		LCD.drawString(sonic.getVersion(), 0, 0);
+		LCD.drawString(sonic.getProductID(), 0, 1);
+		LCD.drawString(sonic.getSensorType(), 0, 2);
+		
 		while(!Button.ESCAPE.isPressed()) {
 			LCD.asyncRefreshWait();
-			LCD.clear();
-			LCD.drawString(sonic.getVersion(), 0, 0);
-			LCD.drawString(sonic.getProductID(), 0, 1);
-			LCD.drawString(sonic.getSensorType(), 0, 2);
+			LCD.drawString("     ", 0, 3);
 			LCD.drawInt(sonic.getDistance(), 0, 3);
 			LCD.asyncRefresh();
-			Thread.sleep(500);
+			//Thread.sleep(500);
 		}
 	}	
 }
