e24b7a1c6653eaaa9730c57ad61e98f4bd83721d
skoehler <skoehler@06c8f799-c779-4783-8af7-179afedb67c8>
2010-07-20 13:08:48 +0000

    adjusted class to NXT version

--- trunk/pccomms/lejos/nxt/I2CSensor.java
+++ trunk/pccomms/lejos/nxt/I2CSensor.java
@@ -34,6 +34,7 @@ public class I2CSensor implements SensorConstants {
 	
 	protected byte port;
 	protected int address;
+	protected byte[] byteBuff = new byte[8];
 	
 	public I2CSensor(I2CPort port)
 	{
