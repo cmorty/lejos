e12dc8e8ee7087a74d7377bc7bdde520c68f5e21
skoehler <skoehler@06c8f799-c779-4783-8af7-179afedb67c8>
2010-07-25 12:40:12 +0000

    mc
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@3963 06c8f799-c779-4783-8af7-179afedb67c8

--- scripts/templates/linux.sh
+++ scripts/templates/linux.sh
@@ -2,31 +2,30 @@
 
 # for OSX
 function my_resolve() {
-	if [[ "$2" =~ ^/ ]]; then
+	if [ "${2:0:1}" == "/" ]; then
 		echo "$2"
-	elif [[ "$2" == "." ]]; then
+	elif [ "$2" == "." ]; then
 		echo "$1"
-	elif [[ "$1" == "." ]]; then
+	elif [ "$1" == "." ]; then
 		echo "$2"
 	else
 		echo "$1/$2"
 	fi
 }
 function my_readlink() {
-	if [ -L "$1" ]; then
-		local TMP1="$(dirname -- "$1")"
-		local TMP2="$(readlink -- "$1")"
-		my_resolve "$TMP1" "$TMP2"
-	else
-		echo "$1"
-	fi
+	local TMP1="$(dirname -- "$1")"
+	local TMP2="$(readlink -- "$1")"
+	my_resolve "$TMP1" "$TMP2"
 }
 
 NXJ_COMMAND="$(basename -- "$0")"
 if [ -n "$NXJ_HOME" ]; then
 	NXJ_BIN="$NXJ_HOME/bin"
 else
-	NXJ_BIN="$(my_readlink "$0")"
+	NXJ_BIN="$0"
+	while [ -L "$NXJ_BIN" ]; do
+		NXJ_BIN="$(my_readlink "$NXJ_BIN")"
+	done
 	NXJ_BIN="$(my_resolve "$(pwd)" "$NXJ_BIN")"
 	NXJ_BIN="$(dirname -- "$NXJ_BIN")"
 	NXJ_HOME="$NXJ_BIN/.."
