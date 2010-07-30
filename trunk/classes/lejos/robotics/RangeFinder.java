5f14fc2f2697d417295de70c621cc0b0db4b896d
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2010-07-30 12:25:09 +0000

    Deleted old copy warning

--- trunk/classes/lejos/robotics/RangeFinder.java
+++ trunk/classes/lejos/robotics/RangeFinder.java
@@ -9,10 +9,6 @@ package lejos.robotics;
  * Abstraction for a range finder sensor that returns the distance to the nearest object
  * 
  * @author Lawrie Griffiths
- *
- * <br/><br/>WARNING: THIS CLASS IS SHARED BETWEEN THE classes AND pccomms PROJECTS.
- * DO NOT EDIT THE VERSION IN pccomms AS IT WILL BE OVERWRITTEN WHEN THE PROJECT IS BUILT.
-
  */
 public interface RangeFinder {
 	/**
