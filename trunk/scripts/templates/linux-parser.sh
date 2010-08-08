1bb749fb99430516369fd5b4d8b1b70a34f0c990
skoehler <skoehler@06c8f799-c779-4783-8af7-179afedb67c8>
2010-08-08 14:37:42 +0000

    parse -Djava.library.path=

--- trunk/scripts/templates/linux-parser.sh
+++ trunk/scripts/templates/linux-parser.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # set classpath to . by default (matches behaviour of java and javac)
-NXJ_CP_CMDLINE="$NXJ_CP_TOOL:."
+NXJ_CMDLINE_CP="$NXJ_CP_TOOL$SEP."
+NXJ_CMDLINE_LP="$NXJ_BIN"
 for (( i=1; i<=$#; i++ )); do
 	case "${!i}" in
 		#handle classpath parameters
 		-cp|-classpath)
 			(( i++ ))
-			NXJ_CP_CMDLINE="$NXJ_CP_TOOL:${!i}"
+			NXJ_CMDLINE_CP="$NXJ_CP_TOOL$SEP${!i}"
 			;;
 		#handle other parameters that accept arguments
 		-sourcepath|-bootclasspath|-extdirs|-endorseddirs|-processor|-processorpath|-d|-s|-encoding|-source|-target|-Xmaxerrs|-Xmaxwarns|-Xstdout)
@@ -20,6 +21,10 @@ for (( i=1; i<=$#; i++ )); do
 			for (( ; i<=$#; i++ )); do
 				NXJ_CMDLINE[$i]="${!i}"
 			done
+			;;
+		#handle -Djava.library.path=
+		-Djava.library.path=*)
+			NXJ_CMDLINE_LP="$NXJ_BIN$SEP${!i:20}"
 			;;
 		#handle parameters without arguments
 		*)
