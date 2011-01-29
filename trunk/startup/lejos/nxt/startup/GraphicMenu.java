42efbc25249d873acd2f9efb91aa69533f074c43
legoabram <legoabram@06c8f799-c779-4783-8af7-179afedb67c8>
2011-01-29 20:14:19 +0000

    **Graphic Menu Updates** Reduced from 2736 bytes to 2428 bytes larger than StartUpText SMOOTH Animations Reduced Code Placed Main Menu in center of the screen. Most menus are setup.  (Just need to test all of them)
    Legoabram

--- trunk/startup/lejos/nxt/startup/GraphicMenu.java
+++ trunk/startup/lejos/nxt/startup/GraphicMenu.java
@@ -5,7 +5,20 @@ import lejos.nxt.LCD;
 import lejos.util.TextMenu;
 
 public class GraphicMenu extends TextMenu{
+	private static final byte xArea = 0; // x of Menu Area
+	//private static final byte yArea = 40; // y of Menu Area
+	private static final byte xWidth = 20; // Distance between icon IDs on x axis
+	private static final byte yWidth = 4; // " on y axis
+	private static final byte xOffset = 2;
+	private static final byte yOffset = 0;
+	
+	private static final int interval = 40;
+	private static final int tickCount = 5;
+	
 	private String _parent = null;
+	
+	private int labelLine = 4;
+	private int yArea = 40;
 
 	private String[] _icons;
 	
@@ -32,6 +45,11 @@ public class GraphicMenu extends TextMenu{
 		_topRow = topRow;
 		setTitle(title);
 		this.setItems(items,icons);
+	}
+	
+	public void setYLocation (int charLine){
+		labelLine = charLine;
+		yArea = (charLine+1)*8;
 	}
 	
 	/**
@@ -74,7 +92,7 @@ public class GraphicMenu extends TextMenu{
 			_topIndex = selectedIndex;
 		if (_topIndex > _length - _height)
 			_topIndex = _length - _height;			
-		display(selectedIndex, _topIndex);
+		display(selectedIndex, 0,0);
 		int buttons = Button.readButtons();
 		while(true)
 		{
@@ -93,13 +111,16 @@ public class GraphicMenu extends TextMenu{
 				buttons = buttons2;
 			} while (button == 0);
 			
-			if(button == Button.ID_ENTER && selectedIndex >= 0 && selectedIndex < _length)
+			if(button == Button.ID_ENTER && selectedIndex >= 0 && selectedIndex < _length){
+				clearArea();
 				return selectedIndex;
+			}
 			if(button == Button.ID_ESCAPE)
 				return -1; //Escape
+			int temp = selectedIndex;
+			int dir = 0;
 			if(button == Button.ID_RIGHT)//scroll forward
 			{
-				int temp = selectedIndex;
 				selectedIndex++;
 				// check for index out of bounds
 				if(selectedIndex >= _length)
@@ -113,18 +134,11 @@ public class GraphicMenu extends TextMenu{
 				//	_topIndex = selectedIndex - _height + 1;
 				//}
 				else{
-					try {
-						rightRotateAnimate(temp);
-						Thread.sleep(200);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
+					dir = -1;
 				}
 			}
 			if(button == Button.ID_LEFT)//scroll backward
 			{
-				int temp = selectedIndex;
 				selectedIndex --;
 				// check for index out of bounds
 				if(selectedIndex < 0)
@@ -138,26 +152,40 @@ public class GraphicMenu extends TextMenu{
 				//	_topIndex = selectedIndex;
 				//}
 				else{
-					try {
-						leftRotateAnimate(temp);
-						Thread.sleep(200);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
+					dir = 1;
 				}
 			}
-			display(selectedIndex, _topIndex);
+			animate(temp,selectedIndex,dir);
 		}
 	}
 	
-	@Override
-	protected void display(int selectedIndex, int topIndex)
+	private void animate(int selectedIndex, int finalIndex,int animateDirection){
+		int count = 1;
+		while (count < tickCount){
+			display(selectedIndex,animateDirection,(int) ((10.0/tickCount)*count));
+			try {
+				Thread.sleep(interval);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			count++;
+		}
+		display(finalIndex,0,0);
+	}
+	
+	/**
+	 * Displays the Graphic Menu at the index provided with the animation details provided.
+	 * @param selectedIndex
+	 * @param animateDirection #-1 to 1 (-1=Left 0=None 1=Right)
+	 * @param tick #0-10 (0 = No change 10 = Full move)
+	 */
+	protected void display(int selectedIndex, int animateDirection, int tick)
 	{
 		if(_title != null)
 			LCD.drawString(_title, 0, _topRow);
+		clearArea();
 		if(_parent != null)
-			LCD.bitBlt(Utils.stringToBytes(_parent), 16, 16, 0, 0, 41,58, 16, 16, LCD.ROP_COPY);
+			LCD.bitBlt(Utils.stringToBytes(_parent), 16, 16, 0, 0, xArea+41,yArea+18, 16, 16, LCD.ROP_COPY);
 		//Prepare Index Locations
 		int length = _items.length;
 		int[] index = new int[5];
@@ -165,84 +193,52 @@ public class GraphicMenu extends TextMenu{
 			index[i] = selectedIndex + (i-2);
 		//boolean wrap = (length>=5);
 		//Clear Icon Area
-		LCD.bitBlt(new byte[396], 99, 25, 0, 0, 0, 40, 99, 25, LCD.ROP_COPY);
 		//if (((index[0] < 0) && wrap) || index[0] >= 0) // Left Most Icon
 		if (index[0] >= 0)	
-			LCD.bitBlt(Utils.stringToBytes(_icons[((index[0]<0)?length+index[0]:index[0])]),16,16,0,0,2,51,16,16,LCD.ROP_COPY);
+			drawIconAtTick(_icons[((index[0]<0)?length+index[0]:index[0])],0,0+animateDirection,tick);
 		//if (((index[1] < 0) && wrap) || index[1] >= 0) // Left Icon
 		if (index[1] >= 0)	
-			LCD.bitBlt(Utils.stringToBytes(_icons[((index[1]<0)?length+index[1]:index[1])]), 16,16, 0, 0, 21, 45, 16,16, LCD.ROP_COPY);
+			drawIconAtTick(_icons[((index[1]<0)?length+index[1]:index[1])],1,1+animateDirection,tick);
 		//Middle Icon
-			LCD.bitBlt(Utils.stringToBytes(_icons[index[2]]), 16,16, 0, 0, 41, 41, 16,16, LCD.ROP_COPY);
-		//if (((index[3] >= length) && wrap) || index[3] < length) // Right Icon
+			drawIconAtTick(_icons[index[2]],2,2+animateDirection,tick);
+		//if (((index[3] >= length) && wrap) || index[3] < length) // Rightd Icon
 		if (index[3] < length)	
-			LCD.bitBlt(Utils.stringToBytes(_icons[((index[3]>=length)?index[3]-length:index[3])]), 16,16, 0, 0, 61, 45, 16,16, LCD.ROP_COPY);
+			drawIconAtTick(_icons[((index[3]>=length)?index[3]-length:index[3])],3,3+animateDirection,tick);
 		//if (((index[4] >= length) && wrap) || index[4] < length) // Right Most Icon
 		if (index[4] < length)	
-			LCD.bitBlt(Utils.stringToBytes(_icons[((index[4]>=length)?index[4]-length:index[4])]), 16,16, 0,0, 80,51, 16,16, LCD.ROP_COPY);
-		
+			drawIconAtTick(_icons[((index[4]>=length)?index[4]-length:index[4])],4,4+animateDirection,tick);
 		// Draw Label
-		LCD.drawString(BLANK, 0, 4);
+		LCD.drawString(BLANK, 0, labelLine);
 		if (_items[index[2]].length()>16)
-			LCD.drawString(_items[index[2]],0, 4);
+			LCD.drawString(_items[index[2]],0, labelLine);
 		else
-			LCD.drawString(_items[index[2]], 8-(_items[index[2]].length()/2), 4);
+			LCD.drawString(_items[index[2]], 8-(_items[index[2]].length()/2), labelLine);
 		
 		LCD.asyncRefresh();
 	}
 	
-	/**
-	 * GraphicMenu helper method to animate a right rotate animation.
-	 * @param start
-	 * @param end
-	 */
-	private void rightRotateAnimate(int start){
-		int[] index = new int[5];
-		int length = _items.length;
-		for (int i = 0; i < 5; i++)
-			index[i] = start + (i-2);
-		//boolean wrap = (length>=5);
-		//Clear Icon Area
-		LCD.bitBlt(new byte[396], 99, 25, 0, 0, 0, 40, 99, 25, LCD.ROP_COPY);
-		//if (((index[0] < 0) && wrap) || index[0] >= 0) // Left Most Icon
-		if (index[0] >= 0)
-			LCD.bitBlt(Utils.stringToBytes(_icons[((index[0]<0)?length+index[0]:index[0])]),16,16,0,0,-10,54,16,16,LCD.ROP_COPY);
-		//if (((index[1] < 0) && wrap) || index[1] >= 0) // Left Icon
-		if (index[1] >= 0)	
-			LCD.bitBlt(Utils.stringToBytes(_icons[((index[1]<0)?length+index[1]:index[1])]), 16,16, 0, 0, 11, 47, 16,16, LCD.ROP_COPY);
-		//Middle Icon
-			LCD.bitBlt(Utils.stringToBytes(_icons[index[2]]), 16,16, 0, 0, 31, 42, 16,16, LCD.ROP_COPY);
-		//if (((index[3] >= length) && wrap) || index[3] < length) // Right Icon
-		if (index[3] < length)	
-			LCD.bitBlt(Utils.stringToBytes(_icons[((index[3]>=length)?index[3]-length:index[3])]), 16,16, 0, 0, 51, 42, 16,16, LCD.ROP_COPY);
-		//if (((index[4] >= length) && wrap) || index[4] < length) // Right Most Icon
-		if (index[4] < length)	
-			LCD.bitBlt(Utils.stringToBytes(_icons[((index[4]>=length)?index[4]-length:index[4])]), 16,16, 0,0, 71,47, 16,16, LCD.ROP_COPY);
-		LCD.asyncRefresh();
+	public void clearArea(){
+		LCD.bitBlt(new byte[396], 99, 25, 0, 0, 0, yArea, 99, 25, LCD.ROP_COPY);
 	}
-	private void leftRotateAnimate(int start){
-		int[] index = new int[5];
-		int length = _items.length;
-		for (int i = 0; i < 5; i++)
-			index[i] = start + (i-2);
-		//boolean wrap = (length>=5);
-		//Clear Icon Area
-		LCD.bitBlt(new byte[396], 99, 25, 0, 0, 0, 40, 99, 25, LCD.ROP_COPY);
-		//if (((index[0] < 0) && wrap) || index[0] >= 0) // Left Most Icon
-		if (index[0] >= 0)	
-			LCD.bitBlt(Utils.stringToBytes(_icons[((index[0]<0)?length+index[0]:index[0])]), 16,16, 0, 0, 11, 47, 16,16, LCD.ROP_COPY);
-		//if (((index[1] < 0) && wrap) || index[1] >= 0) // Left Icon
-		if (index[1] >= 0)	
-			LCD.bitBlt(Utils.stringToBytes(_icons[((index[1]<0)?length+index[1]:index[1])]), 16,16, 0, 0, 31, 42, 16,16, LCD.ROP_COPY);
-		//Middle Icon
-			LCD.bitBlt(Utils.stringToBytes(_icons[index[2]]), 16,16, 0, 0, 51, 42, 16,16, LCD.ROP_COPY);
-		//if (((index[3] >= length) && wrap) || index[3] < length) // Right Icon
-		if (index[3] < length)	
-			LCD.bitBlt(Utils.stringToBytes(_icons[((index[3]>=length)?index[3]-length:index[3])]), 16,16, 0,0, 71,47, 16,16, LCD.ROP_COPY);
-		//if (((index[4] >= length) && wrap) || index[4] < length) // Right Most Icon
-		if (index[4] < length)	
-			LCD.bitBlt(Utils.stringToBytes(_icons[((index[4]>=length)?index[4]-length:index[4])]), 16,16, 0,0, 90,54, 16,16, LCD.ROP_COPY);
-		LCD.asyncRefresh();
+	
+	/**
+	 * Helper method to draw a menu icon at a variable location (determined by tick) between two icon positions.
+	 * @param sID -1 to 6
+	 * @param eID -1 to 6
+	 * @param tick #0-10
+	 */
+	private void drawIconAtTick(String icon,int sID, int eID,int tick){
+		// Determine sID Coordinates
+		int fx = xArea + xOffset+(sID*xWidth);
+		int fy = yArea + yOffset+(Math.abs(sID-2)*yWidth);
+		// Determine eID Coordinates
+		int sx = xArea + xOffset+(eID*xWidth);
+		int sy = yArea + yOffset+(Math.abs(eID-2)*yWidth);
+		// Determine Icon Offset from sID
+		int ix = (int) (((sx-fx)/10.0)*tick);
+		int iy = (int) (((sy-fy)/10.0)*tick);
+		// Paint Icon
+		LCD.bitBlt(Utils.stringToBytes(icon), 16,16, 0,0, fx+ix,fy+iy, 16,16, LCD.ROP_COPY);
 	}
 
 	public void setParentIcon(String str) {
