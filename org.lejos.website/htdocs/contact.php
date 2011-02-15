ca532df05c6e22fd75bd609fcba0de4e395d2838
skoehler <skoehler@06c8f799-c779-4783-8af7-179afedb67c8>
2011-02-15 17:43:16 +0000

    added XML declaration for standard compliance and switched to UTF-8 (had to use PHP workaround for xml declaration)
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@4221 06c8f799-c779-4783-8af7-179afedb67c8

--- org.lejos.website/htdocs/contact.php
+++ org.lejos.website/htdocs/contact.php
@@ -1,3 +1,7 @@
+<?php
+	echo '<?xml version="1.0" encoding="UTF-8" ?>';
+?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
@@ -31,7 +35,7 @@
 		<meta http-equiv="expires" content="0" />
 
 		<!-- WEB EDITOR -->
-		<meta name="author" content="Juan Antonio Breña Moral" />		
+		<meta name="author" content="Juan Antonio BreÃ±a Moral" />		
 		
 		<link rel="stylesheet" href="r_css/lejos.css" type="text/css" />
 
