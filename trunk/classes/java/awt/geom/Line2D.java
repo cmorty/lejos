0ff74129ceee330c9f7e1c0112020c92583e3f7e
bbagnall <bbagnall@06c8f799-c779-4783-8af7-179afedb67c8>
2011-03-02 22:07:03 +0000

    Added standard methods for determining min distance from a point to a line segment to Line2D. Added non-standard class for measuring min distance from Line to Line.

--- trunk/classes/java/awt/geom/Line2D.java
+++ trunk/classes/java/awt/geom/Line2D.java
@@ -453,4 +453,135 @@ public abstract class Line2D implements Shape, Cloneable {
     public boolean intersects(Rectangle2D r) {
         return r.intersectsLine(getX1(), getY1(), getX2(), getY2());
     }
+
+    /**
+     * Measures the square of the shortest distance from the reference point
+     * to a point on the line segment. If the point is on the segment, the
+     * result will be 0.
+     *
+     * @param x1 the first x coordinate of the segment
+     * @param y1 the first y coordinate of the segment
+     * @param x2 the second x coordinate of the segment
+     * @param y2 the second y coordinate of the segment
+     * @param px the x coordinate of the point
+     * @param py the y coordinate of the point
+     * @return the square of the distance from the point to the segment
+     * @see #ptSegDist(double, double, double, double, double, double)
+     * @see #ptLineDistSq(double, double, double, double, double, double)
+     */
+    public static double ptSegDistSq(double x1, double y1, double x2, double y2,
+                                     double px, double py)
+    {
+      double pd2 = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
+
+      double x, y;
+      if (pd2 == 0)
+        {
+          // Points are coincident.
+          x = x1;
+          y = y2;
+        }
+      else
+        {
+          double u = ((px - x1) * (x2 - x1) + (py - y1) * (y2 - y1)) / pd2;
+
+          if (u < 0)
+            {
+              // "Off the end"
+              x = x1;
+              y = y1;
+            }
+          else if (u > 1.0)
+            {
+              x = x2;
+              y = y2;
+            }
+          else
+            {
+              x = x1 + u * (x2 - x1);
+              y = y1 + u * (y2 - y1);
+            }
+        }
+
+      return (x - px) * (x - px) + (y - py) * (y - py);
+    }
+
+    /**
+     * Measures the shortest distance from the reference point to a point on
+     * the line segment. If the point is on the segment, the result will be 0.
+     *
+     * @param x1 the first x coordinate of the segment
+     * @param y1 the first y coordinate of the segment
+     * @param x2 the second x coordinate of the segment
+     * @param y2 the second y coordinate of the segment
+     * @param px the x coordinate of the point
+     * @param py the y coordinate of the point
+     * @return the distance from the point to the segment
+     * @see #ptSegDistSq(double, double, double, double, double, double)
+     * @see #ptLineDist(double, double, double, double, double, double)
+     */
+    public static double ptSegDist(double x1, double y1, double x2, double y2,
+                                   double px, double py)
+    {
+      return Math.sqrt(ptSegDistSq(x1, y1, x2, y2, px, py));
+    }
+
+    /**
+     * Measures the square of the shortest distance from the reference point
+     * to a point on this line segment. If the point is on the segment, the
+     * result will be 0.
+     *
+     * @param px the x coordinate of the point
+     * @param py the y coordinate of the point
+     * @return the square of the distance from the point to the segment
+     * @see #ptSegDistSq(double, double, double, double, double, double)
+     */
+    public double ptSegDistSq(double px, double py)
+    {
+      return ptSegDistSq(getX1(), getY1(), getX2(), getY2(), px, py);
+    }
+
+    /**
+     * Measures the square of the shortest distance from the reference point
+     * to a point on this line segment. If the point is on the segment, the
+     * result will be 0.
+     *
+     * @param p the point
+     * @return the square of the distance from the point to the segment
+     * @throws NullPointerException if p is null
+     * @see #ptSegDistSq(double, double, double, double, double, double)
+     */
+    public double ptSegDistSq(Point2D p)
+    {
+      return ptSegDistSq(getX1(), getY1(), getX2(), getY2(), p.getX(), p.getY());
+    }
+
+    /**
+     * Measures the shortest distance from the reference point to a point on
+     * this line segment. If the point is on the segment, the result will be 0.
+     *
+     * @param px the x coordinate of the point
+     * @param py the y coordinate of the point
+     * @return the distance from the point to the segment
+     * @see #ptSegDist(double, double, double, double, double, double)
+     */
+    public double ptSegDist(double px, double py)
+    {
+      return ptSegDist(getX1(), getY1(), getX2(), getY2(), px, py);
+    }
+
+    /**
+     * Measures the shortest distance from the reference point to a point on
+     * this line segment. If the point is on the segment, the result will be 0.
+     *
+     * @param p the point
+     * @return the distance from the point to the segment
+     * @throws NullPointerException if p is null
+     * @see #ptSegDist(double, double, double, double, double, double)
+     */
+    public double ptSegDist(Point2D p)
+    {
+      return ptSegDist(getX1(), getY1(), getX2(), getY2(), p.getX(), p.getY());
+    }
+    
 }
