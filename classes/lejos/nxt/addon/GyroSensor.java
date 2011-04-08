c955ff1ff11b765a9475f6cbac6c8c2682282783
kirkpthompson <kirkpthompson@06c8f799-c779-4783-8af7-179afedb67c8>
2011-04-08 05:37:12 +0000

    Created GyroScope interface, implemented it in GyroSensor, added statistical bias calc to GyroSensor, modified GyroDirectionFinder to use modified GyroSensor.
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@4402 06c8f799-c779-4783-8af7-179afedb67c8

--- classes/lejos/nxt/addon/GyroSensor.java
+++ classes/lejos/nxt/addon/GyroSensor.java
@@ -2,6 +2,8 @@ package lejos.nxt.addon;
    
 import lejos.nxt.ADSensorPort;
 import lejos.nxt.SensorConstants;
+import lejos.robotics.Gyroscope;
+import lejos.util.Delay;
 
 /*
  * WARNING: THIS CLASS IS SHARED BETWEEN THE classes AND pccomms PROJECTS.
@@ -9,43 +11,107 @@ import lejos.nxt.SensorConstants;
  */
 
 /**
- * Support the HiTechnic Gyro sensor - untested.
+ * Support the HiTechnic Gyro sensor.
+ * <p>
+ * <b>Note:</b> You may want to use <code>setSpeed()</code> on any motor you will be using before instantiating so the AD sensor voltage stablizes. 
+ * Otherwise, the offset
+ * may be skewed. See LeJOS forum post <a href="http://lejos.sourceforge.net/forum/viewtopic.php?f=7&t=2276">"motor setSpeed() 
+ * changes AD sensor value"</a>
+ * <p>
  * http://www.hitechnic.com/
  * 
  * @author Lawrie Griffiths
+ * @author Kirk Thompson
  *
  */
-public class GyroSensor implements SensorConstants {
+public class GyroSensor implements SensorConstants, Gyroscope {
 	ADSensorPort port;
-	private int offset = 600;
-
-	public GyroSensor(ADSensorPort port)
-	{
+	private int offset = 0;
+    private double gsRawTotal;
+    private int samples;
+    
+    /**
+     * Creates and initializes a new <code>GyroSensor</code> bound to passed <code>ADSensorPort</code>.
+     * 
+     * @param port The <code>SensorPort</code> the Gyro is connected to
+     * @see lejos.nxt.SensorPort
+     */
+    public GyroSensor(ADSensorPort port) {
 		this.port = port;
-		port.setTypeAndMode(TYPE_CUSTOM,
-                            MODE_RAW);
+		port.setTypeAndMode(TYPE_CUSTOM, MODE_RAW);
 	}
 	
-	public GyroSensor(ADSensorPort port, int offset)
-	{
+    /**
+     * Creates and initializes a new <code>GyroSensor</code> bound to passed <code>ADSensorPort</code> and sets the 
+     * offset.
+     * 
+     * @param port The <code>SensorPort</code> the Gyro is connected to
+     * @param offset The offset to apply to <code>readValue()</code>
+     * @see lejos.nxt.SensorPort
+     */
+	public GyroSensor(ADSensorPort port, int offset) {
 		this(port);
 		this.offset = offset;
 	}
 	
 	/**
-	 * Read the gyro value
+	 * Read the gyro raw value and return with offset applied. Set offset to zero to return raw value.
 	 * 
 	 * @return gyro value
+     * @see #setOffset(int)
+     * @see #getAngularVelocity
 	 */
-	public int readValue()
-	{ 
+	public int readValue() { 
 		return (port.readRawValue() - offset); 
 	}
 	
 	/**
-	 * Set the offset
+	 * Set the offset used by <code>readValue()</code>. Default at instantiation is zero.
+     * @param offset The <code>int</code> offset value
+     * @see #readValue
 	 */
 	public void setOffset(int offset) {
 		this.offset = offset;
 	}
+
+    /** Calculate and return the current angular velocity. When integrating for a heading, values less than 1.0 can be ignored 
+     * to minimize perceived drift since the resolution of the Gyroscope sensor is 1 deg/sec.
+     * 
+     * @return The current angular velocity in degrees/sec
+     */
+    public double getAngularVelocity() {
+        int gsVal;
+        double stdev;
+        
+        gsVal=readValue();
+        // get the standard deviation of the raw value against the total bias population + itself.
+        stdev=Math.sqrt(Math.pow(gsVal-(gsRawTotal+gsVal)/(samples+1),2));
+        // if less than 2 standard deviations, allow to be used as the bias population
+        if (stdev<2.0f) {
+            gsRawTotal+=gsVal;
+            samples++; // if run long enough, this will... wrap?, blowup?
+        }
+        // subtract the mean bias from the raw value and return it
+        return gsVal-gsRawTotal/samples;
+    }
+
+
+    /** Sample the <u>stationary</u> (make sure it is) Gyro Sensor to determine the offset. Will reset the offset for
+     * <code>ReadValue()</code> to 0 (zero). Takes 5 seconds.
+     * 
+     * @see #setOffset(int)
+     */
+    public void setOffset() {
+        // seed the initial bias population
+        offset = 0;
+        gsRawTotal=readValue();
+        Delay.msDelay(10);
+        gsRawTotal+=readValue();
+        samples=2;
+        // populate bias population for 5 seconds
+        for (int i=0;i<500;i++) {
+            getAngularVelocity();
+            Delay.msDelay(10);
+        }
+    }
 }
