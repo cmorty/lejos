9e48c0306ee6ab4413f1aa206d37a71e11bb4eaa
kirkpthompson <kirkpthompson@06c8f799-c779-4783-8af7-179afedb67c8>
2011-04-09 06:45:52 +0000

    rename setOffset() to recalibrateOffset(), change doubles to floats, fixed error in stddev calc, and refactored offset calc algorithm
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@4404 06c8f799-c779-4783-8af7-179afedb67c8

--- classes/lejos/robotics/Gyroscope.java
+++ classes/lejos/robotics/Gyroscope.java
@@ -8,10 +8,10 @@ public interface Gyroscope {
     /** Implementor must calculate and return the angular velocity in degrees per second.
      * @return Angular velocity in degrees/second
      */
-    public double getAngularVelocity();
+    public float getAngularVelocity();
 
     /** Implementor must calculate and set the offset/bias value for use in <code>getAngularVelocity()</code>.
      */
-    public void setOffset();
+    public void recalibrateOffset();
 
  }
