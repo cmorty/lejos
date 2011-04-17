dc68d27b33969feef4524b355ad93a51ec4e2be2
gloomyandy <gloomyandy@06c8f799-c779-4783-8af7-179afedb67c8>
2011-04-17 17:06:25 +0000

    Allow PWM mode to be set when the motor is created and set the default operating mode be PWM_BRAKE (which offers better control of the motor).

--- trunk/classes/lejos/nxt/NXTMotor.java
+++ trunk/classes/lejos/nxt/NXTMotor.java
@@ -14,11 +14,27 @@ import lejos.robotics.UnregulatedMotor;
 public class NXTMotor extends BasicMotor implements UnregulatedMotor {
     protected Encoder encoderPort;
 
-	public NXTMotor(TachoMotorPort port)
-	{
-		this.port = port;
+    /**
+     * Create an instance of a NXTMotor using the specified motor port and
+     * PWM operating mode.
+     * @param port The motor port that the motor will be attached to.
+     * @param PWMMode see {@link lejos.nxt.BasicMotorPort#PWM_FLOAT} and see {@link lejos.nxt.BasicMotorPort#PWM_BRAKE}
+     */
+    public NXTMotor(TachoMotorPort port, int PWMMode)
+    {
+        this.port = port;
         // We use extra var to avoid cost of a cast check later
         encoderPort = port;
+        port.setPWMMode(PWMMode);
+    }
+    /**
+     * Create an instance of a NXTMotor using the specified motor port the
+     * PWM operating mode will be PWM_BREAK {@link lejos.nxt.BasicMotorPort#PWM_BRAKE}
+     * @param port The motor port that the motor will be attached to.
+     */
+	public NXTMotor(TachoMotorPort port)
+	{
+        this(port, TachoMotorPort.PWM_BRAKE);
 	}
     
     public int getTachoCount()
