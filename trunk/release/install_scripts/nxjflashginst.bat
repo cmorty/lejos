5517d5801a414b1e626cf979990c77e545ff291a
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2011-04-24 17:33:10 +0000

    Fixed Installation version of nxjflashg

--- trunk/release/install_scripts/nxjflashginst.bat
+++ trunk/release/install_scripts/nxjflashginst.bat
@@ -2,5 +2,5 @@
 rem This is for use by the Windows Installer only
 set NXJ_HOME=..
 
-java "-Djava.library.path=%NXJ_HOME%\bin" -Dnxj.home="%NXJ_HOME%" -classpath "%NXJ_HOME%\lib\pctools.jar;%NXJ_HOME%\lib\pccomm.jar" lejos.pc.tools.NXJFlashG %*
+call nxjflashg.bat
 
