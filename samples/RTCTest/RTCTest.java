ab3e8e9f098d71544556720c23e0e7c58cc5f173
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2011-04-25 22:16:02 +0000

    Changes to work correctly with 0.9 release
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@4481 06c8f799-c779-4783-8af7-179afedb67c8

--- samples/RTCTest/RTCTest.java
+++ samples/RTCTest/RTCTest.java
@@ -2,6 +2,9 @@ import lejos.nxt.*;
 import lejos.util.Delay;
 import java.util.Random;
 import java.io.IOException;
+
+import lejos.nxt.addon.Clock;
+import lejos.nxt.addon.DummyClock;
 import lejos.nxt.addon.RealTimeClock;
 
 public class RTCTest {
@@ -14,37 +17,35 @@ public class RTCTest {
 		byte[] ramTest = new byte[56];
 		Random r = new Random();
 		int i;
-		RealTimeClock clock = new RealTimeClock(SensorPort.S1);
+		Clock clock = new RealTimeClock(SensorPort.S1);
+		//Clock clock = new DummyClock();
 		int cur = 0;
 
 		try {
 			clock.setHourMode(true);
 		} catch (IOException e) {			
 		}
-
-		LCD.clear();
-
-		while (Button.ENTER.isPressed())
-			Delay.msDelay(100);
 		
+		// Display date and time components
 		try {
 			dateVals[0] = new UpDown(0,0,clock.getMonth(),1,12,2);
-			dateVals[1] = new UpDown(3*LCD.CELL_WIDTH,0,clock.getDay(),1,31,2);
-			dateVals[2] = new UpDown(6*LCD.CELL_WIDTH,0,clock.getYear(),2000,2099,4);
-			dateVals[3] = new UpDown(11*LCD.CELL_WIDTH,0,0,0,1,1);			
-			dateVals[4] = new UpDown(0,2*LCD.CELL_HEIGHT,clock.getHour(),0,23,2);
-			dateVals[5] = new UpDown(3*LCD.CELL_WIDTH,2*LCD.CELL_HEIGHT,clock.getMinute(),0,59,2);
-			dateVals[6] = new UpDown(6*LCD.CELL_WIDTH,2*LCD.CELL_HEIGHT,clock.getSecond(),0,59,2);
+			dateVals[1] = new UpDown(3,0,clock.getDay(),1,31,2);
+			dateVals[2] = new UpDown(6,0,clock.getYear(),2000,2099,4);
+			dateVals[3] = new UpDown(11,0,0,0,1,1);			
+			dateVals[4] = new UpDown(0,2,clock.getHour(),0,23,2);
+			dateVals[5] = new UpDown(3,2,clock.getMinute(),0,59,2);
+			dateVals[6] = new UpDown(6,2,clock.getSecond(),0,59,2);
 		} catch (IOException e) {
 			  LCD.clear();
 			  LCD.drawString("I/O Exception",0,0);
-			  while (!Button.LEFT.isPressed())
-				  Delay.msDelay(100);
+			  Button.ESCAPE.waitForPressAndRelease();
 			  return;
 		}
 
+		// Set the month active
 		dateVals[0].setActive(true);
 
+		// RAM test
 		for (i=0;i<56;i++)
 			ramTest[i] = (byte)(r.nextInt(256));
 
@@ -59,8 +60,7 @@ public class RTCTest {
 		} catch (IOException e) {
 			  LCD.clear();
 			  LCD.drawString("I/O Exception RAM",0,0);
-			  while (!Button.LEFT.isPressed())
-				  Delay.msDelay(100);
+			  Button.ESCAPE.waitForPressAndRelease();
 			  return;
 		}
 
@@ -69,41 +69,41 @@ public class RTCTest {
 		else
 			LCD.drawString("RAM Fail", 0, 7);
 
+		// Incrementing 4th field terminates test
 		while (dateVals[3].getVal() == 0) {
+			// Show the current date and time
 			try {
 				LCD.drawString(clock.getDateString(), 0, 4);
 				LCD.drawString(""+clock.getDayOfWeek()+" "+clock.getAMPM(), 0, 5);
 				LCD.drawString(clock.getTimeString(), 0, 6);
-				//clock.setYear(0);
-
 			} catch (IOException e) {
 				  LCD.clear();
 				  LCD.drawString("I/O Exception",0,0);
-				  while (!Button.LEFT.isPressed())
-					  Delay.msDelay(100);
+				  Button.ESCAPE.waitForPressAndRelease();
 				  return;
 			}
-				
+			
+			// Decrement the current component
 			if (Button.LEFT.isPressed()) {
 				dateVals[cur].decrement();
-				while (Button.LEFT.isPressed())
-					Delay.msDelay(100);
+				while (Button.LEFT.isPressed()) Delay.msDelay(100);
 			}
 
+			// Increment the current component
 			if (Button.RIGHT.isPressed()) {
 				dateVals[cur].increment();				
-				while (Button.RIGHT.isPressed())
-					Delay.msDelay(100);
+				while (Button.RIGHT.isPressed()) Delay.msDelay(100);
 			}
 
+			// Move to next component
 			if (Button.ENTER.isPressed()) {
 				dateVals[cur].setActive(false);
 				cur = (cur + 1) % 7;
 				dateVals[cur].setActive(true);
-				while (Button.ENTER.isPressed())
-					Delay.msDelay(100);
+				while (Button.ENTER.isPressed()) Delay.msDelay(100);
 			}
 
+			// Set the date and time
 			if (Button.ESCAPE.isPressed()) {
 				dateVals[cur].setActive(false);
 				cur = 0;
@@ -120,20 +120,16 @@ public class RTCTest {
 				} catch (IOException e) {
 					  LCD.clear();
 					  LCD.drawString("I/O Exception",0,0);
-					  while (!Button.LEFT.isPressed())
-						  Delay.msDelay(100);
+					  Button.ESCAPE.waitForPressAndRelease();
 
 					  return;
-					} catch (IllegalArgumentException e) {
-						  LCD.clear();
-						  LCD.drawString("Illegal Argument",0,0);
-						  while (!Button.LEFT.isPressed())
-							  Delay.msDelay(100);
-						  return;
-					}
-
-				while (Button.ESCAPE.isPressed())
-					Delay.msDelay(100);
+				} catch (IllegalArgumentException e) {
+					  LCD.clear();
+					  LCD.drawString("Illegal Argument",0,0);
+					  Button.ESCAPE.waitForPressAndRelease();
+					  return;
+				}
+				Button.ESCAPE.waitForPressAndRelease();
 			}
 			Delay.msDelay(100);
 		}
