c19ae8083d7c0662906f81c13d25b1269814192e
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2011-04-30 09:36:50 +0000

    Copied classes
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@4515 06c8f799-c779-4783-8af7-179afedb67c8

--- pccomms/lejos/robotics/objectdetection/FeatureDetector.java
+++ pccomms/lejos/robotics/objectdetection/FeatureDetector.java
@@ -5,6 +5,7 @@ package lejos.robotics.objectdetection;
  * a term for any property that can be added to map data. The FeatureListeneer is notified when an object is detected,
  * even if it has previously detected the same object.
  * @see lejos.robotics.objectdetection.FeatureListener
+ * @author BB based on concepts by Lawrie Griffiths
  *
  */
 public interface FeatureDetector {
@@ -15,6 +16,19 @@ public interface FeatureDetector {
 	 * @param listener The FeatureListener that is notified every time a feature is detected.
 	 * 
 	 */
-	public abstract void addListener(FeatureListener listener);
+	public void addListener(FeatureListener listener);
+	
+	/**
+	 * Performs a single scan for an object and returns the results.
+	 * @return A feature it has detected. null if nothing found. 
+	 */
+	public DetectableFeature scan();
+	
+	/**
+	 * Enable or disable detection of objects.
+	 * 
+	 * @param on true enables detection and notifications, false disables this class until it is enabled again.
+	 */
+	public void enableDetection(boolean on);
 	
 }
