30722d824bc8e02d1a268bd18a2ab6e32b652acf
skoehler <skoehler@06c8f799-c779-4783-8af7-179afedb67c8>
2011-04-30 17:11:38 +0000

    classes to notify users of changes

---
+++ trunk/pctools/lejos/pc/tools/Console.java
@@ -0,0 +1,15 @@
+package lejos.pc.tools;
+
+/**
+ * @deprecated use lejos.pc.tools.NXJLink instead
+ */
+@Deprecated
+public class Console
+{
+	public static void main(String[] args)
+	{
+		System.err.println("Upgrade your build file. Use the class lejos.pc.tools.NXJConsole");
+		System.err.println("instead of lejos.pc.tools.Console.");
+		System.exit(1);
+	}
+}
