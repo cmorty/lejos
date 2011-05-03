f7773925dcdade57a9fc8b542df2923e6a25a415
skoehler <skoehler@06c8f799-c779-4783-8af7-179afedb67c8>
2011-05-03 21:17:48 +0000

    implement second read() method in order to be compatible with BufferedInputStream

--- trunk/pccomms/lejos/pc/comm/NXTCommInputStream.java
+++ trunk/pccomms/lejos/pc/comm/NXTCommInputStream.java
@@ -1,6 +1,7 @@
 package lejos.pc.comm;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Implementation of InputStream over NXTComm using Bluetooth.
@@ -18,25 +19,61 @@ public class NXTCommInputStream extends InputStream {
 		bufSize = 0;
 	}
 	
+	@Override
+	public int read(byte[] b, int off, int len) throws IOException
+	{
+		if (endOfFile)
+			return -1;
+		
+		int avail = bufSize - bufIdx;
+		if (avail <= 0)
+		{
+			buf = nxtComm.read();
+			bufIdx = 0;
+			if (buf == null || buf.length == 0)
+			{
+				bufSize = 0;
+				endOfFile = true;
+				return -1;
+			}
+			avail = bufSize = buf.length;
+		}
+		
+		if (len > avail)
+			len = avail;
+		
+		System.arraycopy(buf, bufIdx, b, off, len);
+		bufIdx += len;
+		
+		return len;
+	}
+	
     /**
      * Returns one byte as an integer between 0 and 255.  
      * Returns -1 if the end of the stream is reached.
      * Does not return till some bytes are available.
      */
+	@Override
 	public int read() throws IOException
     {
-	   if (endOfFile) return -1;
-	   if (bufIdx >= bufSize) bufSize = 0;
-       if (bufSize == 0) {
-    	   bufIdx = 0;
-    	   buf = nxtComm.read();
-    	   if (buf == null || buf.length ==0) {
-    		   endOfFile = true;
-    		   return -1;
-    	   }
-    	   bufSize = buf.length;
-       }
-       return buf[bufIdx++] & 0xFF;
+		if (endOfFile)
+			return -1;
+		
+		int avail = bufSize - bufIdx;
+		if (avail <= 0)
+		{
+			buf = nxtComm.read();
+			bufIdx = 0;
+			if (buf == null || buf.length == 0)
+			{
+				bufSize = 0;
+				endOfFile = true;
+				return -1;
+			}
+			bufSize = buf.length;
+		}
+		
+		return buf[bufIdx++] & 0xFF;
 	}
 	
     /**
