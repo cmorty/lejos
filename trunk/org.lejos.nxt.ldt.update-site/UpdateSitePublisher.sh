21aae14bac62590b1e1d1912f4fee83ee2fca945
jtangney <jtangney@06c8f799-c779-4783-8af7-179afedb67c8>
2011-05-12 04:53:34 +0000

    Reworked shell script to generate an update site in a local directory.

--- trunk/org.lejos.nxt.ldt.update-site/UpdateSitePublisher.sh
+++ trunk/org.lejos.nxt.ldt.update-site/UpdateSitePublisher.sh
@@ -1,29 +1,26 @@
 #!/bin/bash
-## Push a complete update site to a local Tomcat install so that you can use a
-# URL of the following form within Eclipse:
-# http://localhost:8080/update
-# The directory that gets pushed to Tomcat's webapps directory is exactly what needs 
-# to be published on the public web site as the "update site".
+## Generates all the files need for the update site and puts them into
+# the dist directory. From here, all you need to do is copy them to 
+# the live update site.
 
 # Until we convert this into a grownup Ant script, please modify the
 # following variables to match your environment. If you've set these
 # in your .profile, you don't need to change it here.
 [ -z "$ECLIPSE_HOME" ] && ECLIPSE_HOME=/Applications/helios
-[ -z "$CATALINA_HOME" ] && CATALINA_HOME=/usr/share/tomcat6
 
 # Assume that this script is being run from within the workspace
 CURRENT_DIR=`dirname "$0"`
 [ -z "$WORKSPACE" ] && WORKSPACE=`cd "$CURRENT_DIR/.." ; pwd`
 
 PROJECT=${WORKSPACE}/org.lejos.nxt.ldt.update-site
-WEBAPP=${CATALINA_HOME}/webapps/lejos/update
+DIST=${PROJECT}/dist
 
 java \
 	-jar ${ECLIPSE_HOME}/plugins/org.eclipse.equinox.launcher_*.jar \
 	-application org.eclipse.equinox.p2.publisher.UpdateSitePublisher \
-	-metadataRepository file:${WEBAPP} \
+	-metadataRepository file:${DIST} \
     -metadataRepositoryName "LeJOS Update Site" \
-	-artifactRepository file:${WEBAPP} \
+	-artifactRepository file:${DIST} \
     -artifactRepositoryName "LeJOS Eclipse Plugin" \
 	-source ${PROJECT} \
 	-configs gtk.linux.x86 \
@@ -33,7 +30,7 @@ java \
 java \
     -jar ${ECLIPSE_HOME}/plugins/org.eclipse.equinox.launcher_*.jar \
     -application org.eclipse.equinox.p2.publisher.CategoryPublisher \
-    -metadataRepository file:${WEBAPP} \
+    -metadataRepository file:${DIST} \
     -categoryDefinition file:${PROJECT}/category.xml	
 
-cp ${PROJECT}/site.xml ${WEBAPP}
+cp ${PROJECT}/site.xml ${DIST}
