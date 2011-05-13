053a3f39b74458f12a14d695c3b0dd4e27cadfd5
skoehler <skoehler@06c8f799-c779-4783-8af7-179afedb67c8>
2011-05-13 14:31:52 +0000

    decode+encode methods

--- trunk/classes/lejos/util/EndianTools.java
+++ trunk/classes/lejos/util/EndianTools.java
@@ -2,55 +2,96 @@ package lejos.util;
 
 public class EndianTools
 {
-	public static long beLong(byte[] b, int off)
+	public static long decodeLongBE(byte[] b, int off)
 	{
-		return ((long) beInt(b, off) << 32) | (beInt(b, off + 4) & 0xFFFFFFFFL);
+		return ((long) decodeIntBE(b, off) << 32) | (decodeIntBE(b, off + 4) & 0xFFFFFFFFL);
 	}
 
-	public static long beUInt(byte[] b, int off)
+	public static long decodeUIntBE(byte[] b, int off)
 	{
-		return beInt(b, off) & 0xFFFFFFFFL;
+		return decodeIntBE(b, off) & 0xFFFFFFFFL;
 	}
 
-	public static int beUShort(byte[] b, int off)
+	public static int decodeUShortBE(byte[] b, int off)
 	{
-		return beShort(b, off) & 0xFFFF;
+		return decodeShortBE(b, off) & 0xFFFF;
 	}
 
-	public static int beInt(byte[] b, int off)
+	public static int decodeIntBE(byte[] b, int off)
 	{
 		return (b[off] << 24) | ((b[off + 1] & 0xFF) << 16)
 			| ((b[off + 2] & 0xFF) << 8) | (b[off + 3] & 0xFF);
 	}
 
-	public static short beShort(byte[] b, int off)
+	public static short decodeShortBE(byte[] b, int off)
 	{
 		return (short) ((b[off] << 8) | (b[off + 1] & 0xFF));
 	}
-
-	public static long leLong(byte[] b, int off)
+	
+	public static long decodeLongLE(byte[] b, int off)
 	{
-		return (leInt(b, off) & 0xFFFFFFFFL) | ((long) leInt(b, off + 4) << 32);
+		return (decodeIntLE(b, off) & 0xFFFFFFFFL) | ((long) decodeIntLE(b, off + 4) << 32);
 	}
 
-	public static long leUInt(byte[] b, int off)
+	public static long decodeUIntLE(byte[] b, int off)
 	{
-		return leInt(b, off) & 0xFFFFFFFFL;
+		return decodeIntLE(b, off) & 0xFFFFFFFFL;
 	}
 
-	public static int leUShort(byte[] b, int off)
+	public static int decodeUShortLE(byte[] b, int off)
 	{
-		return leShort(b, off) & 0xFFFF;
+		return decodeShortLE(b, off) & 0xFFFF;
 	}
 
-	public static int leInt(byte[] b, int off)
+	public static int decodeIntLE(byte[] b, int off)
 	{
 		return (b[off] & 0xFF) | ((b[off + 1] & 0xFF) << 8)
 			| ((b[off + 2] & 0xFF) << 16) | (b[off + 3] << 24);
 	}
 	
-	public static short leShort(byte[] b, int off)
+	public static short decodeShortLE(byte[] b, int off)
 	{
 		return (short)((b[off] & 0xFF) | (b[off + 1] << 8));
 	}
+
+	public static void encodeLongBE(long v, byte[] b, int off)
+	{
+		encodeIntBE((int)(v >>> 32), b, off);
+		encodeIntBE((int)v, b, off+4);
+	}
+
+	public static void encodeIntBE(int v, byte[] b, int off)
+	{
+		b[off] = (byte)(v >>> 24);
+		b[off+1] = (byte)(v >>> 16);
+		b[off+2] = (byte)(v >>> 8);
+		b[off+3] = (byte)v;
+	}
+
+	public static void encodeShortBE(int v, byte[] b, int off)
+	{
+		b[off] = (byte)(v >>> 8);
+		b[off+1] = (byte)v;
+	}
+
+	public static void encodeLongLE(long v, byte[] b, int off)
+	{
+		encodeIntLE((int)v, b, off);
+		encodeIntLE((int)(v >>> 32), b, off+4);
+	}
+
+	public static void encodeIntLE(int v, byte[] b, int off)
+	{
+		b[off] = (byte)v;
+		b[off+1] = (byte)(v >>> 8);
+		b[off+2] = (byte)(v >>> 16);
+		b[off+3] = (byte)(v >>> 24);
+	}
+
+	public static void encodeShortLE(int v, byte[] b, int off)
+	{
+		b[off] = (byte)v;
+		b[off+1] = (byte)(v >>> 8);
+	}
+
 }
