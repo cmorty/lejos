88cf07ef21184e6567ea95ddab364f0fc2d2e56f
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2011-05-13 14:13:18 +0000

    Used Gyroscope interface
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@4739 06c8f799-c779-4783-8af7-179afedb67c8

--- samples/GyroTest/GyroTest.java
+++ samples/GyroTest/GyroTest.java
@@ -2,6 +2,7 @@ import lejos.nxt.Button;
 import lejos.nxt.LCD;
 import lejos.nxt.SensorPort;
 import lejos.nxt.addon.GyroSensor;
+import lejos.robotics.Gyroscope;
 
 /**
  * Test of Gyro Sensor
@@ -11,8 +12,8 @@ import lejos.nxt.addon.GyroSensor;
  */
 public class GyroTest {
 	public static void main(String[] args) {
-		GyroSensor gyro = new GyroSensor(SensorPort.S1);
-		int minValue = 0, maxValue = 0;
+		Gyroscope gyro = new GyroSensor(SensorPort.S1);
+		float minValue = 0, maxValue = 0;
 		
 		LCD.drawString("Gyro Test:", 0, 0);
 		LCD.drawString("Min:", 0, 2);
@@ -20,14 +21,14 @@ public class GyroTest {
 		LCD.drawString("Current:", 0, 4);
 		
 		while(!Button.ESCAPE.isPressed()) {
-			int value = gyro.readValue();
+			float value = gyro.getAngularVelocity();
 			
 			minValue = Math.min(minValue, value);
 			maxValue = Math.max(maxValue, value);
 			
-			LCD.drawInt(minValue, 6, 5, 2);
-			LCD.drawInt(maxValue, 6, 5, 3);
-			LCD.drawInt(value, 6, 9, 4);
+			LCD.drawInt((int)minValue, 6, 5, 2);
+			LCD.drawInt((int) maxValue, 6, 5, 3);
+			LCD.drawInt((int) value, 6, 9, 4);
 		}
 	}
 }
