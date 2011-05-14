733000714eacfb7f4e9bdea6c887d9aaa0390a13
kirkpthompson <kirkpthompson@06c8f799-c779-4783-8af7-179afedb67c8>
2011-05-14 02:47:17 +0000

    changed isRotating() to isMoving() in code example comments
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@4752 06c8f799-c779-4783-8af7-179afedb67c8

--- classes/lejos/nxt/NXTRegulatedMotor.java
+++ classes/lejos/nxt/NXTRegulatedMotor.java
@@ -40,7 +40,7 @@ import lejos.util.Delay;
  *   Motor.C.stop();
  *   Motor.A.rotateTo( 360);
  *   Motor.A.rotate(-720,true);
- *   while(Motor.A.isRotating() :Thread.yield();
+ *   while(Motor.A.isMoving() :Thread.yield();
  *   int angle = Motor.A.getTachoCount(); // should be -360
  *   LCD.drawInt(angle,0,0);
  * </pre></code>
