b42d0bbcdeb7ceecd668e3070487a6451a0dfe9f
kirkpthompson <kirkpthompson@06c8f799-c779-4783-8af7-179afedb67c8>
2011-05-16 04:39:43 +0000

    changed to work with MMXRegulatedMotor

--- trunk/samples/NXTMMXSample/NXTMMXSample.java
+++ trunk/samples/NXTMMXSample/NXTMMXSample.java
@@ -1,11 +1,20 @@
 
-import lejos.nxt.*;
+import lejos.nxt.Button;
+import lejos.nxt.LCD;
+import lejos.nxt.SensorPort;
+import lejos.nxt.addon.MMXRegulatedMotor;
 import lejos.nxt.addon.NXTMMX;
-import lejos.nxt.addon.NXTMMXMotor;
+
+import lejos.robotics.RegulatedMotor;
+import lejos.robotics.RegulatedMotorListener;
+
+import lejos.util.Delay;
+
 /**
  *This is sample code demonstrating how to use the mindsensors NXTMMX.
  * 
  * @author Michael D. Smith mdsmitty@gmail.com
+ * @author Kirk P. Thompson
  *
  */
 public class NXTMMXSample {
@@ -13,31 +22,57 @@ public class NXTMMXSample {
 	public static void main(String[] args) {
 		NXTMMX mux = new NXTMMX(SensorPort.S1);
 		
-		NXTMMXMotor cat = mux.A;
-		NXTMMXMotor dog = mux.B;
+		MMXRegulatedMotor cat = new MMXRegulatedMotor(mux, NXTMMX.MMX_MOTOR_1);
+		MMXRegulatedMotor dog = new MMXRegulatedMotor(mux, NXTMMX.MMX_MOTOR_2);
 		
+	    // add a listener to tell us when cat starts and stops
+        RegulatedMotorListener rml = new RegulatedMotorListener(){
+	        public void rotationStarted(RegulatedMotor motor,int tachoCount, boolean stalled, long timeStamp){
+	            LCD.drawString("cat start:" + tachoCount + "  ", 0,2); 
+	        }
+
+	        public void rotationStopped(RegulatedMotor motor, int tachoCount, boolean stalled, long timeStamp) {
+	            LCD.drawString("cat stop:" + tachoCount + "   ", 0,2); 
+	        }
+	    };
+	    cat.addListener(rml); 
+        
 		//Demo of basic forwards and backwards operations
-		cat.setSpeed(100);
-		dog.setSpeed(50);
+		cat.setPower(10);
+		dog.setSpeed(500);
 				
 		cat.forward();
 		dog.backward();
 
-		Button.ENTER.waitForPressAndRelease();
-
-		mux.breakMotors();
+		LCD.drawString("Enter to stop ",0,1);
+	    while(!Button.ENTER.isPressed()) {
+	        Delay.msDelay(130);
+	        LCD.drawString("cat tach:" + cat.getTachoCount() + " ", 0,4);
+	        LCD.drawString("dog tach:" + dog.getTachoCount() + " ", 0,5);
+	        LCD.drawString("cat dps:" + cat.getRotationSpeed() + " ", 0,6);
+	        LCD.drawString("dog dps:" + dog.getRotationSpeed() + " ",0,7);
+	    }
+        
+	    LCD.clearDisplay();
+		mux.stopMotors();
 		
-		
+		Delay.msDelay(500);
+	    LCD.drawString("Enter to Rotate",0,1);
 		Button.ENTER.waitForPressAndRelease();
 		
 		//demo tacho stuff
-		cat.rotate(500);
-		dog.rotateTo(-400, true);
+		cat.rotate(-2000, true);
+		dog.rotateTo(8000, true);
 		
-		Button.ENTER.waitForPressAndRelease();
-		
-		//demo time stuff
-		cat.setSpeed(5);
-		cat.rotateTime(15, true);
+	    LCD.drawString("Enter to exit  ",0,1);
+	    while(!Button.ENTER.isPressed()) {
+            Delay.msDelay(130);
+            LCD.drawString("cat stall:" + cat.isStalled() + " ", 0,4); 
+            LCD.drawString("dog moving:" + dog.isMoving() + " ", 0,5); 
+            LCD.drawString("cat tach:" + cat.getTachoCount() + " ", 0,6);
+            LCD.drawString("dog dps:" + dog.getRotationSpeed() + " ",0,7);
+        }
+        mux.fltMotors();
+
 	}
 }
