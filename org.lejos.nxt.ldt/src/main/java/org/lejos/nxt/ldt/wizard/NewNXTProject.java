fdbd1d2f738584b085c642bd1f77f73ded5c08f1
skoehler <skoehler@06c8f799-c779-4783-8af7-179afedb67c8>
2011-05-25 20:30:16 +0000

    first RC of wizards
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@4944 06c8f799-c779-4783-8af7-179afedb67c8

--- org.lejos.nxt.ldt/src/main/java/org/lejos/nxt/ldt/wizard/NewNXTProject.java
+++ org.lejos.nxt.ldt/src/main/java/org/lejos/nxt/ldt/wizard/NewNXTProject.java
@@ -1,42 +1,20 @@
 package org.lejos.nxt.ldt.wizard;
 
-import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.wizard.Wizard;
-import org.eclipse.ui.INewWizard;
-import org.eclipse.ui.IWorkbench;
 
-public class NewNXTProject extends Wizard implements INewWizard {
+public class NewNXTProject extends AbstractNewProjectWizard {
+	/*
+	 * code partly inspired or copied from
+	 * org.eclipse.jdt.internal.ui.wizards.JavaProjectWizard 
+	 */
 	
-	private NewNXTProjectPageOne pageOne = new NewNXTProjectPageOne();
-	private NewNXTProjectPageTwo pageTwo = new NewNXTProjectPageTwo(pageOne);
-	
-
 	public NewNXTProject() {
-	    setWindowTitle("New LeJOS NXT Project");
-	    
+		super();
+		pageOne = new NewNXTProjectPageOne();
+		pageTwo = new NewNXTProjectPageTwo(pageOne);
+		
 		pageOne.setTitle("New LeJOS NXT Project");
 		pageOne.setDescription("Create a new LeJOS Project for programs running on the NXT");
-	}
-
-	public void init(IWorkbench workbench, IStructuredSelection selection) {
-		//nothing to do
-	}
-
-	@Override
-	public void addPages() {
-		super.addPages();
-		addPage(pageOne);
-		addPage(pageTwo);
-	}
-
-	@Override
-	public boolean performFinish() {
-		return true;
-	}
-
-	@Override
-	public boolean performCancel() {
-		pageTwo.performCancel();
-		return super.performCancel();
+		
+	    setWindowTitle("New LeJOS NXT Project");
 	}
 }
