e5b2aae56b96642de674f0a6808f4a8d6a9f90c6
skoehler <skoehler@06c8f799-c779-4783-8af7-179afedb67c8>
2011-05-25 20:30:16 +0000

    first RC of wizards

---
+++ trunk/org.lejos.nxt.ldt/src/main/java/org/lejos/nxt/ldt/wizard/AbstractNewProjectWizard.java
@@ -0,0 +1,54 @@
+package org.lejos.nxt.ldt.wizard;
+
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExecutableExtension;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.ui.wizards.NewJavaProjectWizardPageOne;
+import org.eclipse.jdt.ui.wizards.NewJavaProjectWizardPageTwo;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkingSet;
+import org.eclipse.ui.wizards.newresource.BasicNewProjectResourceWizard;
+import org.eclipse.ui.wizards.newresource.BasicNewResourceWizard;
+
+public abstract class AbstractNewProjectWizard extends Wizard implements INewWizard, IExecutableExtension{
+
+	protected IWorkbench fWorkbench;
+	protected NewJavaProjectWizardPageOne pageOne;
+	protected NewJavaProjectWizardPageTwo pageTwo;
+	protected IConfigurationElement fConfigElement;
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		fWorkbench = workbench;
+	}
+	@Override
+	public void addPages() {
+		super.addPages();
+		addPage(pageOne);
+		addPage(pageTwo);
+	}
+	public void setInitializationData(IConfigurationElement cfig, String propertyName, Object data) {
+		fConfigElement= cfig;
+	}
+	@Override
+	public boolean performCancel() {
+		pageTwo.performCancel();
+		return super.performCancel();
+	}
+	@Override
+	public boolean performFinish() {
+		final IJavaProject newProject = pageTwo.getJavaProject();
+	
+		IWorkingSet[] workingSets= pageOne.getWorkingSets();
+		if (workingSets != null && workingSets.length > 0) {
+			fWorkbench.getWorkingSetManager().addToWorkingSets(newProject, workingSets);
+		}
+	
+		BasicNewProjectResourceWizard.updatePerspective(fConfigElement);
+		BasicNewResourceWizard.selectAndReveal(newProject.getProject(), fWorkbench.getActiveWorkbenchWindow());
+		
+		return true;
+	}
+
+}
--- trunk/org.lejos.nxt.ldt/src/main/java/org/lejos/nxt/ldt/wizard/NewPCProject.java
+++ trunk/org.lejos.nxt.ldt/src/main/java/org/lejos/nxt/ldt/wizard/NewPCProject.java
@@ -1,42 +1,17 @@
 package org.lejos.nxt.ldt.wizard;
 
 import org.eclipse.jdt.ui.wizards.NewJavaProjectWizardPageTwo;
-import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.wizard.Wizard;
-import org.eclipse.ui.INewWizard;
-import org.eclipse.ui.IWorkbench;
 
-public class NewPCProject extends Wizard implements INewWizard {
+public class NewPCProject extends AbstractNewProjectWizard {
 
-	private NewPCProjectPageOne pageOne = new NewPCProjectPageOne();
-	private NewJavaProjectWizardPageTwo pageTwo = new NewJavaProjectWizardPageTwo(pageOne);
-	
 	public NewPCProject() {
-	    setWindowTitle("New LeJOS NXT Project");
-
+		super();
+		pageOne = new NewPCProjectPageOne();
+		pageTwo = new NewJavaProjectWizardPageTwo(pageOne);
+		
 		pageOne.setTitle("New LeJOS NXT Project");
 		pageOne.setDescription("Create a new LeJOS Project for programs running on the NXT");
-	}
-
-	public void init(IWorkbench workbench, IStructuredSelection selection) {
-		// noting to do
-	}
-	
-	@Override
-	public void addPages() {
-		super.addPages();
-		addPage(pageOne);
-		addPage(pageTwo);
-	}
-
-	@Override
-	public boolean performFinish() {
-		return true;
-	}
-	
-	@Override
-	public boolean performCancel() {
-		pageTwo.performCancel();
-		return super.performCancel();
+		
+	    setWindowTitle("New LeJOS NXT Project");
 	}
 }
