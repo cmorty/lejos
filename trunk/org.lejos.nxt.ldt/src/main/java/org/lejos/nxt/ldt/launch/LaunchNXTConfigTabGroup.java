75146d69b8a5d1438a2205c2408e9e1f48a0eae9
skoehler <skoehler@06c8f799-c779-4783-8af7-179afedb67c8>
2011-05-27 23:44:16 +0000

    preparations for launch configurations

---
+++ trunk/org.lejos.nxt.ldt/src/main/java/org/lejos/nxt/ldt/launch/LaunchNXTConfigTabGroup.java
@@ -0,0 +1,17 @@
+package org.lejos.nxt.ldt.launch;
+
+import org.eclipse.debug.ui.AbstractLaunchConfigurationTabGroup;
+import org.eclipse.debug.ui.ILaunchConfigurationDialog;
+
+public class LaunchNXTConfigTabGroup extends AbstractLaunchConfigurationTabGroup {
+
+	public LaunchNXTConfigTabGroup() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void createTabs(ILaunchConfigurationDialog dialog, String mode) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
