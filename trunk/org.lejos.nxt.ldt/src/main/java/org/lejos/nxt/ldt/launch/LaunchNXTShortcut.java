b145308c049ae7025ad03d98bdffe2b70995fe5f
skoehler <skoehler@06c8f799-c779-4783-8af7-179afedb67c8>
2011-05-28 17:52:13 +0000

    fixed shortcut

--- trunk/org.lejos.nxt.ldt/src/main/java/org/lejos/nxt/ldt/launch/LaunchNXTShortcut.java
+++ trunk/org.lejos.nxt.ldt/src/main/java/org/lejos/nxt/ldt/launch/LaunchNXTShortcut.java
@@ -1,7 +1,13 @@
 package org.lejos.nxt.ldt.launch;
 
+import org.eclipse.debug.core.DebugPlugin;
+import org.eclipse.debug.core.ILaunchConfigurationType;
 import org.eclipse.jdt.debug.ui.launchConfigurations.JavaApplicationLaunchShortcut;
 
-public class LaunchNXTShortcut extends JavaApplicationLaunchShortcut {
-
+public class LaunchNXTShortcut extends JavaApplicationLaunchShortcut
+{
+	@Override
+	protected ILaunchConfigurationType getConfigurationType() {
+		return DebugPlugin.getDefault().getLaunchManager().getLaunchConfigurationType(LaunchNXTConfigDelegate.ID_TYPE);
+	}
 }
