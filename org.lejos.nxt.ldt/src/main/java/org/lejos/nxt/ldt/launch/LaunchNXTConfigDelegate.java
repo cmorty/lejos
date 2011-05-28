462a3da0627cc98bd12bd8b85e1e97e502059d1b
skoehler <skoehler@06c8f799-c779-4783-8af7-179afedb67c8>
2011-05-28 17:52:13 +0000

    fixed shortcut
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@4981 06c8f799-c779-4783-8af7-179afedb67c8

--- org.lejos.nxt.ldt/src/main/java/org/lejos/nxt/ldt/launch/LaunchNXTConfigDelegate.java
+++ org.lejos.nxt.ldt/src/main/java/org/lejos/nxt/ldt/launch/LaunchNXTConfigDelegate.java
@@ -7,6 +7,7 @@ import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.model.ILaunchConfigurationDelegate;
 
 public class LaunchNXTConfigDelegate implements ILaunchConfigurationDelegate {
+	public static final String ID_TYPE = "org.lejos.nxt.ldt.LaunchType";
 
 	public void launch(ILaunchConfiguration configuration, String mode,
 			ILaunch launch, IProgressMonitor monitor) throws CoreException {
