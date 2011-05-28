a34d1e59befac4f8382cd7d6280fc78cab38fa36
skoehler <skoehler@06c8f799-c779-4783-8af7-179afedb67c8>
2011-05-28 21:02:33 +0000

    mc
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@4987 06c8f799-c779-4783-8af7-179afedb67c8

--- org.lejos.nxt.ldt/src/main/java/org/lejos/nxt/ldt/launch/LaunchNXTConfigTabGroup.java
+++ org.lejos.nxt.ldt/src/main/java/org/lejos/nxt/ldt/launch/LaunchNXTConfigTabGroup.java
@@ -1,17 +1,30 @@
 package org.lejos.nxt.ldt.launch;
 
+import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
 import org.eclipse.debug.ui.AbstractLaunchConfigurationTabGroup;
+import org.eclipse.debug.ui.CommonTab;
 import org.eclipse.debug.ui.ILaunchConfigurationDialog;
+import org.eclipse.debug.ui.ILaunchConfigurationTab;
+import org.eclipse.debug.ui.sourcelookup.SourceLookupTab;
+import org.eclipse.jdt.debug.ui.launchConfigurations.JavaClasspathTab;
+import org.eclipse.jdt.debug.ui.launchConfigurations.JavaMainTab;
+import org.eclipse.jdt.launching.IJavaLaunchConfigurationConstants;
 
 public class LaunchNXTConfigTabGroup extends AbstractLaunchConfigurationTabGroup {
-
-	public LaunchNXTConfigTabGroup() {
-		// TODO Auto-generated constructor stub
-	}
-
 	public void createTabs(ILaunchConfigurationDialog dialog, String mode) {
-		// TODO Auto-generated method stub
-
+		setTabs(new ILaunchConfigurationTab[] {
+				new JavaMainTab(),
+				new JavaClasspathTab(),
+				new SourceLookupTab(),
+				new CommonTab(),
+			});
 	}
-
+	
+	@Override
+	public void setDefaults(ILaunchConfigurationWorkingCopy configuration) {
+		super.setDefaults(configuration);
+		
+		configuration.setAttribute(IJavaLaunchConfigurationConstants.ATTR_CLASSPATH_PROVIDER,
+				LaunchNXTClasspathProvider.ID);
+	}
 }
