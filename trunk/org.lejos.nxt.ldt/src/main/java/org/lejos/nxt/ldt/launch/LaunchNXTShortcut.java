46005c39135ea758874f508e99715521ea86aaa3
skoehler <skoehler@06c8f799-c779-4783-8af7-179afedb67c8>
2011-05-28 17:44:13 +0000

    mc

--- trunk/org.lejos.nxt.ldt/src/main/java/org/lejos/nxt/ldt/launch/LaunchNXTShortcut.java
+++ trunk/org.lejos.nxt.ldt/src/main/java/org/lejos/nxt/ldt/launch/LaunchNXTShortcut.java
@@ -1,19 +1,7 @@
 package org.lejos.nxt.ldt.launch;
 
-import org.eclipse.debug.ui.ILaunchShortcut;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.ui.IEditorPart;
+import org.eclipse.jdt.debug.ui.launchConfigurations.JavaApplicationLaunchShortcut;
 
-public class LaunchNXTShortcut implements ILaunchShortcut {
-
-	public void launch(ISelection selection, String mode) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public void launch(IEditorPart editor, String mode) {
-		// TODO Auto-generated method stub
-
-	}
+public class LaunchNXTShortcut extends JavaApplicationLaunchShortcut {
 
 }
