0176121fcb720df5745c4a4fd8c7bdd8dd2a177a
kirkpthompson <kirkpthompson@06c8f799-c779-4783-8af7-179afedb67c8>
2011-06-03 23:32:37 +0000

    augmented javadoc comments
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@4995 06c8f799-c779-4783-8af7-179afedb67c8

--- pccomms/lejos/pc/comm/NXTComm.java
+++ pccomms/lejos/pc/comm/NXTComm.java
@@ -8,39 +8,58 @@ import lejos.nxt.remote.NXTCommRequest;
 
 /**
  * 
- * Interface that all NXTComm implementation classes must implement for low-level communication
+ * Interface that all <code>NXTComm</code> implementation classes must implement for low-level communication
  * with the NXT.
  *
  */
 public interface NXTComm extends NXTCommRequest {
-    public static final int PACKET = 0;
+    /**
+     * Lego Communications Protocol (<code>LCP</code>) I/O mode. The LCP is defined by The Lego Company to allow limited remote 
+     * command control of a NXT brick. 
+     * 
+     * See the <a href="http://mindstorms.lego.com">Lego Mindstorms</a> Site. Look for the Bluetooth Developer Kit in Support |
+     * Files | Advanced
+     */
     public static final int LCP = 1;
+    /**
+     * <code>PACKET</code> I/O mode. This is default and  is probably the best mode to use if you are talking to a
+     * NXT using the leJOS classes. Headers are included for each packet of data sent and received.
+     */
+    public static final int PACKET = 0;
+    /**
+     * <code>RAW</code> I/O mode. This mode is just that and omits any headers. It is used normally for connections to non-NXT 
+     * devices such as cell phones, etc.
+     */
     public static final int RAW = 2;
+    
             
 	/**
 	 * Search for NXTs over USB, Bluetooth or both
-	 * @param name name of the NXT or null
-	 * @param protocol bitwise combination of NXTCommFactory.BLUETOOTH and NXTCommFactory.USB
+     * 
+	 * @param name name of the NXT or <code>null</code>
+	 * @param protocol {@link NXTCommFactory#BLUETOOTH}, {@link NXTCommFactory#USB} or {@link NXTCommFactory#ALL_PROTOCOLS}
+     *   (the bitwise combination of both)
 	 * @return a NXTInfo object describing the NXt found and the connection to it
 	 * @throws NXTCommException
 	 */
 	public NXTInfo[] search(String name, int protocol) throws NXTCommException;
 
 	/**
-	 * Connect to a NXT found by a search or created from mname and address.
+	 * Connect to a NXT found by a search or created from name and address.
 	 * 
-	 * @param nxt the NXTInfo object for the NXT
-     * @param mode the mode for the connection
-	 * @return true iff the open succeeded
+	 * @param nxt the <code>NXTInfo</code> object for the NXT
+     * @param mode the mode for the connection: <code>{@link #LCP}</code>, <code>{@link #PACKET}</code> or
+     * <code>{@link #RAW}</code>
+	 * @return <code>true</code> if the open succeeded
 	 * @throws NXTCommException
 	 */
 	public boolean open(NXTInfo nxt, int mode) throws NXTCommException;
 
 	/**
-	 * Connect to a NXT found by a search or created from mname and address.
+	 * Connect to a NXT found by a search or created from name and address.
 	 * 
-	 * @param nxt the NXTInfo object for the NXT
-	 * @return true if the open succeeded
+	 * @param nxt the <code>NXTInfo</code> object for the NXT
+	 * @return <code>true</code> if the open succeeded
 	 * @throws NXTCommException
 	 */
 	public boolean open(NXTInfo nxt) throws NXTCommException;
@@ -49,7 +68,7 @@ public interface NXTComm extends NXTCommRequest {
 	 * Read data from a NXT that has an open connection.
 	 * Used for stream connections.
 	 * 
-	 * @return the data
+	 * @return the data in a <code>byte[]</code> array
 	 * @throws IOException
 	 */
 	public byte[] read() throws IOException;
@@ -65,24 +84,23 @@ public interface NXTComm extends NXTCommRequest {
 	/**
 	 * Write data to a NXT that has an open connection.
 	 * 
-	 * @param data the data to be written.
-	 * Used for stream connections.
+	 * @param data the data to be written. Used for stream connections.
 	 * 
 	 * @throws IOException
 	 */
 	public void write(byte [] data) throws IOException;
 	
 	/**
-	 * Return an OutputStream for writing a stream of data to the NXT over this connection.
+	 * Return an <code>OutputStream</code> for writing a stream of data to the NXT over this connection.
 	 * 
-	 * @return the OutputStream object
+	 * @return the <code>OutputStream</code> object
 	 */
 	public OutputStream getOutputStream();
 	
 	/**
-	 * Return an InputStream for reading a stream of data from the NXT over this connection.
+	 * Return an <code>InputStream</code> for reading a stream of data from the NXT over this connection.
 	 * 
-	 * @return the InputStream object
+	 * @return the <code>InputStream</code> object
 	 */
 	public InputStream getInputStream();
 }
