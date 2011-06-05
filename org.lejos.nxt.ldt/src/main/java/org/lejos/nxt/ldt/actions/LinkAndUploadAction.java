f1ef4512e645723c85af6e0f3484ed19a0f4161e
skoehler <skoehler@06c8f799-c779-4783-8af7-179afedb67c8>
2011-06-05 21:40:36 +0000

    mc
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@5004 06c8f799-c779-4783-8af7-179afedb67c8

--- org.lejos.nxt.ldt/src/main/java/org/lejos/nxt/ldt/actions/LinkAndUploadAction.java
+++ org.lejos.nxt.ldt/src/main/java/org/lejos/nxt/ldt/actions/LinkAndUploadAction.java
@@ -1,25 +1,13 @@
 package org.lejos.nxt.ldt.actions;
 
-import java.io.File;
-import java.lang.reflect.InvocationTargetException;
-import java.util.ArrayList;
-
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.IncrementalProjectBuilder;
-import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jdt.core.IJavaElement;
-import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.IType;
 import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
-import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.actions.ActionDelegate;
-import org.eclipse.ui.progress.IProgressService;
+import org.lejos.nxt.ldt.LeJOSPlugin;
 import org.lejos.nxt.ldt.util.LeJOSNXJUtil;
 
 /**
@@ -35,27 +23,7 @@ public class LinkAndUploadAction extends ActionDelegate {
 
 	@Override
 	public void run(IAction action) {
-		IJavaElement javaElement = LeJOSNXJUtil.getFirstJavaElementFromSelection(_selection);
-		if (javaElement == null)
-			return;
-		final IType javaType = LeJOSNXJUtil.getJavaTypeFromElement(javaElement);
-		if (javaType == null)
-			return;
-		final IJavaProject project = javaElement.getJavaProject();
-		
-		// open progress monitor
-		IWorkbench wb = PlatformUI.getWorkbench();
-		IProgressService ps = wb.getProgressService();
-		try {
-			ps.busyCursorWhile(new IRunnableWithProgress() {
-				public void run(IProgressMonitor pm) {
-					linkAndUpload(pm, project, javaType);
-				} // end run
-			});
-		} catch (Throwable t) {
-			// log
-			LeJOSNXJUtil.log(t);
-		}
+		MessageDialog.openInformation(LeJOSPlugin.getShell(), "The plugin has changed", "Please use the 'Run As' menu.");	
 	}
 
 	/*
@@ -84,99 +52,4 @@ public class LinkAndUploadAction extends ActionDelegate {
 		action.setEnabled(isEnabled);
 	}
 
-	private void linkAndUpload(IProgressMonitor pm, IJavaProject project, IType javaType) {
-		try {
-			ArrayList<File> cpl = new ArrayList<File>();
-			LeJOSNXJUtil.getProjectClassPath(project, false, cpl);
-			
-			File nxjHome = LeJOSNXJUtil.getNXJHome();
-
-			String fullClass = javaType.getFullyQualifiedName();
-			String simpleClass = javaType.getTypeQualifiedName();
-			
-			IProject project2 = project.getProject();
-			IFile binary = project2.getFile(simpleClass+".nxj");
-			IFile binaryDebug = project2.getFile(simpleClass+".nxd");
-			String binaryPath = binary.getLocation().toOSString();
-			String binaryDebugPath = binaryDebug.getLocation().toOSString();
-			
-			// upload program
-			pm.beginTask("Linking and uploading program to the brick...", IProgressMonitor.UNKNOWN);
-			try
-			{
-				//TODO ask user to save files with changes
-				project2.build(IncrementalProjectBuilder.INCREMENTAL_BUILD, pm);
-				
-				int r;
-				try
-				{
-					pm.subTask("Linking ...");
-					ArrayList<String> args = new ArrayList<String>();
-					LeJOSNXJUtil.getLinkerOpts(args);
-					// Only use what is on the project classpath. No bootclasspath.
-					// classes.jar is expected to be part of the classpath.
-					// Otherwise boom!
-					args.add("--bootclasspath");
-					args.add("");
-					args.add("--classpath");
-					args.add(classpathToString(cpl));
-					args.add("--output");
-					args.add(binaryPath);
-					args.add("--outputdebug");
-					args.add(binaryDebugPath);
-					args.add(fullClass);
-	
-					r = LeJOSNXJUtil.invokeTool(nxjHome, LeJOSNXJUtil.TOOL_LINK, args);
-				}
-				finally
-				{
-					binary.refreshLocal(IResource.DEPTH_ZERO, pm);
-					binaryDebug.refreshLocal(IResource.DEPTH_ZERO, pm);
-				}
-			
-				if (r != 0)
-					LeJOSNXJUtil.error("linking the file failed with exit status "+r);
-				else
-				{
-					LeJOSNXJUtil.message("program has been linked successfully");
-					
-					pm.subTask("Uploading ...");					
-					ArrayList<String> args = new ArrayList<String>();
-					LeJOSNXJUtil.getUploadOpts(args, true);
-					args.add(binaryPath);
-					
-					r = LeJOSNXJUtil.invokeTool(nxjHome, LeJOSNXJUtil.TOOL_UPLOAD, args);
-					if (r == 0)
-						LeJOSNXJUtil.message("program has been uploaded");
-					else
-						LeJOSNXJUtil.error("uploading the program failed with exit status "+r);
-				}
-			}
-			finally
-			{
-				pm.done();
-			}
-		} catch (Exception t) {
-			Throwable t2 = t;
-			if (t2 instanceof InvocationTargetException)
-				t2 = ((InvocationTargetException)t).getTargetException();
-			
-			// log
-			LeJOSNXJUtil.error("Linking or uploading the program failed", t2);
-		}
-	}
-
-	private String classpathToString(ArrayList<File> cpl)
-	{
-		if (cpl.isEmpty())
-			return "";
-		
-		StringBuilder sb = new StringBuilder();
-		for (File f : cpl)
-		{
-			sb.append(File.pathSeparatorChar);
-			sb.append(f.getAbsolutePath());
-		}
-		return sb.substring(1);
-	}
 }
