832526da4eeb8e42c7f9decdfce92079d54c250b
skoehler <skoehler@06c8f799-c779-4783-8af7-179afedb67c8>
2011-06-07 15:03:57 +0000

    switch to different preamble, fixed little html error in index.php
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@5016 06c8f799-c779-4783-8af7-179afedb67c8

--- org.lejos.website/htdocs/rcx-downloads.php
+++ org.lejos.website/htdocs/rcx-downloads.php
@@ -1,11 +1,9 @@
-<?php
-	echo '<?xml version="1.0" encoding="UTF-8" ?>';
-?>
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
 	<head>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	
 		<!-- METADATOS -->
 		<title>LeJOS, Java for Lego Mindstorms / RCX Downloads</title>
 		<meta name="title" content="LeJOS, Java for Lego Mindstorms / RCX Downloads" />
