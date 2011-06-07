3f0ca92cd61b9c03c0293fe8f6ebcf34f164f805
skoehler <skoehler@06c8f799-c779-4783-8af7-179afedb67c8>
2011-06-07 20:47:08 +0000

    mc

--- trunk/org.lejos.nxt.ldt/src/main/java/org/lejos/nxt/ldt/wizard/NewPCProjectPageOne.java
+++ trunk/org.lejos.nxt.ldt/src/main/java/org/lejos/nxt/ldt/wizard/NewPCProjectPageOne.java
@@ -4,17 +4,10 @@ import org.eclipse.core.runtime.Path;
 import org.eclipse.jdt.core.IClasspathEntry;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.ui.wizards.NewJavaProjectWizardPageOne;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Control;
 import org.lejos.nxt.ldt.container.LeJOSLibContainer;
 import org.lejos.nxt.ldt.util.LeJOSNXJUtil;
 
 public class NewPCProjectPageOne extends NewJavaProjectWizardPageOne {
-	@Override
-	protected Control createJRESelectionControl(Composite composite) {
-		return new Composite(composite, SWT.NONE);
-	}
 	
 	@Override
 	public IClasspathEntry[] getDefaultClasspathEntries() {
