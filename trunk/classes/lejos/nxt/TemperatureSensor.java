4a932e539722c1f42c8cb006265af9bed735d9dd
skoehler <skoehler@06c8f799-c779-4783-8af7-179afedb67c8>
2011-06-12 00:00:18 +0000

    support for NXT Temperator sensor (thanks to Michael Mirwaldt!)

---
+++ trunk/classes/lejos/nxt/TemperatureSensor.java
@@ -0,0 +1,81 @@
+package lejos.nxt;
+
+import lejos.util.EndianTools;
+
+/** 
+ * Lego Education Temperature Sensor 9749
+ * 
+ * @author Michael Mirwaldt
+ */
+public class TemperatureSensor extends I2CSensor
+{
+	/*
+	 * Documentation of the chip can be found here: http://focus.ti.com/docs/prod/folders/print/tmp275.html
+	 * Some details from LEGOTMP-driver.h, http://rdpartyrobotcdr.sourceforge.net/
+	 */	
+
+	protected final static int I2C_ADDRESS 		= 0x98;
+	protected final static int REG_TEMPERATURE 	= 0x00;
+	protected final static int REG_CONFIG 	= 0x01;
+	
+	public enum TempAccuracy {
+		/** 0.5 C° accuracy */
+		C0_5(0x00, 28),
+		/** 0.25 C° accuracy */
+		C0_25(0x20, 55),
+		/** 0.125 C° accuracy */
+		C0_125(0x40, 110),
+		/** 0.0625 C° accuracy */
+		C0_0625(0x60, 220);
+		
+		final int bitmask;
+		final int delay;
+
+		private TempAccuracy(int index, int wait) {
+			this.bitmask = index;
+			this.delay = wait;
+		}
+		
+		/**
+		 * How many milli seconds each measurement takes.
+		 * @return the delay in ms
+		 */
+		public int getDelay()
+		{
+			return this.delay;
+		}
+		
+		static TempAccuracy toAccuracy(int index) {
+			for (TempAccuracy tempAccuracy : values()) {
+				if(tempAccuracy.bitmask==index) {
+					return tempAccuracy;
+				}
+			}
+			throw new IllegalArgumentException("index(='"+index+"') cannot be associated with a TempAccuracy!");
+		}		 
+		
+	}
+
+	public TemperatureSensor(I2CPort port) {
+		super(port, I2C_ADDRESS, I2CPort.LEGO_MODE, TYPE_LOWSPEED);
+	}
+
+	public float getTemp() {
+		byte[] buf = new byte[2];
+		getData(REG_TEMPERATURE, buf, 2);
+		return EndianTools.decodeShortBE(buf, 0)  * 0x1p-8f;
+	}
+	
+	public TempAccuracy getAccuracy() {
+		byte[] buf = new byte[1];
+		getData(REG_CONFIG, buf, 1);
+		return TempAccuracy.toAccuracy(buf[0] & 0x60);
+	}
+	
+	public void setAccuracy(TempAccuracy ta) {
+		//TODO preserve other bits
+		sendData(REG_CONFIG, (byte) ta.bitmask);
+	}
+	
+	// TODO sensor can be turned off and on, supports single-shot and continous mode
+}
