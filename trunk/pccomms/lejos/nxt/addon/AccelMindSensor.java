1c725e16e26645d16fd701c9c26e7ccca1bb03a4
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2011-06-13 19:35:18 +0000

    Copied classes

--- trunk/pccomms/lejos/nxt/addon/AccelMindSensor.java
+++ trunk/pccomms/lejos/nxt/addon/AccelMindSensor.java
@@ -3,6 +3,7 @@ package lejos.nxt.addon;
 import lejos.nxt.I2CPort;
 import lejos.nxt.I2CSensor;
 import lejos.robotics.Accelerometer;
+import lejos.util.EndianTools;
 
 /*
  * WARNING: THIS CLASS IS SHARED BETWEEN THE classes AND pccomms PROJECTS.
@@ -10,21 +11,30 @@ import lejos.robotics.Accelerometer;
  */
 
 /**
- * This class works with the Mindsensors acceleration (tilt) sensor.
+ * This class works with the Mindsensors acceleration (tilt) sensor ACCL-Nx-v2/v3.
  * 
+ * TODO there are a lot of features not supported by this class
  */
 public class AccelMindSensor extends I2CSensor implements Accelerometer {
-	byte[] buf = new byte[2];
-	byte[] buf2 = new byte[1];
+	/*
+	 * Documentation: http://www.mindsensors.com/index.php?module=pagemaster&PAGE_user_op=view_page&PAGE_id=101
+	 * Some details from MSAC-driver.h from http://rdpartyrobotcdr.sourceforge.net/
+	 */
 	
-	private static byte X_TILT = 0x42;
-	private static byte Y_TILT = 0x43;
-	private static byte Z_TILT = 0x44;
+	private byte[] buf = new byte[6];
+
+	private static final byte BASE_TILT = 0x42;
+	private static final byte OFF_X_TILT = 0x00;
+	private static final byte OFF_Y_TILT = 0x01;
+	private static final byte OFF_Z_TILT = 0x02;
+
+	private static final byte BASE_ACCEL = 0x45;
+	private static final byte OFF_X_ACCEL = 0x00;
+	private static final byte OFF_Y_ACCEL = 0x02;
+	private static final byte OFF_Z_ACCEL = 0x04;
 	
-	private static byte MINDSTORMS_X_ACCEL_LSB = 0x45;
-	private static byte MINDSTORMS_Y_ACCEL_LSB = 0x47;
-	private static byte MINDSTORMS_Z_ACCEL_LSB = 0x49;
-	
+	public static final int ERROR = Integer.MIN_VALUE;
+
 	public AccelMindSensor(I2CPort port) {
 		this(port, DEFAULT_I2C_ADDRESS);
 	}
@@ -35,65 +45,119 @@ public class AccelMindSensor extends I2CSensor implements Accelerometer {
 
 	/**
 	 * Tilt of sensor along X-axis (see top of Mindsensors.com sensor for
-	 * diagram of axis).  128 is level. 
+	 * diagram of axis). 128 is level.
 	 * 
 	 * @return X tilt value, or -1 if call failed
 	 */
-	public int getXTilt() {		
-		int ret = getData(X_TILT, buf, 1);		
-		return (ret == 0 ? (buf[0] & 0xff) : -1);
+	public int getXTilt() {
+		int ret = getData(BASE_TILT + OFF_X_TILT, buf, 1);
+		if (ret != 0)
+			return ERROR;
+
+		return (buf[0] & 0xFF) - 128;
 	}
-	
+
 	/**
 	 * Returns Y tilt value.
 	 * 
 	 * @return Y tilt value, or -1 if call failed
 	 */
-	public int getYTilt() {		
-		int ret = getData(Y_TILT, buf, 1);	
-		return (ret == 0 ? (buf[0] & 0xff) : -1);
+	public int getYTilt() {
+		int ret = getData(BASE_TILT + OFF_Y_TILT, buf, 1);
+		if (ret != 0)
+			return ERROR;
+
+		return (buf[0] & 0xFF) - 128;
 	}
-	
+
 	/**
 	 * Returns Z tilt value.
 	 * 
 	 * @return Z tilt value, or -1 if call failed
 	 */
-	public int getZTilt() {		
-		int ret = getData(Z_TILT, buf, 1);	
-		return (ret == 0 ? (buf[0] & 0xff) : -1);
+	public int getZTilt() {
+		int ret = getData(BASE_TILT + OFF_Z_TILT, buf, 1);
+		if (ret != 0)
+			return ERROR;
+
+		return (buf[0] & 0xFF) - 128;
 	}
-	
+
 	/**
-	 * Acceleration along X axis. Positive or negative values in mg.
-	 * (g = acceleration due to gravity = 9.81 m/s^2)
+	 * Acceleration along X axis. Positive or negative values in mg. (g =
+	 * acceleration due to gravity = 9.81 m/s^2)
+	 * 
 	 * @return Acceleration e.g. 9810 mg (falling on earth)
 	 */
 	public int getXAccel() {
-			int ret = getData(MINDSTORMS_X_ACCEL_LSB, buf, 2);
-			int accel = (buf[0] & 0xFF) | ((buf[1]) << 8);
-			return (ret == 0 ? accel : -1);
+		int ret = getData(BASE_ACCEL + OFF_X_ACCEL, buf, 2);
+		if (ret != 0)
+			return ERROR;
+		
+		return EndianTools.decodeShortLE(buf, 0);
 	}
-	
+
 	/**
-	 * Acceleration along Y axis. Positive or negative values in mg.
-	 * (g = acceleration due to gravity = 9.81 m/s^2)
+	 * Acceleration along Y axis. Positive or negative values in mg. (g =
+	 * acceleration due to gravity = 9.81 m/s^2)
+	 * 
 	 * @return Acceleration e.g. 9810 mg (falling on earth)
 	 */
 	public int getYAccel() {
-			int ret = getData(MINDSTORMS_Y_ACCEL_LSB, buf, 2);
-			int accel = (buf[0] & 0xFF) | ((buf[1]) << 8);
-			return (ret == 0 ? accel : -1);
+		int ret = getData(BASE_ACCEL + OFF_Y_ACCEL, buf, 2);
+		if (ret != 0)
+			return ERROR;
+		
+		return EndianTools.decodeShortLE(buf, 0);
 	}
-	
+
 	/**
-	 * Acceleration along Z axis. Positive or negative values in mg.
-	 * (g = acceleration due to gravity = 9.81 m/s^2)
+	 * Acceleration along Z axis. Positive or negative values in mg. (g =
+	 * acceleration due to gravity = 9.81 m/s^2)
+	 * 
 	 * @return Acceleration e.g. 9810 mg (falling on earth)
 	 */
 	public int getZAccel() {
-			int ret = getData(MINDSTORMS_Z_ACCEL_LSB, buf, 2);
-			int accel = (buf[0] & 0xFF) | ((buf[1]) << 8);
-			return (ret == 0 ? accel : -1);
+		int ret = getData(BASE_ACCEL + OFF_Z_ACCEL, buf, 2);
+		if (ret != 0)
+			return ERROR;
+		
+		return EndianTools.decodeShortLE(buf, 0);
+	}
+	
+	/**
+	 * Reads all 3 tilt values into the given array.
+	 * Elements off+0, off+1, and off+2 are filled with X, Y, and Z axis.
+	 * @param dst destination array.
+	 * @param off offset
+	 * @return true on success, false on error
+	 */
+	public boolean getAllTilt(int[] dst, int off) {
+		int ret = getData(BASE_TILT, buf, 0, 3);
+		if (ret != 0)
+			return false;
+		
+		dst[off+0] = (buf[0] & 0xFF) - 128;
+		dst[off+1] = (buf[1] & 0xFF) - 128;
+		dst[off+2] = (buf[2] & 0xFF) - 128;
+		return true;
+	}
+	
+	/**
+	 * Reads all 3 acceleration values into the given array.
+	 * Elements off+0, off+1, and off+2 are filled with X, Y, and Z axis.
+	 * @param dst destination array.
+	 * @param off offset
+	 * @return true on success, false on error
+	 */
+	public boolean getAllAccel(int[] dst, int off) {
+		int ret = getData(BASE_ACCEL, buf, 0, 6);
+		if (ret != 0)
+			return false;
+		
+		dst[off+0] = EndianTools.decodeShortLE(buf, OFF_X_ACCEL);
+		dst[off+1] = EndianTools.decodeShortLE(buf, OFF_Y_ACCEL);
+		dst[off+2] = EndianTools.decodeShortLE(buf, OFF_Z_ACCEL);
+		return true;
 	}
 }
