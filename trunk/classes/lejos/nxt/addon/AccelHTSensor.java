097634f786c45289e50666d4e291bfab959d525a
skoehler <skoehler@06c8f799-c779-4783-8af7-179afedb67c8>
2011-06-13 17:58:06 +0000

    improved implementation, tested by Michael Mirwaldt

--- trunk/classes/lejos/nxt/addon/AccelHTSensor.java
+++ trunk/classes/lejos/nxt/addon/AccelHTSensor.java
@@ -11,26 +11,31 @@ import lejos.robotics.Accelerometer;
 
 /**
  * Class to access the HiTechnic NXT Acceleration / Tilt Sensor (NAC1040).
+ * 
+ * Some sensors seem to be badly calibrated, so 0 is not always level.
  */
 public class AccelHTSensor extends I2CSensor implements Accelerometer {
 	/*
 	 * Documentation: http://www.hitechnic.com/cgi-bin/commerce.cgi?preadd=action&key=NAC1040
 	 * Some details from HTAC-driver.h from http://rdpartyrobotcdr.sourceforge.net/
+	 * 
+	 * ProductId: HITECHNC
+	 * SensorType: Accel.
+	 * 
+	 * Tested by Michael Mirwaldt:
+	 * one 4byte read is 20% faster than 2 reads of 1 byte.
 	 */
 
 	private byte[] buf = new byte[6];
 	
-	//TODO what is faster? Doing two 1 byte reads, or one 4 byte read to determine value of one axis?
 	//TODO we might add setPrecision(boolean) method which can be used to enable/disabled the reading of the lower 2 bits
 	
-	private static final byte BASE_ACCEL = 0x42;
+	private static final int BASE_ACCEL = 0x42;
 	
-	private static final byte OFF_X_HIGH = 0x00;
-	private static final byte OFF_Y_HIGH = 0x01;
-	private static final byte OFF_Z_HIGH = 0x02;
-	private static final byte OFF_X_2BITS = 0x03;
-	private static final byte OFF_Y_2BITS = 0x04;
-	private static final byte OFF_Z_2BITS = 0x05;
+	private static final int OFF_X_HIGH = 0x00;
+	private static final int OFF_Y_HIGH = 0x01;
+	private static final int OFF_Z_HIGH = 0x02;
+	private static final int OFF_2BITS = 3;
 
 	public static final int ERROR = Integer.MIN_VALUE;
 
@@ -51,13 +56,11 @@ public class AccelHTSensor extends I2CSensor implements Accelerometer {
 	 * @return x-axis acceleration or {@link #ERROR}.
 	 */
 	public int getXAccel() {
-		int ret = getData(BASE_ACCEL + OFF_X_HIGH, buf, 0, 1);
+		int ret = getData(BASE_ACCEL + OFF_X_HIGH, buf, 0, OFF_2BITS + 1);
 		if (ret != 0)
 			return ERROR;
-		ret = getData(BASE_ACCEL + OFF_X_2BITS, buf, 1, 1);
-		if (ret != 0)
-			return ERROR;
-		return (buf[0] << 2) | (buf[1] & 0xFF);
+
+		return (buf[0] << 2) | (buf[OFF_2BITS] & 0xFF);
 	}
 
 	/**
@@ -67,13 +70,11 @@ public class AccelHTSensor extends I2CSensor implements Accelerometer {
 	 * @return y-axis acceleration or {@link #ERROR}.
 	 */
 	public int getYAccel() {
-		int ret = getData(BASE_ACCEL + OFF_Y_HIGH, buf, 0, 1);
+		int ret = getData(BASE_ACCEL + OFF_Y_HIGH, buf, 0, OFF_2BITS + 1);
 		if (ret != 0)
 			return ERROR;
-		ret = getData(BASE_ACCEL + OFF_Y_2BITS, buf, 1, 1);
-		if (ret != 0)
-			return ERROR;
-		return (buf[0] << 2) | (buf[1] & 0xFF);
+
+		return (buf[0] << 2) | (buf[OFF_2BITS] & 0xFF);
 	}
 
 	/**
@@ -83,13 +84,11 @@ public class AccelHTSensor extends I2CSensor implements Accelerometer {
 	 * @return z-axis acceleration or {@link #ERROR}.
 	 */
 	public int getZAccel() {
-		int ret = getData(BASE_ACCEL + OFF_Z_HIGH, buf, 0, 1);
+		int ret = getData(BASE_ACCEL + OFF_Z_HIGH, buf, 0, OFF_2BITS + 1);
 		if (ret != 0)
 			return ERROR;
-		ret = getData(BASE_ACCEL + OFF_Z_2BITS, buf, 1, 1);
-		if (ret != 0)
-			return ERROR;
-		return (buf[0] << 2) | (buf[1] & 0xFF);
+
+		return (buf[0] << 2) | (buf[OFF_2BITS] & 0xFF);
 	}
 
 	/**
@@ -104,9 +103,9 @@ public class AccelHTSensor extends I2CSensor implements Accelerometer {
 		if (ret != 0)
 			return false;
 		
-		dst[off+0] = (buf[OFF_X_HIGH] << 2) + (buf[OFF_X_2BITS] & 0xFF);
-		dst[off+1] = (buf[OFF_Y_HIGH] << 2) + (buf[OFF_Y_2BITS] & 0xFF);
-		dst[off+2] = (buf[OFF_Z_HIGH] << 2) + (buf[OFF_Z_2BITS] & 0xFF);
+		dst[off+0] = (buf[OFF_X_HIGH] << 2) + (buf[OFF_X_HIGH + OFF_2BITS] & 0xFF);
+		dst[off+1] = (buf[OFF_Y_HIGH] << 2) + (buf[OFF_Y_HIGH + OFF_2BITS] & 0xFF);
+		dst[off+2] = (buf[OFF_Z_HIGH] << 2) + (buf[OFF_Z_HIGH + OFF_2BITS] & 0xFF);
 		return true;
 	}
 }
