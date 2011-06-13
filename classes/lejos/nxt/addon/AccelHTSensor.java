fe4d8fe4f0811aabe46dccac47cd49b0d552d8b7
skoehler <skoehler@06c8f799-c779-4783-8af7-179afedb67c8>
2011-06-13 15:37:50 +0000

    redesign of acceleration sensor classes
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@5102 06c8f799-c779-4783-8af7-179afedb67c8

--- classes/lejos/nxt/addon/AccelHTSensor.java
+++ classes/lejos/nxt/addon/AccelHTSensor.java
@@ -10,98 +10,103 @@ import lejos.robotics.Accelerometer;
  */
 
 /**
- * This class works with the HiTechnic acceleration sensor.
- * 
+ * Class to access the HiTechnic NXT Acceleration / Tilt Sensor (NAC1040).
  */
 public class AccelHTSensor extends I2CSensor implements Accelerometer {
-	byte[] buf = new byte[2];
-	byte[] buf2 = new byte[1];
+	/*
+	 * Documentation: http://www.hitechnic.com/cgi-bin/commerce.cgi?preadd=action&key=NAC1040
+	 * Some details from HTAC-driver.h from http://rdpartyrobotcdr.sourceforge.net/
+	 */
+
+	private byte[] buf = new byte[6];
 	
-	private static byte X_TILT = 0x42;
-	private static byte Y_TILT = 0x43;
-	private static byte Z_TILT = 0x44;
+	//TODO what is faster? Doing two 1 byte reads, or one 4 byte read to determine value of one axis?
+	//TODO we might add setPrecision(boolean) method which can be used to enable/disabled the reading of the lower 2 bits
 	
-	private static byte HITECHNIC_X_ACCEL_2BITS = 0x45;
-	private static byte HITECHNIC_Y_ACCEL_2BITS = 0x46;
-	private static byte HITECHNIC_Z_ACCEL_2BITS = 0x47;
+	private static final byte BASE_ACCEL = 0x42;
 	
-	public AccelHTSensor(I2CPort port)
-	{
+	private static final byte OFF_X_HIGH = 0x00;
+	private static final byte OFF_Y_HIGH = 0x01;
+	private static final byte OFF_Z_HIGH = 0x02;
+	private static final byte OFF_X_2BITS = 0x03;
+	private static final byte OFF_Y_2BITS = 0x04;
+	private static final byte OFF_Z_2BITS = 0x05;
+
+	public static final int ERROR = Integer.MIN_VALUE;
+
+	public AccelHTSensor(I2CPort port) {
 		this(port, DEFAULT_I2C_ADDRESS);
 	}
 
-	public AccelHTSensor(I2CPort port, int address)
-	{
-		// TODO: Needs to be able to accept high-speed! Might be problem I was having.
+	public AccelHTSensor(I2CPort port, int address) {
+		// TODO: Needs to be able to accept high-speed! Might be problem I was
+		// having.
 		super(port, address, I2CPort.LEGO_MODE, TYPE_LOWSPEED_9V);
 	}
 
 	/**
-	 * Tilt of sensor along X-axis.  128 is level. 
+	 * Acceleration along X axis. Positive or negative values in mg. (g =
+	 * acceleration due to gravity = 9.81 m/s^2)
 	 * 
-	 * @return X tilt value, or -1 if call failed
-	 */
-	public int getXTilt() {		
-		int ret = getData(X_TILT, buf, 1);		
-		return (ret == 0 ? (buf[0] & 0xff) : -1);
-	}
-	
-	/**
-	 * Returns Y tilt value.
-	 * 
-	 * @return Y tilt value, or -1 if call failed
-	 */
-	public int getYTilt() {		
-		int ret = getData(Y_TILT, buf, 1);	
-		return (ret == 0 ? (buf[0] & 0xff) : -1);
-	}
-	
-	/**
-	 * Returns Z tilt value.
-	 * 
-	 * @return Z tilt value, or -1 if call failed
-	 */
-	public int getZTilt() {		
-		int ret = getData(Z_TILT, buf, 1);	
-		return (ret == 0 ? (buf[0] & 0xff) : -1);
-	}
-	
-	/**
-	 * Acceleration along X axis. Positive or negative values in mg.
-	 * (g = acceleration due to gravity = 9.81 m/s^2)
 	 * @return Acceleration e.g. 9810 mg (falling on earth)
 	 */
 	public int getXAccel() {
-		int ret = getData(X_TILT, buf, 1);
-		if (ret != 0) return -1;
-		ret = getData(HITECHNIC_X_ACCEL_2BITS, buf2, 1);
-		if (ret != 0) return -1;
-		return ((buf[0] & 0xFF) << 2) + (buf2[0] & 0xFF);
+		int ret = getData(BASE_ACCEL + OFF_X_HIGH, buf, 0, 1);
+		if (ret != 0)
+			return ERROR;
+		ret = getData(BASE_ACCEL + OFF_X_2BITS, buf, 1, 1);
+		if (ret != 0)
+			return ERROR;
+		return (buf[0] << 2) | (buf[1] & 0xFF);
 	}
-	
+
 	/**
-	 * Acceleration along Y axis. Positive or negative values in mg.
-	 * (g = acceleration due to gravity = 9.81 m/s^2)
+	 * Acceleration along Y axis. Positive or negative values in mg. (g =
+	 * acceleration due to gravity = 9.81 m/s^2)
+	 * 
 	 * @return Acceleration e.g. 9810 mg (falling on earth)
 	 */
 	public int getYAccel() {
-		int ret = getData(Y_TILT, buf, 1);
-		if (ret != 0) return -1;
-		ret = getData(HITECHNIC_Y_ACCEL_2BITS, buf2, 1);
-		if (ret != 0) return -1;
-		return ((buf[0] & 0xFF) << 2) + (buf2[0] & 0xFF);
+		int ret = getData(BASE_ACCEL + OFF_Y_HIGH, buf, 1);
+		if (ret != 0)
+			return ERROR;
+		ret = getData(BASE_ACCEL + OFF_Y_2BITS, buf, 1, 1);
+		if (ret != 0)
+			return ERROR;
+		return (buf[0] << 2) | (buf[1] & 0xFF);
 	}
-	
+
 	/**
-	 * Acceleration along Z axis. Positive or negative values in mg.
-	 * (g = acceleration due to gravity = 9.81 m/s^2)
+	 * Acceleration along Z axis. Positive or negative values in mg. (g =
+	 * acceleration due to gravity = 9.81 m/s^2)
+	 * 
 	 * @return Acceleration e.g. 9810 mg (falling on earth)
 	 */
 	public int getZAccel() {
-		int ret = getData(Z_TILT, buf, 1);
-		if (ret != 0) return -1;
-		ret = getData(HITECHNIC_Z_ACCEL_2BITS, buf2, 1);
-		if (ret != 0) return -1;
-		return ((buf[0] & 0xFF) << 2) + (buf2[0] & 0xFF);
+		int ret = getData(BASE_ACCEL + OFF_Z_HIGH, buf, 0, 1);
+		if (ret != 0)
+			return ERROR;
+		ret = getData(BASE_ACCEL + OFF_Z_2BITS, buf, 0, 1);
+		if (ret != 0)
+			return ERROR;
+		return (buf[0] << 2) | (buf[1] & 0xFF);
+	}
+
+	/**
+	 * Reads all 3 acceleration values into the given array.
+	 * Elements off+0, off+1, and off+2 are filled with X, Y, and Z axis.
+	 * @param dst destination array.
+	 * @param off offset
+	 * @return true on success, false on error
+	 */
+	public boolean getAllAccel(int[] dst, int off) {
+		int ret = getData(BASE_ACCEL, buf, 0, 6);
+		if (ret != 0)
+			return false;
+		
+		dst[off+0] = (buf[OFF_X_HIGH] << 2) + (buf[OFF_X_2BITS] & 0xFF);
+		dst[off+1] = (buf[OFF_Y_HIGH] << 2) + (buf[OFF_Y_2BITS] & 0xFF);
+		dst[off+2] = (buf[OFF_Z_HIGH] << 2) + (buf[OFF_Z_2BITS] & 0xFF);
+		return true;
 	}
 }
