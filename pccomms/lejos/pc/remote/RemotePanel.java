20d03e33e9415522d6c14f729c1498d88d8cf185
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2011-06-13 22:14:00 +0000

    Generic message processing
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@5126 06c8f799-c779-4783-8af7-179afedb67c8

--- pccomms/lejos/pc/remote/RemotePanel.java
+++ pccomms/lejos/pc/remote/RemotePanel.java
@@ -5,8 +5,12 @@ import java.awt.event.*;
 import java.awt.*;
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+
 import lejos.pc.comm.NXTCommFactory;
 import lejos.pc.comm.NXTConnector;
+import lejos.robotics.navigation.Pose;
 
 /**
  * This class is useful for creating a PC GUI Application that connects to the NXT
@@ -24,9 +28,7 @@ public abstract class RemotePanel extends JPanel implements ActionListener, Mous
   // Data input and output streams for communicating with the NXT
   protected DataOutputStream dos;
   protected DataInputStream dis;
-  
-  public RemotePanel() {
-  }
+  protected Pose pose;
 
   /**
    * Create a frame to display the panel in
@@ -87,34 +89,68 @@ public abstract class RemotePanel extends JPanel implements ActionListener, Mous
   }
   
   public void createButton(CommandButton button) {
-	  buttons.add(button);
-	  button.addActionListener(this);
-	  buttonPanel.add(button);
+	buttons.add(button);
+    button.addActionListener(this);
+	buttonPanel.add(button);
   }
   
   public JButton createButton(String name) {
-	  JButton button = new JButton(name);
-	  button.addActionListener(this);
-	  return button;
+    JButton button = new JButton(name);
+	button.addActionListener(this);
+	return button;
   }
  
   /**
    * Process buttons
    */
   public void actionPerformed(ActionEvent e) {
-	  sendCommand(((CommandButton) e.getSource()).getCommand());
+	try {
+	  Command c = ((CommandButton) e.getSource()).getCommand();
+	  sendCommand(c, null);
+	  HashMap<String,Number> reply = readReply(c);
+	  updateData(c.getReply(),reply);
+	} catch (IOException ioe) {
+	  // Todo: 
+	}
   }
   
   /**
    * Send a command to the NXT
    */
-  private void sendCommand(Command command) {
-    try {
+  private void sendCommand(Command command, HashMap<String,Number> data) throws IOException {
       dos.writeByte(command.getValue());
       dos.flush();
-    } catch (IOException ioe) {
-      error("IO Exception");
-    }
+      Message request = command.getRequest();
+      Collection<MessageElement> c = request.getMessageElements().values();
+      for(MessageElement e : c ) {
+    	  switch (e.getType()) {
+    		  case BYTE: dos.writeByte((Byte) data.get(e.getName())); break;
+    		  case SHORT: dos.writeShort((Short) data.get(e.getName())); break;
+    		  case INT: dos.writeInt((Integer) data.get(e.getName())); break;
+    		  case FLOAT: dos.writeFloat((Float) data.get(e.getName())); break;
+    	  }
+      }
+  }
+  
+  private HashMap<String,Number> readReply(Command command) throws IOException {
+	  HashMap<String,Number> data = new HashMap<String,Number>();
+      Message reply = command.getReply();
+      Collection<MessageElement> c = reply.getMessageElements().values();
+      for(MessageElement e : c ) {
+    	  switch (e.getType()) {
+    		  case BYTE: data.put(e.getName(), (Byte) dis.readByte()); break;
+    		  case SHORT: data.put(e.getName(), (Short) dis.readShort()); break;
+    		  case INT: data.put(e.getName(), (Integer) dis.readInt()); break;
+    		  case FLOAT: data.put(e.getName(), (Float) dis.readFloat()); break;
+    	  }
+      }
+	  return data;
+  }
+  
+  private void updateData(Message reply, HashMap<String,Number> data) {
+	if (reply.getName().equals("getPose")) {
+	  pose = new Pose((Float) data.get("x"), (Float) data.get("y"), (Float) data.get("heading"));
+  	}	  
   }
 }
 
