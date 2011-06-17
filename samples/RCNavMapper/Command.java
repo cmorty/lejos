a786226bb0cb8fd15370f5c5018853060d4303ff
rogergla <rogergla@06c8f799-c779-4783-8af7-179afedb67c8>
2011-06-17 05:36:48 +0000

    RCNavMapper runs under control of NavMapControl running on a PC. Reports its pose as it moves; stops and reports when it detects and obstacle.  Uses the Commmand enum and a NXTCommunicator
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@5157 06c8f799-c779-4783-8af7-179afedb67c8

---
+++ samples/RCNavMapper/Command.java
@@ -0,0 +1,14 @@
+
+
+
+
+/**
+ * Use this enum in both the RCNavigationControl program on the PC
+ * and the RCNavigator that runs on the NXT
+ * @author Roger Glassey
+ */
+public enum Command
+{
+  STOP,ROTATE, TRAVEL, GOTO, FIX, POSE, SETPOSE, OBSTACLE,
+  PINGFRONT, PINGLEFT, PINGRIGHT; 
+}
