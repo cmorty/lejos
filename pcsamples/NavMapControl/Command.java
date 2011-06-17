4bed014cd3a7adfa4485ffd3408ea60db6eacc46
rogergla <rogergla@06c8f799-c779-4783-8af7-179afedb67c8>
2011-06-17 01:03:13 +0000

    NavMapControl draws a map of the robot position as it moves, also shows obstacles detected.  Communications are handeled by RCCummunicator,  the map is drawn by the OffScreenDrawingM.  It can control the RCNavMapper in samples.  Both use the same enum Command
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@5156 06c8f799-c779-4783-8af7-179afedb67c8

---
+++ pcsamples/NavMapControl/Command.java
@@ -0,0 +1,13 @@
+
+
+
+/**
+ * Use this enum in both the NavMappcontrol Control program on the PC
+ * and the RCNavMapper that runs on the NXT
+ * @author Roger Glassey
+ */
+public enum Command
+{
+  STOP,ROTATE, TRAVEL, GOTO, FIX, POSE, SETPOSE, OBSTACLE,
+  PINGFRONT, PINGLEFT, PINGRIGHT; 
+}
---
+++ pcsamples/NavMapControl/RemoteController.java
@@ -0,0 +1,10 @@
+
+/**
+ *Interface used by RCCommunicator 
+ * @author owner
+ */
+public interface RemoteController {
+
+  public void execute(int code, float v0, float v1, float v2, boolean bit);
+
+}
