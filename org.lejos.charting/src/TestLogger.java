56b31431f96b82189b1b9b6ac5b93253518c4563
kirkpthompson <kirkpthompson@06c8f799-c779-4783-8af7-179afedb67c8>
2011-06-19 14:37:55 +0000

    initial.
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@5187 06c8f799-c779-4783-8af7-179afedb67c8

---
+++ org.lejos.charting/src/TestLogger.java
@@ -0,0 +1,45 @@
+package net.mosen.nxt.instrumentation;
+
+import lejos.nxt.Button;
+import lejos.nxt.LCD;
+
+import lejos.util.NXTDataLogger;
+
+
+public class TestLogger {
+    public TestLogger() {
+    }
+
+    public static void main(String[] args) {
+        TestLogger testLogger = new TestLogger();
+        testLogger.doTest();
+    }
+    
+    private void doTest(){
+        NXTDataLogger dlog = new NXTDataLogger(); 
+        
+//        boolean success=dlog.waitForConnection(15000,NXTDataLogger.CONN_USB);
+        boolean success=dlog.waitForConnection(15000,NXTDataLogger.CONN_BLUETOOTH);
+        if (!success) {
+            LCD.drawString("IO error! ",0,3);
+            LCD.drawString("Press ENT ",0,4, true);
+            Button.ENTER.waitForPress();
+            return;
+        }
+        
+        double value=0;
+        // TODO fix bug in LogChartPanel that does not display data with no headers defined
+//        dlog.setHeaders(new String[]{"sine(v)","testRandom"}); 
+        dlog.setHeaders(new String[]{"sine(v)","upper","lower"}); 
+
+        for (int i=0;i<975;i++){ // 975
+            dlog.logDouble(Math.sin(value));
+//            dlog.logDouble((Math.random()*3)-1.5);
+            dlog.logDouble(1);
+            dlog.logDouble(-1);
+
+            value+=.1f;
+        }
+        dlog.closeConnection();
+    }
+}
