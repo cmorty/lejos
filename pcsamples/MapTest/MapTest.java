a358dd2bc5c3a5e8d1ed59bae9cad08322ef5460
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2011-06-19 19:36:51 +0000

    Changed MapTest to use new navigation model classes
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@5192 06c8f799-c779-4783-8af7-179afedb67c8

--- pcsamples/MapTest/MapTest.java
+++ pcsamples/MapTest/MapTest.java
@@ -1,55 +1,28 @@
 import java.awt.*;
-import lejos.pc.remote.MapPanel;
-import lejos.pc.remote.MessageElement.ElementType;
-import lejos.robotics.mapping.LineMap;
-import lejos.robotics.mapping.SVGMapLoader;
-import java.io.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-
 import lejos.pc.remote.*;
 
-
-public class MapTest extends MapPanel {
-  private static LineMap map;
+public class MapTest extends NavigationPanel {
   private static final long serialVersionUID = 1L;
 
   // GUI Window size
   private static final int FRAME_WIDTH = 800;
   private static final int FRAME_HEIGHT = 800;
-	
-  public MapTest(LineMap map, float xOffset, float yOffset, float pixelsPerUnit) {
-	super(map, xOffset, yOffset, pixelsPerUnit);
-  }
   
   /**
    * Create a MapTest object and display it in a GUI frame.
    * Then connect to the NXT.
    */
-  public static void main(String[] args) throws Exception {   
-	File f = new File(args[0]);
-	FileInputStream in = new FileInputStream(f);
-	
-	map = (new SVGMapLoader(in)).readLineMap();
-	
-	MessageElement x = new MessageElement("x",ElementType.FLOAT);
-	MessageElement y = new MessageElement("y",ElementType.FLOAT);
-	MessageElement heading = new MessageElement("heading",ElementType.FLOAT);
-	ArrayList<MessageElement> elements = new ArrayList<MessageElement>(); 
-	elements.add(x);
-	elements.add(y);
-	elements.add(heading);
-	Message reply = new Message("getPose", elements);
-	
-	lejos.pc.remote.Command getPose = new lejos.pc.remote.Command((byte) 0, null, reply);
-	CommandButton poseButton = new CommandButton("Get Pose", getPose);
-	
-	
-	MapTest mapTest = new MapTest(map, 0,0,2);
-	
-    openInJFrame(mapTest, FRAME_WIDTH, FRAME_HEIGHT, "Map Test", Color.white);
-    
-    mapTest.createButton(poseButton);
-    //mapTest.connect(args[1]);
+  public static void main(String[] args) throws Exception {
+	  (new MapTest()).run();
   }
+  
+  public void run() throws Exception {   
+	model.setPanel(this);
+	model.loadMap("Room.svg");;
+	
+    openInJFrame(this, FRAME_WIDTH, FRAME_HEIGHT, "Map Test", Color.white);
+    
+    model.connect("NOISY");
+  }
+ 
 }
