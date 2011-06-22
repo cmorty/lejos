b480b9a50bb48aabbe34182962268c2fa46499ee
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2011-06-22 20:08:38 +0000

    More remote mapping and copied classes
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@5207 06c8f799-c779-4783-8af7-179afedb67c8

--- pccomms/lejos/pc/remote/MapPanel.java
+++ pccomms/lejos/pc/remote/MapPanel.java
@@ -6,34 +6,29 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Line2D;
-import java.util.HashMap;
-
+import javax.swing.JPanel;
 import lejos.geom.Line;
-import lejos.geom.Rectangle;
 import lejos.robotics.mapping.LineMap;
 import lejos.robotics.navigation.Pose;
 
-public class MapPanel extends RemotePanel {
-	private static final long serialVersionUID = 1L;
-	protected LineMap map;
-	protected Line[] lines;
-	protected float xOffset, yOffset;
-	protected float pixelsPerUnit;
-	protected Rectangle boundingRect;
+public class MapPanel extends JPanel {
+	private static final long serialVersionUID = 1L;;
+	protected float xOffset = 0, yOffset = 0;
+	protected float pixelsPerUnit = 2;
 	protected static final float ARROW_LENGTH = 10f;
 	protected static final int ROBOT_SIZE = 2;
+	protected PCNavigationModel model;
+	protected Dimension size;
 	
-	protected Pose pose = new Pose();
+	public MapPanel(PCNavigationModel model, Dimension size) {
+		this.size = size;
+		this.model = model;
+		setPreferredSize(size);
+	}
 	
-	public MapPanel(LineMap map, float xOffset, float yOffset, float pixelsPerUnit) {
-		this.map = map;
-		this.lines = map.getLines();
-		this.xOffset = xOffset;
-		this.yOffset = yOffset;
-		this.pixelsPerUnit = pixelsPerUnit;
-		this.boundingRect = map.getBoundingRect();
-		Dimension size = new Dimension((int) (boundingRect.width * pixelsPerUnit + 1), (int) (boundingRect.height * pixelsPerUnit + 1));
-		this.setPreferredSize(size);
+	public void setSize(Dimension size) {
+		this.size = size;
+		repaint();
 	}
 	
 	/**
@@ -42,13 +37,16 @@ public class MapPanel extends RemotePanel {
 	 * @param g2d the Graphics2D object
 	 */
 	public void paintMap(Graphics2D g2d) {
+		LineMap map = model.getMap();
+		if (map == null) return;
+		Line[] lines = map.getLines();
 		g2d.setColor(Color.black);
 		for (int i = 0; i < lines.length; i++) {
 			Line2D line = new Line2D.Float(
     		  xOffset + lines[i].x1 * pixelsPerUnit, 
-    		  CANVAS_OFFSET + yOffset + lines[i].y1 * pixelsPerUnit, 
+    		  yOffset + lines[i].y1 * pixelsPerUnit, 
     		  xOffset + lines[i].x2 * pixelsPerUnit, 
-    		  CANVAS_OFFSET + yOffset + lines[i].y2 * pixelsPerUnit);
+    		  yOffset + lines[i].y2 * pixelsPerUnit);
 			g2d.draw(line);
 		}
 	}
@@ -60,7 +58,7 @@ public class MapPanel extends RemotePanel {
 	}
 	
 	protected void paintRobot(Graphics2D g) {
-		paintPose(g, pose);
+		//paintPose(g, pose);
 	}
 	
 	/**
@@ -69,7 +67,7 @@ public class MapPanel extends RemotePanel {
 	 * @param g2d the Graphics2D object
 	 */
 	public void paintPose(Graphics2D g2d, Pose pose) {
-		Ellipse2D c = new Ellipse2D.Float(xOffset + pose.getX() * pixelsPerUnit - 1, CANVAS_OFFSET + yOffset + pose.getY() * pixelsPerUnit - 1, ROBOT_SIZE, ROBOT_SIZE);
+		Ellipse2D c = new Ellipse2D.Float(xOffset + pose.getX() * pixelsPerUnit - 1, yOffset + pose.getY() * pixelsPerUnit - 1, ROBOT_SIZE, ROBOT_SIZE);
 		Line rl = getArrowLine(pose);
 		Line2D l2d = new Line2D.Float(rl.x1, rl.y1, rl.x2, rl.y2);
 		g2d.draw(l2d);
@@ -84,18 +82,8 @@ public class MapPanel extends RemotePanel {
 	 */
 	protected Line getArrowLine(Pose pose) {
 		return new Line(xOffset + pose.getX() * pixelsPerUnit,
-    		        CANVAS_OFFSET + yOffset + pose.getY() * pixelsPerUnit, 
+    		        yOffset + pose.getY() * pixelsPerUnit, 
     		        xOffset + pose.getX() * pixelsPerUnit + ARROW_LENGTH * (float) Math.cos(Math.toRadians(pose.getHeading())), 
-    		        CANVAS_OFFSET + yOffset + pose.getY() * pixelsPerUnit + ARROW_LENGTH * (float) Math.sin(Math.toRadians(pose.getHeading())));
-	}
-	
-	/** 
-	 * Update relevant data items from data returned from NXT
-	 */
-	protected void updateData(Message reply, HashMap<String,Number> data) {
-		if (data == null) return;
-		if (reply.getName().equals("getPose")) {
-			pose = new Pose((Float) data.get("x"), (Float) data.get("y"), (Float) data.get("heading"));
-		}	  
+    		        yOffset + pose.getY() * pixelsPerUnit + ARROW_LENGTH * (float) Math.sin(Math.toRadians(pose.getHeading())));
 	}
 }
