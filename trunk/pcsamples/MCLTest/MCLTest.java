395eb7fee6979faa5a231cfef54feb8dc1d21c55
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2011-06-25 22:07:34 +0000

    Remote MCL

--- trunk/pcsamples/MCLTest/MCLTest.java
+++ trunk/pcsamples/MCLTest/MCLTest.java
@@ -11,13 +11,15 @@ public class MCLTest extends NavigationPanel {
   // GUI Window size
   private static final int FRAME_WIDTH = 1000;
   private static final int FRAME_HEIGHT = 800;
-  
-  private static final int NUM_PARTICLES = 300;
+  private static final int NUM_PARTICLES = 200;
+
   private static MCLPoseProvider mcl;
-  
   private JButton randomButton = new JButton("Random move");
   private JButton getPoseButton = new JButton("Get Pose");
-  
+  private JButton generateButton = new JButton("Generate Particles");
+  private JButton loadMapButton = new JButton("Load Map");
+  private JButton getParticlesButton = new JButton("Get Particles");
+  private String mapFileName = "Room.svg";
   
   /**
    * Create a MapTest object and display it in a GUI frame.
@@ -28,29 +30,53 @@ public class MCLTest extends NavigationPanel {
   }
   
   public MCLTest() {
+		commandPanel.add(loadMapButton);
+		commandPanel.add(generateButton);
 		commandPanel.add(randomButton);
 		commandPanel.add(getPoseButton);
-		
+		commandPanel.add(getParticlesButton);
+	
 		randomButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent event) {
-				model.randomMove();
+				if (!model.isConnected()) error("Not Connected");
+				else model.randomMove();
 			}
 		});
 		
-		randomButton.addActionListener(new ActionListener() {
+		getPoseButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent event) {
-				model.getPose();
+				if (!model.isConnected()) error("Not Connected");
+				else model.getPose();
+			}
+		});
+		
+		getParticlesButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				if (!model.isConnected()) error("Not Connected");
+				else model.getParticles();
+			}
+		});
+		
+		generateButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				model.generateParticles();
+			}
+		});
+		
+		loadMapButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				model.loadMap(mapFileName);
 			}
 		});
   }
   
   public void run() throws Exception {   
 	model.setPanel(this);
-	model.loadMap("Room.svg");
-	mcl = new MCLPoseProvider(null,null, model.getMap(),NUM_PARTICLES,0);
+	mcl = new MCLPoseProvider(null,NUM_PARTICLES,0);
 	model.setParticleSet(mcl.getParticles());
+	model.setMCL(mcl);
 	
     openInJFrame(this, FRAME_WIDTH, FRAME_HEIGHT, "Map Test", Color.white);;
   }
- 
+
 }
