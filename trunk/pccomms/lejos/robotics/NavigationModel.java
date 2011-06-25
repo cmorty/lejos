e5dbb42cf5fa75756456ab8bb3ba4cbf3390ab44
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2011-06-25 08:38:13 +0000

    Moved common stuff to NavigationModel

--- trunk/pccomms/lejos/robotics/NavigationModel.java
+++ trunk/pccomms/lejos/robotics/NavigationModel.java
@@ -2,7 +2,11 @@ package lejos.robotics;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+
+import lejos.robotics.localization.MCLParticleSet;
 import lejos.robotics.mapping.LineMap;
+import lejos.robotics.navigation.Pose;
+import lejos.robotics.navigation.Waypoint;
 
 public abstract class NavigationModel {
 	protected LineMap map;
@@ -10,6 +14,11 @@ public abstract class NavigationModel {
 	protected DataInputStream dis;
 	protected DataOutputStream dos;
 	protected int numReadings = 0;
+	protected Pose currentPose = new Pose(0,0,0);
+	protected Waypoint target = new Waypoint(0,0);
+	protected MCLParticleSet particles;
+	protected RangeReadings readings = new RangeReadings(0);
+	
 	public enum NavEvent {LOAD_MAP, GOTO, TRAVEL, ROTATE, STOP, GET_POSE, 
 		SET_POSE, RANDOM_MOVE, TAKE_READINGS, GET_READINGS, FIND_CLOSEST, ADD_WAYPOINT, PARTICLE_SET,
 		RANGE_READINGS, MOVE_STARTED, MOVE_STOPPED, WAYPOINT_REACHED, CLOSEST_PARTICLE, GET_ESTIMATED_POSE,
@@ -25,5 +34,21 @@ public abstract class NavigationModel {
 	
 	public void setNumReadings(int number) {
 		numReadings = number;
+	}
+	
+	public Pose getRobotPose() {
+		return currentPose;
+	}
+	
+	public MCLParticleSet getParticles() {
+		return particles;
+	}
+	
+	public void setRobotPose(Pose p) {
+		currentPose = p;
+	}
+	
+	public void setParticleSet(MCLParticleSet particles) {
+		this.particles = particles;
 	}
 }
