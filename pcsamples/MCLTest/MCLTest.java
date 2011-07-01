7535f4b3d9e6f820f89d44a154385e2c5939b910
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2011-07-01 21:37:43 +0000

    More diagnostics
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@5308 06c8f799-c779-4783-8af7-179afedb67c8

--- pcsamples/MCLTest/MCLTest.java
+++ pcsamples/MCLTest/MCLTest.java
@@ -2,7 +2,10 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
+import javax.swing.JLabel;
+
 import lejos.pc.remote.*;
+import lejos.robotics.RangeReadings;
 import lejos.robotics.localization.MCLPoseProvider;
 
 public class MCLTest extends NavigationPanel {
@@ -16,10 +19,9 @@ public class MCLTest extends NavigationPanel {
   private static MCLPoseProvider mcl;
   private JButton randomButton = new JButton("Random move");
   private JButton getPoseButton = new JButton("Get Pose");
-  private JButton generateButton = new JButton("Generate Particles");
   private JButton loadMapButton = new JButton("Load Map");
-  private JButton getParticlesButton = new JButton("Get Particles");
   private String mapFileName = "Room.svg";
+  private JLabel readingsLabel = new JLabel("readings");
   
   /**
    * Create a MapTest object and display it in a GUI frame.
@@ -31,43 +33,51 @@ public class MCLTest extends NavigationPanel {
   
   public MCLTest() {
 		commandPanel.add(loadMapButton);
-		commandPanel.add(generateButton);
 		commandPanel.add(randomButton);
 		commandPanel.add(getPoseButton);
-		commandPanel.add(getParticlesButton);
 	
 		randomButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent event) {
 				if (!model.isConnected()) error("Not Connected");
-				else model.randomMove();
+				else {
+					model.randomMove();
+					model.getRemoteParticles();
+				}
 			}
 		});
 		
 		getPoseButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent event) {
 				if (!model.isConnected()) error("Not Connected");
-				else model.getPose();
-			}
-		});
-		
-		getParticlesButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent event) {
-				if (!model.isConnected()) error("Not Connected");
-				else model.getRemoteParticles();
-			}
-		});
-		
-		generateButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent event) {
-				model.generateParticles();
+				else {
+					model.getPose();
+					model.getRemoteParticles();
+					model.getRemoteReadings();
+					model.getEstimatedPose();
+					System.out.println("Max weight:" + model.getParticles().getMaxWeight());
+					RangeReadings readings = model.getReadings();
+				
+					String s = "";
+					for(int i=0;i<readings.getNumReadings();i++) {
+						s += readings.getRange(i) + " ";
+						System.out.println(readings.getAngle(i)+ ":" + readings.getRange(i));
+					}
+					readingsLabel.setText(s);
+				}
 			}
 		});
 		
 		loadMapButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent event) {
-				model.loadMap(mapFileName);
+				if (!model.isConnected()) error("Not Connected");
+				else {
+					model.loadMap(mapFileName);
+					model.generateParticles();
+				}
 			}
 		});
+		
+		statusPanel.add(readingsLabel);
   }
   
   public void run() throws Exception {   
