4b00be6bed33855fb1f304b73cfc61fc067929e0
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2011-07-09 18:39:15 +0000

    Updated MapTest application
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@5380 06c8f799-c779-4783-8af7-179afedb67c8

--- pcsamples/MapTest/MapTest.java
+++ pcsamples/MapTest/MapTest.java
@@ -4,108 +4,141 @@ import javax.swing.*;
 import lejos.robotics.navigation.*;
 import lejos.robotics.mapping.MenuAction;
 import lejos.robotics.mapping.NavigationModel;
+import lejos.robotics.mapping.NavigationModel.NavEvent;
 import lejos.robotics.mapping.NavigationPanel;
 
+/**
+ * Use with the MapTest sample running on the NXT.
+ * 
+ * This PC sample uses extends Navigation panel to implement a GUI that controls a robot in a mapped area.
+ * 
+ * The map should be in SVG format. Only <line> elements are processed. Tools such as svg-edit can produce 
+ * suitable SVG files.
+ * 
+ * This sample lets you set the pose of a robot in a mapped area, and then goto a target position.
+ * You can also rotate the robot to a desired heading.
+ * 
+ * If obstacles are detected the robot stops and the onstacles are displayed on the map.
+ * 
+ * A future version will let you add waypoints so that you can define the path that the robot takes to the
+ * target.
+ * 
+ * @author Lawrie Griffiths
+ *
+ */
 public class MapTest extends NavigationPanel {
-  private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-  // GUI Window size
-  private static final int FRAME_WIDTH = 1000;
-  private static final int FRAME_HEIGHT = 800;
+	// GUI Window size
+	private static final int FRAME_WIDTH = 1000;
+	private static final int FRAME_HEIGHT = 800;
   
-  private static int GRID_SPACE = 39;
-  private static int CLEARANCE = 10;
+	private static final int GRID_SPACE = 39;
+	private static final int CLEARANCE = 10;
+	
+	private static final int INITIAL_ZOOM = 120;
+	private static final Dimension MAP_AREA_SIZE = new Dimension(800,600);
+	private static final String MAP_FILE = "floor.svg";
+	private static final Pose INITIAL_ROBOT_POSE = new Pose(450,90,180);
+	private static final String FRAME_TITLE = "Map Test";
   
-  private JLabel xLabel = new JLabel("X:");
-  private JTextField xField = new JTextField(6);
-  private JLabel yLabel = new JLabel("Y:");
-  private JTextField yField = new JTextField(6);
-  private JLabel headingLabel = new JLabel("Heading:");
-  private JTextField headingField = new JTextField(6);
-  private JButton gotoButton = new JButton("Go to");
-  private JLabel distanceLabel = new JLabel("Distance:");
-  private JTextField distanceField = new JTextField(10);
-  private JButton travelButton = new JButton("Travel");
-  private JLabel angleLabel = new JLabel("Angle:");
-  private JTextField angleField = new JTextField(10);
-  private JButton rotateButton = new JButton("Rotate");
-  private JButton calculateButton = new JButton("Calculate path");
+	
+	private JLabel setHeadingLabel = new JLabel("Set Heading:");
+	private JSlider setHeadingSlider = new JSlider(0,360);
+	private JButton setHeadingButton = new JButton("Set");
+	
+	private JLabel rotateLabel = new JLabel("Rotate To:");
+	private JSlider rotateSlider = new JSlider(0,360);
+	private JButton rotateButton = new JButton("Go");
   
-  /**
-   * Create a MapTest object and display it in a GUI frame.
-   * Then connect to the NXT.
-   */
-  public static void main(String[] args) throws Exception {
-	  (new MapTest()).run();
-  }
+	/**
+	 * Create a MapTest object and display it in a GUI frame.
+	 * Then connect to the NXT.
+	 */
+	public static void main(String[] args) throws Exception {
+		(new MapTest()).run();
+	}
   
-  public MapTest() {
-	  buildGUI();
-  }
+	public MapTest() {
+		buildGUI();
+	}
   
-  protected void buildGUI() {
+	@Override
+	protected void buildGUI() {
 	    showReadingsPanel = false;
 	    showLastMovePanel = false;
 	    showParticlePanel = false;
-	    showMoves = true;
+	    showMoves = true;    
+	    showMesh = false;
+	    
+	    mapSize = MAP_AREA_SIZE;
 	    super.buildGUI();
-		commandPanel.add(xLabel);
-		commandPanel.add(xField);
-		commandPanel.add(yLabel);
-		commandPanel.add(yField);
-		commandPanel.add(headingLabel);
-		commandPanel.add(headingField);
-		commandPanel.add(gotoButton);
-		commandPanel.add(distanceLabel);
-		commandPanel.add(distanceField);
-		commandPanel.add(travelButton);
-		commandPanel.add(angleLabel);
-		commandPanel.add(angleField);
+	    
+	    slider.setValue(INITIAL_ZOOM);
+
+		commandPanel.add(setHeadingLabel);
+		commandPanel.add(setHeadingSlider);
+		commandPanel.add(setHeadingButton);
+		
+		setHeadingSlider.setValue((int) INITIAL_ROBOT_POSE.getHeading());
+		setHeadingSlider.setMajorTickSpacing(90);
+		setHeadingSlider.setPaintTicks(true);
+		setHeadingSlider.setPaintLabels(true);
+		
+		setHeadingButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Pose current = model.getRobotPose();
+				model.setPose(new Pose(current.getX(), current.getY(), setHeadingSlider.getValue()));
+			}
+		});
+				
+		commandPanel.add(rotateLabel);
+		commandPanel.add(rotateSlider);
 		commandPanel.add(rotateButton);
 		
-		gotoButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent event) {
-				try {
-					model.goTo(new Waypoint( new Pose(Float.parseFloat(xField.getText()), 
-						Float.parseFloat(yField.getText()), Float.parseFloat(headingField.getText()))));
-				} catch (NumberFormatException e) {}
-			}
-		});
-		
-		travelButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent event) {
-				try {
-					model.travel(Float.parseFloat(distanceField.getText()));
-				} catch (NumberFormatException e) {}
-			}
-		});
+		rotateSlider.setValue(0);
+		rotateSlider.setMajorTickSpacing(90);
+		rotateSlider.setPaintTicks(true);
+		rotateSlider.setPaintLabels(true);
 		
 		rotateButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent event) {
-				try {
-					model.rotate(Float.parseFloat(angleField.getText()));
-				} catch (NumberFormatException e) {}
+			public void actionPerformed(ActionEvent e) {
+				model.rotate(rotateSlider.getValue());
 			}
 		});
-  }
+	}
+	
+	@Override
+	protected void eventReceived(NavEvent navEvent) {
+		if (navEvent == NavEvent.SET_POSE) {
+			int heading = (int) model.getRobotPose().getHeading();
+			if (heading < 0) heading += 360;
+			rotateSlider.setValue(heading);
+			setHeadingSlider.setValue(heading);
+		}
+	}
+	
+	@Override
+	public void whenConnected() {
+		model.setPose(model.getRobotPose());
+	}
   
+	@Override
 	protected void popupMenu(MouseEvent me) {
 	    Point pt = SwingUtilities.convertPoint(me.getComponent(), me.getPoint(), this);
 	    
 	    JPopupMenu menu = new JPopupMenu(); 
-	    menu.add(new MenuAction(NavigationModel.NavEvent.ADD_WAYPOINT, "Add Way Point",me.getPoint(), model, this));
 	    menu.add(new MenuAction(NavigationModel.NavEvent.GOTO, "Go To",me.getPoint(),model, this));
-	    menu.add(new MenuAction(NavigationModel.NavEvent.SET_POSE, "Set pose",me.getPoint(),model, this));
-	    menu.add(new MenuAction(NavigationModel.NavEvent.SET_TARGET, "Set target",me.getPoint(),model, this));
+	    menu.add(new MenuAction(NavigationModel.NavEvent.SET_POSE, "Place robot",me.getPoint(),model, this));
 	
 	    menu.show(this, pt.x, pt.y);
 	}
   
-  public void run() throws Exception {
-	model.loadMap("Room.svg");
-	model.setMeshParams(GRID_SPACE, CLEARANCE);
+	public void run() throws Exception {
+		model.loadMap(MAP_FILE);
+		model.setMeshParams(GRID_SPACE, CLEARANCE);
+		model.setPose(INITIAL_ROBOT_POSE);
 	
-    openInJFrame(this, FRAME_WIDTH, FRAME_HEIGHT, "Map Test", Color.white);
-  }
- 
+		openInJFrame(this, FRAME_WIDTH, FRAME_HEIGHT, FRAME_TITLE, Color.white);
+	}
 }
