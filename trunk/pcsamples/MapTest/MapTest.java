0d699a05b842616fb188a5082a229b7ed91440f0
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2011-07-11 15:01:03 +0000

    Show values with sliders and only allow context menu inside the mapped area.

--- trunk/pcsamples/MapTest/MapTest.java
+++ trunk/pcsamples/MapTest/MapTest.java
@@ -1,6 +1,9 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
 import lejos.robotics.navigation.*;
 import lejos.robotics.mapping.MenuAction;
 import lejos.robotics.mapping.NavigationModel;
@@ -36,20 +39,21 @@ public class MapTest extends NavigationPanel {
 	private static final int GRID_SPACE = 39;
 	private static final int CLEARANCE = 10;
 	
-	private static final int INITIAL_ZOOM = 120;
+	private static final int INITIAL_ZOOM = 110;
 	private static final Dimension MAP_AREA_SIZE = new Dimension(800,600);
 	private static final String MAP_FILE = "floor.svg";
 	private static final Pose INITIAL_ROBOT_POSE = new Pose(450,90,180);
 	private static final String FRAME_TITLE = "Map Test";
   
+	private static final String setHeadingText = "Set Heading:";
+	private final JLabel setHeadingLabel = new JLabel(setHeadingText + " 180");
+	private final JSlider setHeadingSlider = new JSlider(0,360);
+	private final JButton setHeadingButton = new JButton("Set");
 	
-	private JLabel setHeadingLabel = new JLabel("Set Heading:");
-	private JSlider setHeadingSlider = new JSlider(0,360);
-	private JButton setHeadingButton = new JButton("Set");
-	
-	private JLabel rotateLabel = new JLabel("Rotate To:");
-	private JSlider rotateSlider = new JSlider(0,360);
-	private JButton rotateButton = new JButton("Go");
+	private static final String rotateText = "Rotate To:";
+	private final JLabel rotateLabel = new JLabel(rotateText);
+	private final JSlider rotateSlider = new JSlider(0,360);
+	private final JButton rotateButton = new JButton("Go");
   
 	/**
 	 * Create a MapTest object and display it in a GUI frame.
@@ -80,15 +84,25 @@ public class MapTest extends NavigationPanel {
 		commandPanel.add(setHeadingSlider);
 		commandPanel.add(setHeadingButton);
 		
+		setHeadingLabel.setPreferredSize(new Dimension(95,20));
+		
 		setHeadingSlider.setValue((int) INITIAL_ROBOT_POSE.getHeading());
 		setHeadingSlider.setMajorTickSpacing(90);
 		setHeadingSlider.setPaintTicks(true);
 		setHeadingSlider.setPaintLabels(true);
 		
+		setHeadingSlider.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent event) {
+				int value = setHeadingSlider.getValue();
+				setHeadingLabel.setText(setHeadingText +  " " + value);
+			}		
+		});
+		
 		setHeadingButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				Pose current = model.getRobotPose();
-				model.setPose(new Pose(current.getX(), current.getY(), setHeadingSlider.getValue()));
+				int value = setHeadingSlider.getValue();
+				model.setPose(new Pose(current.getX(), current.getY(), value));			
 			}
 		});
 				
@@ -96,10 +110,19 @@ public class MapTest extends NavigationPanel {
 		commandPanel.add(rotateSlider);
 		commandPanel.add(rotateButton);
 		
+		rotateLabel.setPreferredSize(new Dimension(90,20));
+		
 		rotateSlider.setValue(0);
 		rotateSlider.setMajorTickSpacing(90);
 		rotateSlider.setPaintTicks(true);
 		rotateSlider.setPaintLabels(true);
+		
+		rotateSlider.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent event) {
+				int value = rotateSlider.getValue();
+				rotateLabel.setText(rotateText +  " " + value);
+			}		
+		});
 		
 		rotateButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -126,6 +149,8 @@ public class MapTest extends NavigationPanel {
 	@Override
 	protected void popupMenu(MouseEvent me) {
 	    Point pt = SwingUtilities.convertPoint(me.getComponent(), me.getPoint(), this);
+	    boolean inside = model.getMap().inside(new lejos.geom.Point(me.getX() / pixelsPerUnit, me.getY() / pixelsPerUnit));  
+	    if (!inside) return;
 	    
 	    JPopupMenu menu = new JPopupMenu(); 
 	    menu.add(new MenuAction(NavigationModel.NavEvent.GOTO, "Go To",me.getPoint(),model, this));
