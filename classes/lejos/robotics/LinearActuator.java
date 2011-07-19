25b1482df6ea732734c838dce04d88cbbc9562db
kirkpthompson <kirkpthompson@06c8f799-c779-4783-8af7-179afedb67c8>
2011-07-19 13:23:25 +0000

    javadoc changes. add actuateTo() method. Add explicit tacho methods specifying absolute position
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@5465 06c8f799-c779-4783-8af7-179afedb67c8

--- classes/lejos/robotics/LinearActuator.java
+++ classes/lejos/robotics/LinearActuator.java
@@ -3,8 +3,8 @@ package lejos.robotics;
 
 /**
  * Interface that defines the minimal implementation for a Linear Actuator device. Linear Actuator classes should provide 
- * non-blocking extend/retract actions through the <code>actuate()</code> method
- * and stall detection to avoid motor damage due to running against the end stops, etc.
+ * non-blocking extend/retract actions through the <code>actuate()</code> and  <code>actuateTo()</code> methods.
+ * Stall detection must be privided to avoid motor damage due to running against the end stops, etc.
  * <p>
  * Motor regulation is not specified in this interface as it may be difficult to determine the accurate length per time 
  * (ie. mm/sec) rate due to encoder tick granularity of the linear actuator. It is up to the implementor to decide if the 
@@ -40,20 +40,42 @@ public interface LinearActuator extends Encoder {
      */
     public void actuate(int distance, boolean immediateReturn);
     
-    /** Return <code>true</code> if the actuator is in motion due to an <code>actuate()</code> order.
+    /** The actuator should move to absolute <code>position</code> in encoder ticks. The <code>position</code> of the actuator
+     * shaft on startup should be zero. The <code>position</code> of the actuator shaft should be set to zero when 
+     * <code>resetTachoCount()</code> is called.
+     * @param position The absolute shaft position in encoder ticks.
+     * @param immediateReturn <code>true</code> returns immediately, <code>false</code> waits for the action to complete (or a stall)
+     */
+    public void actuateTo(int position, boolean immediateReturn);
+    
+    /** Return <code>true</code> if the actuator is in motion due to an <code>actuate()</code> or <code>actuateTo()</code> order.
      * @return <code>true</code> if the actuator is in motion. <code>false</code> otherwise.
      */
     public boolean isMoving();
     
     /**
-     * Returns true if an <code>actuate()</code> order ended due to a stalled motor. This should behave like a latch where the 
-     * reset of the stall status is done on a new <code>actuate()</code> order.
-     * @return <code>true</code> if actuator motor stalled during an <code>actuate()</code> order. <code>false</code> otherwise.
+     * Returns true if an <code>actuate()</code> or <code>actuateTo()</code> order ended due to a stalled motor. This should 
+     * behave like a latch where the 
+     * reset of the stall status is done on a new <code>actuate()</code> or <code>actuateTo()</code> order.
+     * @return <code>true</code> if actuator motor stalled during an <code>actuate()</code> or <code>actuateTo()</code> order. 
+     *  <code>false</code> otherwise.
      */
     public boolean isStalled();
     
     /**
-     * Cause the actuator to stop immediately and resist any further motion. Cancel any <code>actuate()</code> orders in progress.
+     * Cause the actuator to stop immediately and resist any further motion. Cancel any <code>actuate()</code> or 
+     * <code>actuateTo()</code>orders in progress.
      */
     public void stop();
+    
+    /**Returns the absolute tachometer (encoder) position of the actuator shaft. The zero position of the actuator shaft is where 
+     * <code>resetTachoCount()</code> was last called or the position of the shaft when instantiated. 
+     * 
+     * @return tachometer count in encoder ticks.
+     */
+    public int getTachoCount();
+    
+    /**Reset the tachometer (encoder) count to zero at the current actuator position.
+     */
+    public void resetTachoCount();
 }
