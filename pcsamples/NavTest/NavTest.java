8016dfabc6929be877d920cf69a61687e22785d2
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2011-07-21 11:09:49 +0000

    New NavTest sample using EventPanel
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@5475 06c8f799-c779-4783-8af7-179afedb67c8

---
+++ pcsamples/NavTest/NavTest.java
@@ -0,0 +1,85 @@
+import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.SystemColor;
+import javax.swing.JPopupMenu;
+import lejos.robotics.mapping.MenuAction;
+import lejos.robotics.mapping.NavigationModel;
+import lejos.robotics.mapping.NavigationPanel;
+import lejos.robotics.mapping.NavigationModel.NavEvent;
+import lejos.robotics.navigation.Pose;
+
+
+public class NavTest extends NavigationPanel {
+	private static final long serialVersionUID = 1L;
+
+	private static final int FRAME_WIDTH = 1000;
+	private static final int FRAME_HEIGHT = 800;	
+	private static final int INITIAL_ZOOM = 100;
+	private static final Point INITIAL_VIEW_START = new Point(-80,-10);
+	private static final Dimension MAP_AREA_SIZE = new Dimension(800,550);
+	private static final String MAP_FILE = "floor.svg";
+	private static final Pose INITIAL_ROBOT_POSE = new Pose(450,430,180);
+	private static final String FRAME_TITLE = "Nav Test";
+  
+	/**
+	 * Create a MapTest object and display it in a GUI frame.
+	 * Then connect to the NXT.
+	 */
+	public static void main(String[] args) {
+		(new NavTest()).run();
+	}
+  
+	public NavTest() {
+		buildGUI();
+	}
+  
+	@Override
+	protected void buildGUI() {
+		title = "Nav Test";
+		description = "NavTest demonstrates sending navigation events to the PC";
+		
+	    showMoves = true;    
+	    showMesh = false;
+	    showZoomLabels = true;
+	    
+	    mapPaneSize = MAP_AREA_SIZE;
+	    initialViewStart = INITIAL_VIEW_START;
+	    
+	    createConnectPanel();
+	    createControlPanel();
+	    createMousePanel();
+	    
+	    add(eventPanel);
+	    createMapPanel();
+
+	    zoomSlider.setValue(INITIAL_ZOOM);
+		createMenu();
+	}
+	
+	@Override
+	protected void eventReceived(NavEvent navEvent) {
+	}
+	
+	/**
+	 * Send the pose when connected
+	 */
+	@Override
+	public void whenConnected() {
+		model.setPose(model.getRobotPose());
+	}
+  
+	@Override
+	protected void popupMenuItems(Point p, JPopupMenu menu) {
+	    menu.add(new MenuAction(NavigationModel.NavEvent.GOTO, "Go To", p, model, this));
+	    menu.add(new MenuAction(NavigationModel.NavEvent.SET_POSE, "Place robot", p, model, this));
+	    menu.add(new MenuAction(NavigationModel.NavEvent.ADD_WAYPOINT, "Add Waypoint", p, model, this));
+	}
+  
+	public void run(){
+		model.setDebug(true);
+		model.loadMap(MAP_FILE);
+		model.setPose(INITIAL_ROBOT_POSE);
+	
+		openInJFrame(this, FRAME_WIDTH, FRAME_HEIGHT, FRAME_TITLE, SystemColor.controlShadow, menuBar);
+	}
+}
