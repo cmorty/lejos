11cf4c3d8b8b02c28e8dd6adc5fe7594ffba3695
skoehler <skoehler@06c8f799-c779-4783-8af7-179afedb67c8>
2011-08-02 15:01:04 +0000

    added read(byte[], int, int) implementation

--- trunk/classes/lejos/nxt/comm/NXTInputStream.java
+++ trunk/classes/lejos/nxt/comm/NXTInputStream.java
@@ -1,6 +1,7 @@
 package lejos.nxt.comm;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Extends InputStream for BlueTooth; implements available()
@@ -16,11 +17,33 @@ public class NXTInputStream extends InputStream {
 		this.conn = conn;
         buf = new byte[buffSize];
 	}
+	
+	@Override
+	public int read(byte[] b, int off, int len) throws IOException {
+		int avail = bufSize - bufIdx;
+		if (avail <= 0) {
+			bufSize = conn.read(buf, buf.length);
+			if (bufSize < -1) throw new IOException();
+			if (bufSize <= 0) return -1;
+			bufIdx = 0;
+			avail = bufSize;
+		}
+
+		if (len > avail)
+			len = avail;
+
+		System.arraycopy(buf, bufIdx, b, off, len);
+		bufIdx += len;
+
+		return len;
+	}
+
     /**
      * Returns one byte as an integer between 0 and 255.  
      * Returns -1 if the end of the stream is reached.
      * Does not return till some bytes are available.
      */
+	@Override
 	public int read() throws IOException
     {
 	   if (bufIdx >= bufSize) bufSize = 0;
@@ -37,7 +60,8 @@ public class NXTInputStream extends InputStream {
     /**
      * returns the number of bytes in the input buffer - can be read without blocking
      */
-    public int available() throws IOException
+    @Override
+	public int available() throws IOException
     {
        if (bufIdx >= bufSize) bufSize = 0;
        if (bufSize == 0) {
@@ -52,7 +76,8 @@ public class NXTInputStream extends InputStream {
     /**
      * the stream is restored to its original state - ready to receive more data.
      */
-    public void close()
+    @Override
+	public void close()
     { 
        bufIdx = 0;
        bufSize = 0;
