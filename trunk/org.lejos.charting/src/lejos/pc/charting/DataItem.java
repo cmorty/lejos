8e7edf2c909ceeb49adae32e41c85e6900c923b8
kirkpthompson <kirkpthompson@06c8f799-c779-4783-8af7-179afedb67c8>
2011-08-23 04:19:53 +0000

    javadoc

--- trunk/org.lejos.charting/src/lejos/pc/charting/DataItem.java
+++ trunk/org.lejos.charting/src/lejos/pc/charting/DataItem.java
@@ -1,7 +1,7 @@
 package lejos.pc.charting;
 
 /** Each logged data item is represented by a 
- * <code>DataItem</code> instance
+ * <code>DataItem</code> instance which provide the value as a wrapper and it's datatype.
  * @see DataLogger
  * @see DataLogger.LoggerListener#logLineAvailable
  */
@@ -15,11 +15,12 @@ public class DataItem {
     public static final int DT_DOUBLE  = 6;
     public static final int DT_STRING  = 7;
     
-    /** The <code>Number</code> wrapper object of the datatype.
+    /** The <code>Number</code> wrapper object of the datatype. Use <code>datatype</code> to determine correct cast type.
      */
     public Object value = null;
 
     /** The datatype. 
+     *@see #DT_BOOLEAN
      *@see #DT_BYTE
      *@see #DT_SHORT
      *@see #DT_INTEGER
