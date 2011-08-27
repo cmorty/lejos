35fe3d36504cb081b7a7019769c1f8eae208b4c8
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2011-08-27 11:26:16 +0000

    Define new constants so that things compile on the PC side even if the modes do not work
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@5678 06c8f799-c779-4783-8af7-179afedb67c8

--- pccomms/lejos/nxt/I2CPort.java
+++ pccomms/lejos/nxt/I2CPort.java
@@ -15,5 +15,12 @@ public interface I2CPort extends BasicSensorPort {
     public static final int LEGO_MODE = 1;
     public static final int ALWAYS_ACTIVE = 2;
     
+    /** Do not release the i2c bus between requests */
+    public static final int NO_RELEASE = 4;
+    /** Use high speed I/O (125KHz) */
+    public static final int HIGH_SPEED = 8;
+    /** Maximum read/write request length */
+    public static final int MAX_IO = 32;
+    
 	public int getId();
 }
