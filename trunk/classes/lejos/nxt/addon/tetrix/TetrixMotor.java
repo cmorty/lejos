2f4c0b6d2b0ebecd3c68775910f6a94bfd2ddb86
kirkpthompson <kirkpthompson@06c8f799-c779-4783-8af7-179afedb67c8>
2011-10-10 05:48:23 +0000

    javadoc, mc, CONSTANTized

--- trunk/classes/lejos/nxt/addon/tetrix/TetrixMotor.java
+++ trunk/classes/lejos/nxt/addon/tetrix/TetrixMotor.java
@@ -1,13 +1,13 @@
 package lejos.nxt.addon.tetrix;
 
-/** Basic Tetrix DC motor abstraction without encoder support.
+/** Tetrix basic DC motor abstraction without encoder support. The default power at instantiation is 100%.
+ * <p>Use <code>{@link TetrixMotorController#getBasicMotor}</code> to retrieve a <code>TetrixMotor</code> instance.
  * 
  * @author Kirk P. Thompson
  */
 public class TetrixMotor implements lejos.robotics.DCMotor{
     TetrixMotorController mc;
     int channel;
-    int retVal;
     
     TetrixMotor(TetrixMotorController mc, int channel) {
         this.mc=mc;
@@ -18,7 +18,7 @@ public class TetrixMotor implements lejos.robotics.DCMotor{
     public void setPower(int power) {
         power=Math.abs(power);
         if (power>100) power=100;
-        retVal = mc.doCommand(TetrixMotorController.CMD_SETPOWER, power, channel);
+        mc.doCommand(TetrixMotorController.CMD_SETPOWER, power, channel);
     }
 
     public int getPower() {
@@ -26,23 +26,30 @@ public class TetrixMotor implements lejos.robotics.DCMotor{
     }
     
     public void forward() {
-        retVal = mc.doCommand(TetrixMotorController.CMD_FORWARD, 0, channel);
+        mc.doCommand(TetrixMotorController.CMD_FORWARD, 0, channel);
     }
 
     public void backward() {
-        retVal = mc.doCommand(TetrixMotorController.CMD_BACKWARD, 0, channel);
+        mc.doCommand(TetrixMotorController.CMD_BACKWARD, 0, channel);
     }
 
     public void stop() {
-        retVal = mc.doCommand(TetrixMotorController.CMD_STOP, 0, channel);
+        mc.doCommand(TetrixMotorController.CMD_STOP, 0, channel);
     }
 
     public void flt() {
-        retVal = mc.doCommand(TetrixMotorController.CMD_FLT, 0, channel);
+        mc.doCommand(TetrixMotorController.CMD_FLT, 0, channel);
     }
-
+    
+    /**
+     * Return <code>true</code> if the motor is moving. Note that this method reports based on the current control
+     * state (i.e. commanded to move) and not if the motor is actually moving. This means a motor may be stalled but this
+     * method would return <code>true</code>.
+     *
+     * @return <code>true</code> if the motor is executing a movement command, <code>false</code> if stopped.
+     */
     public boolean isMoving() {
-        return 1==mc.doCommand(TetrixMotorController.CMD_ISMOVING, 0, channel);
+        return TetrixMotorController.MOTPARAM_OP_TRUE==mc.doCommand(TetrixMotorController.CMD_ISMOVING, 0, channel);
     }
 
     /** Used to alter the forward/reverse direction mapping for the motor output. This is primarily intended to
@@ -52,8 +59,8 @@ public class TetrixMotor implements lejos.robotics.DCMotor{
      * @param reverse <code>true</code> to reverse direction mapping for this motor
      */
     public void setReverse(boolean reverse) {
-        int op=0;
-        if (reverse) op=1;
-        retVal = mc.doCommand(TetrixMotorController.CMD_SETREVERSE, op, channel);
+        int op=TetrixMotorController.MOTPARAM_OP_FALSE;
+        if (reverse) op=TetrixMotorController.MOTPARAM_OP_TRUE;
+        mc.doCommand(TetrixMotorController.CMD_SETREVERSE, op, channel);
     }
 }
