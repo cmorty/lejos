45e33fe95ef12819568cacf8b3bbfe9a69201d16
kirkpthompson <kirkpthompson@06c8f799-c779-4783-8af7-179afedb67c8>
2011-10-10 05:46:28 +0000

    cleaned imports, javadoc, mc, CONSTANTized
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@5745 06c8f799-c779-4783-8af7-179afedb67c8

--- classes/lejos/nxt/addon/tetrix/TetrixEncoderMotor.java
+++ classes/lejos/nxt/addon/tetrix/TetrixEncoderMotor.java
@@ -1,14 +1,14 @@
 package lejos.nxt.addon.tetrix;
 
-import lejos.nxt.NXTRegulatedMotor;
-
 import lejos.robotics.Encoder;
 
 import lejos.util.Delay;
 
+
 /** Tetrix DC motor abstraction with encoder support. The Tetrix motor must have an encoder installed and connected to
  * the controller for the methods in this class to work. If an encoder is not installed, use the <code>{@link TetrixMotor}</code>
  * class instead.
+ * <p>Use <code>{@link TetrixMotorController#getEncoderMotor}</code> to retrieve a <code>TetrixEncoderMotor</code> instance.
  * 
  * @author Kirk P. Thompson
  */
@@ -16,21 +16,21 @@ public class TetrixEncoderMotor extends TetrixMotor implements Encoder{
     TetrixEncoderMotor(TetrixMotorController mc, int channel) {
         super(mc, channel);
     }
-
+    
     public int getTachoCount() {
-        return (int)(mc.getEncoderValue(channel)*.25);
+        return (int)(mc.doCommand(TetrixMotorController.CMD_GETTACHO, 0, channel) * .25);
     }
-
+    
     /** Reset the the tachometer count. Calling this method will stop any current motor action. This is imposed by the HiTechic
      * Motor Controller firmware. 
      */
-    public void resetTachoCount() {
+    public synchronized void resetTachoCount() {
         mc.doCommand(TetrixMotorController.CMD_RESETTACHO, 0, channel);
     }
    
     private synchronized void waitRotateComplete() {
-        while (mc.rotateIsBUSY(channel)) {
-            Delay.msDelay(100);
+        while (isMoving()) {
+            Delay.msDelay(50);
         }
     }
     
@@ -39,11 +39,11 @@ public class TetrixEncoderMotor extends TetrixMotor implements Encoder{
      * completes its rotation asynchronously.
      * 
      * @param degrees number of degrees to rotate relative to the current position.
-     * @param immediateReturn if <code>true</code>, do not wait for the move to complete.
+     * @param immediateReturn if <code>true</code>, do not wait for the move to complete. <code>false</code> will block
+     * until the rotation completes.
      */
     public void rotate(int degrees, boolean immediateReturn){
         int cmd=TetrixMotorController.CMD_ROTATE;
-        if (!immediateReturn) cmd=TetrixMotorController.CMD_ROTATE_WAIT;
         mc.doCommand(cmd, degrees, channel);
         if (!immediateReturn) waitRotateComplete();
     }
@@ -53,11 +53,11 @@ public class TetrixEncoderMotor extends TetrixMotor implements Encoder{
      * completes its rotation asynchronously.
      * 
      * @param limitAngle Angle [in degrees] to rotate to.
-     * @param immediateReturn if <code>true</code>, do not wait for the move to complete
+     * @param immediateReturn if <code>true</code>, do not wait for the move to complete. <code>false</code> will block
+     * until the rotation completes.
      */
     public void rotateTo(int limitAngle, boolean immediateReturn){
         int cmd=TetrixMotorController.CMD_ROTATE_TO;
-        if (!immediateReturn) cmd=TetrixMotorController.CMD_ROTATE_TO_WAIT;
         mc.doCommand(cmd, limitAngle, channel);
         if (!immediateReturn) waitRotateComplete();
     }
@@ -69,9 +69,9 @@ public class TetrixEncoderMotor extends TetrixMotor implements Encoder{
      * 
      * @param regulate <code>true</code> to enable regulation, <code>false</code> otherwise.
      */
-    public void setRegulated(boolean regulate){
-        int operand=0;
-        if (regulate) operand=1;
+    public void setRegulate(boolean regulate){
+        int operand=TetrixMotorController.MOTPARAM_OP_FALSE;
+        if (regulate) operand=TetrixMotorController.MOTPARAM_OP_TRUE;
         mc.doCommand(TetrixMotorController.CMD_SETREGULATE, operand, channel);
     }
 }
