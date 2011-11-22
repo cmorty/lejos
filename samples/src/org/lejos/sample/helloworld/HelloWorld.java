95dc0a16b5e6ff17236d468e785689e8ddfec4bd
skoehler <skoehler@06c8f799-c779-4783-8af7-179afedb67c8>
2011-11-22 21:44:53 +0000

    reorganization (build.xml with menu system to build individual samples follows)
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@5832 06c8f799-c779-4783-8af7-179afedb67c8

---
+++ samples/src/org/lejos/sample/helloworld/HelloWorld.java
@@ -0,0 +1,18 @@
+package org.lejos.sample.helloworld;
+import lejos.nxt.*;
+
+/**
+ * $Id: HelloWorld.java 1587 2008-05-02 17:19:41Z lgriffiths $
+ * 
+ * @author Lawrie Griffiths
+ * 
+ */
+public class HelloWorld
+{
+  public static void main (String[] aArg)
+  throws Exception
+  {
+     LCD.drawString("Hello World",3,4);
+     Thread.sleep(2000);
+  }
+}
