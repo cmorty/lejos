7c97a5bc1657fcd0d2408d2bfbc1255f58b000de
skoehler <skoehler@06c8f799-c779-4783-8af7-179afedb67c8>
2011-11-24 19:37:09 +0000

    restructuring
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@5851 06c8f799-c779-4783-8af7-179afedb67c8

---
+++ pcsamples/src/org/lejos/pcsample/acceldemo/AccelDemo.java
@@ -0,0 +1,26 @@
+package org.lejos.pcsample.acceldemo;
+import lejos.nxt.*;
+import lejos.nxt.addon.*;
+
+/**
+ * Test of remote access to Acceleration (Tilt) sensor using
+ * iCommand-like classes in pccomm.jar.
+ * 
+ * @author Lawrie Griffiths
+ *
+ */
+public class AccelDemo {
+	public static void main(String [] args) throws Exception {
+		AccelMindSensor a = new AccelMindSensor(SensorPort.S1);
+		//AccelHTSensor a = new AccelHTSensor(SensorPort.S1);
+		
+		System.out.println("Prod ID " + a.getProductID());
+		System.out.println("Vendor ID " + a.getVendorID());
+		System.out.println("Version " + a.getVersion());
+				
+		for(;;) {
+			System.out.println("TILT: x " + a.getXTilt() + "  y " + a.getYTilt() + " z " + a.getZTilt());
+			System.out.println("ACCEL: x " + a.getXAccel() + "  y " + a.getYAccel() + " z " + a.getZAccel());
+		}
+	}
+}
