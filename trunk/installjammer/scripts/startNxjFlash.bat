0cbec0e3b6e5ebfd9fd79b29d2ba59a9b72fc955
skoehler <skoehler@06c8f799-c779-4783-8af7-179afedb67c8>
2011-12-28 17:45:48 +0000

    mc

--- trunk/installjammer/scripts/startNxjFlash.bat
+++ trunk/installjammer/scripts/startNxjFlash.bat
@@ -1,3 +1,3 @@
 set "NXJ_HOME=%~f1"
 set "LEJOS_NXT_JAVA_HOME=%~f2"
-start "NXJFlash" /MIN cmd.exe /C "%NXJ_HOME%\bin\nxjflashg.bat"
+start "NXJFlashG" /MIN "%COMSPEC%" /C "%NXJ_HOME%\bin\nxjflashg.bat"
