3ebb0cd2e226be3c6b88e7a9ad3c153288973219
skoehler <skoehler@06c8f799-c779-4783-8af7-179afedb67c8>
2011-12-29 03:32:51 +0000

    java.util.Arrays based on Juan's Bubblesort class
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@6045 06c8f799-c779-4783-8af7-179afedb67c8

---
+++ classes/src/java/util/Arrays.java
@@ -0,0 +1,217 @@
+package java.util;
+
+/**
+ * Various tools for arrays.
+ * NOTE: currently bubblesort is used for sorting arrays.
+ * 
+ * @author Juan Antonio Brenha Moral, Sven Köhler
+ */
+public class Arrays {
+
+	private Arrays() {
+		// class cannot be instantiated
+	}
+
+	/**
+	 * Method used to sort an Array
+	 * @param a the array to sort
+	 */
+	public static void sort(byte[] a) {
+		sort(a, 0, a.length);
+	}
+	
+	/**
+	 * Method used to sort an Array
+	 * @param a the array to sort
+	 * @param fromIndex index of first element (inclusive) to sort
+	 * @param toIndex index of last element (exclusive) to sort
+	 */
+	public static void sort(byte[] a, int fromIndex, int toIndex) {
+		// outer loop (backward)
+		for (int out = toIndex - 1; out > fromIndex; out--){
+			// inner loop (forward)
+			for (int in = fromIndex; in < out; in++){
+				int in2 = in + 1;
+				byte temp = a[in];
+				if (temp > a[in2]){ // out of order?
+					a[in] = a[in2];
+					a[in2] = temp;
+				}
+			}
+		}
+	}
+
+	/**
+	 * Method used to sort an Array
+	 * @param a the array to sort
+	 */
+	public static void sort(short[] a) {
+		sort(a, 0, a.length);
+	}
+	
+	/**
+	 * Method used to sort an Array
+	 * @param a the array to sort
+	 * @param fromIndex index of first element (inclusive) to sort
+	 * @param toIndex index of last element (exclusive) to sort
+	 */
+	public static void sort(short[] a, int fromIndex, int toIndex) {
+		// outer loop (backward)
+		for (int out = toIndex - 1; out > fromIndex; out--){
+			// inner loop (forward)
+			for (int in = fromIndex; in < out; in++){
+				int in2 = in + 1;
+				short temp = a[in];
+				if (temp > a[in2]){ // out of order?
+					a[in] = a[in2];
+					a[in2] = temp;
+				}
+			}
+		}
+	}
+
+	/**
+	 * Method used to sort an Array
+	 * @param a the array to sort
+	 */
+	public static void sort(char[] a) {
+		sort(a, 0, a.length);
+	}
+	
+	/**
+	 * Method used to sort an Array
+	 * @param a the array to sort
+	 * @param fromIndex index of first element (inclusive) to sort
+	 * @param toIndex index of last element (exclusive) to sort
+	 */
+	public static void sort(char[] a, int fromIndex, int toIndex) {
+		// outer loop (backward)
+		for (int out = toIndex - 1; out > fromIndex; out--){
+			// inner loop (forward)
+			for (int in = fromIndex; in < out; in++){
+				int in2 = in + 1;
+				char temp = a[in];
+				if (temp > a[in2]){ // out of order?
+					a[in] = a[in2];
+					a[in2] = temp;
+				}
+			}
+		}
+	}
+
+	/**
+	 * Method used to sort an Array
+	 * @param a the array to sort
+	 */
+	public static void sort(int[] a) {
+		sort(a, 0, a.length);
+	}
+	
+	/**
+	 * Method used to sort an Array
+	 * @param a the array to sort
+	 * @param fromIndex index of first element (inclusive) to sort
+	 * @param toIndex index of last element (exclusive) to sort
+	 */
+	public static void sort(int[] a, int fromIndex, int toIndex) {
+		// outer loop (backward)
+		for (int out = toIndex - 1; out > fromIndex; out--){
+			// inner loop (forward)
+			for (int in = fromIndex; in < out; in++){
+				int in2 = in + 1;
+				int temp = a[in];
+				if (temp > a[in2]){ // out of order?
+					a[in] = a[in2];
+					a[in2] = temp;
+				}
+			}
+		}
+	}
+
+	/**
+	 * Method used to sort an Array
+	 * @param a the array to sort
+	 */
+	public static void sort(long[] a) {
+		sort(a, 0, a.length);
+	}
+	
+	/**
+	 * Method used to sort an Array
+	 * @param a the array to sort
+	 * @param fromIndex index of first element (inclusive) to sort
+	 * @param toIndex index of last element (exclusive) to sort
+	 */
+	public static void sort(long[] a, int fromIndex, int toIndex) {
+		// outer loop (backward)
+		for (int out = toIndex - 1; out > fromIndex; out--){
+			// inner loop (forward)
+			for (int in = fromIndex; in < out; in++){
+				int in2 = in + 1;
+				long temp = a[in];
+				if (temp > a[in2]){ // out of order?
+					a[in] = a[in2];
+					a[in2] = temp;
+				}
+			}
+		}
+	}
+
+	/**
+	 * Method used to sort an Array
+	 * @param a the array to sort
+	 */
+	public static void sort(float[] a) {
+		sort(a, 0, a.length);
+	}
+	
+	/**
+	 * Method used to sort an Array
+	 * @param a the array to sort
+	 * @param fromIndex index of first element (inclusive) to sort
+	 * @param toIndex index of last element (exclusive) to sort
+	 */
+	public static void sort(float[] a, int fromIndex, int toIndex) {
+		// outer loop (backward)
+		for (int out = toIndex - 1; out > fromIndex; out--){
+			// inner loop (forward)
+			for (int in = fromIndex; in < out; in++){
+				int in2 = in + 1;
+				float temp = a[in];
+				if (Float.compare(temp, a[in2]) > 0){ // out of order?
+					a[in] = a[in2];
+					a[in2] = temp;
+				}
+			}
+		}
+	}
+
+	/**
+	 * Method used to sort an Array
+	 * @param a the array to sort
+	 */
+	public static void sort(double[] a) {
+		sort(a, 0, a.length);
+	}
+	
+	/**
+	 * Method used to sort an Array
+	 * @param a the array to sort
+	 * @param fromIndex index of first element (inclusive) to sort
+	 * @param toIndex index of last element (exclusive) to sort
+	 */
+	public static void sort(double[] a, int fromIndex, int toIndex) {
+		// outer loop (backward)
+		for (int out = toIndex - 1; out > fromIndex; out--){
+			// inner loop (forward)
+			for (int in = fromIndex; in < out; in++){
+				int in2 = in + 1;
+				double temp = a[in];
+				if (Double.compare(temp, a[in2]) > 0){ // out of order?
+					a[in] = a[in2];
+					a[in2] = temp;
+				}
+			}
+		}
+	}
+}
