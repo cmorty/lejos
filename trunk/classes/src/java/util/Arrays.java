61f62f860b7a9e018bc01aec52fb2a6b94c0f266
skoehler <skoehler@06c8f799-c779-4783-8af7-179afedb67c8>
2011-12-30 19:33:06 +0000

    even more bits of Arrays class

--- trunk/classes/src/java/util/Arrays.java
+++ trunk/classes/src/java/util/Arrays.java
@@ -13,6 +13,284 @@ public class Arrays {
 		// class cannot be instantiated
 	}
 	
+	//TODO checks whether from/toIndex are valid etc.
+	
+	/****************** BINARY SEARCH ******************/
+	
+	/**
+	 * Binary search for element in sorted array.
+	 * @param a the sorted array
+	 * @param val the value
+	 * @return the index at which the element was found, or otherwise <code>(-index -1)</code> where <code>index</code>
+	 * 	denotes the index at which the element should be inserted
+	 */
+	public static int binarySearch(byte[] a, byte val) {
+		return binarySearch(a, 0, a.length, val);
+	}
+	
+	/**
+	 * Binary search for element in sorted array.
+	 * @param a the sorted array
+	 * @param fromIndex index of first element (inclusive) to sort
+	 * @param toIndex index of last element (exclusive) to sort
+	 * @param key the value to search for
+	 * @return the index at which the element was found, or otherwise <code>(-index -1)</code> where <code>index</code>
+	 * 	denotes the index at which the element should be inserted
+	 */
+	public static int binarySearch(byte[] a, int fromIndex, int toIndex, byte key) {
+		while (fromIndex < toIndex) {				
+			int mid = (fromIndex + toIndex) >>> 1;
+			byte amid = a[mid];
+			if (key < amid)
+				toIndex = mid;
+			else if (key > amid)
+				fromIndex = mid+1;
+			else
+				return mid;
+		}
+		return -1 - fromIndex;
+	}
+	
+	/**
+	 * Binary search for element in sorted array.
+	 * @param a the sorted array
+	 * @param val the value
+	 * @return the index at which the element was found, or otherwise <code>(-index -1)</code> where <code>index</code>
+	 * 	denotes the index at which the element should be inserted
+	 */
+	public static int binarySearch(short[] a, short val) {
+		return binarySearch(a, 0, a.length, val);
+	}
+	
+	/**
+	 * Binary search for element in sorted array.
+	 * @param a the sorted array
+	 * @param fromIndex index of first element (inclusive) to sort
+	 * @param toIndex index of last element (exclusive) to sort
+	 * @param key the value to search for
+	 * @return the index at which the element was found, or otherwise <code>(-index -1)</code> where <code>index</code>
+	 * 	denotes the index at which the element should be inserted
+	 */
+	public static int binarySearch(short[] a, int fromIndex, int toIndex, short key) {
+		while (fromIndex < toIndex) {				
+			int mid = (fromIndex + toIndex) >>> 1;
+			short amid = a[mid];
+			if (key < amid)
+				toIndex = mid;
+			else if (key > amid)
+				fromIndex = mid+1;
+			else
+				return mid;
+		}
+		return -1 - fromIndex;
+	}
+	
+	/**
+	 * Binary search for element in sorted array.
+	 * @param a the sorted array
+	 * @param val the value
+	 * @return the index at which the element was found, or otherwise <code>(-index -1)</code> where <code>index</code>
+	 * 	denotes the index at which the element should be inserted
+	 */
+	public static int binarySearch(char[] a, char val) {
+		return binarySearch(a, 0, a.length, val);
+	}
+	
+	/**
+	 * Binary search for element in sorted array.
+	 * @param a the sorted array
+	 * @param fromIndex index of first element (inclusive) to sort
+	 * @param toIndex index of last element (exclusive) to sort
+	 * @param key the value to search for
+	 * @return the index at which the element was found, or otherwise <code>(-index -1)</code> where <code>index</code>
+	 * 	denotes the index at which the element should be inserted
+	 */
+	public static int binarySearch(char[] a, int fromIndex, int toIndex, char key) {
+		while (fromIndex < toIndex) {				
+			int mid = (fromIndex + toIndex) >>> 1;
+			char amid = a[mid];
+			if (key < amid)
+				toIndex = mid;
+			else if (key > amid)
+				fromIndex = mid+1;
+			else
+				return mid;
+		}
+		return -1 - fromIndex;
+	}
+	
+	/**
+	 * Binary search for element in sorted array.
+	 * @param a the sorted array
+	 * @param val the value
+	 * @return the index at which the element was found, or otherwise <code>(-index -1)</code> where <code>index</code>
+	 * 	denotes the index at which the element should be inserted
+	 */
+	public static int binarySearch(int[] a, int val) {
+		return binarySearch(a, 0, a.length, val);
+	}
+	
+	/**
+	 * Binary search for element in sorted array.
+	 * @param a the sorted array
+	 * @param fromIndex index of first element (inclusive) to sort
+	 * @param toIndex index of last element (exclusive) to sort
+	 * @param key the value to search for
+	 * @return the index at which the element was found, or otherwise <code>(-index -1)</code> where <code>index</code>
+	 * 	denotes the index at which the element should be inserted
+	 */
+	public static int binarySearch(int[] a, int fromIndex, int toIndex, int key) {
+		while (fromIndex < toIndex) {				
+			int mid = (fromIndex + toIndex) >>> 1;
+			int amid = a[mid];
+			if (key < amid)
+				toIndex = mid;
+			else if (key > amid)
+				fromIndex = mid+1;
+			else
+				return mid;
+		}
+		return -1 - fromIndex;
+	}
+	
+	/**
+	 * Binary search for element in sorted array.
+	 * @param a the sorted array
+	 * @param val the value
+	 * @return the index at which the element was found, or otherwise <code>(-index -1)</code> where <code>index</code>
+	 * 	denotes the index at which the element should be inserted
+	 */
+	public static int binarySearch(long[] a, long val) {
+		return binarySearch(a, 0, a.length, val);
+	}
+	
+	/**
+	 * Binary search for element in sorted array.
+	 * @param a the sorted array
+	 * @param fromIndex index of first element (inclusive) to sort
+	 * @param toIndex index of last element (exclusive) to sort
+	 * @param key the value to search for
+	 * @return the index at which the element was found, or otherwise <code>(-index -1)</code> where <code>index</code>
+	 * 	denotes the index at which the element should be inserted
+	 */
+	public static int binarySearch(long[] a, int fromIndex, int toIndex, long key) {
+		while (fromIndex < toIndex) {				
+			int mid = (fromIndex + toIndex) >>> 1;
+			long amid = a[mid];
+			if (key < amid)
+				toIndex = mid;
+			else if (key > amid)
+				fromIndex = mid+1;
+			else
+				return mid;
+		}
+		return -1 - fromIndex;
+	}
+	
+	/**
+	 * Binary search for element in sorted array.
+	 * @param a the sorted array
+	 * @param val the value
+	 * @return the index at which the element was found, or otherwise <code>(-index -1)</code> where <code>index</code>
+	 * 	denotes the index at which the element should be inserted
+	 */
+	public static int binarySearch(float[] a, float val) {
+		return binarySearch(a, 0, a.length, val);
+	}
+	
+	/**
+	 * Binary search for element in sorted array.
+	 * @param a the sorted array
+	 * @param fromIndex index of first element (inclusive) to sort
+	 * @param toIndex index of last element (exclusive) to sort
+	 * @param key the value to search for
+	 * @return the index at which the element was found, or otherwise <code>(-index -1)</code> where <code>index</code>
+	 * 	denotes the index at which the element should be inserted
+	 */
+	public static int binarySearch(float[] a, int fromIndex, int toIndex, float key) {
+		while (fromIndex < toIndex) {				
+			int mid = (fromIndex + toIndex) >>> 1;
+			int cmp = Float.compare(key, a[mid]);
+			if (cmp < 0)
+				toIndex = mid;
+			else if (cmp > 0)
+				fromIndex = mid+1;
+			else
+				return mid;
+		}
+		return -1 - fromIndex;
+	}
+	
+	/**
+	 * Binary search for element in sorted array.
+	 * @param a the sorted array
+	 * @param val the value
+	 * @return the index at which the element was found, or otherwise <code>(-index -1)</code> where <code>index</code>
+	 * 	denotes the index at which the element should be inserted
+	 */
+	public static int binarySearch(double[] a, double val) {
+		return binarySearch(a, 0, a.length, val);
+	}
+	
+	/**
+	 * Binary search for element in sorted array.
+	 * @param a the sorted array
+	 * @param fromIndex index of first element (inclusive) to sort
+	 * @param toIndex index of last element (exclusive) to sort
+	 * @param key the value to search for
+	 * @return the index at which the element was found, or otherwise <code>(-index -1)</code> where <code>index</code>
+	 * 	denotes the index at which the element should be inserted
+	 */
+	public static int binarySearch(double[] a, int fromIndex, int toIndex, double key) {
+		while (fromIndex < toIndex) {				
+			int mid = (fromIndex + toIndex) >>> 1;
+			int cmp = Double.compare(key, a[mid]);
+			if (cmp < 0)
+				toIndex = mid;
+			else if (cmp > 0)
+				fromIndex = mid+1;
+			else
+				return mid;
+		}
+		return -1 - fromIndex;
+	}
+	
+	/**
+	 * Binary search for element in sorted array.
+	 * @param a the sorted array
+	 * @param val the value
+	 * @return the index at which the element was found, or otherwise <code>(-index -1)</code> where <code>index</code>
+	 * 	denotes the index at which the element should be inserted
+	 */
+	public static int binarySearch(Object[] a, Object val) {
+		return binarySearch(a, 0, a.length, val);
+	}
+	
+	/**
+	 * Binary search for element in sorted array.
+	 * @param a the sorted array
+	 * @param fromIndex index of first element (inclusive) to sort
+	 * @param toIndex index of last element (exclusive) to sort
+	 * @param key the value to search for
+	 * @return the index at which the element was found, or otherwise <code>(-index -1)</code> where <code>index</code>
+	 * 	denotes the index at which the element should be inserted
+	 */
+	public static int binarySearch(Object[] a, int fromIndex, int toIndex, Object key) {
+		@SuppressWarnings("unchecked")
+		Comparable<? super Object> key2 = (Comparable<? super Object>)key; 
+		while (fromIndex < toIndex) {				
+			int mid = (fromIndex + toIndex) >>> 1;
+			int cmp = key2.compareTo(a[mid]);
+			if (cmp < 0)
+				toIndex = mid;
+			else if (cmp > 0)
+				fromIndex = mid+1;
+			else
+				return mid;
+		}
+		return -1 - fromIndex;
+	}
+		
 	/****************** FILL ******************/
 	
 	/**
@@ -20,7 +298,7 @@ public class Arrays {
 	 * @param a the array
 	 * @param val the value
 	 */
-	public void fill(boolean[] a, boolean val) {
+	public static void fill(boolean[] a, boolean val) {
 		fill(a, 0, a.length, val);
 	}
 	
@@ -31,7 +309,7 @@ public class Arrays {
 	 * @param toIndex index of last element (exclusive) to sort
 	 * @param val the value
 	 */
-	public void fill(boolean[] a, int fromIndex, int toIndex, boolean val) {
+	public static void fill(boolean[] a, int fromIndex, int toIndex, boolean val) {
 		for (int i=fromIndex; i<toIndex; i++) {
 			a[i] = val;
 		}
@@ -42,7 +320,7 @@ public class Arrays {
 	 * @param a the array
 	 * @param val the value
 	 */
-	public void fill(byte[] a, byte val) {
+	public static void fill(byte[] a, byte val) {
 		fill(a, 0, a.length, val);
 	}
 	
@@ -53,7 +331,7 @@ public class Arrays {
 	 * @param toIndex index of last element (exclusive) to sort
 	 * @param val the value
 	 */
-	public void fill(byte[] a, int fromIndex, int toIndex, byte val) {
+	public static void fill(byte[] a, int fromIndex, int toIndex, byte val) {
 		for (int i=fromIndex; i<toIndex; i++) {
 			a[i] = val;
 		}
@@ -64,7 +342,7 @@ public class Arrays {
 	 * @param a the array
 	 * @param val the value
 	 */
-	public void fill(short[] a, short val) {
+	public static void fill(short[] a, short val) {
 		fill(a, 0, a.length, val);
 	}
 	
@@ -75,7 +353,7 @@ public class Arrays {
 	 * @param toIndex index of last element (exclusive) to sort
 	 * @param val the value
 	 */
-	public void fill(short[] a, int fromIndex, int toIndex, short val) {
+	public static void fill(short[] a, int fromIndex, int toIndex, short val) {
 		for (int i=fromIndex; i<toIndex; i++) {
 			a[i] = val;
 		}
@@ -86,7 +364,7 @@ public class Arrays {
 	 * @param a the array
 	 * @param val the value
 	 */
-	public void fill(char[] a, char val) {
+	public static void fill(char[] a, char val) {
 		fill(a, 0, a.length, val);
 	}
 	
@@ -97,7 +375,7 @@ public class Arrays {
 	 * @param toIndex index of last element (exclusive) to sort
 	 * @param val the value
 	 */
-	public void fill(char[] a, int fromIndex, int toIndex, char val) {
+	public static void fill(char[] a, int fromIndex, int toIndex, char val) {
 		for (int i=fromIndex; i<toIndex; i++) {
 			a[i] = val;
 		}
@@ -108,7 +386,7 @@ public class Arrays {
 	 * @param a the array
 	 * @param val the value
 	 */
-	public void fill(int[] a, int val) {
+	public static void fill(int[] a, int val) {
 		fill(a, 0, a.length, val);
 	}
 	
@@ -119,7 +397,7 @@ public class Arrays {
 	 * @param toIndex index of last element (exclusive) to sort
 	 * @param val the value
 	 */
-	public void fill(int[] a, int fromIndex, int toIndex, int val) {
+	public static void fill(int[] a, int fromIndex, int toIndex, int val) {
 		for (int i=fromIndex; i<toIndex; i++) {
 			a[i] = val;
 		}
@@ -130,7 +408,7 @@ public class Arrays {
 	 * @param a the array
 	 * @param val the value
 	 */
-	public void fill(long[] a, long val) {
+	public static void fill(long[] a, long val) {
 		fill(a, 0, a.length, val);
 	}
 	
@@ -141,7 +419,7 @@ public class Arrays {
 	 * @param toIndex index of last element (exclusive) to sort
 	 * @param val the value
 	 */
-	public void fill(long[] a, int fromIndex, int toIndex, long val) {
+	public static void fill(long[] a, int fromIndex, int toIndex, long val) {
 		for (int i=fromIndex; i<toIndex; i++) {
 			a[i] = val;
 		}
@@ -152,7 +430,7 @@ public class Arrays {
 	 * @param a the array
 	 * @param val the value
 	 */
-	public void fill(float[] a, float val) {
+	public static void fill(float[] a, float val) {
 		fill(a, 0, a.length, val);
 	}
 	
@@ -163,7 +441,7 @@ public class Arrays {
 	 * @param toIndex index of last element (exclusive) to sort
 	 * @param val the value
 	 */
-	public void fill(float[] a, int fromIndex, int toIndex, float val) {
+	public static void fill(float[] a, int fromIndex, int toIndex, float val) {
 		for (int i=fromIndex; i<toIndex; i++) {
 			a[i] = val;
 		}
@@ -174,7 +452,7 @@ public class Arrays {
 	 * @param a the array
 	 * @param val the value
 	 */
-	public void fill(double[] a, double val) {
+	public static void fill(double[] a, double val) {
 		fill(a, 0, a.length, val);
 	}
 	
@@ -185,7 +463,7 @@ public class Arrays {
 	 * @param toIndex index of last element (exclusive) to sort
 	 * @param val the value
 	 */
-	public void fill(double[] a, int fromIndex, int toIndex, double val) {
+	public static void fill(double[] a, int fromIndex, int toIndex, double val) {
 		for (int i=fromIndex; i<toIndex; i++) {
 			a[i] = val;
 		}
@@ -196,7 +474,7 @@ public class Arrays {
 	 * @param a the array
 	 * @param val the value
 	 */
-	public void fill(Object[] a, Object val) {
+	public static void fill(Object[] a, Object val) {
 		fill(a, 0, a.length, val);
 	}
 	
@@ -207,7 +485,7 @@ public class Arrays {
 	 * @param toIndex index of last element (exclusive) to sort
 	 * @param val the value
 	 */
-	public void fill(Object[] a, int fromIndex, int toIndex, Object val) {
+	public static void fill(Object[] a, int fromIndex, int toIndex, Object val) {
 		for (int i=fromIndex; i<toIndex; i++) {
 			a[i] = val;
 		}
