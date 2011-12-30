86b67cd5d1c541a8ea823892f80ab5ea5c3e5345
skoehler <skoehler@06c8f799-c779-4783-8af7-179afedb67c8>
2011-12-30 18:21:32 +0000

    more bits of Arrays class

--- trunk/classes/src/java/util/Arrays.java
+++ trunk/classes/src/java/util/Arrays.java
@@ -12,9 +12,211 @@ public class Arrays {
 	private Arrays() {
 		// class cannot be instantiated
 	}
+	
+	/****************** FILL ******************/
+	
+	/**
+	 * Set array elements to given value.
+	 * @param a the array
+	 * @param val the value
+	 */
+	public void fill(boolean[] a, boolean val) {
+		fill(a, 0, a.length, val);
+	}
+	
+	/**
+	 * Set array elements to given value.
+	 * @param a the array
+	 * @param fromIndex index of first element (inclusive) to sort
+	 * @param toIndex index of last element (exclusive) to sort
+	 * @param val the value
+	 */
+	public void fill(boolean[] a, int fromIndex, int toIndex, boolean val) {
+		for (int i=fromIndex; i<toIndex; i++) {
+			a[i] = val;
+		}
+	}
+	
+	/**
+	 * Set array elements to given value.
+	 * @param a the array
+	 * @param val the value
+	 */
+	public void fill(byte[] a, byte val) {
+		fill(a, 0, a.length, val);
+	}
+	
+	/**
+	 * Set array elements to given value.
+	 * @param a the array
+	 * @param fromIndex index of first element (inclusive) to sort
+	 * @param toIndex index of last element (exclusive) to sort
+	 * @param val the value
+	 */
+	public void fill(byte[] a, int fromIndex, int toIndex, byte val) {
+		for (int i=fromIndex; i<toIndex; i++) {
+			a[i] = val;
+		}
+	}
+	
+	/**
+	 * Set array elements to given value.
+	 * @param a the array
+	 * @param val the value
+	 */
+	public void fill(short[] a, short val) {
+		fill(a, 0, a.length, val);
+	}
+	
+	/**
+	 * Set array elements to given value.
+	 * @param a the array
+	 * @param fromIndex index of first element (inclusive) to sort
+	 * @param toIndex index of last element (exclusive) to sort
+	 * @param val the value
+	 */
+	public void fill(short[] a, int fromIndex, int toIndex, short val) {
+		for (int i=fromIndex; i<toIndex; i++) {
+			a[i] = val;
+		}
+	}
+	
+	/**
+	 * Set array elements to given value.
+	 * @param a the array
+	 * @param val the value
+	 */
+	public void fill(char[] a, char val) {
+		fill(a, 0, a.length, val);
+	}
+	
+	/**
+	 * Set array elements to given value.
+	 * @param a the array
+	 * @param fromIndex index of first element (inclusive) to sort
+	 * @param toIndex index of last element (exclusive) to sort
+	 * @param val the value
+	 */
+	public void fill(char[] a, int fromIndex, int toIndex, char val) {
+		for (int i=fromIndex; i<toIndex; i++) {
+			a[i] = val;
+		}
+	}
+	
+	/**
+	 * Set array elements to given value.
+	 * @param a the array
+	 * @param val the value
+	 */
+	public void fill(int[] a, int val) {
+		fill(a, 0, a.length, val);
+	}
+	
+	/**
+	 * Set array elements to given value.
+	 * @param a the array
+	 * @param fromIndex index of first element (inclusive) to sort
+	 * @param toIndex index of last element (exclusive) to sort
+	 * @param val the value
+	 */
+	public void fill(int[] a, int fromIndex, int toIndex, int val) {
+		for (int i=fromIndex; i<toIndex; i++) {
+			a[i] = val;
+		}
+	}
+	
+	/**
+	 * Set array elements to given value.
+	 * @param a the array
+	 * @param val the value
+	 */
+	public void fill(long[] a, long val) {
+		fill(a, 0, a.length, val);
+	}
+	
+	/**
+	 * Set array elements to given value.
+	 * @param a the array
+	 * @param fromIndex index of first element (inclusive) to sort
+	 * @param toIndex index of last element (exclusive) to sort
+	 * @param val the value
+	 */
+	public void fill(long[] a, int fromIndex, int toIndex, long val) {
+		for (int i=fromIndex; i<toIndex; i++) {
+			a[i] = val;
+		}
+	}
+	
+	/**
+	 * Set array elements to given value.
+	 * @param a the array
+	 * @param val the value
+	 */
+	public void fill(float[] a, float val) {
+		fill(a, 0, a.length, val);
+	}
+	
+	/**
+	 * Set array elements to given value.
+	 * @param a the array
+	 * @param fromIndex index of first element (inclusive) to sort
+	 * @param toIndex index of last element (exclusive) to sort
+	 * @param val the value
+	 */
+	public void fill(float[] a, int fromIndex, int toIndex, float val) {
+		for (int i=fromIndex; i<toIndex; i++) {
+			a[i] = val;
+		}
+	}
+	
+	/**
+	 * Set array elements to given value.
+	 * @param a the array
+	 * @param val the value
+	 */
+	public void fill(double[] a, double val) {
+		fill(a, 0, a.length, val);
+	}
+	
+	/**
+	 * Set array elements to given value.
+	 * @param a the array
+	 * @param fromIndex index of first element (inclusive) to sort
+	 * @param toIndex index of last element (exclusive) to sort
+	 * @param val the value
+	 */
+	public void fill(double[] a, int fromIndex, int toIndex, double val) {
+		for (int i=fromIndex; i<toIndex; i++) {
+			a[i] = val;
+		}
+	}
+	
+	/**
+	 * Set array elements to given value.
+	 * @param a the array
+	 * @param val the value
+	 */
+	public void fill(Object[] a, Object val) {
+		fill(a, 0, a.length, val);
+	}
+	
+	/**
+	 * Set array elements to given value.
+	 * @param a the array
+	 * @param fromIndex index of first element (inclusive) to sort
+	 * @param toIndex index of last element (exclusive) to sort
+	 * @param val the value
+	 */
+	public void fill(Object[] a, int fromIndex, int toIndex, Object val) {
+		for (int i=fromIndex; i<toIndex; i++) {
+			a[i] = val;
+		}
+	}
+	
+	/****************** SORT ******************/ 
 
 	/**
-	 * Method used to sort an Array
+	 * Sort the specified array in ascending order.
 	 * @param a the array to sort
 	 */
 	public static void sort(byte[] a) {
@@ -22,7 +224,7 @@ public class Arrays {
 	}
 	
 	/**
-	 * Method used to sort an Array
+	 * Sort the specified array in ascending order.
 	 * @param a the array to sort
 	 * @param fromIndex index of first element (inclusive) to sort
 	 * @param toIndex index of last element (exclusive) to sort
@@ -43,7 +245,7 @@ public class Arrays {
 	}
 
 	/**
-	 * Method used to sort an Array
+	 * Sort the specified array in ascending order.
 	 * @param a the array to sort
 	 */
 	public static void sort(short[] a) {
@@ -51,7 +253,7 @@ public class Arrays {
 	}
 	
 	/**
-	 * Method used to sort an Array
+	 * Sort the specified array in ascending order.
 	 * @param a the array to sort
 	 * @param fromIndex index of first element (inclusive) to sort
 	 * @param toIndex index of last element (exclusive) to sort
@@ -72,7 +274,7 @@ public class Arrays {
 	}
 
 	/**
-	 * Method used to sort an Array
+	 * Sort the specified array in ascending order.
 	 * @param a the array to sort
 	 */
 	public static void sort(char[] a) {
@@ -80,7 +282,7 @@ public class Arrays {
 	}
 	
 	/**
-	 * Method used to sort an Array
+	 * Sort the specified array in ascending order.
 	 * @param a the array to sort
 	 * @param fromIndex index of first element (inclusive) to sort
 	 * @param toIndex index of last element (exclusive) to sort
@@ -101,7 +303,7 @@ public class Arrays {
 	}
 
 	/**
-	 * Method used to sort an Array
+	 * Sort the specified array in ascending order.
 	 * @param a the array to sort
 	 */
 	public static void sort(int[] a) {
@@ -109,7 +311,7 @@ public class Arrays {
 	}
 	
 	/**
-	 * Method used to sort an Array
+	 * Sort the specified array in ascending order.
 	 * @param a the array to sort
 	 * @param fromIndex index of first element (inclusive) to sort
 	 * @param toIndex index of last element (exclusive) to sort
@@ -130,7 +332,7 @@ public class Arrays {
 	}
 
 	/**
-	 * Method used to sort an Array
+	 * Sort the specified array in ascending order.
 	 * @param a the array to sort
 	 */
 	public static void sort(long[] a) {
@@ -138,7 +340,7 @@ public class Arrays {
 	}
 	
 	/**
-	 * Method used to sort an Array
+	 * Sort the specified array in ascending order.
 	 * @param a the array to sort
 	 * @param fromIndex index of first element (inclusive) to sort
 	 * @param toIndex index of last element (exclusive) to sort
@@ -159,7 +361,7 @@ public class Arrays {
 	}
 
 	/**
-	 * Method used to sort an Array
+	 * Sort the specified array in ascending order.
 	 * @param a the array to sort
 	 */
 	public static void sort(float[] a) {
@@ -167,7 +369,7 @@ public class Arrays {
 	}
 	
 	/**
-	 * Method used to sort an Array
+	 * Sort the specified array in ascending order.
 	 * @param a the array to sort
 	 * @param fromIndex index of first element (inclusive) to sort
 	 * @param toIndex index of last element (exclusive) to sort
@@ -188,7 +390,7 @@ public class Arrays {
 	}
 
 	/**
-	 * Method used to sort an Array
+	 * Sort the specified array in ascending order.
 	 * @param a the array to sort
 	 */
 	public static void sort(double[] a) {
@@ -196,7 +398,7 @@ public class Arrays {
 	}
 	
 	/**
-	 * Method used to sort an Array
+	 * Sort the specified array in ascending order.
 	 * @param a the array to sort
 	 * @param fromIndex index of first element (inclusive) to sort
 	 * @param toIndex index of last element (exclusive) to sort
@@ -209,6 +411,40 @@ public class Arrays {
 				int in2 = in + 1;
 				double temp = a[in];
 				if (Double.compare(temp, a[in2]) > 0){ // out of order?
+					a[in] = a[in2];
+					a[in2] = temp;
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Sort the specified array in ascending order.
+	 * @param a the array to sort
+	 */
+	public static void sort(Object[] a) {
+		sort(a, 0, a.length);
+	}
+	
+	/**
+	 * Sort the specified array in ascending order.
+	 * @param a the array to sort
+	 * @param fromIndex index of first element (inclusive) to sort
+	 * @param toIndex index of last element (exclusive) to sort
+	 */
+	public static void sort(Object[] a, int fromIndex, int toIndex) {
+		//The JDK's implementation doesn't take care of null values
+		//and simply assumes that the object implement comparable.
+		//So we do the same.
+		
+		// outer loop (backward)
+		for (int out = toIndex - 1; out > fromIndex; out--){
+			// inner loop (forward)
+			for (int in = fromIndex; in < out; in++){
+				int in2 = in + 1;
+				@SuppressWarnings("unchecked")
+				Comparable<? super Object> temp = (Comparable<? super Object>)a[in];
+				if (temp.compareTo(a[in2]) > 0){ // out of order?
 					a[in] = a[in2];
 					a[in2] = temp;
 				}
