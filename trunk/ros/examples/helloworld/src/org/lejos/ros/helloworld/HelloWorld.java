4fd085604170255811b1dc9afc20a940ab84e2a0
esmetaman <esmetaman@06c8f799-c779-4783-8af7-179afedb67c8>
2012-01-01 12:08:42 +0000

    I have created the minimum example using ROSJava

---
+++ trunk/ros/examples/helloworld/src/org/lejos/ros/helloworld/HelloWorld.java
@@ -0,0 +1,46 @@
+package org.lejos.ros.helloworld;
+
+import org.ros.node.Node;
+import org.ros.node.NodeMain;
+
+import com.google.common.base.Preconditions;
+
+public class HelloWorld implements NodeMain{
+
+	private Node node;
+	
+	@Override
+	public void onShutdown(Node arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onShutdownComplete(Node arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStart(Node node) {
+	    Preconditions.checkState(this.node == null);
+	    this.node = node;
+	    
+	    System.out.println("Running your first node");
+	    
+	    try {
+	      int seq = 0;
+	      while (true) {
+	        System.out.println("Hello, world! " + seq);
+	        seq++;
+	        Thread.sleep(1000);
+	      }
+	    } catch (Exception e) {
+	    	if (node != null) {
+	    		node.getLog().fatal(e);
+	    	} else {
+	    		e.printStackTrace();
+	    	}
+	    }
+	 }
+}
