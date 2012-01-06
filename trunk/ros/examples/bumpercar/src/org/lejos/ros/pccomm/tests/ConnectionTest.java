3ade0a0a0adf1669f53b9910bc66dc913c55831c
esmetaman <esmetaman@06c8f799-c779-4783-8af7-179afedb67c8>
2012-01-06 23:09:35 +0000

    This example show a possible solution to the classic problem BumperCar using ROS and LeJOS. The example show how to create a NXT robot and encapsulate the logic in a ROS Node. Another pure ROS node evaluates data from the node BumperCar and stablish the next state for bumperCar. This way to develop software using nodes is really interesting and very collaborative.

---
+++ trunk/ros/examples/bumpercar/src/org/lejos/ros/pccomm/tests/ConnectionTest.java
@@ -0,0 +1,38 @@
+package org.lejos.ros.pccomm.tests;
+
+
+import java.awt.Toolkit;
+
+import org.lejos.pccomm.utils.SimpleConnector;
+
+import lejos.nxt.Battery;
+
+public class ConnectionTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		boolean connetionStatus = false;
+		
+		connetionStatus = SimpleConnector.connectByBT("ROSBRICK1");
+
+		if(connetionStatus){
+			
+			int voltage = 0;
+			
+			while(true){
+
+				voltage = Battery.getVoltageMilliVolt();
+				System.out.println("NXT Voltage: " + voltage);
+				try {Thread.sleep(1000);} catch (InterruptedException e) {}
+			}
+			
+		}else{
+			System.out.println("I can't know NXT Status");
+		}
+		
+	}
+
+}
