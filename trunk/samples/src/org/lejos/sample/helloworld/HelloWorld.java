86e6b814f9d42732948f79d205496acbe22aefde
skoehler <skoehler@06c8f799-c779-4783-8af7-179afedb67c8>
2012-01-07 16:12:34 +0000

    mc

--- trunk/samples/src/org/lejos/sample/helloworld/HelloWorld.java
+++ trunk/samples/src/org/lejos/sample/helloworld/HelloWorld.java
@@ -1,18 +1,15 @@
 package org.lejos.sample.helloworld;
-import lejos.nxt.*;
+
+import lejos.nxt.LCD;
 
 /**
- * $Id: HelloWorld.java 1587 2008-05-02 17:19:41Z lgriffiths $
+ * A simple hello world program.
  * 
  * @author Lawrie Griffiths
- * 
  */
-public class HelloWorld
-{
-  public static void main (String[] aArg)
-  throws Exception
-  {
-     LCD.drawString("Hello World",3,4);
-     Thread.sleep(2000);
-  }
+public class HelloWorld {
+	public static void main(String[] aArg) throws Exception {
+		LCD.drawString("Hello World", 3, 4);
+		Thread.sleep(2000);
+	}
 }
