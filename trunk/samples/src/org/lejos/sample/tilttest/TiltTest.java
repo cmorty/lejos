f50f471c5f3d7076a10e90311031c925762d34c9
skoehler <skoehler@06c8f799-c779-4783-8af7-179afedb67c8>
2012-01-16 20:41:10 +0000

    new accel sample, made tilt sample specific to mindsensors sensor

--- trunk/samples/src/org/lejos/sample/tilttest/TiltTest.java
+++ trunk/samples/src/org/lejos/sample/tilttest/TiltTest.java
@@ -1,28 +1,28 @@
 package org.lejos.sample.tilttest;
-import lejos.nxt.*;
-import lejos.nxt.addon.*;
-import lejos.robotics.Accelerometer;
+
+import lejos.nxt.Button;
+import lejos.nxt.LCD;
+import lejos.nxt.SensorPort;
+import lejos.nxt.addon.AccelMindSensor;
 
 /**
  * Simple test of Acceleration (Tilt) sensors.
+ * This should work with Mindsensors Acceleration sensor.
  * 
- * This should work with Mindsensors and HiTechnic Acceleration
- * sensors.
- * 
- * @author Lawrie Griffiths
+ * @author Sven Köhler
  *
  */
 public class TiltTest {
 	
 	public static void main(String[] args) throws Exception {
-		Accelerometer tilt = SensorSelector.createAccelerometer(SensorPort.S1);
+		AccelMindSensor tilt = new AccelMindSensor(SensorPort.S1);
 			
 		while(!Button.ESCAPE.isDown()) {
 			LCD.clear();
-			// LCD.drawString("Tilt", 0, 0);
-			// LCD.drawInt(tilt.getXTilt(), 6, 0, 1);
-			// LCD.drawInt(tilt.getYTilt(), 6, 0, 2);
-			// LCD.drawInt(tilt.getZTilt(), 6, 0, 3);
+			LCD.drawString("Tilt", 0, 0);
+			LCD.drawInt(tilt.getXTilt(), 6, 0, 1);
+			LCD.drawInt(tilt.getYTilt(), 6, 0, 2);
+			LCD.drawInt(tilt.getZTilt(), 6, 0, 3);
 			LCD.drawString("Accel", 0, 4);
 			LCD.drawInt(tilt.getXAccel(), 6, 0, 5);
 			LCD.drawInt(tilt.getYAccel(), 6, 0, 6);
