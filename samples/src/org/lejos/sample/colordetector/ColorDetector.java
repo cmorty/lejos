35dec59af48171cec9afe724d637f5dd412685c1
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2012-01-17 15:39:38 +0000

    Include color names
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@6199 06c8f799-c779-4783-8af7-179afedb67c8

--- samples/src/org/lejos/sample/colordetector/ColorDetector.java
+++ samples/src/org/lejos/sample/colordetector/ColorDetector.java
@@ -18,6 +18,10 @@ public class ColorDetector {
 		String g = "G";
 		String b = "B";
 		
+		String[] colorNames = {"Red", "Green", "Blue", "Yellow", "Magenta", "Orange",
+				             "White", "Black", "Pink", "Gray", "Light gray", "Dark Gray", "Cyan"			
+		};
+		
 		while(!Button.ESCAPE.isDown()) {
 			LCD.clear();
 			LCD.drawString(cmps.getVendorID(), 0, 0);
@@ -25,6 +29,7 @@ public class ColorDetector {
 			LCD.drawString(cmps.getVersion(), 9, 1);
 			LCD.drawString(color, 0, 3);
 			LCD.drawInt(cmps.getColorID(),7,3);
+			LCD.drawString(colorNames[cmps.getColorID()], 0, 4);
 			LCD.drawString(r, 0, 5);
 			LCD.drawInt(cmps.getRGBComponent(Color.RED),1,5);
 			LCD.drawString(g, 5, 5);
