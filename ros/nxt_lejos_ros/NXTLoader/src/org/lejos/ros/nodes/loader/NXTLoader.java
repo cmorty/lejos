676d1092f4d4b1f47d21b243ad1bda32e7b88cb1
esmetaman <esmetaman@06c8f799-c779-4783-8af7-179afedb67c8>
2012-01-21 18:41:47 +0000

    I have improved NXTLoader to add a support for a Differential Actuator System (Differential Pilot) Besides, I have added new features to properties file.
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@6246 06c8f799-c779-4783-8af7-179afedb67c8

--- ros/nxt_lejos_ros/NXTLoader/src/org/lejos/ros/nodes/loader/NXTLoader.java
+++ ros/nxt_lejos_ros/NXTLoader/src/org/lejos/ros/nodes/loader/NXTLoader.java
@@ -17,6 +17,7 @@ import org.lejos.ros.nxt.NXTDevice;
 import org.lejos.ros.nxt.actuators.NXTServoMotor;
 import org.lejos.ros.nxt.sensors.BatterySensor;
 import org.lejos.ros.nxt.sensors.UltrasonicSensor;
+import org.lejos.ros.nxt.systems.DifferentialActuatorSystem;
 
 import org.ros.message.MessageListener;
 import org.ros.node.Node;
@@ -40,14 +41,21 @@ public class NXTLoader extends LEJOSNode{
 	String BRICK_NAME = "";
     String CONNECTION_TYPE = "";
 	String YAML = "";
+
+	boolean DIFFERENTIAL_NAVIGATION_FEATURES = false;
+	float WHEEL_DIAMETER = 0.0f;
+	float TRACK_WIDTH = 0.0f;
+	boolean REVERSE = false;
 	
     final String USB_CONNECTION = "USB";
     final String BLUETOOTH_CONNECTION = "BLUETOOTH";
 
+    boolean connectionStatus = false;
 	
     //Topic Management    
     ArrayList<NXTServoMotor> motorList = new ArrayList();
     ArrayList<NXTDevice> sensorList = new ArrayList();
+    ArrayList<NXTDevice> actuatorSystemsList = new ArrayList();
     
 	final org.ros.message.sensor_msgs.JointState jointState = 
 		new org.ros.message.sensor_msgs.JointState();
@@ -59,10 +67,37 @@ public class NXTLoader extends LEJOSNode{
 	    Preconditions.checkState(this.node == null);
 	    this.node = node;
 
+	    //1. Configurate Node
+	    configurate();
+		
+	    //2. Connect with NXT Brick
+	    connect();
+	    
+	    //3. Bind NXT with ROS
+	    bind();
+	}
+	
+	/**
+	 * 
+	 */
+	protected void configurate(){
+		
+		System.out.println("* Configurate ROS Node");
+		
 	    //Show information about header
-	    showHeader();
+	    this.showHeader();
 	    
 	    //Get ROS Path
+	    getROSPath();
+		
+	    //Load properties
+		readProperties();
+	}
+	
+	/**
+	 * 
+	 */
+	protected void getROSPath(){
 		try {
 		    ROSNodePath = new File(".").getCanonicalPath() + "/"; 
 			System.out.println("ROS Node Path: " + ROSNodePath );
@@ -70,43 +105,12 @@ public class NXTLoader extends LEJOSNode{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-	    //Load properties
-		readProperties();
-		
-	    //1. Connect with NXT Brick
-		System.out.println("* Connecting with a NXT brick");
-		
-	    boolean connectionStatus = false;
-	    
-	    if(CONNECTION_TYPE.equals(BLUETOOTH_CONNECTION)){
-			connectionStatus = SimpleConnector.connectByBT(BRICK_NAME);	    	
-	    }else{
-	    	connectionStatus = SimpleConnector.connectByUSB();
-	    }
-		
-		if(connectionStatus){
-			System.out.println("ROS Node connected with a NXT brick");
-			
-			//2. Process YAML file
-			processYAML();
-			
-			//3. Process subcriptions
-			if(motorList.size() > 0){
-			    processSubscriptions();
-			}
-			
-			//4. Update topics
-			if(sensorList.size() >0){
-				updateTopics();				
-			}
-		}else{
-			System.err.println("I can't connect with a NXT brick");
-			System.exit(0);
-		}
 	}
 	
-	private void readProperties(){
+	/**
+	 * 
+	 */
+	protected void readProperties(){
 		
 		Properties prop = new Properties();
 
@@ -129,6 +133,23 @@ public class NXTLoader extends LEJOSNode{
             System.out.println("BRICK-NAME: " + BRICK_NAME);
     		System.out.println("CONNECTION: " + CONNECTION_TYPE);
     		System.out.println("YAML: " + YAML);
+    		
+    		if(Boolean.parseBoolean(prop.getProperty("DIFFERENTIAL_NAVIGATION_FEATURES"))){
+    			System.out.println("Differential navigation features enabled");
+    			
+    			DIFFERENTIAL_NAVIGATION_FEATURES = Boolean.parseBoolean(prop.getProperty("DIFFERENTIAL_NAVIGATION_FEATURES"));
+    			WHEEL_DIAMETER = Float.parseFloat(prop.getProperty("WHEEL_DIAMETER"));
+    			TRACK_WIDTH = Float.parseFloat(prop.getProperty("TRACK_WIDTH"));
+    			REVERSE = Boolean.parseBoolean(prop.getProperty("REVERSE"));
+
+                System.out.println("WHEEL_DIAMETER: " + WHEEL_DIAMETER);
+        		System.out.println("TRACK_WIDTH: " + TRACK_WIDTH);
+        		System.out.println("REVERSE: " + REVERSE);
+    		
+    		}
+    		
+
+    		
 			
 		} catch (FileNotFoundException e) {
 			
@@ -160,14 +181,54 @@ public class NXTLoader extends LEJOSNode{
 	/**
 	 * 
 	 */
-	private void processYAML(){
+	protected void connect(){
+		System.out.println("* Connecting with a NXT brick");
+	    
+	    if(CONNECTION_TYPE.equals(BLUETOOTH_CONNECTION)){
+			connectionStatus = SimpleConnector.connectByBT(BRICK_NAME);	    	
+	    }else{
+	    	connectionStatus = SimpleConnector.connectByUSB();
+	    }
+		
+		if(connectionStatus){
+			System.out.println("ROS Node connected with a NXT brick");
+		}else{
+			System.err.println("I can't connect with a NXT brick");
+			System.exit(0);
+		}
+	}
+	
+	/**
+	 * 
+	 */
+	protected void bind(){
+		if(connectionStatus){
+			System.out.println("* Bind NXT brick with ROS");
+			
+			//2. Process YAML file
+			processYAML();
+			
+			//3. Process subcriptions
+			processSubscriptions();
+			
+			//4. Update topics
+			updateTopics();
+		}else{
+			System.err.println("I can't connect with a NXT brick");
+			System.exit(0);
+		}
+	}
+	
+	/**
+	 * 
+	 */
+	protected void processYAML(){
 	    
 		System.out.println("* Processing YAML file");
 		
 		try{
 		
 			String path = ROSNodePath + YAML;
-			
 			System.out.println("YAML Path: " + path);
 			
 			//Read a YAML File
@@ -216,46 +277,89 @@ public class NXTLoader extends LEJOSNode{
         			motorList.add(sm);
 	        		
 	        	//Sensors
-	        	}else{
+	        	}else if(type.equals("battery")){
+	        		System.out.println("I found a sensor description");	        		
+	        		
+	        		port = "PORT_0";
+	        		
+	        		name = map2.get("name").toString().trim();
+
+	        		desiredFrequency = Float.parseFloat(map2.get("desired_frequency").toString().trim());
+	        		
+	        		System.out.println(name);
+	        		System.out.println(port);
+	        		System.out.println(desiredFrequency);
+
+			        BatterySensor batteryObj = new BatterySensor();
+			        batteryObj.setName(name);
+			        batteryObj.setDesiredFrequency(desiredFrequency);
+			        batteryObj.publishTopic(node);
+			        sensorList.add(batteryObj);
+	        	
+	        	}else if(type.equals("ultrasonic")){
+	        	//}else{
 	        		System.out.println("I found a sensor description");
 	        		
 	        		name = map2.get("name").toString().trim();
-	        		if(map2.get("port") != null){
-	        			port = map2.get("port").toString().trim();
-	        		}else{
-	        			port = "PORT_0";
-	        		}
+	        		port = map2.get("port").toString().trim();
 	        		desiredFrequency = Float.parseFloat(map2.get("desired_frequency").toString().trim());
 	        				        		
 	        		System.out.println(name);
 	        		System.out.println(port);
 	        		System.out.println(desiredFrequency);
 	        		
-	        		if(type.equals("battery")){
-
-				        BatterySensor batteryObj = new BatterySensor();
-				        batteryObj.setName(name);
-				        batteryObj.setDesiredFrequency(desiredFrequency);
-				        batteryObj.publishTopic(node);
-				        sensorList.add(batteryObj);
+        			//String frame_id = map2.get("frame_id").toString().trim();
+        			//Float range_max = Float.parseFloat(map2.get("range_max").toString().trim());
+        			//Float range_mix = Float.parseFloat(map2.get("range_min").toString().trim());
+        			
+        			//System.out.println(frame_id);
+        			//System.out.println(range_max);
+        			//System.out.println(range_mix);
+        			
+        			UltrasonicSensor us = new UltrasonicSensor(port);
+        			us.setName(name);
+        			us.setDesiredFrequency(desiredFrequency);
+        			us.publishTopic(node);
+        			sensorList.add(us);		        			
+	        	
+	        	}else if(type.equals("differential_actuator_system")){
+        		
+	        		System.out.println("I found a differential actuator system");
 	        		
-	        		}else if(type.equals("ultrasonic")){
-	        			
-	        			//String frame_id = map2.get("frame_id").toString().trim();
-	        			//Float range_max = Float.parseFloat(map2.get("range_max").toString().trim());
-	        			//Float range_mix = Float.parseFloat(map2.get("range_min").toString().trim());
-	        			
-	        			//System.out.println(frame_id);
-	        			//System.out.println(range_max);
-	        			//System.out.println(range_mix);
-	        			
-	        			UltrasonicSensor us = new UltrasonicSensor(port);
-	        			us.setName(name);
-	        			us.setDesiredFrequency(desiredFrequency);
-	        			us.publishTopic(node);
-	        			sensorList.add(us);		        			
-	        			
+	        		//Business exception
+	        		if(!DIFFERENTIAL_NAVIGATION_FEATURES){
+	        			String message = "Differential pilot detected in YAML. Navigation features not enabled.";
+	        			throw new YAMLException(message);
 	        		}
+	        		
+	        		name = map2.get("name").toString().trim();
+	        		port = map2.get("port").toString().trim();
+	        		desiredFrequency = Float.parseFloat(map2.get("desired_frequency").toString().trim());
+	        				        		
+	        		System.out.println(name);
+	        		System.out.println(port);
+	        		System.out.println(desiredFrequency);
+	        		
+	        		final String pattern = "PORT_";
+	        		String motorPortLetter1 = port.substring(pattern.length(), port.length()-1);
+	        		String motorPortLetter2 = port.substring(pattern.length()+1, port.length());
+	        				
+	        		//System.out.println(motorPortLetter1);
+	        		//System.out.println(motorPortLetter2);
+	        		
+	        		DifferentialActuatorSystem df = new DifferentialActuatorSystem(motorPortLetter1, motorPortLetter2,WHEEL_DIAMETER, TRACK_WIDTH, REVERSE);
+        			df.setName(name);
+        			df.setDesiredFrequency(desiredFrequency);
+	        		df.publishTopic(node);
+	        		actuatorSystemsList.add(df);	
+	        		
+	        	}else{
+	        		System.out.println("I found a rare device");
+	        		
+	        		name = map2.get("name").toString().trim();
+	        		System.out.println(type);
+	        		System.out.println(name);
+	        		
 	        	}
 	        	
 	        	i++;
@@ -278,41 +382,62 @@ public class NXTLoader extends LEJOSNode{
 	/**
 	 * 
 	 */
-	private void processSubscriptions(){
+	protected void processSubscriptions(){
 		
     	System.out.println("* Enabling subscriptions");
     	
-		//Subscription to joint_command
-        Subscriber<org.ros.message.nxt_msgs.JointCommand> subscriberMotorA =
-	        node.newSubscriber("joint_command", "nxt_msgs/JointCommand");
-        subscriberMotorA.addMessageListener(new MessageListener<org.ros.message.nxt_msgs.JointCommand>() {
-	    	@Override
-	    	public void onNewMessage(org.ros.message.nxt_msgs.JointCommand message) {
-	    		
-	    		String name = message.name;
-	    		//System.err.println(message.name);
+    	if(motorList.size()>0){
 
-				//Actuators
-				for (NXTDevice device : motorList){
-		        	if(device instanceof org.lejos.ros.nxt.actuators.NXTServoMotor){
-		        		NXTServoMotor motor = (org.lejos.ros.nxt.actuators.NXTServoMotor) device;
-		        		//motor.updateTopic();
-		        		if(motor.getName().equals(name)){
-		    	    		node.getLog().info("State: \"" + message.name + " " + message.effort + "\"");
-		    	    		
-		    	    		motor.updateJoint(message.effort);
-		        		}
-		        	}
-				}	    		
-	    	}
-	    });
+    		//Subscription to joint_command
+            Subscriber<org.ros.message.nxt_msgs.JointCommand> subscriberMotorA =
+    	        node.newSubscriber("joint_command", "nxt_msgs/JointCommand");
+            subscriberMotorA.addMessageListener(new MessageListener<org.ros.message.nxt_msgs.JointCommand>() {
+    	    	@Override
+    	    	public void onNewMessage(org.ros.message.nxt_msgs.JointCommand message) {
+    	    		
+    	    		String name = message.name;
+    	    		//System.err.println(message.name);
+
+    				//Actuators
+    				for (NXTDevice device : motorList){
+    		        	if(device instanceof org.lejos.ros.nxt.actuators.NXTServoMotor){
+    		        		NXTServoMotor motor = (org.lejos.ros.nxt.actuators.NXTServoMotor) device;
+    		        		//motor.updateTopic();
+    		        		if(motor.getName().equals(name)){
+    		    	    		node.getLog().info("State: \"" + message.name + " " + message.effort + "\"");
+    		    	    		
+    		    	    		motor.updateJoint(message.effort);
+    		        		}
+    		        	}
+    				}	    		
+    	    	}
+    	    });
+    	}
+    	
+    	//TODO: Datatype must change soon
+    	if(actuatorSystemsList.size() > 0){
+    		
+    		//Subscription to joint_command
+            Subscriber<org.ros.message.std_msgs.String> subscriberDifferentialActuatorSystem =
+    	        node.newSubscriber("das_command", "std_msgs/String");
+            subscriberDifferentialActuatorSystem.addMessageListener(new MessageListener<org.ros.message.std_msgs.String>() {
+    	    	@Override
+    	    	public void onNewMessage(org.ros.message.std_msgs.String message) {
+    	    		
+    	    		String cmd = message.data;
+    	    		System.err.println(cmd);
+	    		
+    	    	}
+    	    });
+    		
+    	}
 
 	}
 	
 	/**
 	 * 
 	 */
-	private void updateTopics(){
+	protected void updateTopics(){
 
 		System.out.println("* Updating Topics");
 		
@@ -320,13 +445,18 @@ public class NXTLoader extends LEJOSNode{
         int seq = 0;
 		while(true){
         
-			//Actuators
-			for (NXTDevice device : motorList){
-	        	if(device instanceof org.lejos.ros.nxt.actuators.NXTServoMotor){
-	        		NXTServoMotor motor = (org.lejos.ros.nxt.actuators.NXTServoMotor) device;
-	        		motor.updateTopic();
-	        	}
+			if(motorList.size() > 0){
+
+				//Actuators
+				for (NXTDevice device : motorList){
+		        	if(device instanceof org.lejos.ros.nxt.actuators.NXTServoMotor){
+		        		NXTServoMotor motor = (org.lejos.ros.nxt.actuators.NXTServoMotor) device;
+		        		motor.updateTopic();
+		        	}
+				}
+				
 			}
+
 			
 			//Sensors
 	        for (NXTDevice device : sensorList){
@@ -341,6 +471,14 @@ public class NXTLoader extends LEJOSNode{
 	        	}
 	        }
 
+	        //Actuator Systems
+	        for (NXTDevice device : actuatorSystemsList){
+
+        		DifferentialActuatorSystem das = (org.lejos.ros.nxt.systems.DifferentialActuatorSystem) device;
+        		das.updateTopic();
+	       	
+	        }
+	        
 			seq++;
 		}
 	}
