16ea1d90cd15eb0a542742d1ea468be4ebe19d16
skoehler <skoehler@06c8f799-c779-4783-8af7-179afedb67c8>
2012-02-06 15:37:01 +0000

    mc
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@6327 06c8f799-c779-4783-8af7-179afedb67c8

--- classes/src/lejos/addon/gps/RMCSentence.java
+++ classes/src/lejos/addon/gps/RMCSentence.java
@@ -124,6 +124,7 @@ public class RMCSentence extends NMEASentence{
 	 */
 	public void parse (String sentence){
 		
+		//TODO StringTokenizer must not be sued to parse NMEA sentences since it doesn't return empty tokens 
 		st = new StringTokenizer(sentence,",");
 	
 		try{
