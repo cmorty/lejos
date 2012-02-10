74a2aa25998a94abf152a5e2ede5dd5187e8f1e5
skoehler <skoehler@06c8f799-c779-4783-8af7-179afedb67c8>
2012-02-10 23:24:33 +0000

    added use defaults checkbox
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@6362 06c8f799-c779-4783-8af7-179afedb67c8

--- org.lejos.nxt.ldt/src/main/java/org/lejos/nxt/ldt/launch/LaunchConstants.java
+++ org.lejos.nxt.ldt/src/main/java/org/lejos/nxt/ldt/launch/LaunchConstants.java
@@ -7,18 +7,20 @@ public class LaunchConstants
 {
 	public static final String PREFIX = LeJOSPlugin.ID+".";
 	
+	public static final String SUFFIX_USE_DEFAULT = ".useDefaults";
 	public static final String SUFFIX_LINK_VERBOSE = ".linkVerbose";
 	public static final String SUFFIX_RUN_AFTER_UPLOAD = ".runAfterDownload";
 	public static final String SUFFIX_START_CONSOLE = ".startConsole";
 	public static final String SUFFIX_MONITOR_TYPE = ".monitorType";
-
-	public static final String KEY_NORMAL_LINK_VERBOSE = PREFIX + ILaunchManager.RUN_MODE+SUFFIX_LINK_VERBOSE;
+	
+	public static final String KEY_NORMAL_USE_DEFAULTS = PREFIX+ILaunchManager.RUN_MODE+SUFFIX_USE_DEFAULT;
+	public static final String KEY_NORMAL_LINK_VERBOSE = PREFIX+ILaunchManager.RUN_MODE+SUFFIX_LINK_VERBOSE;
 	public static final String KEY_NORMAL_RUN_AFTER_UPLOAD = PREFIX+ILaunchManager.RUN_MODE+SUFFIX_RUN_AFTER_UPLOAD;
 	public static final String KEY_NORMAL_START_CONSOLE = PREFIX+ILaunchManager.RUN_MODE+SUFFIX_START_CONSOLE;
 	
+	public static final String KEY_DEBUG_USE_DEFAULTS = PREFIX+ILaunchManager.DEBUG_MODE+SUFFIX_USE_DEFAULT;
 	public static final String KEY_DEBUG_LINK_VERBOSE = PREFIX+ILaunchManager.DEBUG_MODE+SUFFIX_LINK_VERBOSE;
 	public static final String KEY_DEBUG_RUN_AFTER_UPLOAD = PREFIX+ILaunchManager.DEBUG_MODE+SUFFIX_RUN_AFTER_UPLOAD;
 	public static final String KEY_DEBUG_START_CONSOLE = PREFIX+ILaunchManager.DEBUG_MODE+SUFFIX_START_CONSOLE;
 	public static final String KEY_DEBUG_MONITOR_TYPE = PREFIX+ILaunchManager.DEBUG_MODE+SUFFIX_MONITOR_TYPE;
-	
 }
