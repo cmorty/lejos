a6c28363587f32cb246214a2d520c9771bc0943b
skoehler <skoehler@06c8f799-c779-4783-8af7-179afedb67c8>
2012-03-13 00:38:45 +0000

    new snapshot
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@6458 06c8f799-c779-4783-8af7-179afedb67c8

--- snapshot/bin/nxjpc.bat
+++ snapshot/bin/nxjpc.bat
@@ -77,5 +77,5 @@ if "%OS%" == "Windows_NT" goto :winnt
 	)
 
 
-"%JAVA%" -Dnxj.home="%NXJ_HOME%" -Xbootclasspath/a:"%NXJ_CP_PC%" %*
+"%JAVA%" -Dnxj.home="%NXJ_HOME%" "-Xbootclasspath/a:%NXJ_CP_PC%" %*
 :eof
