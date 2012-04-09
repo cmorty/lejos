feabac14cc0c304f27378480c2708ddb7dc80751
mpscholz <mpscholz@06c8f799-c779-4783-8af7-179afedb67c8>
2012-04-09 14:07:52 +0000

    Driver class for HITechnic's Barometric sensor
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@6485 06c8f799-c779-4783-8af7-179afedb67c8

---
+++ classes/src_shared/lejos/nxt/addon/BarometricHTSensor.java
@@ -0,0 +1,117 @@
+package lejos.nxt.addon;
+
+import lejos.nxt.I2CPort;
+import lejos.nxt.I2CSensor;
+
+/**
+ * This class supports the <a href="http://www.hitechnic.com">HiTechnic</a>
+ * barometric sensor.
+ * 
+ * @author Matthias Paul Scholz
+ * 
+ */
+public class BarometricHTSensor extends I2CSensor {
+
+	private static final int BAROMETRIC_TEMPERATURE = 0x42;
+	private static final int BAROMETRIC_PRESSURE = 0x44;
+	private static final int BAROMETRIC_PRESSURE_CALIBRATION = 0x46;
+	private final double INHG_TO_HPA = 2992 / 1013.25;
+
+	private final byte[] buffer = new byte[2];
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param port
+	 *            the {@link I2CPort} the sensor is connected to.
+	 */
+	public BarometricHTSensor(final I2CPort port) {
+		super(port);
+	}
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param port
+	 *            the {@link I2CPort} the sensor is connected to.
+	 * @param address
+	 *            the address
+	 */
+	public BarometricHTSensor(final I2CPort port, final int address) {
+		super(port, address, I2CPort.LEGO_MODE, TYPE_LOWSPEED);
+	}
+
+	/**
+	 * @return the barometric pressure measured by the sensor in units of 1/1000
+	 *         inches of mercury (inHg).
+	 */
+	public long getPressureImperial() {
+		long result = Long.MIN_VALUE;
+		if (0 == getData(BAROMETRIC_PRESSURE, buffer, 2)) {
+			result = ((buffer[0] & 0xff) << 8) + buffer[1];
+		}
+		return result;
+	}
+
+	/**
+	 * @return the barometric pressure measured by the sensor in units of 1/10
+	 *         hectopascals (hPa).
+	 */
+	public long getPressureMetric() {
+		long result = getPressureImperial();
+		if (Long.MIN_VALUE != result) {
+			result /= INHG_TO_HPA;
+		}
+		return result;
+	}
+
+	/**
+	 * @return the temperature measured by the sensor in units of 1/10 °C.
+	 */
+	public int getTemperature() {
+		final int result = Integer.MIN_VALUE;
+		if (0 == getData(BAROMETRIC_TEMPERATURE, buffer, 2)) {
+			return (buffer[0] << 2) | (buffer[1] & 0xFF);
+		}
+		return result;
+	}
+
+	/**
+	 * Re-calibrates the sensor.
+	 * 
+	 * @param calibrationImperial
+	 *            the recalibration value in units of 1/1000 inches of mercury
+	 *            (inHg).
+	 */
+	public void recalibrate(final int calibrationImperial) {
+		buffer[0] = (byte) ((calibrationImperial & 0xff00) >> 8);
+		buffer[1] = (byte) (calibrationImperial & 0x00ff);
+		super.sendData(BAROMETRIC_PRESSURE_CALIBRATION, buffer, 2);
+	}
+
+	/**
+	 * @return the present calibration value in units of of 1/1000 inches of
+	 *         mercury (inHg). Will be 0 in case no explicit recalibration has
+	 *         been performed.
+	 */
+	public int getCalibrationImperial() {
+		int result = Integer.MIN_VALUE;
+		if (0 == getData(BAROMETRIC_PRESSURE_CALIBRATION, buffer, 2)) {
+			result = ((buffer[0] & 0xff) << 8) + buffer[1];
+		}
+		return result;
+	}
+
+	/**
+	 * @return the present calibration value in units of of 1/10 hectopascals
+	 *         (hPa). Will be 0 in case no explicit recalibration has been
+	 *         performed.
+	 */
+	public int getCalibrationMetric() {
+		int result = getCalibrationImperial();
+		if (Integer.MIN_VALUE != result) {
+			result /= INHG_TO_HPA;
+		}
+		return result;
+	}
+}
