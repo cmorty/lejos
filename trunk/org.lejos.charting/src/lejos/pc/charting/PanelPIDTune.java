5a364b4f86f59c26c3ea64251c90ef8d4f3e5e0f
kirkpthompson <kirkpthompson@06c8f799-c779-4783-8af7-179afedb67c8>
2012-04-23 12:10:45 +0000

    cleaned up unused imports

--- trunk/org.lejos.charting/src/lejos/pc/charting/PanelPIDTune.java
+++ trunk/org.lejos.charting/src/lejos/pc/charting/PanelPIDTune.java
@@ -1,6 +1,8 @@
 package lejos.pc.charting;
 
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.text.NumberFormat;
@@ -12,14 +14,11 @@ import javax.swing.JCheckBox;
 import javax.swing.JFormattedTextField;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.UIManager;
 import javax.swing.border.TitledBorder;
 
 import lejos.util.EndianTools;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 
 public class PanelPIDTune extends JPanel {
 	/**
