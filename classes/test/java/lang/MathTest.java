cbaf032d751d825519586a8d7f0aa737dc1e420f
skoehler <skoehler@06c8f799-c779-4783-8af7-179afedb67c8>
2012-05-10 23:53:19 +0000

    more fixes for min/max/floor/ceil
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@6539 06c8f799-c779-4783-8af7-179afedb67c8

--- classes/test/java/lang/MathTest.java
+++ classes/test/java/lang/MathTest.java
@@ -2,31 +2,41 @@ package java.lang;
 
 public class MathTest {
 	
-	private static final double[] FLOORCEIL_INPUT = { 
-		-2.5, -2.3, -2.0, -1.7, -1.5, -1.3, -1.0, -0.7, -0.5, -0.3, -0.0,
-		0.0, 0.3, 0.5, 0.7, 1.0, 1.3, 1.5, 1.7, 2.0, 2.3, 2.5, 
+	private static final double[] FLOORCEIL_INPUT = {
+		Double.NEGATIVE_INFINITY, -2.5, -2.3, -2.0, -1.7, -1.5, -1.3, -1.0, -0.7, -0.5, -0.3, -0.0,
+		0.0, 0.3, 0.5, 0.7, 1.0, 1.3, 1.5, 1.7, 2.0, 2.3, 2.5, Double.POSITIVE_INFINITY,
+		Double.NaN,
 	};
 	private static final double[] FLOOR_EXPECT = {
-		-3.0, -3.0, -2.0, -2.0, -2.0, -2.0, -1.0, -1.0, -1.0, -1.0, -0.0,
-		0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0, 2.0, 
+		Double.NEGATIVE_INFINITY, -3.0, -3.0, -2.0, -2.0, -2.0, -2.0, -1.0, -1.0, -1.0, -1.0, -0.0,
+		0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0, 2.0, Double.POSITIVE_INFINITY,
+		Double.NaN,
 	};
 	private static final double[] CEIL_EXPECT = {
-		-2.0, -2.0, -2.0, -1.0, -1.0, -1.0, -1.0, -0.0, -0.0, -0.0, -0.0,
-		0.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0, 2.0, 2.0, 3.0, 3.0, 
+		Double.NEGATIVE_INFINITY, -2.0, -2.0, -2.0, -1.0, -1.0, -1.0, -1.0, -0.0, -0.0, -0.0, -0.0,
+		0.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0, 2.0, 2.0, 3.0, 3.0, Double.POSITIVE_INFINITY,
+		Double.NaN,
 	};
 	
+	private static final double[] ORDER = {
+		Double.NEGATIVE_INFINITY, -2.5, -2.3, -2.0, -1.7, -1.5, -1.3, -1.0, -0.7, -0.5, -0.3, -0.0,
+		0.0, 0.3, 0.5, 0.7, 1.0, 1.3, 1.5, 1.7, 2.0, 2.3, 2.5, Double.POSITIVE_INFINITY,
+		Double.NaN,
+	};
 	
 
 	private static void assertEqualsExact(float actual, float expected) {
 		int a2 = Float.floatToIntBits(actual);
 		int e2 = Float.floatToIntBits(expected);
-		assertEquals(a2, e2);
+		if (a2 != e2)
+			throw new RuntimeException("expected "+expected+" but was "+actual);
 	}
 
 	private static void assertEqualsExact(double actual, double expected) {
 		long a2 = Double.doubleToLongBits(actual);
 		long e2 = Double.doubleToLongBits(expected);
-		assertEquals(a2, e2);
+		if (a2 != e2)
+			throw new RuntimeException("expected "+expected+" but was "+actual);
 	}
 
 
@@ -45,6 +55,50 @@ public class MathTest {
 			assertEqualsExact(Math.ceil(FLOORCEIL_INPUT[i]), CEIL_EXPECT[i]);
 	}
 
+	private void testCompare() {
+		assertEquals(Double.compare(ORDER[0], ORDER[0]), 0);
+		for (int i=1; i<ORDER.length; i++)
+		{
+			assertEquals(Double.compare(ORDER[i-1], ORDER[i]), -1);
+			assertEquals(Double.compare(ORDER[i], ORDER[i-1]), 1);
+			assertEquals(Double.compare(ORDER[i], ORDER[i]), 0);
+		}
+	}
+
+	private void testMin() {
+		for (int i=0; i<ORDER.length-1; i++)
+		{
+			assertEqualsExact(Math.min(Float.NaN, (float)ORDER[i]), Float.NaN);
+			assertEqualsExact(Math.min((float)ORDER[i], Float.NaN), Float.NaN);
+			assertEqualsExact(Math.min(Double.NaN, ORDER[i]), Double.NaN);
+			assertEqualsExact(Math.min(ORDER[i], Double.NaN), Double.NaN);
+		}
+		for (int i=1; i<ORDER.length-1; i++)
+		{
+			assertEqualsExact(Math.min((float)ORDER[i-1], (float)ORDER[i]), (float)ORDER[i-1]);
+			assertEqualsExact(Math.min((float)ORDER[i], (float)ORDER[i-1]), (float)ORDER[i-1]);
+			assertEqualsExact(Math.min(ORDER[i-1], ORDER[i]), ORDER[i-1]);
+			assertEqualsExact(Math.min(ORDER[i], ORDER[i-1]), ORDER[i-1]);
+		}
+	}
+
+	private void testMax() {
+		for (int i=0; i<ORDER.length-1; i++)
+		{
+			assertEqualsExact(Math.max(Float.NaN, (float)ORDER[i]), Float.NaN);
+			assertEqualsExact(Math.max((float)ORDER[i], Float.NaN), Float.NaN);
+			assertEqualsExact(Math.max(Double.NaN, ORDER[i]), Double.NaN);
+			assertEqualsExact(Math.max(ORDER[i], Double.NaN), Double.NaN);
+		}
+		for (int i=1; i<ORDER.length-1; i++)
+		{
+			assertEqualsExact(Math.max((float)ORDER[i-1], (float)ORDER[i]), (float)ORDER[i]);
+			assertEqualsExact(Math.max((float)ORDER[i], (float)ORDER[i-1]), (float)ORDER[i]);
+			assertEqualsExact(Math.max(ORDER[i-1], ORDER[i]), ORDER[i]);
+			assertEqualsExact(Math.max(ORDER[i], ORDER[i-1]), ORDER[i]);
+		}
+	}
+
 
 	/**
 	 * @param args
@@ -53,6 +107,9 @@ public class MathTest {
 		MathTest t = new MathTest();
 		t.testFloor();
 		t.testCeil();
+		t.testCompare();
+		t.testMin();
+		t.testMax();
 	}
 
 
