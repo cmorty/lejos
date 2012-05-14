6b4011de71276d2ed279c69b70eaf1458df02cf9
markcrosbie <markcrosbie@06c8f799-c779-4783-8af7-179afedb67c8>
2012-05-14 14:05:25 +0000

    Initial version of test programs for Dexter Industries NXTBee driver
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@6561 06c8f799-c779-4783-8af7-179afedb67c8

---
+++ samples/src/org/lejos/sample/NXTBeeReceive/NXTBeeReceive.java
@@ -0,0 +1,75 @@
+package org.lejos.sample.NXTBeeReceive;
+
+
+import lejos.nxt.*;
+
+import java.io.*;
+
+import lejos.nxt.addon.*;
+import lejos.nxt.comm.RConsole;
+import lejos.util.Delay;
+
+/**
+ * Receive data from another NXT/PC using a Dexter Industries NXTBee on port 4.
+ * 
+ * @author mcrosbie
+ *
+ */
+public class NXTBeeReceive {
+
+	static NXTBee nb;
+	
+	/**
+	 * Test sending data to the NXTBee for transmission to a remote host connected to another NXTBee
+	 * or with an XStick/Sparkfun board attached.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		RConsole.openUSB(3000);
+		
+		// My NXTBee runs at 115200 baud, and I want the internal data reading thread active
+		//nb = new NXTBee();
+		nb = new NXTBee(115200, true, true);
+				
+		Thread t = new Thread(nb);
+		t.setDaemon(true);
+		t.start();
+		
+		InputStream is = nb.getInputStream();		
+		DataInputStream dis = new DataInputStream(is);
+
+		byte[] b = new byte[20];
+		
+		Delay.msDelay(1000);
+
+		LCD.clear();
+		LCD.drawString("NXTBee Recv", 0, 0);
+		
+
+		try {
+			while(Button.ENTER.isUp()){
+				
+				if(dis.available() > 0) {
+					int bytesRead = dis.read(b);
+					
+					LCD.drawString("Read " + bytesRead + " bytes",0, 3);
+					String s = new String(b);
+					LCD.drawString(s, 0, 5);
+					
+					RConsole.println("Read " + bytesRead + " bytes");
+					RConsole.println(s);
+				}
+				Delay.msDelay(1000);
+			}
+		} catch(Exception e) {
+			RConsole.println("***** EXCEPTION " + e);
+		}
+						
+		RConsole.close();
+	}
+
+}
+
---
+++ samples/src/org/lejos/sample/NXTBeeSend/NXTBeeSend.java
@@ -0,0 +1,77 @@
+package org.lejos.sample.NXTBeeSend;
+
+
+import lejos.nxt.*;
+
+import java.io.*;
+
+import lejos.nxt.addon.*;
+import lejos.nxt.comm.RConsole;
+import lejos.util.Delay;
+
+/**
+ * Send data from the NXT to a receiver, be it a PC or another NXT, using the Dexter Industries NXTBee. 
+ * The NXTBee must be connected on port 4.
+ * 
+ * Uses the NXTBee class.
+ * 
+ * @author Mark Crosbie, mark@mastincrosbie.com
+ *
+ */
+
+public class NXTBeeSend {
+
+	static NXTBee nb;
+	
+	/**
+	 * Test sending data to the NXTBee for transmission to a remote host connected to another NXTBee
+	 * or with an XStick/Sparkfun board attached.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Remove this if you do not want debug output
+		RConsole.openUSB(3000);
+		
+		// My NXTBee runs at 115200 baud, and I want the internal data reading thread active
+		//nb = new NXTBee(); // default 9600 baud
+		nb = new NXTBee(115200);
+		
+		int len;
+		byte[] b = {'H', 'e', 'l', 'l', 'o', '!', 0};
+		
+		Thread t = new Thread(nb);
+		t.setDaemon(true);
+		t.start();
+		
+		OutputStream os = nb.getOutputStream();
+		DataOutputStream dos = new DataOutputStream(os);
+		
+		LCD.drawString("NXTBeeSend", 0, 1);
+		
+		try {
+			int i = 0;
+			while(Button.ENTER.isUp()){
+				
+				dos.writeBytes("Hello " + i + "\n");
+				
+				// Change to test sending other values and types
+				//dos.writeInt(i);
+				//dos.writeByte(i);
+				//dos.write(b);
+				i++;
+				
+				LCD.drawString("Iter: " + i, 0, 2);
+				
+				Delay.msDelay(500);
+			}
+		} catch(Exception e) {
+		}
+	
+		RConsole.close();
+	}
+
+}
+
