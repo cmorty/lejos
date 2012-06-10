a24560d0a1a8b51e29d98febb0857c6ac498c18a
kirkpthompson <kirkpthompson@06c8f799-c779-4783-8af7-179afedb67c8>
2012-06-10 23:35:59 +0000

    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@6635 06c8f799-c779-4783-8af7-179afedb67c8

---
+++ leJOSlab/SensorFramework/src/lejos/nxt/sensor/KPTtesting.java
@@ -0,0 +1,43 @@
+package lejos.nxt.sensor;
+
+public class KPTtesting implements SensorValueProvider{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getRefreshRate() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float getValue() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getRawValue() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public byte[] getRaw() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int[] getUnits() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
