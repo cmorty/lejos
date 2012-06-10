394ef3a5b5f86a25c5fcf9109029357058c62445
kirkpthompson <kirkpthompson@06c8f799-c779-4783-8af7-179afedb67c8>
2012-06-10 23:21:22 +0000

    intial commit for sensor framework project
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@6634 06c8f799-c779-4783-8af7-179afedb67c8

---
+++ leJOSlab/SensorFramework/src/lejos/nxt/sensor/SensorSIUnits.java
@@ -0,0 +1,37 @@
+package lejos.nxt.sensor;
+
+/**
+ * Base SI units to be returned by 
+ * @author Kirk
+ *
+ */
+public interface SensorSIUnits {
+	/**
+	 * metre
+	 */
+	public static final int LENGTH = 0;
+	/**
+	 * kilogram
+	 */
+	public static final int MASS = 1;
+	/**
+	 * second
+	 */
+	public static final int TIME = 2;
+	/**
+	 * ampere
+	 */
+	public static final int ELECTRIC_CURRENT = 3;
+	/**
+	 * kelvin
+	 */
+	public static final int TEMPERATURE = 4;
+	/**
+	 * candela
+	 */
+	public static final int LUMINOUS_INTENSITY = 5;
+	/**
+	 * mole
+	 */
+	public static final int AMOUNT_OF_SUBSTANCE = 6;
+}
