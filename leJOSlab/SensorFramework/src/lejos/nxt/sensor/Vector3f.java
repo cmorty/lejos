240203dae11769f8c898c180ed75c03a03a91db6
aswin0 <aswin0@06c8f799-c779-4783-8af7-179afedb67c8>
2012-06-14 12:30:12 +0000

    Initial commit
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@6642 06c8f799-c779-4783-8af7-179afedb67c8

---
+++ leJOSlab/SensorFramework/src/lejos/nxt/sensor/Vector3f.java
@@ -0,0 +1,11 @@
+package lejos.nxt.sensor;
+
+/**
+ * A 3-element vector that is represented by single-precision floating point 
+ * x,y,z coordinates.  If this value represents a normal, then it should
+ * be normalized.
+ *
+ */
+public class Vector3f {
+	float x=0,y=0,z=0;
+}
