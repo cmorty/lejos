58cb5db0d21086a7706e04741d33b0b28ce54366
kirkpthompson <kirkpthompson@06c8f799-c779-4783-8af7-179afedb67c8>
2012-06-22 13:57:39 +0000

    POC#2 for adapter

---
+++ trunk/leJOSlab/SensorFramework/src/lejos/nxt/sensor/LightSensorAdapterPOC2.java
@@ -0,0 +1,29 @@
+package lejos.nxt.sensor;
+
+import lejos.nxt.ADSensorPort;
+
+/**
+ * Adapter POC class lejos.nxt.LightSensor-to-SensorDataProvider 
+ * 
+ * @author Kirk
+ *
+ */
+public class LightSensorAdapterPOC2 extends lejos.nxt.LightSensor implements SensorDataProvider{
+	
+	/**
+	 * create instance of LightSensorAdapterPOC2.
+	 * @param port
+	 */
+	public LightSensorAdapterPOC2(ADSensorPort port){
+		super(port);
+	}
+	
+	public int getMinimumFetchInterval() {
+		return 4;
+	}
+
+	public float fetchData() {
+		return this.getLightValue();
+	}
+	
+}
