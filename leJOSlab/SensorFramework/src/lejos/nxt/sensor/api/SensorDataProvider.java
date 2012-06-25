d11155620783341afa86787e58ac326e8e34f777
aswin0 <aswin0@06c8f799-c779-4783-8af7-179afedb67c8>
2012-06-25 15:44:03 +0000

    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@6692 06c8f799-c779-4783-8af7-179afedb67c8

---
+++ leJOSlab/SensorFramework/src/lejos/nxt/sensor/api/SensorDataProvider.java
@@ -0,0 +1,25 @@
+package lejos.nxt.sensor.api;
+
+/**
+ * @author Aswin Bouwmeester
+ * @author Sven Kohler
+ * @author Kirk P. Thompson
+ *
+ */
+public interface SensorDataProvider {
+
+	/**
+	 * The refresh rate is the minumum delay between calls to query the sensor value. 
+	 * Should probbaly be at least 4 ms for analog due to AVR constraints. I2C=?
+	 * 
+	 * @return the minumum delay in milliseconds
+	 */
+	int getMinimumFetchInterval();
+	
+	/**
+	 * The value is what the sensor reads and returns
+	 * @return the unit quantity read by the sensor
+	 */
+	float fetchData();
+	
+}
---
+++ leJOSlab/SensorFramework/src/lejos/nxt/sensor/api/SensorVectorDataProvider.java
@@ -0,0 +1,27 @@
+package lejos.nxt.sensor.api;
+
+import lejos.nxt.vecmath.Vector3f;
+
+
+
+/**
+ * @author Aswin Bouwmeester
+ */
+public interface SensorVectorDataProvider {
+	
+
+	/**
+		 * The refresh rate is the minumum delay between calls to query the sensor value. 
+		 * Should probbaly be at least 4 ms for analog due to AVR constraints. I2C=?
+		 * 
+		 * @return the minumum delay in milliseconds
+		 */
+		int getMinimumFetchInterval();
+		
+		/**
+		 * The value is what the sensor reads and returns
+		 * @return the unit quantity read by the sensor
+		 */
+		void fetchData(Vector3f data);
+
+}
