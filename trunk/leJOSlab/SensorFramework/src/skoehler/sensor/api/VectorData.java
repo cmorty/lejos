7d45114ce3e101da3ca7153dbf18fceb15ba8a77
skoehler <skoehler@06c8f799-c779-4783-8af7-179afedb67c8>
2012-06-25 21:34:08 +0000

    mc

--- trunk/leJOSlab/SensorFramework/src/skoehler/sensor/api/VectorData.java
+++ trunk/leJOSlab/SensorFramework/src/skoehler/sensor/api/VectorData.java
@@ -9,8 +9,16 @@ import skoehler.sensor.filter.AveragingFilter;
 public interface VectorData {
 	int getQuantity();
 	int getAxisCount();
+	
 	/**
-	 * Intentially, this does not use Vector3f. See {@link AveragingFilter} for an example why.
+	 * Intentionally, this does not use Vector3f. See {@link AveragingFilter} for an example why.
+	 * Samples are returned in the default unit of the quantity returned by {@link #getQuantity()}.
+	 * Take a look {@link Quantities} to see which unit is the default unit.
+	 * This method writes as many samples as this sensor has axes (see {@link #getAxisCount()})
+	 * to the given destination array.
+	 * 
+	 * @param dst destination array 
+	 * @param off index of first value 
 	 */
 	void fetchSamples(float[] dst, int off);
 }
