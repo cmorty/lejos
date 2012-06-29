4983eb77c9c453f9c6f95a30d946e0740556961e
skoehler <skoehler@06c8f799-c779-4783-8af7-179afedb67c8>
2012-06-29 14:05:18 +0000

    mc

--- trunk/classes/src/lejos/robotics/kinematics/PointUnreachableException.java
+++ trunk/classes/src/lejos/robotics/kinematics/PointUnreachableException.java
@@ -1,5 +1,20 @@
 package lejos.robotics.kinematics;
 
 public class PointUnreachableException extends Exception {
-	// Inherits everything from Exception
+
+    public PointUnreachableException() {
+        super();
+    }
+
+    public PointUnreachableException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public PointUnreachableException(String message) {
+        super(message);
+    }
+
+    public PointUnreachableException(Throwable cause) {
+        super(cause);
+    }
 }
