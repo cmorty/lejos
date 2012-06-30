84cb26fb182b08127e7b4c7262b601510379b409
aswin0 <aswin0@06c8f799-c779-4783-8af7-179afedb67c8>
2012-06-30 22:32:33 +0000

    Added addRotation method that allows to specify sensor alignment as a series of subsequent rotations

--- trunk/leJOSlab/SensorFramework/src/lejos/nxt/sensor/filter/Align.java
+++ trunk/leJOSlab/SensorFramework/src/lejos/nxt/sensor/filter/Align.java
@@ -8,7 +8,9 @@ import lejos.nxt.sensor.api.*;
 /**
  * This filter rotates a point in space around the origin.
  * Used to translate from one (sensor) system of axis, to another (robot) system of axis.
- * Helpful for misaligned sensors or sensors that can rotate.
+ * Helpful for misaligned sensors or sensors that can rotate. <br>
+ * 
+ * Class still needs proper testing and debugging
  * @author Aswin
  *
  */
@@ -46,15 +48,83 @@ public class Align implements SensorVectorDataProvider {
   	double phi=Math.toRadians(aroundY);
   	double psi=Math.toRadians(aroundZ);
 
-  	rotateAxis.m00=(float) (Math.cos(theta)*Math.cos(psi));
-  	rotateAxis.m01=(float) (Math.sin(phi)*Math.sin(theta)*Math.cos(psi)-Math.cos(phi)*Math.sin(psi));
-  	rotateAxis.m02=(float) (Math.cos(phi)*Math.sin(theta)*Math.cos(psi)+Math.sin(phi)*Math.sin(psi));
-  	rotateAxis.m10=(float) (Math.cos(theta)*Math.sin(psi));
-  	rotateAxis.m11=(float) (Math.sin(phi)*Math.sin(theta)*Math.cos(psi)+Math.cos(phi)*Math.cos(psi));
-  	rotateAxis.m12=(float) (Math.cos(phi)*Math.sin(theta)*Math.sin(psi)+Math.sin(phi)*Math.cos(psi));
-  	rotateAxis.m20=(float) (-Math.sin(theta));
-  	rotateAxis.m21=(float) (Math.sin(phi)*Math.cos(theta));
-  	rotateAxis.m22=(float) (Math.cos(phi)*Math.cos(theta));
+  	rotateAxis.m00= (cos(theta)*cos(psi));
+  	rotateAxis.m01= (sin(phi)*sin(theta)*cos(psi)-cos(phi)*sin(psi));
+  	rotateAxis.m02= (cos(phi)*sin(theta)*cos(psi)+sin(phi)*sin(psi));
+  	rotateAxis.m10= (cos(theta)*sin(psi));
+  	rotateAxis.m11= (sin(phi)*sin(theta)*cos(psi)+cos(phi)*cos(psi));
+  	rotateAxis.m12= (cos(phi)*sin(theta)*sin(psi)+sin(phi)*cos(psi));
+  	rotateAxis.m20= (-sin(theta));
+  	rotateAxis.m21= (sin(phi)*cos(theta));
+  	rotateAxis.m22= (cos(phi)*cos(theta));
   }
+	
+	/**
+	 * Adds a rotation the the rotation matrix. <p>
+	 * The alignment of a sensor can be regarded as a series of rotations of the sensor where the sensor starts of aligned with the robot.
+	 * Each rotation is a rotation around one of the sensor axis. The position of a sensor that points backwards in an upward angle of 45 degrees for example
+	 * can be described with two subsequent rotations. The first rotation is 180 degrees around the Z-axis, the second rotation is 45 degrees around the Y-axis. <br>
+	 * Please note that the order of rotations does matter. Also note that this class uses the right hand system for rotations.
+	 * @param axis
+	 * The axis that the sensor has rotated around: X, Y or Z.
+	 * @param angle
+	 * The angle of rotation expressed in degrees. A positive angle means a counter clockwise rotation.
+	 * 
+	 */
+	public void addRotation(String axis, int angle) {
+		axis = axis.toUpperCase();
+		if ("XYZ".indexOf(axis) == -1)
+			throw new IllegalArgumentException("Invalid axis");
+		if (axis.equals("X")) addXRotation(Math.toRadians(angle));
+		if (axis.equals("Y")) addYRotation(Math.toRadians(angle));
+		if (axis.equals("Z")) addZRotation(Math.toRadians(angle));
+	}
+	
+	/**
+	 * Resets the sensor to be aligned with the robot.
+	 */
+	public void reset() {
+		rotateAxis=new Matrix3f(1,0,0,0,1,0,0,0,1);	
+	}
+		
+	private void addXRotation(double a) {
+		Matrix3f r=new Matrix3f(1,0,0,0,cos(a),-sin(a),0,sin(a),cos(a));
+		r.mul(rotateAxis);
+		rotateAxis=r;
+	}
+	
+	private void addYRotation(double a) {
+		Matrix3f r=new Matrix3f(cos(a),0,sin(a),0,1,0,-sin(a),0,cos(a));
+		r.mul(rotateAxis);
+		rotateAxis=r;
+	}
+
+	private void addZRotation(double a) {
+		Matrix3f r=new Matrix3f(cos(a),-sin(a),0,sin(a),cos(a),0,0,0,1);
+		r.mul(rotateAxis);
+		rotateAxis=r;
+	}
+	
+	
+	/**
+	 * Returns sinus, Just to get more readable code
+	 * @param angle (in radians)
+	 * @return
+	 * sinus of angle
+	 */
+	private float sin(double angle) {
+		return (float)Math.sin(angle);
+	}
+	
+	/**
+	 * Returns cosinus, Just to get more readable code
+	 * @param angle (in radians)
+	 * @return
+	 * cosinus of angle
+	 */
+	private float cos(double angle) {
+		return (float)Math.sin(angle);
+	}
+	
 
 }
