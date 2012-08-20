582586a71e3a6855f64180b1a126da8144321d62
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2012-08-20 10:09:26 +0000

    JNI project for the Raspberry Pi

---
+++ trunk/rpi/rpijni/sensorport.c
@@ -0,0 +1,39 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#include "lejos_nxt_SensorPort.h"
+
+JNIEXPORT void JNICALL Java_lejos_nxt_SensorPort_i2cEnableById
+  (JNIEnv *env, jclass cls, jint id, jint mode)
+{
+  printf("Enabling port %d in mode %d/n",id,mode);
+}
+
+JNIEXPORT void JNICALL Java_lejos_nxt_SensorPort_i2cDisableById
+  (JNIEnv *env, jclass cls, jint id) 
+{
+  printf("Disabling port %d\n",id);
+}
+
+JNIEXPORT jint JNICALL Java_lejos_nxt_SensorPort_i2cStartById
+  (JNIEnv *env, jclass cls, jint id, jint addr, jbyteArray buf, jint offset, jint wlen, jint rlen)
+{
+  printf("Writing %d bytes on port %d at address %d\n",wlen,id,addr);
+  return wlen;
+}
+
+JNIEXPORT jint JNICALL Java_lejos_nxt_SensorPort_i2cCompleteById
+  (JNIEnv *env, jclass cls, jint id, jbyteArray buf, jint offset, jint rlen) 
+{
+  printf("Reading %d bytes from port %d\n",rlen,id);
+  return rlen;
+}
+
+JNIEXPORT jint JNICALL Java_lejos_nxt_SensorPort_i2cStatusById
+  (JNIEnv *, jclass, jint)
+{
+  return 0;
+}
