baa838d78a4e31a4a6b5c53a47885854458ecb2c
lgriffiths <lgriffiths@06c8f799-c779-4783-8af7-179afedb67c8>
2012-08-21 08:45:27 +0000

    Removed unused class
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@6795 06c8f799-c779-4783-8af7-179afedb67c8

--- rpi/rpiclasses/src/lejos/nxt/RPiI2c.java
+++
@@ -1,8 +0,0 @@
-package lejos.nxt;
-
-public class RPiI2c {
-	public native int open(int address);
-	public native int read(byte[] data, int len);
-	public native int write(byte[] data, int len);
-	public native int close();
-}
