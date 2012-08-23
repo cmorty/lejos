ebd91192fa6d340f9a306434999b7dbfc04dc1df
aswin0 <aswin0@06c8f799-c779-4783-8af7-179afedb67c8>
2012-08-23 12:15:44 +0000

    Optimized statistics filters + abstract buffer for statistics and delay filters

---
+++ trunk/leJOSlab/SensorFramework/src/lejos/nxt/sensor/filter/MinimumFilter.java
@@ -0,0 +1,35 @@
+package lejos.nxt.sensor.filter;
+
+import lejos.nxt.sensor.api.SampleProvider;
+
+public class MinimumFilter extends SampleBuffer {
+
+	float[]	min;
+	float[]	buf, oldest;
+
+	@Override
+	public void fetchSample(float[] sample, int off) {
+		getOldest(oldest, 0);
+		super.fetchSample(buf, 0);
+		for (int i = 0; i < elements; i++) {
+			// if the dropped sample happens to be the smallest sample, then rescan
+			// the buffer for a smallest value;
+			if (oldest[i] == min[i] || buf[i] < min[i]) {
+				min[i] = Float.POSITIVE_INFINITY;
+				for (int j = 0; j < actualSize; j++) {
+					min[i] = Math.min(sampleBuffer[toPos(i, j)], min[i]);
+				}
+			}
+			sample[i + off] = min[i];
+		}
+	}
+
+	public MinimumFilter(SampleProvider source, int bufferSize) {
+		super(source, bufferSize);
+		min = new float[elements];
+		buf = new float[elements];
+		oldest = new float[elements];
+
+	}
+
+}
