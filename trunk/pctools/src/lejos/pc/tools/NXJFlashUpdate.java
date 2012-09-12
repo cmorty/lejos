5af9df6c9e40a0594f911e0bc8c5edc58070143a
gloomyandy <gloomyandy@06c8f799-c779-4783-8af7-179afedb67c8>
2012-09-12 20:15:35 +0000

    Increase reserved area

--- trunk/pctools/src/lejos/pc/tools/NXJFlashUpdate.java
+++ trunk/pctools/src/lejos/pc/tools/NXJFlashUpdate.java
@@ -19,7 +19,7 @@ import lejos.pc.comm.NXTSamba;
  */
 public class NXJFlashUpdate {
 
-	private static final int MAX_FIRMWARE_PAGES = 416;
+	private static final int MAX_FIRMWARE_PAGES = 420;
 	private static final int TOTAL_PAGES = 1024;
 	private static final int SETTINGS_PAGES = 1;
 	private static final int DIRECTORY_PAGES = 2;
