a68760126f32c9ba74a80ed5d7a65ccfd91aa784
kirkpthompson <kirkpthompson@06c8f799-c779-4783-8af7-179afedb67c8>
2013-02-08 03:38:53 +0000

    check bounds of saved NCL window and move within current device context if out of bounds

--- trunk/org.lejos.charting/src/lejos/pc/charting/ChartingLogger.java
+++ trunk/org.lejos.charting/src/lejos/pc/charting/ChartingLogger.java
@@ -3,9 +3,12 @@ package lejos.pc.charting;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Frame;
+import java.awt.GraphicsConfiguration;
+import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
 import java.awt.Image;
 import java.awt.Point;
+import java.awt.Rectangle;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.awt.event.WindowAdapter;
@@ -31,18 +34,35 @@ public class ChartingLogger {
         // restore previous window state, size, location if saved
         String[] stateItems = 
         		ConfigurationManager.getConfigItem(ConfigurationManager.CONFIG_WINDOWSTATE, "none").split(",");
+        
+     // Toolkit.getDefaultToolkit().getScreenSize() doesn't work for multi monitor setups
+        Point center = GraphicsEnvironment.getLocalGraphicsEnvironment().getCenterPoint();
+        Dimension frameSize = frame.getSize();
         if (stateItems[0].equals("none") || stateItems.length<5) {
-	        Dimension frameSize = frame.getSize();
-	        // Toolkit.getDefaultToolkit().getScreenSize() doesn't work for multi monitor setups
-	        Point center = GraphicsEnvironment.getLocalGraphicsEnvironment().getCenterPoint();
+	        
 	        frame.setLocation(center.x - frameSize.width / 2, 
 	                          center.y - frameSize.height / 2);
         } else {
-        	frame.setBounds(
-        			Integer.parseInt(stateItems[0]),
-        			Integer.parseInt(stateItems[1]),
-        			Integer.parseInt(stateItems[2]),
-        			Integer.parseInt(stateItems[3]));
+        	int x= Integer.parseInt(stateItems[0]);
+        	int y= Integer.parseInt(stateItems[1]);
+        	int width= Integer.parseInt(stateItems[2]);
+        	int height= Integer.parseInt(stateItems[3]);
+        	frame.setBounds(x, y, width, height);
+        
+        	//  check bounds of saved NCL window and move within current device context if out of bounds
+        	boolean inSceenArea=false;
+        	for (GraphicsDevice gd : GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices()) {
+    		  for (GraphicsConfiguration graphicsConfiguration : gd.getConfigurations()) {
+    		    if (graphicsConfiguration.getBounds().outcode(x, y)==0 ||
+    		    		graphicsConfiguration.getBounds().outcode(x+width, y+height)==0) {
+    		    	inSceenArea = true;
+    		    }
+    		  }
+    		}
+        	if (!inSceenArea){
+		    	frame.setLocation(center.x - frameSize.width / 2, center.y - frameSize.height / 2);
+        	}
+        	// maximize if flagged
         	if (stateItems[4].equals("Max")) frame.setExtendedState(Frame.MAXIMIZED_BOTH);
         }
         frame.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
