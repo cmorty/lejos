4642968a2c0e4f97e0d981211fee704aed4c06f9
skoehler <skoehler@06c8f799-c779-4783-8af7-179afedb67c8>
2013-07-31 23:39:42 +0000

    mc

--- trunk/org.lejos.website/htdocs/index.php
+++ trunk/org.lejos.website/htdocs/index.php
@@ -102,6 +102,7 @@
 		<b>leJOS</b> on the new <b>LEGO Mindstorms EV3</b>.
 		Read all about the status of the port of leJOS to the new EV3 platform
 		on the <a href="/forum/viewtopic.php?t=4480">forums</a>.
+		Also watch the <a href="http://www.youtube.com/watch?v=luTdnyIElWU">youtube video</a>.
 	</td>
 </tr>
 <tr>
