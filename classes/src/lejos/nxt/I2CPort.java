dc7183f395bd1e189e3b45d4107bfff5505334b0
kirkpthompson <kirkpthompson@06c8f799-c779-4783-8af7-179afedb67c8>
2013-08-31 22:14:13 +0000

    Added javadoc derived from info from forum provided by Andy
    git-svn-id: http://svn.code.sf.net/p/lejos/code/trunk@6985 06c8f799-c779-4783-8af7-179afedb67c8

--- classes/src/lejos/nxt/I2CPort.java
+++ classes/src/lejos/nxt/I2CPort.java
@@ -2,7 +2,35 @@ package lejos.nxt;
 
 /**
  * Abstraction for a port that supports I2C sensors.
- * 
+ * <p>
+ * from http://lejos.sourceforge.net/forum/viewtopic.php?f=7&t=4597&p=19491#p19491<p>
+ * A little history about the rather odd Lego i2c implementation...
+ *  <ol><li>
+ * The Lego implementation is not standard i2c it has various oddities (like extra delays 
+ * and strange handling of stop and restart cases).<li>
+ * There have been several implementations of i2c in leJOS over the years, some less efficient than the current one.
+ * </ol>
+ * Anyway:<br>
+ * LEGO_MODE turns on support for pretty much all of the strange i2c features of the LEGO firmware. You probably 
+ * only need this if you are talking to the Lego Ultrasonic sensor, or some strange homebrew devices that was 
+ * built to be compatible with the Lego protocol.<br>
+ * STANDARD_MODE is the alternative to LEGO_MODE as it disables all of the Lego strange stuff and tries to be 
+ * as close to normal i2c as possible (but remember this is a bit banged implementation, not hardware).
+ * <br>The above two are mode settings. Use one or the other.
+ * <p>
+ * The remaining settings are flags that can be added to either of the above modes. 
+ * <ul><li>HIGH_SPEED enables higher speed operation (approx 125KHz). If this bit is not set then 
+ * the default speed of 9.6KHz is used.
+ * <li>ALWAYS_ACTIVE enables compatibility with an older leJOS i2c driver feature which kept the 
+ * timer interrupt running all of the time (even if no i2c operations are in progress).
+ *  This adds considerable cpu load and can impact program timing. It is unlikely that any 
+ *  program will need it unless they are using cpu based timing loops and are old.
+ * <li>NO_RELEASE means that after the completion of an i2c operation the i2c bus will 
+ * not be released, instead the clock line will be held low. I seem to remember that 
+ * this mode was needed for some devices that required a series of i2c operations
+ *  to be issued without the bus being released to reprogram them.
+ *  </ul>
+
  * @author Lawrie Griffiths
  *
  */
